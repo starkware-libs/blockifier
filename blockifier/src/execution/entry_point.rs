549a023ae839e5be5642fe2201859d1715efacf9
Alon Haramati <alonh@starkware.co>
2022-11-07 15:22:26 +0200

    Create and use CallEntryPoint struct.

---
+++ blockifier/src/execution/entry_point.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+
+use super::cairo_run::{cairo_run, CairoRunConfig};
+
+pub struct CallEntryPoint {
+    /// Represents a Cairo entry point of a StarkNet contract.
+    pub contract_file_path: PathBuf,
+    // TODO(AlonH, 15/12/2022): Change to selector.
+    pub name: String,
+    pub call_data: Vec<i32>,
+}
+
+impl CallEntryPoint {
+    pub fn new(contract_file_path: &str, name: &str, call_data: Vec<i32>) -> Self {
+        let contract_file_path = PathBuf::from(contract_file_path);
+        let name = name.to_string();
+        Self { contract_file_path, name, call_data }
+    }
+
+    pub fn execute(&self) -> Result<()> {
+        cairo_run(self, CairoRunConfig::default(), &BuiltinHintProcessor::new_empty())
+    }
+}
