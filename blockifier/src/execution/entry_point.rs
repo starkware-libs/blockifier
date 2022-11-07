3c35a0d0d2b7218cdaba530e69452118bea2d533
Alon Haramati <alonh@starkware.co>
2022-11-07 15:22:26 +0200

    Create and use ExecuteEntryPoint struct.

---
+++ blockifier/src/execution/entry_point.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+
+use cairo_rs::cairo_run;
+use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+use cairo_rs::vm::errors::cairo_run_errors::CairoRunError;
+
+pub struct EntryPoint {
+    /// Represents a Cairo entry point of a StarkNet contract.
+    contract_file_path: PathBuf,
+    selector: String,
+}
+
+impl EntryPoint {
+    pub fn new(contract_file_path: &str, selector: &str) -> Self {
+        let contract_file_path = PathBuf::from(contract_file_path);
+        let selector = selector.to_string();
+        Self { contract_file_path, selector }
+    }
+
+    pub fn execute(&self) -> Result<(), Box<CairoRunError>> {
+        // TODO(AlonH, 15/11/2022): Create config struct for these.
+        let (trace_enabled, print_output, proof_mode, layout) = (false, false, false, "all");
+        cairo_run::cairo_run(
+            &self.contract_file_path,
+            &self.selector,
+            trace_enabled,
+            print_output,
+            layout,
+            proof_mode,
+            &BuiltinHintProcessor::new_empty(),
+        )?;
+        Ok(())
+    }
+}
---
+++ blockifier/src/execution/entry_point_test.rs
@@ -0,0 +1,12 @@
+use cairo_rs::vm::errors::cairo_run_errors::CairoRunError;
+use pretty_assertions::assert_eq;
+
+use crate::execution::entry_point::EntryPoint;
+
+#[test]
+fn test_entry_point() -> Result<(), Box<CairoRunError>> {
+    let test_contract = "./test_contracts/array_sum.json";
+    let entry_point = EntryPoint::new(test_contract, "main");
+    assert_eq!(entry_point.execute()?, ());
+    Ok(())
+}
