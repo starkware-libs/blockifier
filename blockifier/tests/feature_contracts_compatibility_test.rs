9724d0dcf71b47675eb3dc0402ee2f8454ef9563
Gilad Chase <gilad@starkware.co>
2022-11-09 11:59:48 +0200

    Commit compiled test contract and verify contents on CI
    We plan on having unit-tests that depend on compiled cairo code.
    To do that we commit the compiled cairo code, and verify in CI
    that they indeed match their original cairo source.
    
    Assumptions:
    * all cairo source files are in blockifier/test_contracts, which contains
    no other file types.
    * for every cairo source file in test_contracts there is a compiled cairo
    json (with the same name) in blockifier/test_contracts/compiled/
    
    Since we don't want to run `cairo-compile` on every `cargo test`, we
    mark it as a slow test (using `ignore` in Rust), and run the CI with
    `include-ignore`.
    
    Compiling without debug info since otherwise CI will get a different
    filepath than local compilation.
    
    If this gets too inconvenient, consider switching to Bazel (which works
    well with cargo).

---
+++ blockifier/tests/feature_contracts_compatibility_test.rs
@@ -0,0 +1,57 @@
+use std::fs;
+use std::process::Command;
+
+use anyhow::{ensure, Context, Result};
+use pretty_assertions::assert_eq;
+
+const FEATURE_CONTRACTS_DIR: &str = "feature_contracts";
+const COMPILED_CONTRACTS_SUBDIR: &str = "compiled";
+
+// Checks that:
+// 1. `TEST_CONTRACTS` dir exists and contains only `.cairo` files and the subdirectory
+// `COMPILED_CONTRACTS_SUBDIR`.
+// 2. for each `X.cairo` file in `TEST_CONTRACTS` there exists an `X.json` file in
+// `COMPILED_CONTRACTS_SUBDIR` which equals `cairo-compile X.cairo -- -no_debug_info`.
+#[test]
+#[ignore]
+fn check_feature_contracts_compatibility() -> Result<()> {
+    for file in fs::read_dir(FEATURE_CONTRACTS_DIR).unwrap() {
+        let path = file.unwrap().path();
+
+        // Test `TEST_CONTRACTS` file and directory structure.
+        if !path.is_file() {
+            if let Some(dir_name) = path.file_name() {
+                ensure!(
+                    dir_name == COMPILED_CONTRACTS_SUBDIR,
+                    "Found directory '{}' in `{FEATURE_CONTRACTS_DIR}`, which should contain only \
+                     the `{COMPILED_CONTRACTS_SUBDIR}` directory.",
+                    dir_name.to_string_lossy()
+                );
+                continue;
+            }
+        }
+        let path_str = path.to_string_lossy();
+        ensure!(
+            path.extension().unwrap() == "cairo",
+            "Found a non-Cairo file '{path_str}' in `{FEATURE_CONTRACTS_DIR}`"
+        );
+
+        // Compare output of cairo-file on file with existing compiled file.
+        let file_name = path.file_stem().unwrap().to_string_lossy();
+        let existing_compiled_path =
+            format!("{FEATURE_CONTRACTS_DIR}/{COMPILED_CONTRACTS_SUBDIR}/{file_name}.json");
+        let existing_compiled_contents = fs::read_to_string(&existing_compiled_path)
+            .context(format!("Cannot read {existing_compiled_path}."))?;
+        let expected_compiled_output = Command::new("cairo-compile")
+            .args([&path_str, "--no_debug_info"])
+            .output()
+            .unwrap()
+            .stdout;
+
+        assert_eq!(
+            String::from_utf8(expected_compiled_output).unwrap(),
+            existing_compiled_contents
+        );
+    }
+    Ok(())
+}
