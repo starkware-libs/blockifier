6fa6c2b4946454bd45f7c2bd45365d9afaf3db1b
Gilad Chase <gilad@starkware.co>
2022-11-09 11:59:48 +0200

    Commit compiled test contract and verify contents on CI
    We plan on having unit-tests that depend on compiled cairo code.
    To do that we commit the compiled cairo code, and verify in CI
    that they indeed match their original cairo source.
    
    Assumptions:
    * all cairo source files are in blockifier/test_contracts, which contains
    no other file types.
    * for every cairo source file in test_contracts there is a compiled cairo
    json (with the same name) in blockifier/test_contracts/compiled/
    
    Since we don't want to run `cairo-compile` on every `cargo test`, we
    mark it as a slow test (using `ignore` in Rust), and run the CI with
    `include-ignore`.
    
    Compiling without debug info since otherwise CI will get a different
    filepath than local compilation.
    
    If this gets too inconvenient, consider switching to Bazel (which works
    well with cargo).

---
+++ blockifier/tests/test_contracts_correctness.rs
@@ -0,0 +1,51 @@
+use std::fs;
+use std::process::Command;
+
+use pretty_assertions::assert_eq;
+
+#[test]
+#[ignore]
+fn compiled_files_correctness() -> Result<(), String> {
+    for file in fs::read_dir("test_contracts/").unwrap() {
+        let path = file.unwrap().path();
+
+        // test `test_contracts/` file and directory structure.
+        if !path.is_file() {
+            match path.file_name() {
+                Some(dir_name) if dir_name == "compiled" => continue,
+                Some(dir_name) => {
+                    return Err(format!(
+                        "Found directory '{}' in 'test_contracts/', which should contain only the \
+                         'compiled' directory.",
+                        dir_name.to_string_lossy()
+                    ));
+                }
+                None => return Err("IO error".to_string()),
+            }
+        }
+        let path_str = path.to_str().unwrap();
+        if path.extension().unwrap() != "cairo" {
+            return Err(format!("Found non-cairo file '{}' in test_contracts/", path_str));
+        }
+
+        // Compare output of cairo-file on file with existing compiled file.
+        let file_name = path.file_stem().unwrap().to_str().unwrap();
+        let existing_compiled_path = format!("test_contracts/compiled/{file_name}.json");
+        let existing_compiled_contents = match fs::read_to_string(&existing_compiled_path) {
+            Ok(json_contents) => json_contents,
+            Err(_) => return Err(format!("Can't read {}.", existing_compiled_path)),
+        };
+
+        let expected_compiled_output = Command::new("cairo-compile")
+            .args([path_str, "--no_debug_info"])
+            .output()
+            .unwrap()
+            .stdout;
+
+        assert_eq!(
+            String::from_utf8(expected_compiled_output).unwrap(),
+            existing_compiled_contents
+        );
+    }
+    Ok(())
+}
