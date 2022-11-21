ae7a102b1cae63c73d6dc651659639d8db69a11e
Alon Haramati <91828241+alonh5@users.noreply.github.com>
2022-11-21 15:53:20 +0200

    Get return values from cairo run. (#18)

--- blockifier/src/execution/cairo_run_utils.rs
+++ blockifier/src/execution/cairo_run_utils.rs
@@ -1,11 +1,12 @@
 use std::any::Any;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use cairo_rs::bigint;
-use cairo_rs::cairo_run::write_output;
 use cairo_rs::hint_processor::hint_processor_definition::HintProcessor;
+use cairo_rs::types::errors::program_errors::ProgramError::EntrypointNotFound;
 use cairo_rs::types::program::Program;
 use cairo_rs::types::relocatable::MaybeRelocatable;
+use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
 use cairo_rs::vm::runners::cairo_runner::CairoRunner;
 use cairo_rs::vm::vm_core::VirtualMachine;
 use num_bigint::BigInt;
@@ -41,36 +42,43 @@ pub fn cairo_run(
     call_entry_point: &CallEntryPoint,
     config: CairoRunConfig,
     hint_executor: &dyn HintProcessor,
-) -> Result<()> {
+) -> Result<Vec<BigInt>> {
     let layout: String = config.layout.into();
     let program =
         Program::from_file(&call_entry_point.contract_file_path, Some(&call_entry_point.name))?;
 
     let mut cairo_runner = CairoRunner::new(&program, &layout, config.proof_mode)?;
     let mut vm = VirtualMachine::new(program.prime, config.enable_trace);
-    // TODO(AlonH, 21/11/2022): Remove `unwrap`s and handle errors instead.
-    let entry_point_pc = program
-        .identifiers
-        .get(&format!("__main__.{}", &call_entry_point.name))
-        .unwrap()
-        .pc
-        .unwrap();
+    // TODO(AlonH, 21/11/2022): Get pc from ContractClass once it is used.
+    let entry_point_identifier =
+        match program.identifiers.get(&format!("__wrappers__.{}", &call_entry_point.name)) {
+            Some(identifier) => identifier,
+            None => bail!(EntrypointNotFound(call_entry_point.name.clone())),
+        };
+    let entry_point_pc = match entry_point_identifier.pc {
+        Some(pc) => pc,
+        None => bail!(EntrypointNotFound(call_entry_point.name.clone())),
+    };
 
-    cairo_runner.initialize_builtins(&mut vm)?;
-    cairo_runner.initialize_segments(&mut vm, None);
+    cairo_runner.initialize_function_runner(&mut vm)?;
 
-    let mut args = Vec::new();
-    for (_name, builtin_runner) in vm.get_builtin_runners().iter() {
-        args.append(&mut builtin_runner.initial_stack());
-    }
-    for arg in &call_entry_point.calldata {
-        // TODO(AlonH, 21/12/2022): Consider using StarkFelt.
-        args.push(MaybeRelocatable::Int(bigint!(*arg)));
-    }
+    let mut args: Vec<Box<dyn Any>> = Vec::new();
+    // TODO(AlonH, 21/12/2022): Push the entry point selector to args once it is used.
+    let os_context: Vec<MaybeRelocatable> = vm
+        .get_builtin_runners()
+        .iter()
+        .flat_map(|(_name, builtin_runner)| builtin_runner.initial_stack())
+        .collect();
+    args.push(Box::new(os_context));
+    // TODO(AlonH, 21/12/2022): Consider using StarkFelt.
+    args.push(Box::new(MaybeRelocatable::Int(bigint!(call_entry_point.calldata.len()))));
+    let calldata: Vec<MaybeRelocatable> =
+        call_entry_point.calldata.iter().map(|arg| MaybeRelocatable::Int(bigint!(*arg))).collect();
+    args.push(Box::new(calldata));
 
     cairo_runner.run_from_entrypoint(
         entry_point_pc,
-        args.iter().map(|x| x as &dyn Any).collect(),
+        args.iter().map(|x| x.as_ref()).collect(),
         false,
         true,
         true,
@@ -78,9 +86,33 @@ pub fn cairo_run(
         hint_executor,
     )?;
 
-    if config.print_output {
-        write_output(&mut cairo_runner, &mut vm)?;
-    }
+    get_return_values(&vm)
+}
 
-    Ok(())
+fn get_return_values(vm: &VirtualMachine) -> Result<Vec<BigInt>> {
+    let [ret_data_size, ret_data_ptr]: [MaybeRelocatable; 2] = vm
+        .get_return_values(2)?
+        .try_into()
+        .unwrap_or_else(|_| panic!("Return values should be of size 2."));
+    // Convert ret_data_size from MaybeRelocatable to BigInt.
+    let ret_data_size = match ret_data_size {
+        MaybeRelocatable::Int(ret_data_size) => ret_data_size,
+        relocatable => bail!(VirtualMachineError::ExpectedInteger(relocatable)),
+    };
+    // Convert ret_data_size from BigInt to usize.
+    // TODO(AlonH, 21/12/2022): Handle case where res_data_size is larger than usize.
+    let ret_data_size = ret_data_size.bits() as usize;
+
+    let values = vm.get_range(&ret_data_ptr, ret_data_size)?;
+    // Extract BigInt values.
+    let values: Result<Vec<BigInt>> = values
+        .into_iter()
+        .map(|x| x.as_deref().cloned())
+        .map(|x| match x {
+            Some(MaybeRelocatable::Int(value)) => Ok(value),
+            Some(relocatable) => bail!(VirtualMachineError::ExpectedInteger(relocatable)),
+            None => bail!(VirtualMachineError::NoneInMemoryRange),
+        })
+        .collect();
+    values
 }
--- blockifier/src/execution/entry_point_test.rs
+++ blockifier/src/execution/entry_point_test.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use cairo_rs::bigint;
+use num_bigint::BigInt;
 use pretty_assertions::assert_eq;
 
 use crate::execution::entry_point::CallEntryPoint;
@@ -7,7 +9,7 @@ use crate::execution::entry_point::CallEntryPoint;
 fn test_entry_point_without_arg() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
     let entry_point = CallEntryPoint::new(test_contract, "without_arg", vec![]);
-    assert_eq!(entry_point.execute()?, ());
+    assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
@@ -15,7 +17,7 @@ fn test_entry_point_without_arg() -> Result<()> {
 fn test_entry_point_with_arg() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
     let entry_point = CallEntryPoint::new(test_contract, "with_arg", vec![25]);
-    assert_eq!(entry_point.execute()?, ());
+    assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
@@ -23,7 +25,7 @@ fn test_entry_point_with_arg() -> Result<()> {
 fn test_entry_point_with_builtin() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
     let entry_point = CallEntryPoint::new(test_contract, "bitwise_and", vec![47, 31]);
-    assert_eq!(entry_point.execute()?, ());
+    assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
@@ -31,6 +33,14 @@ fn test_entry_point_with_builtin() -> Result<()> {
 fn test_entry_point_with_hint() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
     let entry_point = CallEntryPoint::new(test_contract, "sqrt", vec![81]);
-    assert_eq!(entry_point.execute()?, ());
+    assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
+    Ok(())
+}
+
+#[test]
+fn test_entry_point_with_return_value() -> Result<()> {
+    let test_contract = "./feature_contracts/compiled/simple_contract.json";
+    let entry_point = CallEntryPoint::new(test_contract, "return_result", vec![23]);
+    assert_eq!(entry_point.execute()?, vec![bigint!(23)]);
     Ok(())
 }
