760cef0d1ee6feae2fef0ba7b96d1ce71c25c8fd
Noa Oved <noa@starkware.co>
2022-11-21 18:08:14 +0200

    Add ContractClass to CallEntryPoint.

---
+++ blockifier/src/execution/contract_class.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use starknet_api::{EntryPoint, EntryPointType, Program};
+
+/// Represents a StarkNet contract class.
+#[derive(Debug, Clone, Default, Eq, PartialEq, Deserialize, Serialize)]
+pub struct ContractClass {
+    // TODO(Noa, 30/12/22): Consider using a more specific field type or add a layer to
+    // appropriately deserialize it.
+    pub program: Program,
+    /// The selector of each entry point is a unique identifier in the program.
+    pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
+    /// Not required for execution, thus can be omitted from the raw contract file.
+    pub abi: Option<serde_json::Value>,
+}
+
+impl ContractClass {
+    /// Instantiates a contract class object given a compiled contract file path.
+    pub fn from_file(path: &Path) -> Result<ContractClass> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let raw_contract_class = serde_json::from_reader(reader)?;
+        Ok(raw_contract_class)
+    }
+}
--- blockifier/src/execution/entry_point_test.rs
+++ blockifier/src/execution/entry_point_test.rs
@@ -1,46 +1,78 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use cairo_rs::bigint;
 use num_bigint::BigInt;
 use pretty_assertions::assert_eq;
 
+use crate::execution::contract_class::ContractClass;
 use crate::execution::entry_point::CallEntryPoint;
+
+const TEST_CONTRACT_PROGRAM_PATH: &str =
+    "./feature_contracts/compiled/simple_contract_program.json";
+const TEST_CONTRACT_PATH: &str = "./feature_contracts/compiled/simple_contract_compiled.json";
+
+fn initialize_contract_class_for_test() -> ContractClass {
+    let path = PathBuf::from(TEST_CONTRACT_PATH);
+    ContractClass::from_file(&path).unwrap()
+}
 
 #[test]
 fn test_entry_point_without_arg() -> Result<()> {
-    let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "without_arg", vec![]);
+    let entry_point = CallEntryPoint::new(
+        initialize_contract_class_for_test(),
+        TEST_CONTRACT_PROGRAM_PATH,
+        "without_arg",
+        vec![],
+    );
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
 #[test]
 fn test_entry_point_with_arg() -> Result<()> {
-    let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "with_arg", vec![25]);
+    let entry_point = CallEntryPoint::new(
+        initialize_contract_class_for_test(),
+        TEST_CONTRACT_PROGRAM_PATH,
+        "with_arg",
+        vec![25],
+    );
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
 #[test]
 fn test_entry_point_with_builtin() -> Result<()> {
-    let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "bitwise_and", vec![47, 31]);
+    let entry_point = CallEntryPoint::new(
+        initialize_contract_class_for_test(),
+        TEST_CONTRACT_PROGRAM_PATH,
+        "bitwise_and",
+        vec![47, 31],
+    );
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
 #[test]
 fn test_entry_point_with_hint() -> Result<()> {
-    let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "sqrt", vec![81]);
+    let entry_point = CallEntryPoint::new(
+        initialize_contract_class_for_test(),
+        TEST_CONTRACT_PROGRAM_PATH,
+        "sqrt",
+        vec![81],
+    );
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
 
 #[test]
 fn test_entry_point_with_return_value() -> Result<()> {
-    let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "return_result", vec![23]);
+    let entry_point = CallEntryPoint::new(
+        initialize_contract_class_for_test(),
+        TEST_CONTRACT_PROGRAM_PATH,
+        "return_result",
+        vec![23],
+    );
     assert_eq!(entry_point.execute()?, vec![bigint!(23)]);
     Ok(())
 }
