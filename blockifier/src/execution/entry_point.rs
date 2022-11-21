f238440a5b7054c7ff8220bc539ea051d5d033e7
Adi Yakovian <adi@starkware.co>
2022-11-21 16:03:00 +0200

    Fix CallData type.

--- blockifier/src/execution/entry_point.rs
+++ blockifier/src/execution/entry_point.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use anyhow::Result;
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
 use num_bigint::BigInt;
+use starknet_api::CallData;
 
 use super::cairo_run_utils::{cairo_run, CairoRunConfig};
 
@@ -11,16 +12,17 @@ pub struct CallEntryPoint {
     pub contract_file_path: PathBuf,
     // TODO(AlonH, 15/12/2022): Change to selector.
     pub name: String,
-    pub calldata: Vec<i32>,
+    pub calldata: CallData,
 }
 
 impl CallEntryPoint {
-    pub fn new(contract_file_path: &str, name: &str, calldata: Vec<i32>) -> Self {
+    pub fn new(contract_file_path: &str, name: &str, calldata: CallData) -> Self {
         let contract_file_path = PathBuf::from(contract_file_path);
         let name = name.to_string();
         Self { contract_file_path, name, calldata }
     }
 
+    // TODO(Adi, 27/11/2022): Change BigInt to StarkFelt.
     pub fn execute(&self) -> Result<Vec<BigInt>> {
         // Returns the output of the entry point execution.
         cairo_run(self, CairoRunConfig::default(), &BuiltinHintProcessor::new_empty())
