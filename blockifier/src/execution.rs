f5e7e51281bc6d01dcd7426524272ae61fa3ebdb
Adi Yakovian <adi@starkware.co>
2022-11-21 16:03:00 +0200

    Fix CallData type.

--- blockifier/src/execution.rs
+++ blockifier/src/execution.rs
@@ -1,5 +1,9 @@
 pub mod cairo_run_utils;
 pub mod contract_class;
 pub mod entry_point;
+
 #[cfg(test)]
 pub mod entry_point_test;
+
+#[cfg(test)]
+pub mod cairo_run_utils_test;
--- blockifier/src/execution/entry_point.rs
+++ blockifier/src/execution/entry_point.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use anyhow::Result;
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
 use num_bigint::BigInt;
+use starknet_api::CallData;
 
 use super::cairo_run_utils::{execute_call_entry_point, CairoRunConfig};
 use crate::execution::contract_class::ContractClass;
@@ -14,7 +15,7 @@ pub struct CallEntryPoint {
     pub contract_file_path: PathBuf,
     // TODO(AlonH, 15/12/2022): Change to selector.
     pub name: String,
-    pub calldata: Vec<i32>,
+    pub calldata: CallData,
 }
 
 impl CallEntryPoint {
@@ -22,13 +23,14 @@ impl CallEntryPoint {
         contract_class: ContractClass,
         contract_file_path: &str,
         name: &str,
-        calldata: Vec<i32>,
+        calldata: CallData,
     ) -> Self {
         let contract_file_path = PathBuf::from(contract_file_path);
         let name = name.to_string();
         Self { contract_class, contract_file_path, name, calldata }
     }
 
+    // TODO(Adi, 27/11/2022): Change BigInt to StarkFelt.
     pub fn execute(&self) -> Result<Vec<BigInt>> {
         // Returns the output of the entry point execution.
         execute_call_entry_point(
