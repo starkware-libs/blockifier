a411a3cab4e6a322498cf2bc9468283559df0e22
Adi Yakovian <adi@starkware.co>
2022-11-21 16:03:00 +0200

    Fix CallData type.

--- blockifier/src/execution/entry_point_test.rs
+++ blockifier/src/execution/entry_point_test.rs
@@ -2,13 +2,14 @@ use anyhow::Result;
 use cairo_rs::bigint;
 use num_bigint::BigInt;
 use pretty_assertions::assert_eq;
+use starknet_api::{CallData, StarkFelt};
 
 use crate::execution::entry_point::CallEntryPoint;
 
 #[test]
 fn test_entry_point_without_arg() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "without_arg", vec![]);
+    let entry_point = CallEntryPoint::new(test_contract, "without_arg", &CallData(vec![]));
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
@@ -16,7 +17,8 @@ fn test_entry_point_without_arg() -> Result<()> {
 #[test]
 fn test_entry_point_with_arg() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "with_arg", vec![25]);
+    let entry_point =
+        CallEntryPoint::new(test_contract, "with_arg", &CallData(vec![StarkFelt::from_u64(25)]));
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
@@ -24,7 +26,11 @@ fn test_entry_point_with_arg() -> Result<()> {
 #[test]
 fn test_entry_point_with_builtin() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "bitwise_and", vec![47, 31]);
+    let entry_point = CallEntryPoint::new(
+        test_contract,
+        "bitwise_and",
+        &CallData(vec![StarkFelt::from_u64(47), StarkFelt::from_u64(31)]),
+    );
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
@@ -32,7 +38,8 @@ fn test_entry_point_with_builtin() -> Result<()> {
 #[test]
 fn test_entry_point_with_hint() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "sqrt", vec![81]);
+    let entry_point =
+        CallEntryPoint::new(test_contract, "sqrt", &CallData(vec![StarkFelt::from_u64(81)]));
     assert_eq!(entry_point.execute()?, Vec::<BigInt>::new());
     Ok(())
 }
@@ -40,7 +47,11 @@ fn test_entry_point_with_hint() -> Result<()> {
 #[test]
 fn test_entry_point_with_return_value() -> Result<()> {
     let test_contract = "./feature_contracts/compiled/simple_contract.json";
-    let entry_point = CallEntryPoint::new(test_contract, "return_result", vec![23]);
+    let entry_point = CallEntryPoint::new(
+        test_contract,
+        "return_result",
+        &CallData(vec![StarkFelt::from_u64(23)]),
+    );
     assert_eq!(entry_point.execute()?, vec![bigint!(23)]);
     Ok(())
 }
