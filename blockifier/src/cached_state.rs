a97ac184ff5ade933e0e090b2e45d542e9c22e51
Gilad Chase <gilad@starkware.co>
2022-11-23 18:09:36 +0200

    Define StateReader API
    Currently using the same logic as in Python, except for
    `get_contract_class` and `_get_raw_contract_class`, which will soon be
    removed from it.

---
+++ blockifier/src/cached_state.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use starknet_api::{ClassHash, ContractAddress, Nonce, StarkFelt, StorageKey};
+
+/// A read-only API for accessing StarkNet global state.
+pub trait StateReader {
+    /// Returns the storage value under the given key in the given contract instance (represented by
+    /// its address).
+    fn get_storage_at(
+        &self,
+        _contract_address: ContractAddress,
+        _key: StorageKey,
+    ) -> Result<StarkFelt> {
+        unimplemented!();
+    }
+
+    /// Returns the nonce of the given contract instance.
+    fn get_nonce_at(&self, _contract_address: ContractAddress) -> Result<Nonce> {
+        unimplemented!();
+    }
+
+    /// Returns the class hash of the contract class at the given contract instance;
+    /// uninitialized class hash, if the address is unassigned.
+    fn get_class_hash_at(&self, _contract_address: ContractAddress) -> Result<ClassHash> {
+        unimplemented!();
+    }
+}
