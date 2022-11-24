03b173d70384fb703eb9cd315fa7e616ad985519
Gilad Chase <gilad@starkware.co>
2022-11-24 15:23:52 +0200

    Add `get_storage_at` to `CachedState`

--- blockifier/src/cached_state.rs
+++ blockifier/src/cached_state.rs
@@ -1,7 +1,11 @@
 use std::collections::HashMap;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use starknet_api::{ClassHash, ContractAddress, Nonce, StarkFelt, StorageKey};
+
+#[cfg(test)]
+#[path = "cached_state_test.rs"]
+mod test;
 
 /// Caches read and write requests.
 ///
@@ -9,12 +13,36 @@ use starknet_api::{ClassHash, ContractAddress, Nonce, StarkFelt, StorageKey};
 /// initialization.
 pub struct CachedState<SR: StateReader> {
     pub state_reader: SR,
-    _cache: StateCache,
+    cache: StateCache,
 }
 
 impl<SR: StateReader> CachedState<SR> {
     pub fn new(state_reader: SR) -> Self {
-        Self { state_reader, _cache: StateCache::default() }
+        Self { state_reader, cache: StateCache::default() }
+    }
+
+    pub fn get_storage_at(
+        &mut self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+    ) -> Result<&StarkFelt> {
+        if self.cache.get_storage_at(contract_address, key).is_none() {
+            let storage_value = self.state_reader.get_storage_at(contract_address, key)?;
+            self.cache.set_storage_initial_values(contract_address, key, *storage_value);
+        }
+
+        self.cache
+            .get_storage_at(contract_address, key)
+            .with_context(|| format!("Cannot retrieve '{contract_address:?}' from the cache."))
+    }
+
+    pub fn set_storage_at(
+        &mut self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+        value: StarkFelt,
+    ) {
+        self.cache.set_storage_writes(contract_address, key, value);
     }
 }
 
@@ -26,23 +54,38 @@ pub trait StateReader {
         &self,
         _contract_address: ContractAddress,
         _key: StorageKey,
-    ) -> Result<&StarkFelt> {
-        unimplemented!();
-    }
+    ) -> Result<&StarkFelt>;
 
     /// Returns the nonce of the given contract instance.
     fn get_nonce_at(&self, _contract_address: ContractAddress) -> Result<&Nonce> {
         unimplemented!();
     }
 
-    /// Returns the class hash of the contract class at the given contract instance
+    /// Returns the class hash of the contract class at the given contract instance.
     fn get_class_hash_at(&self, _contract_address: ContractAddress) -> Result<&ClassHash> {
         unimplemented!();
     }
 }
 
-// Used internally by the `StateCache`.
 type ContractStorageKey = (ContractAddress, StorageKey);
+
+/// A simple implementation of `StateReader` using `HashMap`s for storage.
+pub struct DictStateReader {
+    pub contract_storage_key_to_value: HashMap<ContractStorageKey, StarkFelt>,
+}
+
+impl StateReader for DictStateReader {
+    fn get_storage_at(
+        &self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+    ) -> Result<&StarkFelt> {
+        let contract_storage_key = (contract_address, key);
+        self.contract_storage_key_to_value
+            .get(&contract_storage_key)
+            .with_context(|| format!("{contract_address:?} should have storage."))
+    }
+}
 
 /// Caches read and write requests.
 // Invariant: cannot delete keys from fields.
@@ -51,10 +94,43 @@ struct StateCache {
     // Reader's cached information; initial values, read before any write operation (per cell).
     _nonce_initial_values: HashMap<ContractAddress, Nonce>,
     _class_hash_initial_values: HashMap<ContractAddress, ClassHash>,
-    _storage_initial_values: HashMap<ContractStorageKey, StarkFelt>,
+    storage_initial_values: HashMap<ContractStorageKey, StarkFelt>,
 
     // Writer's cached information.
     _nonce_writes: HashMap<ContractAddress, Nonce>,
     _class_hash_writes: HashMap<ContractAddress, ClassHash>,
-    _storage_writes: HashMap<ContractStorageKey, StarkFelt>,
+    storage_writes: HashMap<ContractStorageKey, StarkFelt>,
+}
+
+impl StateCache {
+    fn get_storage_at(
+        &self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+    ) -> Option<&StarkFelt> {
+        let contract_storage_key = (contract_address, key);
+        self.storage_writes
+            .get(&contract_storage_key)
+            .or_else(|| self.storage_initial_values.get(&contract_storage_key))
+    }
+
+    pub fn set_storage_initial_values(
+        &mut self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+        value: StarkFelt,
+    ) {
+        let contract_storage_key = (contract_address, key);
+        self.storage_initial_values.insert(contract_storage_key, value);
+    }
+
+    fn set_storage_writes(
+        &mut self,
+        contract_address: ContractAddress,
+        key: StorageKey,
+        value: StarkFelt,
+    ) {
+        let contract_storage_key = (contract_address, key);
+        self.storage_writes.insert(contract_storage_key, value);
+    }
 }
