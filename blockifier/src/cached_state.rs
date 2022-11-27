d7ae4d7d9df4fd02ad5af5628a601bc2dd7e3df9
Gilad Chase <gilad@starkware.co>
2022-11-27 09:21:50 +0200

    Add `nonce` logic to `CachedState`.

--- blockifier/src/cached_state.rs
+++ blockifier/src/cached_state.rs
@@ -46,6 +46,28 @@ impl<SR: StateReader> CachedState<SR> {
     ) {
         self.cache.set_storage_writes(contract_address, key, value);
     }
+
+    // TODO(Gilad, 1/12/22) consider moving some of this logic into starknet-api, Nonce should
+    // be able to increment itself.
+    pub fn increment_nonce(&mut self, contract_address: ContractAddress) -> Result<()> {
+        let current_nonce = self.get_nonce_at(contract_address)?.nonce();
+        let incremented_nonce_val = u64_try_from_starkfelt(current_nonce)? + 1_u64;
+
+        let incremented_nonce = nonce_try_from_u64(incremented_nonce_val)?;
+        self.cache.set_nonce_writes(contract_address, incremented_nonce);
+        Ok(())
+    }
+
+    pub fn get_nonce_at(&mut self, contract_address: ContractAddress) -> Result<&Nonce> {
+        if self.cache.get_nonce_at(contract_address).is_none() {
+            let nonce = self.state_reader.get_nonce_at(contract_address)?;
+            self.cache.set_nonce_initial_values(contract_address, *nonce);
+        }
+
+        self.cache
+            .get_nonce_at(contract_address)
+            .with_context(|| format!("Cannot retrieve '{contract_address:?}' from the cache."))
+    }
 }
 
 /// A read-only API for accessing StarkNet global state.
@@ -58,9 +80,7 @@ pub trait StateReader {
     ) -> Result<&StarkFelt>;
 
     /// Returns the nonce of the given contract instance (represented by its address).
-    fn get_nonce_at(&self, _contract_address: ContractAddress) -> Result<&Nonce> {
-        unimplemented!();
-    }
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Result<&Nonce>;
 
     /// Returns the class hash of the contract class at the given address.
     fn get_class_hash_at(&self, _contract_address: ContractAddress) -> Result<&ClassHash> {
@@ -70,8 +90,10 @@ pub trait StateReader {
 
 type ContractStorageKey = (ContractAddress, StorageKey);
 
+#[derive(Default)]
 pub struct DictStateReader {
     pub contract_storage_key_to_felt: HashMap<ContractStorageKey, StarkFelt>,
+    pub contract_address_to_nonce: HashMap<ContractAddress, Nonce>,
 }
 
 impl StateReader for DictStateReader {
@@ -85,6 +107,12 @@ impl StateReader for DictStateReader {
             .get(&contract_storage_key)
             .with_context(|| format!("{contract_address:?} should have a nonce."))
     }
+
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Result<&Nonce> {
+        self.contract_address_to_nonce
+            .get(&contract_address)
+            .with_context(|| format!("{contract_address:?} should have a nonce."))
+    }
 }
 
 /// Holds read and write requests.
@@ -92,12 +120,12 @@ impl StateReader for DictStateReader {
 #[derive(Default)]
 struct StateCache {
     // Reader's cached information; initial values, read before any write operation (per cell).
-    _nonce_initial_values: HashMap<ContractAddress, Nonce>,
+    nonce_initial_values: HashMap<ContractAddress, Nonce>,
     _class_hash_initial_values: HashMap<ContractAddress, ClassHash>,
     storage_initial_values: HashMap<ContractStorageKey, StarkFelt>,
 
     // Writer's cached information.
-    _nonce_writes: HashMap<ContractAddress, Nonce>,
+    nonce_writes: HashMap<ContractAddress, Nonce>,
     _class_hash_writes: HashMap<ContractAddress, ClassHash>,
     storage_writes: HashMap<ContractStorageKey, StarkFelt>,
 }
@@ -133,4 +161,31 @@ impl StateCache {
         let contract_storage_key = (contract_address, key);
         self.storage_writes.insert(contract_storage_key, value);
     }
+
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Option<&Nonce> {
+        self.nonce_writes
+            .get(&contract_address)
+            .or_else(|| self.nonce_initial_values.get(&contract_address))
+    }
+
+    fn set_nonce_initial_values(&mut self, contract_address: ContractAddress, nonce: Nonce) {
+        self.nonce_initial_values.insert(contract_address, nonce);
+    }
+
+    fn set_nonce_writes(&mut self, contract_address: ContractAddress, nonce: Nonce) {
+        self.nonce_writes.insert(contract_address, nonce);
+    }
+}
+
+// TODO(Gilad, 1/12/2022): Move this to Starknet_api and convert to `TryFrom`
+fn u64_try_from_starkfelt(hash: &StarkFelt) -> Result<u64> {
+    let as_bytes: [u8; 8] = hash.bytes()[24..32].try_into()?;
+    Ok(u64::from_be_bytes(as_bytes))
+}
+
+// TODO(Gilad, 1/12/2022): Move this to Starknet_api and convert to `From`
+fn nonce_try_from_u64(num: u64) -> Result<Nonce> {
+    let num_hex = format!("0x{num:x}");
+    let felt = StarkFelt::from_hex(&num_hex)?;
+    Ok(Nonce::new(felt))
 }
