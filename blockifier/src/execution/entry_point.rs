0c8953f48dcaaef0fdfd2c4c87c5a5f645fef358
noaov1 <104720318+noaov1@users.noreply.github.com>
2022-11-29 08:59:14 +0200

    Convert struct Program SN API to Program lambdaclass. (#31)

--- blockifier/src/execution/entry_point.rs
+++ blockifier/src/execution/entry_point.rs
@@ -1,5 +1,3 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
 use num_bigint::BigInt;
@@ -12,22 +10,15 @@ use crate::execution::contract_class::ContractClass;
 pub struct CallEntryPoint {
     /// Represents a Cairo entry point of a StarkNet contract.
     pub contract_class: ContractClass,
-    pub contract_file_path: PathBuf,
     // TODO(AlonH, 15/12/2022): Change to selector.
     pub name: String,
     pub calldata: CallData,
 }
 
 impl CallEntryPoint {
-    pub fn new(
-        contract_class: ContractClass,
-        contract_file_path: &str,
-        name: &str,
-        calldata: CallData,
-    ) -> Self {
-        let contract_file_path = PathBuf::from(contract_file_path);
+    pub fn new(contract_class: ContractClass, name: &str, calldata: CallData) -> Self {
         let name = name.to_string();
-        Self { contract_class, contract_file_path, name, calldata }
+        Self { contract_class, name, calldata }
     }
 
     // TODO(Adi, 27/11/2022): Change BigInt to StarkFelt.
