cb3345b31bb28ac642f53ce8c0c496bd5b4515a2
Gilad Chase <gilad@starkware.co>
2022-11-29 11:54:36 +0200

    Add `BlockifierError` and `StateReaderError`
    * `StateReaderError` should be used when `StateReader` fails
      unexpectedly (like Timeout reaching the DB), and should be called using
      `return Err(StateReaderError::from(/* whatever key `StateReader` failed on */))`.
      We won't use this error in `DictStateReader` though, since it doesn't
      depend on external stuff like a DB.
    * `BlockifierError` is a general error enum, and will soon be used by
      `CachedState` to return errors when setting class hashes.

--- blockifier/src/lib.rs
+++ blockifier/src/lib.rs
@@ -1,2 +1,17 @@
+use std::fmt::Debug;
+
+use cached_state::StateReaderError;
+use starknet_api::ContractAddress;
+
 pub mod cached_state;
 pub mod execution;
+
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+pub enum BlockifierError {
+    #[error("Cannot deploy contract at address 0.")]
+    OutOfRangeAddress,
+    #[error("Requested {0:?} is unavailable for deployment.")]
+    ContractAddressUnavailable(ContractAddress),
+    #[error(transparent)]
+    StateError(#[from] StateReaderError),
+}
