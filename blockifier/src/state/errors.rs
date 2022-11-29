55b26af27eddf68d47c5290f4fa3490623e29d41
Gilad Chase <gilad@starkware.co>
2022-11-29 11:54:36 +0200

    Add `CachedStateError`

--- blockifier/src/state/errors.rs
+++ blockifier/src/state/errors.rs
@@ -1,8 +1,18 @@
-use starknet_api::core::ClassHash;
+use starknet_api::core::{ClassHash, ContractAddress};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum StateReaderError {
     #[error("Class with hash {0:#?} is not declared.")]
     UndeclaredClassHash(ClassHash),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum CachedStateError {
+    #[error("Cannot deploy contract at address 0.")]
+    OutOfRangeAddress,
+    #[error("Requested {0:?} is unavailable for deployment.")]
+    ContractAddressUnavailable(ContractAddress),
+    #[error(transparent)]
+    StateError(#[from] StateReaderError),
 }
