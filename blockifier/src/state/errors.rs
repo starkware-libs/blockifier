4c5b7e76972f855c88642b0be5149d126dcddb33
Gilad Chase <gilad@starkware.co>
2022-11-29 11:54:36 +0200

    Add `CachedStateError` and a catch-all variant to StateReaderError

--- blockifier/src/state/errors.rs
+++ blockifier/src/state/errors.rs
@@ -1,8 +1,22 @@
-use starknet_api::core::ClassHash;
+use starknet_api::core::{ClassHash, ContractAddress};
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq)]
 pub enum StateReaderError {
     #[error("Class with hash {0:#?} is not declared.")]
     UndeclaredClassHash(ClassHash),
+
+    /// Represents all unexpected errors that may occur while reading from state.
+    #[error("Failed to read from state: {0}.")]
+    ReadError(String),
+}
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum StateError {
+    #[error("Cannot deploy contract at address 0.")]
+    OutOfRangeAddress,
+    #[error("Requested {0:?} is unavailable for deployment.")]
+    UnavailableContractAddress(ContractAddress),
+    #[error(transparent)]
+    StateReaderError(#[from] StateReaderError),
 }
