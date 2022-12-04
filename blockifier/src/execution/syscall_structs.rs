3639c96c0b01a6a78504f14e2c7fa1e483ca9f67
Alon Haramati <alonh@starkware.co>
2022-12-04 13:10:44 +0200

    Add storage_read syscall.

--- blockifier/src/execution/syscall_structs.rs
+++ blockifier/src/execution/syscall_structs.rs
@@ -1,9 +1,57 @@
 use std::collections::HashMap;
+use std::mem;
 
 use cairo_rs::types::relocatable::Relocatable;
 use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
 use cairo_rs::vm::vm_core::VirtualMachine;
-use starknet_api::StarkHash;
+use starknet_api::{StarkFelt, StarkHash};
+
+use super::cairo_run_utils::felt_to_bigint;
+use crate::execution::cairo_run_utils::get_felt_from_memory_cell;
+
+const STORAGE_READ_SELECTOR: &str = "0x53746f7261676552656164";
+
+pub struct StorageReadRequest {
+    pub selector: StarkFelt,
+    pub address: StarkFelt,
+}
+
+impl SysCallRequest for StorageReadRequest {
+    fn read_request(
+        vm: &VirtualMachine,
+        ptr: &Relocatable,
+    ) -> Result<Box<dyn SysCallRequest>, VirtualMachineError> {
+        let selector = get_felt_from_memory_cell(vm.get_maybe(ptr)?)?;
+        let address = get_felt_from_memory_cell(vm.get_maybe(&(ptr + 1))?)?;
+        Ok(Box::new(StorageReadRequest { selector, address }))
+    }
+
+    fn execute(&self) -> Result<Box<dyn SysCallResponse>, VirtualMachineError> {
+        // TODO(AlonH, 21/12/2022): Perform state read.
+        let value = StarkFelt::from_u64(17);
+        Ok(Box::new(StorageReadResponse { value }))
+    }
+}
+
+pub struct StorageReadResponse {
+    pub value: StarkFelt,
+}
+
+impl SysCallResponse for StorageReadResponse {
+    fn write_response(
+        &self,
+        vm: &mut VirtualMachine,
+        ptr: &Relocatable,
+    ) -> Result<(), VirtualMachineError> {
+        vm.insert_value(ptr, felt_to_bigint(self.value))?;
+        Ok(())
+    }
+}
+
+pub struct StorageRead {
+    pub request: StorageReadRequest,
+    pub response: StorageReadResponse,
+}
 
 pub trait SysCallRequest {
     fn read_request(
@@ -34,5 +82,17 @@ pub struct SysCallInfo {
 }
 
 pub fn get_syscall_info() -> HashMap<StarkHash, SysCallInfo> {
-    HashMap::<StarkHash, SysCallInfo>::new()
+    let selector_error_msg = "Syscall selector should be able to turn into a `StarkHash`.";
+    [(
+        StarkHash::from_hex(STORAGE_READ_SELECTOR).expect(selector_error_msg),
+        SysCallInfo {
+            syscall_request_factory: Box::new(StorageReadRequest::read_request),
+            syscall_request_size: mem::size_of::<StorageReadRequest>()
+                / mem::size_of::<StarkFelt>(),
+            syscall_response_size: mem::size_of::<StorageReadResponse>()
+                / mem::size_of::<StarkFelt>(),
+        },
+    )]
+    .into_iter()
+    .collect()
 }
