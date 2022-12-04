5e1cc3ce9a5523471684191a0547218cd47a3f4a
Gilad Chase <gilad@starkware.co>
2022-12-04 08:41:35 +0200

    Add nonce support to State-API

--- blockifier/src/cached_state.rs
+++ blockifier/src/cached_state.rs
@@ -32,7 +32,7 @@ impl<SR: StateReader> CachedState<SR> {
     ) -> Result<&StarkFelt> {
         if self.cache.get_storage_at(contract_address, key).is_none() {
             let storage_value = self.state_reader.get_storage_at(contract_address, key)?;
-            self.cache.set_storage_initial_values(contract_address, key, storage_value);
+            self.cache.set_storage_initial_value(contract_address, key, storage_value);
         }
 
         self.cache.get_storage_at(contract_address, key).with_context(|| {
@@ -46,7 +46,28 @@ impl<SR: StateReader> CachedState<SR> {
         key: StorageKey,
         value: StarkFelt,
     ) {
-        self.cache.set_storage_writes(contract_address, key, value);
+        self.cache.set_storage_value(contract_address, key, value);
+    }
+
+    pub fn get_nonce_at(&mut self, contract_address: ContractAddress) -> Result<&Nonce> {
+        if self.cache.get_nonce_at(contract_address).is_none() {
+            let nonce = self.state_reader.get_nonce_at(contract_address)?;
+            self.cache.set_nonce_initial_value(contract_address, nonce);
+        }
+
+        self.cache
+            .get_nonce_at(contract_address)
+            .with_context(|| format!("Cannot retrieve '{contract_address:?}' from the cache."))
+    }
+
+    // TODO(Gilad, 1/12/22) consider moving some this logic into starknet-api; Nonce should
+    // be able to increment itself.
+    pub fn increment_nonce(&mut self, contract_address: ContractAddress) -> Result<()> {
+        let current_nonce = *self.get_nonce_at(contract_address)?;
+        let next_nonce = u64_try_from_starkfelt(current_nonce.0)? + 1_u64;
+        let next_nonce = nonce_try_from_u64(next_nonce)?;
+        self.cache.set_nonce_value(contract_address, next_nonce);
+        Ok(())
     }
 }
 
@@ -54,6 +75,7 @@ impl<SR: StateReader> CachedState<SR> {
 pub trait StateReader {
     /// Returns the storage value under the given key in the given contract instance (represented by
     /// its address).
+    /// Default: 0 for an uninitialized contract address.
     fn get_storage_at(
         &self,
         contract_address: ContractAddress,
@@ -61,12 +83,11 @@ pub trait StateReader {
     ) -> Result<StarkFelt>;
 
     /// Returns the nonce of the given contract instance.
-    fn get_nonce_at(&self, _contract_address: ContractAddress) -> Result<Nonce> {
-        unimplemented!();
-    }
+    /// Default: 0 for an uninitialized contract address.
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Result<Nonce>;
 
-    /// Returns the class hash of the contract class at the given contract instance;
-    /// uninitialized class hash, if the address is unassigned.
+    /// Returns the class hash of the contract class at the given contract instance.
+    /// Default: 0 (uninitialized class hash) for an uninitialized contract address
     fn get_class_hash_at(&self, _contract_address: ContractAddress) -> Result<ClassHash> {
         unimplemented!();
     }
@@ -78,6 +99,7 @@ type ContractStorageKey = (ContractAddress, StorageKey);
 #[derive(Default)]
 pub struct DictStateReader {
     pub contract_storage_key_to_value: HashMap<ContractStorageKey, StarkFelt>,
+    pub contract_address_to_nonce: HashMap<ContractAddress, Nonce>,
 }
 
 impl StateReader for DictStateReader {
@@ -91,8 +113,14 @@ impl StateReader for DictStateReader {
             .contract_storage_key_to_value
             .get(&contract_storage_key)
             .copied()
-            .unwrap_or_else(default_storage_value);
+            .unwrap_or_default();
         Ok(value)
+    }
+
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Result<Nonce> {
+        let nonce =
+            self.contract_address_to_nonce.get(&contract_address).copied().unwrap_or_default();
+        Ok(nonce)
     }
 }
 
@@ -101,12 +129,12 @@ impl StateReader for DictStateReader {
 #[derive(Default)]
 struct StateCache {
     // Reader's cached information; initial values, read before any write operation (per cell).
-    _nonce_initial_values: HashMap<ContractAddress, Nonce>,
+    nonce_initial_values: HashMap<ContractAddress, Nonce>,
     _class_hash_initial_values: HashMap<ContractAddress, ClassHash>,
     storage_initial_values: HashMap<ContractStorageKey, StarkFelt>,
 
     // Writer's cached information.
-    _nonce_writes: HashMap<ContractAddress, Nonce>,
+    nonce_writes: HashMap<ContractAddress, Nonce>,
     _class_hash_writes: HashMap<ContractAddress, ClassHash>,
     storage_writes: HashMap<ContractStorageKey, StarkFelt>,
 }
@@ -123,7 +151,7 @@ impl StateCache {
             .or_else(|| self.storage_initial_values.get(&contract_storage_key))
     }
 
-    pub fn set_storage_initial_values(
+    pub fn set_storage_initial_value(
         &mut self,
         contract_address: ContractAddress,
         key: StorageKey,
@@ -133,7 +161,7 @@ impl StateCache {
         self.storage_initial_values.insert(contract_storage_key, value);
     }
 
-    fn set_storage_writes(
+    fn set_storage_value(
         &mut self,
         contract_address: ContractAddress,
         key: StorageKey,
@@ -142,12 +170,32 @@ impl StateCache {
         let contract_storage_key = (contract_address, key);
         self.storage_writes.insert(contract_storage_key, value);
     }
+
+    fn get_nonce_at(&self, contract_address: ContractAddress) -> Option<&Nonce> {
+        self.nonce_writes
+            .get(&contract_address)
+            .or_else(|| self.nonce_initial_values.get(&contract_address))
+    }
+
+    fn set_nonce_initial_value(&mut self, contract_address: ContractAddress, nonce: Nonce) {
+        self.nonce_initial_values.insert(contract_address, nonce);
+    }
+
+    fn set_nonce_value(&mut self, contract_address: ContractAddress, nonce: Nonce) {
+        self.nonce_writes.insert(contract_address, nonce);
+    }
 }
 
-fn uninitialized_felt() -> StarkFelt {
-    StarkFelt::default()
+// TODO(Gilad, 1/12/2022): Move this to Starknet_api and convert to `TryFrom`
+// Also, check why are we using u64 and not BigInt (we are losing information in the cast).
+fn u64_try_from_starkfelt(hash: StarkFelt) -> Result<u64> {
+    let as_bytes: [u8; 8] = hash.bytes()[24..32].try_into()?;
+    Ok(u64::from_be_bytes(as_bytes))
 }
 
-fn default_storage_value() -> StarkFelt {
-    uninitialized_felt()
+// TODO(Gilad, 1/12/2022): Move this to Starknet_api and convert to `From`
+fn nonce_try_from_u64(num: u64) -> Result<Nonce> {
+    let num_hex = format!("0x{num:x}");
+    let felt = StarkFelt::try_from(num_hex.as_str())?;
+    Ok(Nonce(felt))
 }
--- blockifier/src/cached_state_test.rs
+++ blockifier/src/cached_state_test.rs
@@ -12,7 +12,7 @@ fn get_uninitialized_storage_value() -> Result<()> {
     let contract_address = ContractAddress::try_from(shash!("0x1"))?;
     let key = StorageKey::try_from(shash!("0x10"))?;
 
-    assert_eq!(*state.get_storage_at(contract_address, key).unwrap(), default_storage_value());
+    assert_eq!(*state.get_storage_at(contract_address, key).unwrap(), StarkFelt::default());
     Ok(())
 }
 
@@ -30,6 +30,7 @@ fn get_and_set_storage_value() -> Result<()> {
             ((contract_address0, key0), storage_val0),
             ((contract_address1, key1), storage_val1),
         ]),
+        ..Default::default()
     });
     assert_eq!(*state.get_storage_at(contract_address0, key0).unwrap(), storage_val0);
     assert_eq!(*state.get_storage_at(contract_address1, key1).unwrap(), storage_val1);
@@ -43,5 +44,60 @@ fn get_and_set_storage_value() -> Result<()> {
     state.set_storage_at(contract_address1, key1, modified_storage_value1);
     assert_eq!(*state.get_storage_at(contract_address0, key0).unwrap(), modified_storage_value0);
     assert_eq!(*state.get_storage_at(contract_address1, key1).unwrap(), modified_storage_value1);
+    Ok(())
+}
+
+#[test]
+fn get_uninitialized_value() -> Result<()> {
+    let mut state = CachedState::new(DictStateReader::default());
+    let contract_address = ContractAddress::try_from(shash!("0x1"))?;
+
+    assert_eq!(*state.get_nonce_at(contract_address).unwrap(), Nonce::default());
+    Ok(())
+}
+
+#[test]
+fn gets_nonce() -> Result<()> {
+    let contract_address = ContractAddress::try_from(shash!("0x0"))?;
+    let initial_nonce = Nonce(shash!("0x1"));
+    let mut state = CachedState::new(DictStateReader {
+        contract_address_to_nonce: HashMap::from([(contract_address, initial_nonce)]),
+        ..Default::default()
+    });
+    assert_eq!(*state.get_nonce_at(contract_address).unwrap(), initial_nonce);
+    Ok(())
+}
+
+#[test]
+fn get_and_increment_nonce() -> Result<()> {
+    let contract_address1 = ContractAddress::try_from(shash!("0x100"))?;
+    let contract_address2 = ContractAddress::try_from(shash!("0x200"))?;
+    let initial_nonce = Nonce(shash!("0x1"));
+
+    let mut state = CachedState::new(DictStateReader {
+        contract_address_to_nonce: HashMap::from([
+            (contract_address1, initial_nonce),
+            (contract_address2, initial_nonce),
+        ]),
+        ..Default::default()
+    });
+    assert_eq!(*state.get_nonce_at(contract_address1).unwrap(), initial_nonce);
+    assert_eq!(*state.get_nonce_at(contract_address2).unwrap(), initial_nonce);
+
+    state.increment_nonce(contract_address1)?;
+    let nonce1_plus_one = Nonce(shash!("0x2"));
+    assert_eq!(*state.get_nonce_at(contract_address1).unwrap(), nonce1_plus_one);
+    assert_eq!(*state.get_nonce_at(contract_address2).unwrap(), initial_nonce);
+
+    state.increment_nonce(contract_address1)?;
+    let nonce1_plus_two = Nonce(shash!("0x3"));
+    assert_eq!(*state.get_nonce_at(contract_address1).unwrap(), nonce1_plus_two);
+    assert_eq!(*state.get_nonce_at(contract_address2).unwrap(), initial_nonce);
+
+    state.increment_nonce(contract_address2)?;
+    let nonce2_plus_one = Nonce(shash!("0x2"));
+    assert_eq!(*state.get_nonce_at(contract_address1).unwrap(), nonce1_plus_two);
+    assert_eq!(*state.get_nonce_at(contract_address2).unwrap(), nonce2_plus_one);
+
     Ok(())
 }
