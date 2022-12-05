773ccb750c4decd5adff89fc4cf65cc4ba6c1746
Noa Oved <noa@starkware.co>
2022-12-05 14:30:58 +0200

    Add get_contract_class API + implementation in CachedState.

--- blockifier/src/cached_state.rs
+++ blockifier/src/cached_state.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use anyhow::Result;
-use starknet_api::{ClassHash, ContractAddress, Nonce, StarkFelt, StorageKey};
+use starknet_api::{ClassHash, ContractAddress, ContractClass, Nonce, StarkFelt, StorageKey};
 
 /// Caches read and write requests.
 ///
@@ -11,11 +12,33 @@ pub struct CachedState<SR: StateReader> {
     pub state_reader: SR,
     // Invariant: the cache should remain private.
     _cache: StateCache,
+    contract_class_cache: HashMap<ClassHash, Rc<ContractClass>>,
 }
 
 impl<SR: StateReader> CachedState<SR> {
     pub fn new(state_reader: SR) -> Self {
-        Self { state_reader, _cache: StateCache::default() }
+        Self {
+            state_reader,
+            _cache: StateCache::default(),
+            contract_class_cache: HashMap::default(),
+        }
+    }
+
+    pub fn get_contract_class(
+        &mut self,
+        contract_hash: &ClassHash,
+    ) -> Result<Option<Rc<ContractClass>>> {
+        if !self.contract_class_cache.contains_key(contract_hash) {
+            let contract_class = self.state_reader.get_contract_class(contract_hash)?;
+            match contract_class {
+                // TODO(Noa, 01/01/23): Consider optimizing by storing the None value in the cache
+                None => return Ok(None),
+                Some(contract_class) => {
+                    self.contract_class_cache.insert(*contract_hash, Rc::clone(&contract_class));
+                }
+            }
+        }
+        Ok(Some(Rc::clone(self.contract_class_cache.get(contract_hash).unwrap())))
     }
 }
 
@@ -40,6 +63,11 @@ pub trait StateReader {
     /// uninitialized class hash, if the address is unassigned.
     fn get_class_hash_at(&self, _contract_address: ContractAddress) -> Result<ClassHash> {
         unimplemented!();
+    }
+
+    /// Returns the contract class of the given class hash; None if the class hash is uninitialized.
+    fn get_contract_class(&self, _contract_hash: &ClassHash) -> Result<Option<Rc<ContractClass>>> {
+        unimplemented!()
     }
 }
 
