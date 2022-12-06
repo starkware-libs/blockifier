0ee0e91056c6552b47da51f68cfdf130db345b75
Adi Yakovian <adi@starkware.co>
2022-12-06 20:18:14 +0200

    Add transaction module.

---
+++ blockifier/src/transaction/execution_objects.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+use starknet_api::{Fee, StarkFelt};
+
+// TODO(Adi, 10/12/2022): Change to the Python class definition, once the 'execute' function of
+// 'CallEntryPoint' returns a CallInfo.
+pub type CallInfo = Vec<StarkFelt>;
+
+// TODO(Adi, 10/12/2022): Add a 'transaction_type' field.
+/// Contains the information gathered by the execution of a transaction.
+#[derive(Debug, Eq, PartialEq)]
+pub struct TransactionExecutionInfo {
+    /// Transaction-specific validation call info.
+    pub validate_info: Option<CallInfo>,
+    /// Transaction-specific execution call info, None for Declare.
+    pub call_info: Option<CallInfo>,
+    /// Fee transfer call info, executed by the BE for account contract transactions (e.g., declare
+    /// and invoke).
+    pub fee_transfer_info: Option<CallInfo>,
+    /// The actual fee that was charged (in Wei).
+    pub actual_fee: Fee,
+    /// Actual resources the transaction is charged for, including L1 gas and OS additional
+    /// resources estimation.
+    pub actual_resources: ResourcesMapping,
+}
+
+/// A mapping from a transaction resource to its actual usage.
+#[derive(Debug, Eq, PartialEq)]
+pub struct ResourcesMapping(pub HashMap<String, usize>);
+
+impl ResourcesMapping {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl Default for ResourcesMapping {
+    fn default() -> Self {
+        Self::new()
+    }
+}
