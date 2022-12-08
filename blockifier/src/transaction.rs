f782526d7ac06577027e0c8928697ab17483f8c6
Adi Yakovian <adi@starkware.co>
2022-12-08 15:32:54 +0200

    Add InvokeTransaction.

--- blockifier/src/transaction.rs
+++ blockifier/src/transaction.rs
@@ -1,4 +1,15 @@
 pub mod constants;
 pub mod errors;
+pub mod invoke_transaction;
 pub mod objects;
 pub mod transaction_utils;
+
+use crate::cached_state::{CachedState, DictStateReader};
+use crate::transaction::objects::{TransactionExecutionInfo, TransactionExecutionResult};
+
+pub trait ExecuteTransaction {
+    fn execute(
+        &self,
+        state: CachedState<DictStateReader>,
+    ) -> TransactionExecutionResult<TransactionExecutionInfo>;
+}
