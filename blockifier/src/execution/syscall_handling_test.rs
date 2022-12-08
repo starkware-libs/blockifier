c7152b720ecbfe9dafdb82472e2b254a8aa9d45b
Alon Haramati <alonh@starkware.co>
2022-12-08 14:55:07 +0200

    Match syscalls instead of dynamic dispatch.

---
+++ blockifier/src/execution/syscall_handling_test.rs
@@ -0,0 +1,23 @@
+use std::mem;
+
+use anyhow::Result;
+use starknet_api::hash::StarkFelt;
+
+use crate::execution::syscall_structs::{
+    StorageReadRequest, StorageReadResponse, StorageWriteRequest, StorageWriteResponse,
+    STORAGE_READ_REQUEST_SIZE, STORAGE_READ_RESPONSE_SIZE, STORAGE_WRITE_REQUEST_SIZE,
+    STORAGE_WRITE_RESPONSE_SIZE,
+};
+
+pub fn size_in_felts<T>() -> usize {
+    mem::size_of::<T>() / mem::size_of::<StarkFelt>()
+}
+
+#[test]
+fn test_syscall_struct_sizes() -> Result<()> {
+    assert_eq!(size_in_felts::<StorageReadRequest>(), STORAGE_READ_REQUEST_SIZE);
+    assert_eq!(size_in_felts::<StorageReadResponse>(), STORAGE_READ_RESPONSE_SIZE);
+    assert_eq!(size_in_felts::<StorageWriteRequest>(), STORAGE_WRITE_REQUEST_SIZE);
+    assert_eq!(size_in_felts::<StorageWriteResponse>(), STORAGE_WRITE_RESPONSE_SIZE);
+    Ok(())
+}
--- blockifier/src/execution/syscall_structs.rs
+++ blockifier/src/execution/syscall_structs.rs
@@ -1,6 +1,3 @@
-use std::collections::HashMap;
-use std::mem;
-
 use cairo_rs::types::relocatable::Relocatable;
 use cairo_rs::vm::vm_core::VirtualMachine;
 use starknet_api::hash::StarkFelt;
@@ -10,64 +7,59 @@ use crate::execution::entry_point::EntryPointResult;
 use crate::execution::errors::SyscallExecutionError;
 use crate::execution::syscall_handling::SyscallHandler;
 
-pub type ReadRequestResult = EntryPointResult<Box<dyn SyscallRequest>>;
-pub type ExecutionResult = EntryPointResult<Box<dyn SyscallResponse>>;
+pub type ReadRequestResult = EntryPointResult<SyscallRequest>;
+pub type ExecutionResult = EntryPointResult<SyscallResponse>;
 pub type WriteResponseResult = EntryPointResult<()>;
 
-const STORAGE_READ_SELECTOR_BYTES: [u8; 32] = [
+pub const STORAGE_READ_SELECTOR_BYTES: [u8; 32] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 83, 116, 111, 114, 97, 103, 101,
     82, 101, 97, 100,
 ];
-const STORAGE_WRITE_SELECTOR_BYTES: [u8; 32] = [
+pub const STORAGE_WRITE_SELECTOR_BYTES: [u8; 32] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 83, 116, 111, 114, 97, 103, 101,
     87, 114, 105, 116, 101,
 ];
 
-pub trait SyscallRequest {
-    fn read(vm: &VirtualMachine, ptr: &Relocatable) -> ReadRequestResult
-    where
-        Self: Sized;
-
-    fn execute(&self, syscall_handler: &SyscallHandler) -> ExecutionResult;
-}
-
-pub trait SyscallResponse {
-    fn write(&self, vm: &mut VirtualMachine, ptr: &Relocatable) -> WriteResponseResult;
-}
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct EmptyResponse {}
 
-impl SyscallResponse for EmptyResponse {
-    fn write(&self, _vm: &mut VirtualMachine, _ptr: &Relocatable) -> WriteResponseResult {
+impl EmptyResponse {
+    pub fn write(&self, _vm: &mut VirtualMachine, _ptr: &Relocatable) -> WriteResponseResult {
         Ok(())
     }
 }
 
+pub const STORAGE_READ_REQUEST_SIZE: usize = 2;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct StorageReadRequest {
     pub selector: StarkFelt,
     pub address: StarkFelt,
 }
 
-impl SyscallRequest for StorageReadRequest {
-    fn read(vm: &VirtualMachine, ptr: &Relocatable) -> ReadRequestResult {
+impl StorageReadRequest {
+    pub fn read(vm: &VirtualMachine, ptr: &Relocatable) -> ReadRequestResult {
         let selector = get_felt_from_memory_cell(vm.get_maybe(ptr)?)?;
         let address = get_felt_from_memory_cell(vm.get_maybe(&(ptr + 1))?)?;
-        Ok(Box::new(StorageReadRequest { selector, address }))
+        Ok(SyscallRequest::StorageRead(StorageReadRequest { selector, address }))
     }
 
-    fn execute(&self, _syscall_handler: &SyscallHandler) -> ExecutionResult {
+    pub fn execute(&self, _syscall_handler: &SyscallHandler) -> ExecutionResult {
         // TODO(AlonH, 21/12/2022): Perform state read.
         let value = StarkFelt::from(17);
-        Ok(Box::new(StorageReadResponse { value }))
+        Ok(SyscallResponse::Read(StorageReadResponse { value }))
     }
 }
 
+pub const STORAGE_READ_RESPONSE_SIZE: usize = 1;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct StorageReadResponse {
     pub value: StarkFelt,
 }
 
-impl SyscallResponse for StorageReadResponse {
-    fn write(&self, vm: &mut VirtualMachine, ptr: &Relocatable) -> WriteResponseResult {
+impl StorageReadResponse {
+    pub fn write(&self, vm: &mut VirtualMachine, ptr: &Relocatable) -> WriteResponseResult {
         vm.insert_value(ptr, felt_to_bigint(self.value))?;
         Ok(())
     }
@@ -78,80 +70,107 @@ pub struct StorageRead {
     pub response: StorageReadResponse,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct StorageWriteRequest {
     pub selector: StarkFelt,
     pub address: StarkFelt,
     pub value: StarkFelt,
 }
 
-impl SyscallRequest for StorageWriteRequest {
-    fn read(vm: &VirtualMachine, ptr: &Relocatable) -> ReadRequestResult {
+pub const STORAGE_WRITE_REQUEST_SIZE: usize = 3;
+
+impl StorageWriteRequest {
+    pub fn read(vm: &VirtualMachine, ptr: &Relocatable) -> ReadRequestResult {
         let selector = get_felt_from_memory_cell(vm.get_maybe(ptr)?)?;
         let address = get_felt_from_memory_cell(vm.get_maybe(&(ptr + 1))?)?;
         let value = get_felt_from_memory_cell(vm.get_maybe(&(ptr + 2))?)?;
-        Ok(Box::new(StorageWriteRequest { selector, address, value }))
+        Ok(SyscallRequest::StorageWrite(StorageWriteRequest { selector, address, value }))
     }
 
-    fn execute(&self, _syscall_handler: &SyscallHandler) -> ExecutionResult {
+    pub fn execute(&self, _syscall_handler: &SyscallHandler) -> ExecutionResult {
         // TODO(AlonH, 21/12/2022): Perform state write.
         assert_eq!(self.value, StarkFelt::try_from(18).unwrap());
-        Ok(Box::new(EmptyResponse {}))
+        Ok(SyscallResponse::Write(EmptyResponse {}))
     }
 }
+
+pub const STORAGE_WRITE_RESPONSE_SIZE: usize = 0;
+
+pub type StorageWriteResponse = EmptyResponse;
 
 pub struct StorageWrite {
     pub request: StorageWriteRequest,
-    pub response: EmptyResponse,
+    pub response: StorageWriteResponse,
 }
 
-pub type SyscallRequestFactory = dyn Fn(&VirtualMachine, &Relocatable) -> ReadRequestResult;
-
-pub struct SyscallInfo {
-    pub syscall_request_factory: Box<SyscallRequestFactory>,
-    pub syscall_request_size: usize,
-    pub syscall_response_size: usize,
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum SyscallSelector {
-    Read,
-    Write,
+    StorageRead,
+    StorageWrite,
 }
 
 impl SyscallSelector {
     pub fn parse_selector(selector: StarkFelt) -> EntryPointResult<Self> {
         let selector_bytes: [u8; 32] = selector.bytes().try_into().unwrap();
         match selector_bytes {
-            STORAGE_READ_SELECTOR_BYTES => Ok(Self::Read),
-            STORAGE_WRITE_SELECTOR_BYTES => Ok(Self::Write),
+            STORAGE_READ_SELECTOR_BYTES => Ok(Self::StorageRead),
+            STORAGE_WRITE_SELECTOR_BYTES => Ok(Self::StorageWrite),
             bytes => Err(SyscallExecutionError::InvalidSyscallSelector(bytes).into()),
         }
     }
 }
 
-pub fn get_syscall_info() -> HashMap<SyscallSelector, SyscallInfo> {
-    [
-        (
-            SyscallSelector::Read,
-            SyscallInfo {
-                syscall_request_factory: Box::new(StorageReadRequest::read),
-                syscall_request_size: size_in_felts::<StorageReadRequest>(),
-                syscall_response_size: size_in_felts::<StorageReadResponse>(),
-            },
-        ),
-        (
-            SyscallSelector::Write,
-            SyscallInfo {
-                syscall_request_factory: Box::new(StorageWriteRequest::read),
-                syscall_request_size: size_in_felts::<StorageWriteRequest>(),
-                syscall_response_size: size_in_felts::<EmptyResponse>(),
-            },
-        ),
-    ]
-    .into_iter()
-    .collect()
+#[derive(Debug, PartialEq, Eq)]
+pub enum SyscallRequest {
+    StorageRead(StorageReadRequest),
+    StorageWrite(StorageWriteRequest),
 }
 
-pub fn size_in_felts<T>() -> usize {
-    mem::size_of::<T>() / mem::size_of::<StarkFelt>()
+impl SyscallRequest {
+    pub fn read(
+        selector: SyscallSelector,
+        vm: &VirtualMachine,
+        ptr: &Relocatable,
+    ) -> ReadRequestResult {
+        match selector {
+            SyscallSelector::StorageRead => StorageReadRequest::read(vm, ptr),
+            SyscallSelector::StorageWrite => StorageWriteRequest::read(vm, ptr),
+        }
+    }
+
+    pub fn execute(&self, syscall_handler: &SyscallHandler) -> ExecutionResult {
+        match self {
+            SyscallRequest::StorageRead(request) => request.execute(syscall_handler),
+            SyscallRequest::StorageWrite(request) => request.execute(syscall_handler),
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        match self {
+            SyscallRequest::StorageRead(_) => STORAGE_READ_REQUEST_SIZE,
+            SyscallRequest::StorageWrite(_) => STORAGE_WRITE_REQUEST_SIZE,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum SyscallResponse {
+    Read(StorageReadResponse),
+    Write(EmptyResponse),
+}
+
+impl SyscallResponse {
+    pub fn write(&self, vm: &mut VirtualMachine, ptr: &Relocatable) -> WriteResponseResult {
+        match self {
+            SyscallResponse::Read(response) => response.write(vm, ptr),
+            SyscallResponse::Write(response) => response.write(vm, ptr),
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        match self {
+            SyscallResponse::Read(_) => STORAGE_READ_RESPONSE_SIZE,
+            SyscallResponse::Write(_) => STORAGE_WRITE_RESPONSE_SIZE,
+        }
+    }
 }
