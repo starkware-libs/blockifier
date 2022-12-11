91706e71f4f5d37b96b863bda6d7274e70d084b9
Alon Haramati <alonh@starkware.co>
2022-12-11 11:01:06 +0200

    Use state in syscall handler.

--- blockifier/src/execution/syscall_handling.rs
+++ blockifier/src/execution/syscall_handling.rs
@@ -13,7 +13,11 @@ use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
 use cairo_rs::vm::runners::cairo_runner::CairoRunner;
 use cairo_rs::vm::vm_core::VirtualMachine;
 use num_bigint::BigInt;
+use starknet_api::core::ContractAddress;
+use starknet_api::hash::StarkHash;
+use starknet_api::shash;
 
+use crate::cached_state::{CachedState, DictStateReader};
 use crate::execution::entry_point::EntryPointResult;
 use crate::execution::errors::SyscallExecutionError;
 use crate::execution::execution_utils::get_felt_from_memory_cell;
@@ -26,11 +30,17 @@ mod test;
 /// Responsible for managing the state of StarkNet syscalls.
 pub struct SyscallHandler {
     pub expected_syscall_ptr: Relocatable,
+    pub state: CachedState<DictStateReader>,
+    pub contract_address: ContractAddress,
 }
 
 impl SyscallHandler {
-    pub fn new(initial_syscall_ptr: Relocatable) -> Self {
-        SyscallHandler { expected_syscall_ptr: initial_syscall_ptr }
+    pub fn new(
+        initial_syscall_ptr: Relocatable,
+        state: CachedState<DictStateReader>,
+        contract_address: ContractAddress,
+    ) -> Self {
+        SyscallHandler { expected_syscall_ptr: initial_syscall_ptr, state, contract_address }
     }
 
     pub fn verify_syscall_ptr(&self, actual_ptr: &Relocatable) -> EntryPointResult<()> {
@@ -94,7 +104,10 @@ pub fn initialize_syscall_handler(
 ) -> (Relocatable, BuiltinHintProcessor) {
     let syscall_segment = vm.add_memory_segment();
     let mut hint_processor = BuiltinHintProcessor::new_empty();
-    let syscall_handler = SyscallHandler::new(syscall_segment.clone());
+    let state: CachedState<DictStateReader> = CachedState::default();
+    // TODO(AlonH, 21/12/2022): Use actual contract address once it's part of the entry point.
+    let contract_address = ContractAddress::try_from(shash!("0x1")).unwrap();
+    let syscall_handler = SyscallHandler::new(syscall_segment.clone(), state, contract_address);
     add_syscall_hints(&mut hint_processor);
     cairo_runner
         .exec_scopes
