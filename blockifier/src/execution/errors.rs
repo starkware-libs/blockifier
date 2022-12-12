779fe834211b8f2a2a7625310d57e04a9fdf61d6
Alon Haramati <alonh@starkware.co>
2022-12-12 13:19:01 +0200

    Order error hierarchy.

--- blockifier/src/execution/errors.rs
+++ blockifier/src/execution/errors.rs
@@ -1,39 +1,91 @@
+use cairo_rs::types::errors::program_errors::ProgramError;
 use cairo_rs::types::relocatable::Relocatable;
 use cairo_rs::vm::errors::memory_errors::MemoryError;
+use cairo_rs::vm::errors::runner_errors::RunnerError;
 use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
+use starknet_api::core::EntryPointSelector;
 use starknet_api::StarknetApiError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum EntryPointExecutionError {
+pub enum PreExecutionError {
+    #[error("Entry point {0:#?} not found in contract.")]
+    EntryPointNotFound(EntryPointSelector),
     #[error(transparent)]
-    StarknetApiError(#[from] StarknetApiError),
+    ProgramError(#[from] ProgramError),
     #[error(transparent)]
-    SyscallExecutionError(#[from] SyscallExecutionError),
+    RunnerError(Box<RunnerError>),
+}
+
+impl From<RunnerError> for PreExecutionError {
+    fn from(error: RunnerError) -> Self {
+        Self::RunnerError(Box::new(error))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum PostExecutionError {
     #[error(transparent)]
     VirtualMachineError(#[from] VirtualMachineError),
 }
 
-impl From<MemoryError> for EntryPointExecutionError {
+impl From<MemoryError> for PostExecutionError {
     fn from(error: MemoryError) -> Self {
         Self::VirtualMachineError(VirtualMachineError::MemoryError(error))
     }
 }
 
-// TODO(AlonH, 21/12/2022): Reconsider error returned by custom hints with LambdaClass.
-impl From<EntryPointExecutionError> for VirtualMachineError {
+#[derive(Debug, Error)]
+pub enum SyscallExecutionError {
+    #[error("Bad syscall_ptr, Expected {0:?}, got {1:?}.")]
+    BadSyscallPointer(Relocatable, Relocatable),
+    #[error(transparent)]
+    InnerCallExecutionError(Box<EntryPointExecutionError>),
+    #[error("Invalid syscall selector: {0:?}.")]
+    InvalidSyscallSelector([u8; 32]),
+    #[error(transparent)]
+    StarknetApiError(#[from] StarknetApiError),
+    #[error(transparent)]
+    VirtualMachineError(#[from] VirtualMachineError),
+}
+
+impl From<MemoryError> for SyscallExecutionError {
+    fn from(error: MemoryError) -> Self {
+        Self::VirtualMachineError(VirtualMachineError::MemoryError(error))
+    }
+}
+
+impl From<EntryPointExecutionError> for SyscallExecutionError {
     fn from(error: EntryPointExecutionError) -> Self {
+        Self::InnerCallExecutionError(Box::new(error))
+    }
+}
+
+// TODO(AlonH, 21/12/2022): Reconsider error returned by custom hints with LambdaClass.
+impl From<SyscallExecutionError> for VirtualMachineError {
+    fn from(error: SyscallExecutionError) -> Self {
         match error {
-            EntryPointExecutionError::VirtualMachineError(raw_error) => raw_error,
+            SyscallExecutionError::VirtualMachineError(raw_error) => raw_error,
             error => VirtualMachineError::CustomHint(error.to_string()),
         }
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Error)]
-pub enum SyscallExecutionError {
-    #[error("Invalid syscall selector: {0:?}")]
-    InvalidSyscallSelector([u8; 32]),
-    #[error("Bad syscall_ptr, Expected {0:?}, got {1:?}.")]
-    BadSyscallPointer(Relocatable, Relocatable),
+#[derive(Debug, Error)]
+pub enum VirtualMachineExecutionError {
+    #[error(transparent)]
+    VirtualMachineError(#[from] VirtualMachineError),
+}
+
+#[derive(Debug, Error)]
+pub enum EntryPointExecutionError {
+    #[error(transparent)]
+    PostExecutionError(#[from] PostExecutionError),
+    #[error(transparent)]
+    PreExecutionError(#[from] PreExecutionError),
+    #[error(transparent)]
+    SyscallExecutionError(#[from] SyscallExecutionError),
+    /// Gathers all errors from running the Cairo VM, excluding hints.
+    #[error(transparent)]
+    VirtualMachineExecutionError(#[from] VirtualMachineExecutionError),
 }
