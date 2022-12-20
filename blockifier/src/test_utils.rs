df2309e68eeba35db239c382eb855b1d895ed400
Adi Yakovian <adi@starkware.co>
2022-12-20 17:08:18 +0200

    Add fee_charge logic.

--- blockifier/src/test_utils.rs
+++ blockifier/src/test_utils.rs
@@ -26,6 +26,20 @@ pub const TEST_LIBRARY_CALL_SELECTOR: &str =
 pub const TEST_CLASS_HASH: &str = "0x1";
 pub const TEST_CONTRACT_ADDRESS: &str = "0x100";
 
+// TODO(Adi, 10/02/2022): Replace with 'account_contract_without_validations' once the syscalls are
+// implemented.
+pub const ACCOUNT_CONTRACT_PATH: &str =
+    "./feature_contracts/compiled/account_without_some_syscalls_compiled.json";
+
+// TODO(Adi, 15/01/2023): Remove and compute the class hash corresponding to the ERC20 contract in
+// starkgate once we use the real ERC20 contract.
+pub const TEST_ERC20_CONTRACT_CLASS_HASH: &str = "0x1010";
+pub const TEST_ERC20_CONTRACT_ADDRESS: &str = "0x1000";
+pub const TEST_ERC20_SELF_BALANCE_KEY: &str =
+    "0x723973208639b7839ce298f7ffea61e3f9533872defd7abdb91023db4658812";
+pub const TEST_ERC20_ACCOUNT_BALANCE_KEY: &str =
+    "0x2a2c49c4dba0d91b34f2ade85d41d09561f9a77884c15ba2ab0f2241b080deb";
+
 // TODO(Adi, 25/12/2022): Consider removing this function once we use the starknet_api transaction
 // struct instead of our own, which contains a `contract_path` instead of a `contract_address`.
 pub fn get_contract_class(contract_path: &str) -> ContractClass {
--- blockifier/src/transaction/constants.rs
+++ blockifier/src/transaction/constants.rs
@@ -2,11 +2,18 @@
 // The entrypoint selector corresponding to the '__execute__' entrypoint.
 pub const EXECUTE_ENTRY_POINT_SELECTOR: &str =
     "0x15d40a3d6ca2ac30f4031e42be28da9b056fef9bb7357ac5e85627ee876e5ad";
+// The entrypoint selector corresponding to the 'transfer' entrypoint.
+pub const TRANSFER_ENTRY_POINT_SELECTOR: &str =
+    "0x83afd3f4caedc6eebf44246fe54e38c95e3179a5ec9ea81740eca5b482d12e";
 // The entrypoint selector corresponding to the '__validate__' entrypoint.
 pub const VALIDATE_ENTRY_POINT_SELECTOR: &str =
     "0x162da33a4585851fe8d3af3c2a9c60b557814e221e0d4f30ff0b2189d9c7775";
 
-// TODO(Adi, 10/02/2022): Replace with 'account_contract_without_validations' once the syscalls are
-// implemented.
-pub const ACCOUNT_CONTRACT_PATH: &str =
-    "./feature_contracts/compiled/account_without_some_syscalls_compiled.json";
+// The index of the beginning of the called contract calldata in the invoke transaction
+// `__execute__` calldata.
+pub const CALL_CONTRACT_CALLDATA_INDEX: usize = 3;
+
+// TODO(Adi, 15/01/2023): Remove and use the ERC20 contract in starkgate once we use the real
+// ERC20 contract.
+pub const ERC20_CONTRACT_PATH: &str =
+    "./feature_contracts/compiled/erc20_contract_without_some_syscalls_compiled.json";
