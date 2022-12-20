041ebf10e8a55730cd7bafabf4abed972ac4249b
Adi Yakovian <adi@starkware.co>
2022-12-20 17:08:18 +0200

    Add fee_charge logic.

--- blockifier/src/transaction/invoke_transaction_test.rs
+++ blockifier/src/transaction/invoke_transaction_test.rs
@@ -3,47 +3,65 @@ use std::collections::HashMap;
 use assert_matches::assert_matches;
 use pretty_assertions::assert_eq;
 use starknet_api::core::{ClassHash, ContractAddress, EntryPointSelector, Nonce, PatriciaKey};
-use starknet_api::hash::{StarkFelt, StarkHash};
-use starknet_api::state::EntryPointType;
+use starknet_api::hash::StarkHash;
+use starknet_api::state::{EntryPointType, StorageKey};
 use starknet_api::transaction::{
     Calldata, Fee, InvokeTransaction, TransactionHash, TransactionSignature, TransactionVersion,
 };
-use starknet_api::{patky, shash, StarknetApiError};
+use starknet_api::{patky, shash};
 
 use crate::execution::entry_point::{CallEntryPoint, CallExecution, CallInfo, Retdata};
 use crate::state::cached_state::{CachedState, DictStateReader};
+use crate::state::state_api::State;
 use crate::test_utils::{
-    get_contract_class, ACCOUNT_CONTRACT_PATH, RETURN_RESULT_SELECTOR,
+    get_contract_class, ACCOUNT_CONTRACT_PATH, ERC20_CONTRACT_PATH, RETURN_RESULT_SELECTOR,
     TEST_ACCOUNT_CONTRACT_ADDRESS, TEST_ACCOUNT_CONTRACT_CLASS_HASH, TEST_CLASS_HASH,
-    TEST_CONTRACT_ADDRESS, TEST_CONTRACT_PATH,
+    TEST_CONTRACT_ADDRESS, TEST_CONTRACT_PATH, TEST_ERC20_ACCOUNT_BALANCE_KEY,
+    TEST_ERC20_CONTRACT_ADDRESS, TEST_ERC20_CONTRACT_CLASS_HASH, TEST_ERC20_SEQUENCER_BALANCE_KEY,
+    TEST_SEQUENCER_ADDRESS,
 };
-use crate::transaction::constants::{EXECUTE_ENTRY_POINT_SELECTOR, VALIDATE_ENTRY_POINT_SELECTOR};
+use crate::transaction::constants::{
+    EXECUTE_ENTRY_POINT_SELECTOR, TRANSFER_ENTRY_POINT_SELECTOR, VALIDATE_ENTRY_POINT_SELECTOR,
+};
 use crate::transaction::errors::{FeeTransferError, TransactionExecutionError};
-use crate::transaction::objects::{
-    ResourcesMapping, TransactionExecutionInfo, TransactionExecutionResult,
-};
+use crate::transaction::objects::{ResourcesMapping, TransactionExecutionInfo};
 use crate::transaction::ExecuteTransaction;
 
-// TODO(Adi, 25/12/2022): Use (or create) a `create_test_state` test utils function.
 fn create_test_state() -> CachedState<DictStateReader> {
     let test_contract_class_hash = ClassHash(shash!(TEST_CLASS_HASH));
     let test_account_contract_class_hash = ClassHash(shash!(TEST_ACCOUNT_CONTRACT_CLASS_HASH));
+    let test_erc20_class_hash = ClassHash(shash!(TEST_ERC20_CONTRACT_CLASS_HASH));
     let class_hash_to_class = HashMap::from([
         (test_account_contract_class_hash, get_contract_class(ACCOUNT_CONTRACT_PATH)),
         (test_contract_class_hash, get_contract_class(TEST_CONTRACT_PATH)),
+        (test_erc20_class_hash, get_contract_class(ERC20_CONTRACT_PATH)),
     ]);
+    let test_contract_address = ContractAddress(patky!(TEST_CONTRACT_ADDRESS));
+    let test_account_contract_address = ContractAddress(patky!(TEST_ACCOUNT_CONTRACT_ADDRESS));
+    let test_erc20_contract_address = ContractAddress(patky!(TEST_ERC20_CONTRACT_ADDRESS));
     let address_to_class_hash = HashMap::from([
-        (ContractAddress(patky!(TEST_CONTRACT_ADDRESS)), test_contract_class_hash),
-        (ContractAddress(patky!(TEST_ACCOUNT_CONTRACT_ADDRESS)), test_account_contract_class_hash),
+        (test_contract_address, test_contract_class_hash),
+        (test_account_contract_address, test_account_contract_class_hash),
+        (test_erc20_contract_address, test_erc20_class_hash),
+    ]);
+    let test_erc20_account_balance_key = StorageKey(patky!(TEST_ERC20_ACCOUNT_BALANCE_KEY));
+    let test_erc20_sequencer_balance_key = StorageKey(patky!(TEST_ERC20_SEQUENCER_BALANCE_KEY));
+    let storage_view = HashMap::from([
+        ((test_erc20_contract_address, test_erc20_sequencer_balance_key), shash!(0)),
+        (
+            (test_erc20_contract_address, test_erc20_account_balance_key),
+            shash!(get_tested_actual_fee().0 as u64),
+        ),
     ]);
     CachedState::new(DictStateReader {
         address_to_class_hash,
         class_hash_to_class,
+        storage_view,
         ..Default::default()
     })
 }
 
-fn get_tested_valid_invoke_tx() -> Result<InvokeTransaction, StarknetApiError> {
+fn get_tested_valid_invoke_tx() -> InvokeTransaction {
     let execute_calldata = Calldata(
         vec![
             shash!(TEST_CONTRACT_ADDRESS),  // Contract address.
@@ -54,10 +72,10 @@ fn get_tested_valid_invoke_tx() -> Result<InvokeTransaction, StarknetApiError> {
         .into(),
     );
 
-    let invoke_tx = InvokeTransaction {
+    InvokeTransaction {
         transaction_hash: TransactionHash(StarkHash::default()),
         max_fee: Fee(1),
-        version: TransactionVersion(StarkFelt::from(1)),
+        version: TransactionVersion(shash!(1)),
         signature: TransactionSignature(vec![StarkHash::default()]),
         nonce: Nonce::default(),
         // TODO(Adi, 25/12/2022): Use an actual contract_address once there is a mapping from a
@@ -65,9 +83,7 @@ fn get_tested_valid_invoke_tx() -> Result<InvokeTransaction, StarknetApiError> {
         sender_address: ContractAddress::try_from(shash!(TEST_ACCOUNT_CONTRACT_ADDRESS)).unwrap(),
         entry_point_selector: None,
         calldata: execute_calldata,
-    };
-
-    Ok(invoke_tx)
+    }
 }
 
 fn get_tested_actual_fee() -> Fee {
@@ -75,18 +91,22 @@ fn get_tested_actual_fee() -> Fee {
 }
 
 #[test]
-fn test_invoke_tx() -> TransactionExecutionResult<()> {
+fn test_invoke_tx() {
     let mut state = create_test_state();
-    let tx = get_tested_valid_invoke_tx()?;
+    let tx = get_tested_valid_invoke_tx();
+    let calldata = tx.calldata.clone();
 
-    // Create expected result object.
+    let actual_execution_info = tx.execute(&mut state).unwrap();
+
+    // Create expected execution info object.
     let expected_validate_call_info = CallInfo {
         call: CallEntryPoint {
             class_hash: None,
             entry_point_type: EntryPointType::External,
             entry_point_selector: EntryPointSelector(shash!(VALIDATE_ENTRY_POINT_SELECTOR)),
-            calldata: tx.calldata.clone(),
-            storage_address: ContractAddress::try_from(shash!(TEST_ACCOUNT_CONTRACT_ADDRESS))?,
+            calldata,
+            storage_address: ContractAddress::try_from(shash!(TEST_ACCOUNT_CONTRACT_ADDRESS))
+                .unwrap(),
             caller_address: ContractAddress::default(),
         },
         // The account contract we use for testing has a trivial `validate` function.
@@ -120,33 +140,78 @@ fn test_invoke_tx() -> TransactionExecutionResult<()> {
         ..Default::default()
     };
 
+    let expected_actual_fee = get_tested_actual_fee();
+    let expected_fee_transfer_call = CallEntryPoint {
+        class_hash: None,
+        entry_point_type: EntryPointType::External,
+        entry_point_selector: EntryPointSelector(shash!(TRANSFER_ENTRY_POINT_SELECTOR)),
+        calldata: Calldata(
+            vec![
+                // TODO(Adi, 15/01/2023): The sender argument should be removed once
+                // `get_caller_address` is implemented.
+                shash!(TEST_SEQUENCER_ADDRESS),        // Recipient.
+                shash!(TEST_ACCOUNT_CONTRACT_ADDRESS), // Sender.
+                shash!(expected_actual_fee.0 as u64),  // Amount (lower 128-bit).
+                shash!(0),                             // Amount (upper 128-bit).
+            ]
+            .into(),
+        ),
+        storage_address: ContractAddress::try_from(shash!(TEST_ERC20_CONTRACT_ADDRESS)).unwrap(),
+        caller_address: ContractAddress::try_from(shash!(TEST_ACCOUNT_CONTRACT_ADDRESS)).unwrap(),
+    };
+    let expected_fee_transfer_call_info = CallInfo {
+        call: expected_fee_transfer_call,
+        execution: CallExecution { retdata: Retdata(vec![shash!(true as u64)].into()) },
+        ..Default::default()
+    };
+
     let expected_execution_info = TransactionExecutionInfo {
         validate_call_info: expected_validate_call_info,
         execute_call_info: Some(expected_execute_call_info),
-        fee_transfer_call_info: CallInfo::default(),
-        actual_fee: get_tested_actual_fee(),
+        fee_transfer_call_info: expected_fee_transfer_call_info,
+        actual_fee: expected_actual_fee,
         actual_resources: ResourcesMapping::default(),
     };
 
-    let actual_execution_info = tx.execute(&mut state)?;
+    // Test execution info result.
     assert_eq!(actual_execution_info, expected_execution_info);
 
-    Ok(())
+    // Test final balances.
+    let expected_account_balance = shash!(0);
+    let expected_sequencer_balance = shash!(expected_actual_fee.0 as u64);
+    assert_eq!(
+        state
+            .get_storage_at(
+                ContractAddress(patky!(TEST_ERC20_CONTRACT_ADDRESS)),
+                StorageKey(patky!(TEST_ERC20_ACCOUNT_BALANCE_KEY))
+            )
+            .unwrap(),
+        &shash!(expected_account_balance)
+    );
+    assert_eq!(
+        state
+            .get_storage_at(
+                ContractAddress(patky!(TEST_ERC20_CONTRACT_ADDRESS)),
+                StorageKey(patky!(TEST_ERC20_SEQUENCER_BALANCE_KEY))
+            )
+            .unwrap(),
+        &shash!(expected_sequencer_balance)
+    );
 }
 
 #[test]
-fn test_negative_invoke_tx_flows() -> TransactionExecutionResult<()> {
+fn test_negative_invoke_tx_flows() {
     let mut state = create_test_state();
-    let valid_tx = get_tested_valid_invoke_tx()?;
+    let valid_tx = get_tested_valid_invoke_tx();
 
     // Invalid version.
     // Note: there is no need to test for a negative version, as it cannot be constructed.
-    let invalid_tx_version = TransactionVersion(StarkHash::from(0));
+    let invalid_tx_version = TransactionVersion(shash!(0));
     let invalid_tx = InvokeTransaction { version: invalid_tx_version, ..valid_tx.clone() };
     let execution_error = invalid_tx.execute(&mut state).unwrap_err();
 
     // Test error.
-    let expected_allowed_versions = vec![TransactionVersion(StarkFelt::from(1))];
+    let expected_allowed_versions = vec![TransactionVersion(shash!(1))];
     assert_matches!(
         execution_error,
         TransactionExecutionError::InvalidTransactionVersion {
@@ -171,6 +236,4 @@ fn test_negative_invoke_tx_flows() -> TransactionExecutionResult<()> {
         })
         if (max_fee, actual_fee) == (tx_max_fee, expected_actual_fee)
     );
-
-    Ok(())
 }
