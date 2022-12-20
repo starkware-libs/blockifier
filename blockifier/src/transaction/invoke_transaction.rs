0b002059b01113915f4fbf1fb2206655a0c5902d
Adi Yakovian <adi@starkware.co>
2022-12-20 17:08:18 +0200

    Add fee_charge logic.

--- blockifier/src/transaction/invoke_transaction.rs
+++ blockifier/src/transaction/invoke_transaction.rs
@@ -57,9 +57,13 @@ pub fn execute_tx(
     Ok(execute_call.execute(state)?)
 }
 
-pub fn charge_fee(tx: &InvokeTransaction) -> TransactionExecutionResult<(Fee, CallInfo)> {
+pub fn charge_fee(
+    tx: &InvokeTransaction,
+    state: &mut CachedState<DictStateReader>,
+) -> TransactionExecutionResult<(Fee, CallInfo)> {
     let actual_fee = calculate_tx_fee();
-    let fee_transfer_call_info = execute_fee_transfer(actual_fee, tx.max_fee)?;
+    let fee_transfer_call_info =
+        execute_fee_transfer(actual_fee, tx.max_fee, tx.contract_address, state)?;
 
     Ok((actual_fee, fee_transfer_call_info))
 }
@@ -84,7 +88,7 @@ impl ExecuteTransaction for InvokeTransaction {
         // Charge fee.
         // TODO(Adi, 25/12/2022): Get actual resources.
         let actual_resources = ResourcesMapping::default();
-        let (actual_fee, fee_transfer_call_info) = charge_fee(self)?;
+        let (actual_fee, fee_transfer_call_info) = charge_fee(self, state)?;
 
         Ok(TransactionExecutionInfo {
             validate_call_info,
