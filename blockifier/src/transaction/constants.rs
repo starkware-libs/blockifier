61284f5964a09a041efc2efd7acb2739fa40473e
Adi Yakovian <adi@starkware.co>
2022-12-20 17:08:18 +0200

    Add fee_charge logic.

--- blockifier/src/transaction/constants.rs
+++ blockifier/src/transaction/constants.rs
@@ -2,6 +2,9 @@
 // The entrypoint selector corresponding to the '__execute__' entrypoint.
 pub const EXECUTE_ENTRY_POINT_SELECTOR: &str =
     "0x15d40a3d6ca2ac30f4031e42be28da9b056fef9bb7357ac5e85627ee876e5ad";
+// The entrypoint selector corresponding to the 'transfer' entrypoint.
+pub const TRANSFER_ENTRY_POINT_SELECTOR: &str =
+    "0x83afd3f4caedc6eebf44246fe54e38c95e3179a5ec9ea81740eca5b482d12e";
 // The entrypoint selector corresponding to the '__validate__' entrypoint.
 pub const VALIDATE_ENTRY_POINT_SELECTOR: &str =
     "0x162da33a4585851fe8d3af3c2a9c60b557814e221e0d4f30ff0b2189d9c7775";
