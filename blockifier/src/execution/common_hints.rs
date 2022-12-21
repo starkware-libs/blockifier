ed8bf42f567ac8914590f7126fbbce6121fda87d
Adi Yakovian <adi@starkware.co>
2022-12-21 19:44:55 +0200

    Add normalize_address hint.

---
+++ blockifier/src/execution/common_hints.rs
@@ -0,0 +1,74 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::ops::Shl;
+use std::rc::Rc;
+
+use cairo_rs::bigint;
+use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::{
+    BuiltinHintProcessor, HintFunc,
+};
+use cairo_rs::hint_processor::builtin_hint_processor::hint_utils::{
+    get_integer_from_var_name, insert_value_from_var_name,
+};
+use cairo_rs::hint_processor::hint_processor_definition::HintReference;
+use cairo_rs::serde::deserialize_program::ApTracking;
+use cairo_rs::types::exec_scope::ExecutionScopes;
+use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
+use cairo_rs::vm::vm_core::VirtualMachine;
+use num_bigint::BigInt;
+
+/// Implements hint:
+/// %{
+///    # Verify the assumptions on the relationship between 2**250, ADDR_BOUND and PRIME.
+///    ADDR_BOUND = ids.ADDR_BOUND % PRIME
+///    assert (2**250 < ADDR_BOUND <= 2**251) and (2 * 2**250 < PRIME) and (
+///            ADDR_BOUND * 2 > PRIME), \
+///        'normalize_address() cannot be used with the current constants.'
+///    ids.is_small = 1 if ids.addr < ADDR_BOUND else 0
+/// %}
+///
+/// Must comply with the API of a hint function, as defined by the `HintProcessor`.
+pub fn normalize_address(
+    vm: &mut VirtualMachine,
+    _execution_scopes: &mut ExecutionScopes,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+    constants: &HashMap<String, BigInt>,
+) -> Result<(), VirtualMachineError> {
+    let addr_bound =
+        constants.get("ADDR_BOUND").ok_or(VirtualMachineError::MissingConstant("ADDR_BOUND"))?;
+    let addr = get_integer_from_var_name("addr", vm, ids_data, ap_tracking)?;
+
+    let prime = vm.get_prime();
+    let addr_bound = &(addr_bound % prime);
+    let addr_bound_lower_bound = &bigint!(1).shl(250);
+    let addr_bound_inclusive_upper_bound = &bigint!(1).shl(251);
+    if addr_bound <= addr_bound_lower_bound
+        || addr_bound > addr_bound_inclusive_upper_bound
+        || prime <= &(bigint!(2) * addr_bound_lower_bound)
+        || &(2 * addr_bound) <= prime
+    {
+        return Err(VirtualMachineError::AssertionFailed(format!(
+            "assert (2**250 < {} <= 2**251) and (2 * 2**250 < PRIME) and (
+            {} * 2 > PRIME); normalize_address() cannot be used with the current constants.",
+            addr_bound, addr_bound
+        )));
+    }
+
+    let is_small = if addr < Cow::Borrowed(addr_bound) { bigint!(1) } else { bigint!(0) };
+    insert_value_from_var_name("is_small", is_small, vm, ids_data, ap_tracking)
+}
+
+pub fn add_common_hints(hint_processor: &mut BuiltinHintProcessor) {
+    hint_processor.add_hint(
+        String::from(
+            r#"# Verify the assumptions on the relationship between 2**250, ADDR_BOUND and PRIME.
+            ADDR_BOUND = ids.ADDR_BOUND % PRIME
+            assert (2**250 < ADDR_BOUND <= 2**251) and (2 * 2**250 < PRIME) and (
+                    ADDR_BOUND * 2 > PRIME), \
+                'normalize_address() cannot be used with the current constants.'
+            ids.is_small = 1 if ids.addr < ADDR_BOUND else 0"#,
+        ),
+        Rc::new(HintFunc(Box::new(normalize_address))),
+    );
+}
--- blockifier/src/execution/execution_utils.rs
+++ blockifier/src/execution/execution_utils.rs
@@ -19,6 +19,7 @@ use num_bigint::{BigInt, Sign};
 use num_traits::Signed;
 use starknet_api::hash::StarkFelt;
 
+use super::common_hints::add_common_hints;
 use crate::cached_state::{CachedState, DictStateReader};
 use crate::execution::entry_point::{CallEntryPoint, CallExecution, CallInfo, EntryPointResult};
 use crate::execution::errors::{
@@ -69,8 +70,9 @@ pub fn initialize_execution_context(
     let mut vm = VirtualMachine::new(program.prime, false, program.error_message_attributes);
     cairo_runner.initialize_builtins(&mut vm)?;
     cairo_runner.initialize_segments(&mut vm, None);
-    let (syscall_segment, hint_processor) =
+    let (syscall_segment, mut hint_processor) =
         initialize_syscall_handler(&mut cairo_runner, &mut vm, state, call_entry_point);
+    add_common_hints(&mut hint_processor);
 
     // Resolve initial PC from EP indicator.
     let entry_point_pc = call_entry_point.resolve_entry_point_pc(&contract_class)?;
