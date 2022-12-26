dea6be1e220f3db2f8d611deeed9b7d6221a5fbd
Gilad Chase <gilad@starkware.co>
2022-12-26 16:16:14 +0200

    Import `subtract_mappings` util from Python
    Will be used soon in the contruction of `StateDiff` from `CachedState`.
    
    Template bounds: Hash and Eq are HashMap boilerplate, and `V:
    PartialEq` is for the comparison inside `retain`.

---
+++ blockifier/src/utils.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+/// Returns a `HashMap` containing key-value pairs from `a` that are not included in `b` (if
+/// a key appears in `b` with a different value, it will be part of the output).
+/// Uses to take only updated cells from a mapping.
+pub fn subtract_mappings<K, V>(a: &HashMap<K, V>, b: &HashMap<K, V>) -> HashMap<K, V>
+where
+    K: Clone + Eq + std::hash::Hash,
+    V: Clone + PartialEq,
+{
+    let mut map = a.clone();
+    map.retain(|k, v| b.get(k) != Some(v));
+
+    map
+}
