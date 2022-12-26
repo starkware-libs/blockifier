b6cf725abf8d3697f897aa2468509f27792171aa
Alon Haramati <alonh@starkware.co>
2022-12-26 17:18:45 +0200

    Create a syscall hint processor.

--- blockifier/src/execution/syscall_handling.rs
+++ blockifier/src/execution/syscall_handling.rs
@@ -1,16 +1,15 @@
+use std::any::Any;
 use std::collections::HashMap;
-use std::rc::Rc;
 
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::{
-    BuiltinHintProcessor, HintFunc,
+    BuiltinHintProcessor, HintProcessorData,
 };
 use cairo_rs::hint_processor::builtin_hint_processor::hint_utils::get_ptr_from_var_name;
-use cairo_rs::hint_processor::hint_processor_definition::HintReference;
+use cairo_rs::hint_processor::hint_processor_definition::{HintProcessor, HintReference};
 use cairo_rs::serde::deserialize_program::ApTracking;
 use cairo_rs::types::exec_scope::ExecutionScopes;
 use cairo_rs::types::relocatable::{MaybeRelocatable, Relocatable};
 use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
-use cairo_rs::vm::runners::cairo_runner::CairoRunner;
 use cairo_rs::vm::vm_core::VirtualMachine;
 use num_bigint::BigInt;
 use starknet_api::core::{ContractAddress, EntryPointSelector};
@@ -23,31 +22,36 @@ use crate::execution::errors::SyscallExecutionError;
 use crate::execution::execution_utils::{
     felt_to_bigint, get_felt_from_memory_cell, get_felt_range,
 };
+use crate::execution::hint_code::SYSCALL_HINTS;
 use crate::execution::syscalls::{SyscallRequest, SyscallResult};
 use crate::state::cached_state::{CachedState, DictStateReader};
 
 /// Executes StarkNet syscalls (stateful protocol hints) during the execution of an EP call.
-pub struct SyscallHandler {
+pub struct SyscallHintProcessor<'a> {
     expected_syscall_ptr: Relocatable,
-    pub state: CachedState<DictStateReader>,
+    pub state: &'a mut CachedState<DictStateReader>,
     pub storage_address: ContractAddress,
     /// Inner calls invoked by the current execution.
     pub inner_calls: Vec<CallInfo>,
+    builtin_hint_processor: BuiltinHintProcessor,
 }
 
-impl SyscallHandler {
+impl<'a> SyscallHintProcessor<'a> {
     pub fn new(
         initial_syscall_ptr: Relocatable,
-        state: CachedState<DictStateReader>,
+        state: &'a mut CachedState<DictStateReader>,
         // TODO(AlonH, 21/12/2022): Consider referencing outer_call when lifetimes make it possible
         // (LambdaClass).
         storage_address: ContractAddress,
     ) -> Self {
-        SyscallHandler {
+        let mut builtin_hint_processor = BuiltinHintProcessor::new_empty();
+        add_common_hints(&mut builtin_hint_processor);
+        SyscallHintProcessor {
             expected_syscall_ptr: initial_syscall_ptr,
             state,
             inner_calls: vec![],
             storage_address,
+            builtin_hint_processor,
         }
     }
 
@@ -60,86 +64,73 @@ impl SyscallHandler {
         }
         Ok(())
     }
+
+    /// Infers and executes the next syscall.
+    /// Must comply with the API of a hint function, as defined by the `HintProcessor`.
+    pub fn execute_syscall(
+        &mut self,
+        vm: &mut VirtualMachine,
+        ids_data: &HashMap<String, HintReference>,
+        ap_tracking: &ApTracking,
+    ) -> Result<(), VirtualMachineError> {
+        let mut syscall_ptr = get_ptr_from_var_name("syscall_ptr", vm, ids_data, ap_tracking)?;
+        self.verify_syscall_ptr(syscall_ptr)?;
+
+        let selector = get_felt_from_memory_cell(vm.get_maybe(&syscall_ptr)?)?;
+        let selector_size = 1;
+        syscall_ptr = syscall_ptr + selector_size;
+
+        let request = SyscallRequest::read(selector, vm, &syscall_ptr)?;
+        syscall_ptr = syscall_ptr + request.size();
+
+        let response = request.execute(self)?;
+        let response_size = response.size();
+        response.write(vm, &syscall_ptr)?;
+        self.expected_syscall_ptr = syscall_ptr + response_size;
+
+        Ok(())
+    }
 }
 
-/// Infers and executes the next syscall.
-/// Must comply with the API of a hint function, as defined by the `HintProcessor`.
-pub fn execute_syscall(
+impl HintProcessor for SyscallHintProcessor<'_> {
+    fn execute_hint(
+        &mut self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        hint_data: &Box<dyn Any>,
+        constants: &HashMap<String, BigInt>,
+    ) -> Result<(), VirtualMachineError> {
+        let hint = hint_data
+            .downcast_ref::<HintProcessorData>()
+            .ok_or(VirtualMachineError::WrongHintData)?;
+        if SYSCALL_HINTS.contains(&&*hint.code) {
+            return self.execute_syscall(vm, &hint.ids_data, &hint.ap_tracking);
+        }
+
+        self.builtin_hint_processor.execute_hint(vm, exec_scopes, hint_data, constants)
+    }
+
+    fn compile_hint(
+        &self,
+        code: &str,
+        ap_tracking: &ApTracking,
+        reference_ids: &HashMap<String, usize>,
+        references: &HashMap<usize, HintReference>,
+    ) -> Result<Box<dyn Any>, VirtualMachineError> {
+        self.builtin_hint_processor.compile_hint(code, ap_tracking, reference_ids, references)
+    }
+}
+
+pub fn initialize_syscall_handler<'a>(
     vm: &mut VirtualMachine,
-    execution_scopes: &mut ExecutionScopes,
-    ids_data: &HashMap<String, HintReference>,
-    ap_tracking: &ApTracking,
-    _constants: &HashMap<String, BigInt>,
-) -> Result<(), VirtualMachineError> {
-    let syscall_handler = execution_scopes.get_mut_ref::<SyscallHandler>("syscall_handler")?;
-    let mut syscall_ptr = get_ptr_from_var_name("syscall_ptr", vm, ids_data, ap_tracking)?;
-    syscall_handler.verify_syscall_ptr(syscall_ptr)?;
-
-    let selector = get_felt_from_memory_cell(vm.get_maybe(&syscall_ptr)?)?;
-    let selector_size = 1;
-    syscall_ptr = syscall_ptr + selector_size;
-
-    let request = SyscallRequest::read(selector, vm, &syscall_ptr)?;
-    syscall_ptr = syscall_ptr + request.size();
-
-    let response = request.execute(syscall_handler)?;
-    let response_size = response.size();
-    response.write(vm, &syscall_ptr)?;
-    syscall_handler.expected_syscall_ptr = syscall_ptr + response_size;
-
-    Ok(())
-}
-
-pub fn add_syscall_hints(hint_processor: &mut BuiltinHintProcessor) {
-    let execute_syscall_hint = Rc::new(HintFunc(Box::new(execute_syscall)));
-    hint_processor.add_hint(
-        String::from(
-            "syscall_handler.storage_read(segments=segments, syscall_ptr=ids.syscall_ptr)",
-        ),
-        execute_syscall_hint.clone(),
-    );
-    hint_processor.add_hint(
-        String::from(
-            "syscall_handler.storage_write(segments=segments, syscall_ptr=ids.syscall_ptr)",
-        ),
-        execute_syscall_hint.clone(),
-    );
-    hint_processor.add_hint(
-        String::from(
-            "syscall_handler.library_call(segments=segments, syscall_ptr=ids.syscall_ptr)",
-        ),
-        execute_syscall_hint.clone(),
-    );
-    hint_processor.add_hint(
-        String::from(
-            "syscall_handler.call_contract(segments=segments, syscall_ptr=ids.syscall_ptr)",
-        ),
-        execute_syscall_hint.clone(),
-    );
-    hint_processor.add_hint(
-        String::from("syscall_handler.deploy(segments=segments, syscall_ptr=ids.syscall_ptr)"),
-        execute_syscall_hint,
-    );
-}
-
-pub fn initialize_syscall_handler(
-    cairo_runner: &mut CairoRunner,
-    vm: &mut VirtualMachine,
-    state: &CachedState<DictStateReader>,
+    state: &'a mut CachedState<DictStateReader>,
     call_entry_point: &CallEntryPoint,
-) -> (Relocatable, BuiltinHintProcessor) {
+) -> (Relocatable, SyscallHintProcessor<'a>) {
     let syscall_segment = vm.add_memory_segment();
-    let mut hint_processor = BuiltinHintProcessor::new_empty();
-    // TODO(AlonH, 21/12/2022): Remove clone (also Clone attribute and mut. refs.) when `state`
-    // becomes a reference.
     let syscall_handler =
-        SyscallHandler::new(syscall_segment, state.clone(), call_entry_point.storage_address);
-    add_syscall_hints(&mut hint_processor);
-    add_common_hints(&mut hint_processor);
-    cairo_runner
-        .exec_scopes
-        .assign_or_update_variable("syscall_handler", Box::new(syscall_handler));
-    (syscall_segment, hint_processor)
+        SyscallHintProcessor::new(syscall_segment, state, call_entry_point.storage_address);
+
+    (syscall_segment, syscall_handler)
 }
 
 // TODO(Noa, 26/12/2022): Consider implementing it as a From trait.
@@ -199,9 +190,9 @@ pub fn read_call_params(
 
 pub fn execute_inner_call(
     call_entry_point: CallEntryPoint,
-    syscall_handler: &mut SyscallHandler,
+    syscall_handler: &mut SyscallHintProcessor<'_>,
 ) -> SyscallResult<Vec<StarkFelt>> {
-    let call_info = call_entry_point.execute(&mut syscall_handler.state)?;
+    let call_info = call_entry_point.execute(syscall_handler.state)?;
     let retdata = call_info.execution.retdata.clone();
     syscall_handler.inner_calls.push(call_info);
 
