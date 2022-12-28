6b42974b99f8ee624fff1cabf6e7ecf1bac10b5d
Gilad Chase <gilad@starkware.co>
2022-12-28 17:10:12 +0200

    Make `CachedState` work generically
    Next step will be to make `CachedState` into a `State` trait and do this
    again, this time with `State<SR>`.
    
    Tests still use `DictStateReader` as they require a concrete
    implementation.

--- blockifier/src/transaction.rs
+++ blockifier/src/transaction.rs
@@ -4,12 +4,13 @@ pub mod invoke_transaction;
 pub mod objects;
 pub mod transaction_utils;
 
-use crate::state::cached_state::{CachedState, DictStateReader};
+use crate::state::cached_state::CachedState;
+use crate::state::state_reader::StateReader;
 use crate::transaction::objects::{TransactionExecutionInfo, TransactionExecutionResult};
 
-pub trait ExecuteTransaction {
+pub trait ExecuteTransaction<SR: StateReader> {
     fn execute(
         self,
-        state: &mut CachedState<DictStateReader>,
+        state: &mut CachedState<SR>,
     ) -> TransactionExecutionResult<TransactionExecutionInfo>;
 }
