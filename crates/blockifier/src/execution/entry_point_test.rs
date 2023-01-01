141dca1470774536c7fee5d0ab276357d56d037b
Alon Haramati <alonh@starkware.co>
2023-01-01 17:21:33 +0200

    Add more security tests.

--- crates/blockifier/src/execution/entry_point_test.rs
+++ crates/blockifier/src/execution/entry_point_test.rs
@@ -126,7 +126,7 @@ fn test_entry_point_not_found_in_contract() {
         ..trivial_external_entry_point()
     };
     assert_eq!(
-        format!("Entry point {:#?} not found in contract.", entry_point_call.entry_point_selector),
+        format!("Entry point {:?} not found in contract.", entry_point_call.entry_point_selector),
         format!("{}", entry_point_call.execute_directly(&mut state).unwrap_err())
     );
 }
@@ -144,30 +144,273 @@ fn test_storage_var() {
     );
 }
 
+/// Runs test scenarios that could fail the OS run and therefore must be caught in the Blockifier.
+fn run_security_test(
+    expected_error: &str,
+    entry_point_name: &str,
+    calldata: Calldata,
+    state: &mut CachedState<DictStateReader>,
+) {
+    let entry_point_selector = selector_from_name(entry_point_name);
+    let entry_point_call =
+        CallEntryPoint { entry_point_selector, calldata, ..trivial_external_entry_point() };
+    let error = match entry_point_call.execute_directly(state) {
+        Err(error) => error.to_string(),
+        Ok(_) => panic!(
+            "Entry point '{entry_point_name}' did not fail! Expected error: {expected_error}"
+        ),
+    };
+    if !error.contains(expected_error) {
+        panic!("Expected error: {expected_error}.\nGot: {error}")
+    }
+}
+
 #[test]
-fn test_security_failure() {
+fn test_vm_execution_security_failures() {
     let mut state = create_security_test_state();
 
-    fn run_security_test(
-        expected_error: &str,
-        entry_point_name: &str,
-        calldata: Calldata,
-        state: &mut CachedState<DictStateReader>,
-    ) {
-        let entry_point_selector = selector_from_name(entry_point_name);
-        let entry_point_call =
-            CallEntryPoint { entry_point_selector, calldata, ..trivial_external_entry_point() };
-        let error = entry_point_call.execute_directly(state).unwrap_err().to_string();
-        assert!(error.contains(expected_error))
-    }
+    run_security_test(
+        "Expected relocatable",
+        "test_nonrelocatable_syscall_ptr",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test("Couldn't compute operand", "test_unknown_memory", calldata![], &mut state);
+
+    run_security_test(
+        "Can only subtract two relocatable values of the same segment",
+        "test_subtraction_between_relocatables",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Cannot add two relocatable values",
+        "test_relocatables_addition_failure",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "op0 must be known in double dereference",
+        "test_op0_unknown_double_dereference",
+        calldata![],
+        &mut state,
+    );
+
+    // TODO(AlonH, 21/12/2022): Make sure this can't be done without a custom hint, then delete.
+    // run_security_test(
+    //     "Out of bounds access to program segment",
+    //     "test_write_to_program_segment",
+    //     calldata![],
+    //     &mut state,
+    // );
+
+    run_security_test("Cannot exit main scope.", "test_exit_main_scope", calldata![], &mut state);
+
+    run_security_test(
+        "Every enter_scope() requires a corresponding exit_scope()",
+        "test_unbalanced_enter_scope",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "exceeds maximum offset value",
+        "test_out_of_bound_memory_value",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Memory addresses must be relocatable",
+        "test_non_relocatable_memory_address",
+        calldata![],
+        &mut state,
+    );
+
+    // TODO(AlonH, 21/12/2022): Uncomment and fix expected error message after LC implement
+    // substitute_error_message_references.
+    // run_security_test(
+    //     "Bad expr: {test}",
+    //     "test_bad_expr_eval",
+    //     calldata![],
+    //     &mut state,
+    // );
+}
+
+#[test]
+fn test_builtin_execution_security_failures() {
+    let mut state = create_security_test_state();
+
+    run_security_test(
+        "Inconsistent auto-deduction for builtin pedersen",
+        "test_bad_pedersen_values",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Range-check validation failed, number is out of valid range",
+        "test_bad_range_check_values",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Signature not found",
+        "test_missing_signature_hint",
+        calldata![],
+        &mut state,
+    );
+
+    // TODO(AlonH, 21/12/2022): Uncomment after LC implement add_signature hint.
+    // run_security_test(
+    //     "Signature hint must point to the signature builtin segment",
+    //     "test_signature_hint_on_wrong_segment",
+    //     calldata![],
+    //     &mut state,
+    // );
+
+    // TODO(AlonH, 21/12/2022): Uncomment after LC fix the ec_op impl.
+    // run_security_test(
+    //     "Cannot apply EC operation: computation reached two points with the same x coordinate",
+    //     "test_ec_op_invalid_input",
+    //     calldata![],
+    //     &mut state,
+    // );
+
+    // TODO(AlonH, 21/12/2022): Uncomment after LC fix the ec_op point_on_curve.
+    // run_security_test(
+    //     "ec_op builtin: point \\({invalid_pt_x}, {invalid_pt_y}\\) is not on the curve",
+    //     "test_ec_op_point_not_on_curve",
+    //     calldata![],
+    //     &mut state,
+    // );
+}
+
+#[test]
+fn test_syscall_execution_security_failures() {
+    let mut state = create_security_test_state();
 
     for perform_inner_call_to_foo in 0..2 {
         let calldata = calldata![stark_felt!(perform_inner_call_to_foo)];
         run_security_test(
             "Custom Hint Error: Out of range",
             "test_read_bad_address",
+            calldata.clone(),
+            &mut state,
+        );
+
+        run_security_test(
+            "Custom Hint Error: Expected integer",
+            "test_relocatable_storage_address",
             calldata,
             &mut state,
-        )
+        );
     }
+
+    run_security_test(
+        "Requested contract address \
+         ContractAddress(PatriciaKey(StarkFelt(\"\
+         0x0000000000000000000000000000000000000000000000000000000000000017\"))) is not deployed",
+        "test_bad_call_address",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Memory addresses must be relocatable",
+        "test_bad_syscall_request_arg_type",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Entry point \
+         EntryPointSelector(StarkFelt(\"\
+         0x0000000000000000000000000000000000000000000000000000000000000019\")) not found in \
+         contract",
+        "test_bad_call_selector",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "The deploy_from_zero field in the deploy system call must be 0 or 1.",
+        "test_bad_deploy_from_zero_field",
+        calldata![],
+        &mut state,
+    );
+}
+
+#[test]
+fn test_post_run_validation_security_failure() {
+    let mut state = create_security_test_state();
+
+    run_security_test(
+        "Missing memory cells for builtin range_check",
+        "test_builtin_hole",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Missing memory cells for builtin hash",
+        "test_missing_pedersen_values",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Validation failed: Invalid stop pointer for range_check",
+        "test_bad_builtin_stop_ptr",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Validation failed: Syscall segment size",
+        "test_access_after_syscall_stop_ptr",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Validation failed: Syscall segment end",
+        "test_bad_syscall_stop_ptr",
+        calldata![],
+        &mut state,
+    );
+
+    run_security_test(
+        "Validation failed: Read-only segments",
+        "test_out_of_bounds_write_to_signature_segment",
+        calldata![],
+        &mut state,
+    );
+
+    // TODO(Elin, 21/12/2022): Uncomment after get_tx_info syscall is implemented.
+    // run_security_test(
+    //     "Validation failed: Read-only segments",
+    //     "test_out_of_bounds_write_to_tx_info_segment",
+    //     calldata![],
+    //     &mut state,
+    // );
+
+    run_security_test(
+        "Validation failed: Read-only segments",
+        "test_write_to_call_contract_return_value",
+        calldata![],
+        &mut state,
+    );
+
+    let calldata = calldata![stark_felt!(1), stark_felt!(1)];
+    run_security_test(
+        "Validation failed: Read-only segments",
+        "test_out_of_bounds_write_to_calldata_segment",
+        calldata,
+        &mut state,
+    );
 }
