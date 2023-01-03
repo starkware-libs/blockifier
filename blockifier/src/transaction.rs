786e6b7b2115fc5f8d368b8833f3df76b64be9f8
Adi Yakovian <adi@starkware.co>
2023-01-03 16:27:04 +0200

    Implement AccountTransaction trait instead of ExecuteTransaction trait.

--- blockifier/src/transaction.rs
+++ blockifier/src/transaction.rs
@@ -1,13 +1,6 @@
+pub mod account_transaction;
 pub mod constants;
 pub mod errors;
 pub mod invoke_transaction;
 pub mod objects;
 pub mod transaction_utils;
-
-use crate::state::state_api::State;
-use crate::transaction::objects::{TransactionExecutionInfo, TransactionExecutionResult};
-
-pub trait ExecuteTransaction {
-    fn execute(self, state: &mut dyn State)
-    -> TransactionExecutionResult<TransactionExecutionInfo>;
-}
