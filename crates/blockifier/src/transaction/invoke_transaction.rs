200a9279ed961e2d0095ba1cd2c0fef55810c6d4
Adi Yakovian <adi@starkware.co>
2023-01-03 16:27:04 +0200

    Add AccountTransaction enum.

--- crates/blockifier/src/transaction/invoke_transaction.rs
+++ crates/blockifier/src/transaction/invoke_transaction.rs
@@ -1,114 +1,35 @@
 use starknet_api::core::ContractAddress;
 use starknet_api::state::EntryPointType;
-use starknet_api::transaction::{Fee, InvokeTransaction};
+use starknet_api::transaction::InvokeTransaction;
 
 use crate::abi::abi_utils::get_selector_from_name;
 use crate::block_context::BlockContext;
 use crate::execution::entry_point::{CallEntryPoint, CallInfo};
 use crate::state::state_api::State;
-use crate::transaction::constants::{EXECUTE_ENTRY_POINT_NAME, VALIDATE_ENTRY_POINT_NAME};
-use crate::transaction::objects::{
-    AccountTransactionContext, ResourcesMapping, TransactionExecutionInfo,
-    TransactionExecutionResult,
-};
-use crate::transaction::transaction_utils::{
-    calculate_tx_fee, execute_fee_transfer, handle_nonce, verify_tx_version,
-};
-use crate::transaction::ExecuteTransaction;
+use crate::transaction::constants::EXECUTE_ENTRY_POINT_NAME;
+use crate::transaction::execute_transaction::ExecuteTransaction;
+use crate::transaction::objects::{AccountTransactionContext, TransactionExecutionResult};
 
 #[cfg(test)]
 #[path = "invoke_transaction_test.rs"]
 mod test;
 
-pub fn validate_tx(
-    tx: &InvokeTransaction,
-    state: &mut dyn State,
-    block_context: &BlockContext,
-    account_tx_context: &AccountTransactionContext,
-) -> TransactionExecutionResult<CallInfo> {
-    let validate_call = CallEntryPoint {
-        entry_point_type: EntryPointType::External,
-        entry_point_selector: get_selector_from_name(VALIDATE_ENTRY_POINT_NAME),
-        // Gets the same calldata as the execution itself.
-        calldata: tx.calldata.clone(),
-        class_hash: None,
-        storage_address: tx.sender_address,
-        caller_address: ContractAddress::default(),
-    };
-
-    Ok(validate_call.execute(state, block_context, account_tx_context)?)
-}
-
-pub fn execute_tx(
-    tx: InvokeTransaction,
-    state: &mut dyn State,
-    block_context: &BlockContext,
-    account_tx_context: &AccountTransactionContext,
-) -> TransactionExecutionResult<CallInfo> {
-    let execute_call = CallEntryPoint {
-        entry_point_type: EntryPointType::External,
-        entry_point_selector: get_selector_from_name(EXECUTE_ENTRY_POINT_NAME),
-        calldata: tx.calldata.clone(),
-        class_hash: None,
-        storage_address: tx.sender_address,
-        caller_address: ContractAddress::default(),
-    };
-
-    Ok(execute_call.execute(state, block_context, account_tx_context)?)
-}
-
-pub fn charge_fee(
-    state: &mut dyn State,
-    block_context: &BlockContext,
-    account_tx_context: &AccountTransactionContext,
-) -> TransactionExecutionResult<(Fee, CallInfo)> {
-    let actual_fee = calculate_tx_fee(block_context);
-    let fee_transfer_call_info =
-        execute_fee_transfer(state, actual_fee, block_context, account_tx_context)?;
-
-    Ok((actual_fee, fee_transfer_call_info))
-}
-
 impl ExecuteTransaction for InvokeTransaction {
-    fn execute(
+    fn execute_tx(
         self,
         state: &mut dyn State,
         block_context: &BlockContext,
-    ) -> TransactionExecutionResult<TransactionExecutionInfo> {
-        // TODO(Adi, 10/12/2022): Consider moving the transaction version verification to the
-        // TransactionVersion constructor.
-        verify_tx_version(self.version)?;
-
-        let account_tx_context = AccountTransactionContext {
-            transaction_hash: self.transaction_hash,
-            max_fee: self.max_fee,
-            version: self.version,
-            signature: self.signature.clone(),
-            nonce: self.nonce,
-            sender_address: self.sender_address,
+        account_tx_context: &AccountTransactionContext,
+    ) -> TransactionExecutionResult<CallInfo> {
+        let execute_call = CallEntryPoint {
+            entry_point_type: EntryPointType::External,
+            entry_point_selector: get_selector_from_name(EXECUTE_ENTRY_POINT_NAME),
+            calldata: self.calldata.clone(),
+            class_hash: None,
+            storage_address: self.sender_address,
+            caller_address: ContractAddress::default(),
         };
 
-        // Validate transaction.
-        let validate_call_info = validate_tx(&self, state, block_context, &account_tx_context)?;
-
-        // Execute transaction.
-        let execute_call_info = execute_tx(self, state, block_context, &account_tx_context)?;
-
-        // Handle nonce.
-        handle_nonce(&account_tx_context, state)?;
-
-        // Charge fee.
-        // TODO(Adi, 25/12/2022): Get actual resources.
-        let actual_resources = ResourcesMapping::default();
-        let (actual_fee, fee_transfer_call_info) =
-            charge_fee(state, block_context, &account_tx_context)?;
-
-        Ok(TransactionExecutionInfo {
-            validate_call_info,
-            execute_call_info: Some(execute_call_info),
-            fee_transfer_call_info,
-            actual_fee,
-            actual_resources,
-        })
+        Ok(execute_call.execute(state, block_context, account_tx_context)?)
     }
 }
--- crates/blockifier/src/transaction/transaction_utils.rs
+++ crates/blockifier/src/transaction/transaction_utils.rs
@@ -1,76 +1,7 @@
-use starknet_api::calldata;
-use starknet_api::hash::StarkFelt;
-use starknet_api::state::EntryPointType;
-use starknet_api::transaction::{Calldata, Fee, TransactionVersion};
+use starknet_api::transaction::Fee;
 
-use crate::abi::abi_utils::get_selector_from_name;
 use crate::block_context::BlockContext;
-use crate::execution::entry_point::{CallEntryPoint, CallInfo};
-use crate::state::state_api::State;
-use crate::transaction::constants::TRANSFER_ENTRY_POINT_NAME;
-use crate::transaction::errors::{FeeTransferError, TransactionExecutionError};
-use crate::transaction::objects::{AccountTransactionContext, TransactionExecutionResult};
 
 pub fn calculate_tx_fee(_block_context: &BlockContext) -> Fee {
     Fee(1)
-}
-
-pub fn execute_fee_transfer(
-    state: &mut dyn State,
-    actual_fee: Fee,
-    block_context: &BlockContext,
-    account_tx_context: &AccountTransactionContext,
-) -> TransactionExecutionResult<CallInfo> {
-    let max_fee = account_tx_context.max_fee;
-    if actual_fee > max_fee {
-        return Err(FeeTransferError::MaxFeeExceeded { max_fee, actual_fee })?;
-    }
-
-    // The least significant 128 bits of the amount transferred.
-    let lsb_amount = StarkFelt::from(actual_fee.0 as u64);
-    // The most significant 128 bits of the amount transferred.
-    let msb_amount = StarkFelt::from(0);
-
-    let fee_transfer_call = CallEntryPoint {
-        // TODO(Adi, 15/01/2023): Replace with a computed ERC20 class hash.
-        class_hash: None,
-        entry_point_type: EntryPointType::External,
-        entry_point_selector: get_selector_from_name(TRANSFER_ENTRY_POINT_NAME),
-        calldata: calldata![
-            *block_context.sequencer_address.0.key(), // Recipient.
-            lsb_amount,
-            msb_amount
-        ],
-        storage_address: block_context.fee_token_address,
-        caller_address: account_tx_context.sender_address,
-    };
-
-    Ok(fee_transfer_call.execute(state, block_context, account_tx_context)?)
-}
-
-pub fn verify_tx_version(version: TransactionVersion) -> TransactionExecutionResult<()> {
-    // TODO(Adi, 10/12/2022): Consider using the lazy_static crate or some other solution, so the
-    // allowed_versions variable will only be constructed once.
-    let allowed_versions = vec![TransactionVersion(StarkFelt::from(1))];
-    if allowed_versions.contains(&version) {
-        Ok(())
-    } else {
-        Err(TransactionExecutionError::InvalidVersion { version, allowed_versions })
-    }
-}
-
-pub fn handle_nonce(
-    account_tx_context: &AccountTransactionContext,
-    state: &mut dyn State,
-) -> TransactionExecutionResult<()> {
-    let current_nonce = *state.get_nonce_at(account_tx_context.sender_address)?;
-    if current_nonce != account_tx_context.nonce {
-        return Err(TransactionExecutionError::InvalidNonce {
-            expected_nonce: current_nonce,
-            actual_nonce: account_tx_context.nonce,
-        });
-    }
-
-    // Increment nonce.
-    Ok(state.increment_nonce(account_tx_context.sender_address)?)
 }
