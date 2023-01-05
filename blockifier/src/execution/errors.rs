dc447bee45795686b7996450dfb4ad39e03799e1
Noa Oved <noa@starkware.co>
2023-01-05 15:23:21 +0200

    Remove error conversion: SyscallExecutionError -> EntryPointExecutionError.

--- blockifier/src/execution/errors.rs
+++ blockifier/src/execution/errors.rs
@@ -50,8 +50,6 @@ impl From<cairo_rs_vm_errors::runner_errors::RunnerError> for PostExecutionError
 pub enum SyscallExecutionError {
     #[error("Bad syscall_ptr; expected: {expected_ptr:?}, got: {actual_ptr:?}.")]
     BadSyscallPointer { expected_ptr: Relocatable, actual_ptr: Relocatable },
-    #[error(transparent)]
-    InnerCallExecutionError(Box<EntryPointExecutionError>),
     #[error("Invalid syscall input: {input:?}; {info:}")]
     InvalidSyscallInput { input: StarkFelt, info: String },
     #[error("Invalid syscall selector: {0:?}.")]
@@ -63,13 +61,9 @@ pub enum SyscallExecutionError {
     #[error(transparent)]
     StateError(#[from] StateError),
     #[error(transparent)]
+    InnerCallExecutionError(#[from] EntryPointExecutionError),
+    #[error(transparent)]
     VirtualMachineError(#[from] cairo_rs_vm_errors::vm_errors::VirtualMachineError),
-}
-
-impl From<EntryPointExecutionError> for SyscallExecutionError {
-    fn from(error: EntryPointExecutionError) -> Self {
-        Self::InnerCallExecutionError(Box::new(error))
-    }
 }
 
 // TODO(AlonH, 21/12/2022): Reconsider error returned by custom hints with LambdaClass.
@@ -97,8 +91,6 @@ pub enum EntryPointExecutionError {
     PreExecutionError(#[from] PreExecutionError),
     #[error(transparent)]
     StateError(#[from] StateError),
-    #[error(transparent)]
-    SyscallExecutionError(#[from] SyscallExecutionError),
     /// Gathers all errors from running the Cairo VM, excluding hints.
     #[error(transparent)]
     VirtualMachineExecutionError(#[from] VirtualMachineExecutionError),
