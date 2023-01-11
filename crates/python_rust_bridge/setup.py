768ea47f6894b4c6f6a04ae2acc92ea1ba983b5d
alon.dotan <alon.dotan@starkware.co>
2023-01-11 22:58:36 +0200

    add infra for packing blockifier as python package

---
+++ crates/python_rust_bridge/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name='python_rust_bridge',
+    version='1.0',
+    rust_extensions=[RustExtension("python_rust_bridge.python_rust_bridge", binding=Binding.PyO3)],
+    author="Starkware",
+    author_email="info@starkware.co",
+    description="Rust binding for python",
+)
