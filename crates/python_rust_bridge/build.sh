29162f71ded32215d80d1fb8c7af0be05d8087a6
alon.dotan <alon.dotan@starkware.co>
2023-01-12 23:52:32 +0200

    fix pr comments

--- crates/python_rust_bridge/build.sh
+++ crates/python_rust_bridge/build.sh
@@ -2,4 +2,3 @@
 set -ex
 
 docker run --rm -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 bash /io/build_wheels.sh
-
--- crates/python_rust_bridge/build_wheels.sh
+++ crates/python_rust_bridge/build_wheels.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-set -ex
+set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
+
+cpython_bins=$(echo /opt/python/cp{37,38,39,310}*/bin)
+pypy_bins=$(echo /opt/python/pp{37,38,39}*/bin)
+
 # Compile wheels
-for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
+for py_bin in ${cpython_bins} ${pypy_bins}; do
     rm -rf /io/build/
-    "${PYBIN}/pip" install -U setuptools setuptools-rust wheel
-    "${PYBIN}/pip" wheel /io/ -w /io/dist/ --no-deps
+    "${py_bin}/pip" install -U setuptools setuptools-rust wheel
+    "${py_bin}/pip" wheel /io/ -w /io/dist/ --no-deps
 done
 
 # Bundle external shared libraries into the wheels
@@ -17,6 +21,8 @@ for whl in /io/dist/*{cp37,cp38,cp39,cp310}*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
-    "${PYBIN}/pip" install python-rust-bridge -f /io/dist/
+for py_bin in ${cpython_bins} ${pypy_bins}; do
+    "${py_bin}/pip" install python-rust-bridge -f /io/dist/
+    echo "Testing with $("${py_bin}/python" --version) ..."
+    "${py_bin}/python" -c "import python_rust_bridge; python_rust_bridge.hello_world()"
 done
