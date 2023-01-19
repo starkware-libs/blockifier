f50225a3d03879f6a38f2dc97a23659da449e39e
Noa Oved <noa@starkware.co>
2023-01-19 11:10:23 +0200

    Add Declare and DeployAccount tx to AccountTransaction enum.

--- crates/blockifier/src/transaction/constants.rs
+++ crates/blockifier/src/transaction/constants.rs
@@ -1,5 +1,7 @@
 pub const EXECUTE_ENTRY_POINT_NAME: &str = "__execute__";
 pub const TRANSFER_ENTRY_POINT_NAME: &str = "transfer";
 pub const VALIDATE_ENTRY_POINT_NAME: &str = "__validate__";
+pub const VALIDATE_DECLARE_ENTRY_POINT_NAME: &str = "__validate_declare__";
+pub const VALIDATE_DEPLOY_ENTRY_POINT_NAME: &str = "__validate_deploy__";
 
 pub const TRANSFER_EVENT_NAME: &str = "Transfer";
