3f6b2d4fa7126a4f57a42cb77f935387482561e4
Gilad Chase <gilad@starkware.co>
2023-01-19 19:59:26 +0200

    Tidying up
    - reorder `state_api.rs` and fix annotation.
    - reorder `account_transaction.rs` by hoisting up execution-related
      methods.
    - remove `poc` from rust extention name
    - remove commented out code from rustfmt

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -26,111 +26,6 @@ pub enum AccountTransaction {
 }
 
 impl AccountTransaction {
-    fn get_account_transaction_context(&self) -> AccountTransactionContext {
-        match self {
-            Self::Invoke(tx) => AccountTransactionContext {
-                transaction_hash: tx.transaction_hash,
-                max_fee: tx.max_fee,
-                version: tx.version,
-                signature: tx.signature.clone(),
-                nonce: tx.nonce,
-                sender_address: tx.sender_address,
-            },
-        }
-    }
-
-    fn handle_nonce(
-        account_tx_context: &AccountTransactionContext,
-        state: &mut dyn State,
-    ) -> TransactionExecutionResult<()> {
-        let current_nonce = *state.get_nonce_at(account_tx_context.sender_address)?;
-        if current_nonce != account_tx_context.nonce {
-            return Err(TransactionExecutionError::InvalidNonce {
-                expected_nonce: current_nonce,
-                actual_nonce: account_tx_context.nonce,
-            });
-        }
-
-        // Increment nonce.
-        Ok(state.increment_nonce(account_tx_context.sender_address)?)
-    }
-
-    fn verify_tx_version(version: TransactionVersion) -> TransactionExecutionResult<()> {
-        static ALLOWED_VERSIONS: Lazy<Vec<TransactionVersion>> =
-            Lazy::new(|| vec![TransactionVersion(StarkFelt::from(1))]);
-        if ALLOWED_VERSIONS.contains(&version) {
-            Ok(())
-        } else {
-            Err(TransactionExecutionError::InvalidVersion {
-                version,
-                allowed_versions: &ALLOWED_VERSIONS,
-            })
-        }
-    }
-
-    fn validate_tx(
-        state: &mut dyn State,
-        block_context: &BlockContext,
-        account_tx_context: &AccountTransactionContext,
-        validate_entry_point_selector: EntryPointSelector,
-        validate_entry_point_calldata: Calldata,
-    ) -> TransactionExecutionResult<CallInfo> {
-        let validate_call = CallEntryPoint {
-            entry_point_type: EntryPointType::External,
-            entry_point_selector: validate_entry_point_selector,
-            calldata: validate_entry_point_calldata,
-            class_hash: None,
-            storage_address: account_tx_context.sender_address,
-            caller_address: ContractAddress::default(),
-        };
-
-        Ok(validate_call.execute(state, block_context, account_tx_context)?)
-    }
-
-    fn execute_fee_transfer(
-        state: &mut dyn State,
-        block_context: &BlockContext,
-        account_tx_context: &AccountTransactionContext,
-        actual_fee: Fee,
-    ) -> TransactionExecutionResult<CallInfo> {
-        let max_fee = account_tx_context.max_fee;
-        if actual_fee > max_fee {
-            return Err(FeeTransferError::MaxFeeExceeded { max_fee, actual_fee })?;
-        }
-
-        // The least significant 128 bits of the amount transferred.
-        let lsb_amount = StarkFelt::from(actual_fee.0 as u64);
-        // The most significant 128 bits of the amount transferred.
-        let msb_amount = StarkFelt::from(0);
-
-        let fee_transfer_call = CallEntryPoint {
-            class_hash: None,
-            entry_point_type: EntryPointType::External,
-            entry_point_selector: selector_from_name(TRANSFER_ENTRY_POINT_NAME),
-            calldata: calldata![
-                *block_context.sequencer_address.0.key(), // Recipient.
-                lsb_amount,
-                msb_amount
-            ],
-            storage_address: block_context.fee_token_address,
-            caller_address: account_tx_context.sender_address,
-        };
-
-        Ok(fee_transfer_call.execute(state, block_context, account_tx_context)?)
-    }
-
-    fn charge_fee(
-        state: &mut dyn State,
-        block_context: &BlockContext,
-        account_tx_context: &AccountTransactionContext,
-    ) -> TransactionExecutionResult<(Fee, CallInfo)> {
-        let actual_fee = calculate_tx_fee(block_context);
-        let fee_transfer_call_info =
-            Self::execute_fee_transfer(state, block_context, account_tx_context, actual_fee)?;
-
-        Ok((actual_fee, fee_transfer_call_info))
-    }
-
     pub fn execute(
         self,
         state: &mut dyn State,
@@ -173,5 +68,109 @@ impl AccountTransaction {
             actual_fee,
             actual_resources,
         })
+    }
+    fn get_account_transaction_context(&self) -> AccountTransactionContext {
+        match self {
+            Self::Invoke(tx) => AccountTransactionContext {
+                transaction_hash: tx.transaction_hash,
+                max_fee: tx.max_fee,
+                version: tx.version,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                sender_address: tx.sender_address,
+            },
+        }
+    }
+
+    fn verify_tx_version(version: TransactionVersion) -> TransactionExecutionResult<()> {
+        static ALLOWED_VERSIONS: Lazy<Vec<TransactionVersion>> =
+            Lazy::new(|| vec![TransactionVersion(StarkFelt::from(1))]);
+        if ALLOWED_VERSIONS.contains(&version) {
+            Ok(())
+        } else {
+            Err(TransactionExecutionError::InvalidVersion {
+                version,
+                allowed_versions: &ALLOWED_VERSIONS,
+            })
+        }
+    }
+
+    fn handle_nonce(
+        account_tx_context: &AccountTransactionContext,
+        state: &mut dyn State,
+    ) -> TransactionExecutionResult<()> {
+        let current_nonce = *state.get_nonce_at(account_tx_context.sender_address)?;
+        if current_nonce != account_tx_context.nonce {
+            return Err(TransactionExecutionError::InvalidNonce {
+                expected_nonce: current_nonce,
+                actual_nonce: account_tx_context.nonce,
+            });
+        }
+
+        // Increment nonce.
+        Ok(state.increment_nonce(account_tx_context.sender_address)?)
+    }
+
+    fn validate_tx(
+        state: &mut dyn State,
+        block_context: &BlockContext,
+        account_tx_context: &AccountTransactionContext,
+        validate_entry_point_selector: EntryPointSelector,
+        validate_entry_point_calldata: Calldata,
+    ) -> TransactionExecutionResult<CallInfo> {
+        let validate_call = CallEntryPoint {
+            entry_point_type: EntryPointType::External,
+            entry_point_selector: validate_entry_point_selector,
+            calldata: validate_entry_point_calldata,
+            class_hash: None,
+            storage_address: account_tx_context.sender_address,
+            caller_address: ContractAddress::default(),
+        };
+
+        Ok(validate_call.execute(state, block_context, account_tx_context)?)
+    }
+
+    fn charge_fee(
+        state: &mut dyn State,
+        block_context: &BlockContext,
+        account_tx_context: &AccountTransactionContext,
+    ) -> TransactionExecutionResult<(Fee, CallInfo)> {
+        let actual_fee = calculate_tx_fee(block_context);
+        let fee_transfer_call_info =
+            Self::execute_fee_transfer(state, block_context, account_tx_context, actual_fee)?;
+
+        Ok((actual_fee, fee_transfer_call_info))
+    }
+
+    fn execute_fee_transfer(
+        state: &mut dyn State,
+        block_context: &BlockContext,
+        account_tx_context: &AccountTransactionContext,
+        actual_fee: Fee,
+    ) -> TransactionExecutionResult<CallInfo> {
+        let max_fee = account_tx_context.max_fee;
+        if actual_fee > max_fee {
+            return Err(FeeTransferError::MaxFeeExceeded { max_fee, actual_fee })?;
+        }
+
+        // The least significant 128 bits of the amount transferred.
+        let lsb_amount = StarkFelt::from(actual_fee.0 as u64);
+        // The most significant 128 bits of the amount transferred.
+        let msb_amount = StarkFelt::from(0);
+
+        let fee_transfer_call = CallEntryPoint {
+            class_hash: None,
+            entry_point_type: EntryPointType::External,
+            entry_point_selector: selector_from_name(TRANSFER_ENTRY_POINT_NAME),
+            calldata: calldata![
+                *block_context.sequencer_address.0.key(), // Recipient.
+                lsb_amount,
+                msb_amount
+            ],
+            storage_address: block_context.fee_token_address,
+            caller_address: account_tx_context.sender_address,
+        };
+
+        Ok(fee_transfer_call.execute(state, block_context, account_tx_context)?)
     }
 }
