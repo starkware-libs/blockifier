6afce84e8313c6a7e6b4ea50776bb4ad30f95acc
Adi Yakovian <adi@starkware.co>
2023-01-22 11:58:49 +0200

    Uncomment normalize address hint handling.

--- crates/blockifier/src/execution/common_hints.rs
+++ crates/blockifier/src/execution/common_hints.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
+use std::ops::Shl;
 use std::rc::Rc;
 
-use cairo_felt::Felt;
+use cairo_felt::{Felt, FeltOps, PRIME_STR};
 use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::{
     BuiltinHintProcessor, HintFunc,
 };
@@ -12,8 +13,11 @@ use cairo_vm::hint_processor::hint_processor_definition::HintReference;
 use cairo_vm::serde::deserialize_program::ApTracking;
 use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::vm::errors::hint_errors::HintError;
+use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
 use cairo_vm::vm::vm_core::VirtualMachine;
-use num_traits::{One, Zero};
+use num_bigint::BigInt;
+use num_integer::Integer;
+use num_traits::{Num, One, Zero};
 
 use crate::execution::hint_code::{
     NORMALIZE_ADDRESS_SET_IS_250_HINT, NORMALIZE_ADDRESS_SET_IS_SMALL_HINT,
@@ -30,24 +34,27 @@ pub fn normalize_address_set_is_small(
     constants: &HashMap<String, Felt>,
 ) -> HintExecutionResult {
     const ADDR_BOUND: &str = "starkware.starknet.common.storage.ADDR_BOUND";
-    let addr_bound = constants.get(ADDR_BOUND).ok_or(HintError::MissingConstant("ADDR_BOUND"))?;
+    let addr_bound_felt =
+        constants.get(ADDR_BOUND).ok_or(HintError::MissingConstant("ADDR_BOUND"))?;
     let addr = get_integer_from_var_name("addr", vm, ids_data, ap_tracking)?;
 
-    // let prime = vm.get_prime();
-    // let addr_bound = &addr_bound.mod_floor(prime);
-    // if addr_bound <= &bigint!(1).shl(250)
-    //     || addr_bound > &bigint!(1).shl(251)
-    //     || prime <= &(bigint!(2) * bigint!(1).shl(250))
-    //     || &(2 * addr_bound) <= prime
-    // {
-    //     return Err(HintError::AssertionFailed(format!(
-    //         "assert (2**250 < {} <= 2**251) and (2 * 2**250 < PRIME) and (
-    //         {} * 2 > PRIME); normalize_address() cannot be used with the current constants.",
-    //         addr_bound, addr_bound
-    //     )));
-    // }
+    // Move to big int representation, as prime = 0 in the prime field.
+    let prime = BigInt::from_str_radix(&PRIME_STR[2..], 16)
+        .map_err(|_| VirtualMachineError::CouldntParsePrime(PRIME_STR.to_string()))?;
+    let addr_bound = &addr_bound_felt.to_bigint().mod_floor(&prime);
+    if !(addr_bound > &BigInt::from(1).shl(250)
+        && addr_bound <= &BigInt::from(1).shl(251)
+        && prime > (BigInt::from(2) * BigInt::from(1).shl(250))
+        && (2 * addr_bound) > prime)
+    {
+        return Err(HintError::AssertionFailed(format!(
+            "assert (2**250 < {} <= 2**251) and (2 * 2**250 < PRIME) and (
+            {} * 2 > PRIME); normalize_address() cannot be used with the current constants.",
+            addr_bound, addr_bound
+        )));
+    }
 
-    let is_small = if *addr < *addr_bound { Felt::one() } else { Felt::zero() };
+    let is_small = if *addr < *addr_bound_felt { Felt::one() } else { Felt::zero() };
     insert_value_from_var_name("is_small", is_small, vm, ids_data, ap_tracking)
 }
 
