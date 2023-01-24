120cafd7e1163a9dfa7482d828c8beabc9cc6cb3
giladchase <100755702+giladchase@users.noreply.github.com>
2023-01-24 10:47:22 +0200

    Rename remaining python_rust_bridge references into native_blockifier (#234)

--- crates/native_blockifier/setup.py
+++ crates/native_blockifier/setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="native-blockifier",
+    name="native_blockifier",
     version="1.0",
-    rust_extensions=[RustExtension("native-blockifier.native-blockifier", binding=Binding.PyO3)],
+    rust_extensions=[RustExtension("native_blockifier.native_blockifier", binding=Binding.PyO3)],
     author="Starkware",
     author_email="info@starkware.co",
     description="Rust binding for python",
