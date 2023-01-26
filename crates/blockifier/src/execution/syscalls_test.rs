1de5d6f5870e6a67d3f3b0b609deb24bb6b23ef7
Elin Tulchinsky <elin@starkware.co>
2023-01-26 17:56:12 +0200

    Add VM resources to `CallExecution`.

--- crates/blockifier/src/execution/syscalls_test.rs
+++ crates/blockifier/src/execution/syscalls_test.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use cairo_vm::vm::runners::builtin_runner as cairo_vm_builtin_runner;
+use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use pretty_assertions::assert_eq;
 use starknet_api::core::{calculate_contract_address, ClassHash, ContractAddress, PatriciaKey};
 use starknet_api::hash::{StarkFelt, StarkHash};
@@ -67,7 +71,7 @@ fn test_nested_library_call() {
     let (key, value) = (255, 44);
     let outer_entry_point_selector = selector_from_name("test_library_call");
     let inner_entry_point_selector = selector_from_name("test_storage_read_write");
-    let calldata = calldata![
+    let main_entry_point_calldata = calldata![
         stark_felt!(TEST_CLASS_HASH), // Class hash.
         outer_entry_point_selector.0, // Library call function selector.
         inner_entry_point_selector.0, // Storage function selector.
@@ -79,7 +83,7 @@ fn test_nested_library_call() {
     // Create expected call info tree.
     let main_entry_point = CallEntryPoint {
         entry_point_selector: selector_from_name("test_nested_library_call"),
-        calldata,
+        calldata: main_entry_point_calldata,
         class_hash: Some(ClassHash(stark_felt!(TEST_CLASS_HASH))),
         ..trivial_external_entry_point()
     };
@@ -105,24 +109,47 @@ fn test_nested_library_call() {
         calldata: calldata![stark_felt!(key), stark_felt!(value)],
         ..nested_storage_entry_point.clone()
     };
+    let storage_entry_point_vm_resources = ExecutionResources {
+        n_steps: 41,
+        n_memory_holes: 1,
+        builtin_instance_counter: HashMap::default(),
+    };
     let nested_storage_call_info = CallInfo {
         call: nested_storage_entry_point,
         execution: CallExecution::from_retdata(retdata![stark_felt!(value + 1)]),
+        vm_resources: storage_entry_point_vm_resources.clone(),
         ..Default::default()
     };
     let library_call_info = CallInfo {
         call: library_entry_point,
         execution: CallExecution::from_retdata(retdata![stark_felt!(value + 1)]),
+        vm_resources: ExecutionResources {
+            n_steps: 0,
+            n_memory_holes: 1,
+            builtin_instance_counter: HashMap::from([(
+                cairo_vm_builtin_runner::RANGE_CHECK_BUILTIN_NAME.to_string(),
+                1,
+            )]),
+        },
         inner_calls: vec![nested_storage_call_info],
     };
     let storage_call_info = CallInfo {
         call: storage_entry_point,
         execution: CallExecution::from_retdata(retdata![stark_felt!(value)]),
+        vm_resources: storage_entry_point_vm_resources,
         ..Default::default()
     };
     let expected_call_info = CallInfo {
         call: main_entry_point.clone(),
         execution: CallExecution::from_retdata(retdata![stark_felt!(0)]),
+        vm_resources: ExecutionResources {
+            n_steps: 0,
+            n_memory_holes: 1,
+            builtin_instance_counter: HashMap::from([(
+                cairo_vm_builtin_runner::RANGE_CHECK_BUILTIN_NAME.to_string(),
+                1,
+            )]),
+        },
         inner_calls: vec![library_call_info, storage_call_info],
     };
 
