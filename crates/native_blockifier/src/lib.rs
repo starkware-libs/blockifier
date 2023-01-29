c1222bd49bf2ad5e250201d205b534bcd3dccf39
giladchase <100755702+giladchase@users.noreply.github.com>
2023-01-29 16:48:18 +0200

    Add return-value test for native_blockifier (#252)

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -5,9 +5,15 @@ fn hello_world() {
     println!("Hello from rust.");
 }
 
+#[pyfunction]
+fn test_ret_value(x: i32, y: i32) -> i32 {
+    x + y
+}
+
 #[pymodule]
 fn native_blockifier(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_world, m)?)?;
+    m.add_function(wrap_pyfunction!(test_ret_value, m)?)?;
 
     Ok(())
 }
