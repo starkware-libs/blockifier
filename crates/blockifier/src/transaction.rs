1acfc01c6bc2750e490d759d7c29a2a57a49d1f3
Alon Haramati <alonh@starkware.co>
2023-01-30 17:33:58 +0200

    Execute general transactions.

--- crates/blockifier/src/transaction.rs
+++ crates/blockifier/src/transaction.rs
@@ -2,5 +2,6 @@ pub mod account_transaction;
 pub mod constants;
 pub mod errors;
 pub mod objects;
+pub mod transaction_execution;
 pub mod transaction_utils;
 pub mod transactions;
---
+++ crates/blockifier/src/transaction/transaction_execution.rs
@@ -0,0 +1,44 @@
+use starknet_api::transaction::{Fee, L1HandlerTransaction, TransactionSignature};
+
+use crate::block_context::BlockContext;
+use crate::state::state_api::State;
+use crate::transaction::account_transaction::AccountTransaction;
+use crate::transaction::objects::{
+    AccountTransactionContext, ResourcesMapping, TransactionExecutionInfo,
+    TransactionExecutionResult,
+};
+use crate::transaction::transactions::Executable;
+
+pub enum Transaction {
+    AccountTransaction(AccountTransaction),
+    L1HandlerTransaction(L1HandlerTransaction),
+}
+
+impl Transaction {
+    pub fn execute(
+        self,
+        state: &mut dyn State,
+        block_context: &BlockContext,
+    ) -> TransactionExecutionResult<TransactionExecutionInfo> {
+        match self {
+            Self::AccountTransaction(account_tx) => account_tx.execute(state, block_context),
+            Self::L1HandlerTransaction(tx) => {
+                let tx_context = AccountTransactionContext {
+                    transaction_hash: tx.transaction_hash,
+                    max_fee: Fee::default(),
+                    version: tx.version,
+                    signature: TransactionSignature::default(),
+                    nonce: tx.nonce,
+                    sender_address: tx.contract_address,
+                };
+                Ok(TransactionExecutionInfo {
+                    validate_call_info: None,
+                    execute_call_info: tx.execute(state, block_context, &tx_context)?,
+                    fee_transfer_call_info: None,
+                    actual_fee: Fee::default(),
+                    actual_resources: ResourcesMapping::default(),
+                })
+            }
+        }
+    }
+}
