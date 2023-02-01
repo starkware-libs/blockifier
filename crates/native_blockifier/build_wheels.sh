195c2e15389305e91a0d088420c1f8321e7bb1ea
Gilad Chase <gilad@starkware.co>
2023-02-01 17:05:52 +0200

    Fix build script for native extension
    - Added Blockifier as a dependency.
    - Added clang > 3.9 to the manylinux docker that builds the extension.
      One of the transitive dependencies of `blockifier` needs this.

--- crates/native_blockifier/build_wheels.sh
+++ crates/native_blockifier/build_wheels.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 set -e
 
-# The starknet-api dependency requires the dependency `rust-openssl`, which needs openssl-devel
-# to be installed locally.
-yum -y install openssl-devel
+# Install crate dependencies
+yum -y install centos-release-scl
+yum -y install openssl-devel llvm-toolset-7.0
 
+
+# Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 export PATH="$HOME/.cargo/bin:$PATH"
 
-
-cpython_bins=$(echo /opt/python/cp{37,38,39,310}*/bin)
-pypy_bins=$(echo /opt/python/pp{37,38,39}*/bin)
-
-# Compile wheels
-for py_bin in ${cpython_bins} ${pypy_bins}; do
-    rm -rf /io/build/
-    "${py_bin}/pip" install -U setuptools setuptools-rust wheel
-    "${py_bin}/pip" wheel /io/ -w /io/dist/ --no-deps
-done
-
-# Bundle external shared libraries into the wheels
-for whl in /io/dist/*{cp37,cp38,cp39,cp310,pp37,pp38,pp39}*.whl; do
-    auditwheel repair "$whl" -w /io/dist/
-done
-
-# Install packages and test
-for py_bin in ${cpython_bins} ${pypy_bins}; do
-    "${py_bin}/pip" install native_blockifier -f /io/dist/
-    echo "Testing with $("${py_bin}/python" --version) ..."
-    "${py_bin}/python" -c "import native_blockifier; native_blockifier.hello_world()"
-done
+# Required for libclang > 3.9, by default there is only clang 3.4 in this image.
+scl enable llvm-toolset-7.0 "bash /io/run_in_chroot.sh"
