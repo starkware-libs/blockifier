b901589471b69734bb22778a41581497a1dc54bd
Gilad Chase <gilad@starkware.co>
2023-02-02 15:38:47 +0200

    Initial storage implementation for native blockifier
    - Wrap papyrus_storage with a dedicated class to hide the
      implementation.
    - Implement initialization API.
    - Add error handling.

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -8,23 +8,41 @@ use starknet_api::StarknetApiError;
 
 pub type NativeBlockifierResult<T> = Result<T, NativeBlockifierError>;
 
-#[pyfunction]
-fn hello_world() {
-    println!("Hello from rust.");
-}
-
-#[pyfunction]
-fn test_ret_value(x: i32, y: i32) -> i32 {
-    x + y
-}
-
 #[pymodule]
-fn native_blockifier(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_world, m)?)?;
-    m.add_function(wrap_pyfunction!(test_ret_value, m)?)?;
-    m.add_class::<PyTransactionExecutor>()?;
+fn native_blockifier(_py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
+    py_module.add_class::<Storage>()?;
+    py_module.add_class::<PyTransactionExecutor>()?;
+
+    py_module.add_function(wrap_pyfunction!(test_storage, py_module)?)?;
 
     Ok(())
+}
+
+#[pyclass]
+pub struct Storage {
+    pub reader: papyrus_storage::StorageReader,
+    pub writer: papyrus_storage::StorageWriter,
+}
+
+// TODO: Add rest of the storage api.
+#[pymethods]
+impl Storage {
+    #[new]
+    pub fn new(path: String) -> NativeBlockifierResult<Storage> {
+        let db_config = papyrus_storage::db::DbConfig {
+            path,
+            max_size: 1 << 35, // 32GB.
+        };
+
+        let (reader, writer) = papyrus_storage::open_storage(db_config)?;
+        Ok(Storage { reader, writer })
+    }
+}
+
+#[pyfunction]
+fn test_storage() -> Storage {
+    let (reader, writer) = papyrus_storage::test_utils::get_test_storage();
+    Storage { reader, writer }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -35,6 +53,8 @@ pub enum NativeBlockifierError {
     StarknetApiError(#[from] StarknetApiError),
     #[error(transparent)]
     TransactionExecutionError(#[from] TransactionExecutionError),
+    #[error(transparent)]
+    StorageError(#[from] papyrus_storage::StorageError),
 }
 
 impl From<NativeBlockifierError> for PyErr {
