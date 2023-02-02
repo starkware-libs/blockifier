8f612aa022e0c994147e8bac0f35a8512739c190
Gilad Chase <gilad@starkware.co>
2023-02-02 15:38:47 +0200

    Initial storage implementation for native blockifier
    - Wrap papyrus_storage with a dedicated class to hide the
      implementation.
    - Implement initialization API.
    - Add error handling.

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -1,19 +1,55 @@
+use pyo3::exceptions::PyOSError;
 use pyo3::prelude::*;
+use starknet_api::StarknetApiError;
 
-#[pyfunction]
-fn hello_world() {
-    println!("Hello from rust.");
-}
-
-#[pyfunction]
-fn test_ret_value(x: i32, y: i32) -> i32 {
-    x + y
-}
+pub type NativeBlockifierResult<T> = Result<T, NativeBlockifierError>;
 
 #[pymodule]
-fn native_blockifier(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_world, m)?)?;
-    m.add_function(wrap_pyfunction!(test_ret_value, m)?)?;
+fn native_blockifier(_py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
+    py_module.add_class::<Storage>()?;
+
+    py_module.add_function(wrap_pyfunction!(test_storage, py_module)?)?;
 
     Ok(())
+}
+
+#[pyclass]
+pub struct Storage {
+    pub reader: papyrus_storage::StorageReader,
+    pub writer: papyrus_storage::StorageWriter,
+}
+
+// TODO: Add rest of the storage api.
+#[pymethods]
+impl Storage {
+    #[new]
+    pub fn new(path: String) -> NativeBlockifierResult<Storage> {
+        let db_config = papyrus_storage::db::DbConfig {
+            path,
+            max_size: 1 << 35, // 32GB.
+        };
+
+        let (reader, writer) = papyrus_storage::open_storage(db_config)?;
+        Ok(Storage { reader, writer })
+    }
+}
+
+#[pyfunction]
+fn test_storage() -> Storage {
+    let (reader, writer) = papyrus_storage::test_utils::get_test_storage();
+    Storage { reader, writer }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum NativeBlockifierError {
+    #[error(transparent)]
+    StarknetApiError(#[from] StarknetApiError),
+    #[error(transparent)]
+    StorageError(#[from] papyrus_storage::StorageError),
+}
+
+impl From<NativeBlockifierError> for PyErr {
+    fn from(error: NativeBlockifierError) -> PyErr {
+        PyOSError::new_err(error.to_string())
+    }
 }
