f912f6f9dc418ff198f11d8fb22155669244574d
Alon Haramati <alonh@starkware.co>
2023-02-07 14:02:49 +0200

    Get txs from python.

--- crates/native_blockifier/src/py_transaction.rs
+++ crates/native_blockifier/src/py_transaction.rs
@@ -5,13 +5,26 @@ use blockifier::transaction::account_transaction::AccountTransaction;
 use blockifier::transaction::transaction_execution::Transaction;
 use num_bigint::BigUint;
 use pyo3::prelude::*;
-use starknet_api::core::{ContractAddress, EntryPointSelector, Nonce, PatriciaKey};
+use starknet_api::core::{ClassHash, ContractAddress, EntryPointSelector, Nonce};
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{
-    Calldata, Fee, InvokeTransaction, TransactionHash, TransactionSignature, TransactionVersion,
+    Calldata, ContractAddressSalt, DeclareTransaction, DeployAccountTransaction, Fee,
+    InvokeTransaction, L1HandlerTransaction, TransactionHash, TransactionSignature,
+    TransactionVersion,
 };
 
 use crate::NativeBlockifierResult;
+
+/// Common fields used in all account transactions.
+#[derive(Debug)]
+pub struct AccountDataContext {
+    pub transaction_hash: TransactionHash,
+    pub max_fee: Fee,
+    pub version: TransactionVersion,
+    pub signature: TransactionSignature,
+    pub nonce: Nonce,
+    pub sender_address: ContractAddress,
+}
 
 fn biguint_to_felt(biguint: BigUint) -> NativeBlockifierResult<StarkFelt> {
     let biguint_hex = format!("{biguint:#x}");
@@ -28,6 +41,50 @@ fn py_felt_sequence_attr(obj: &PyAny, attr: &str) -> NativeBlockifierResult<Vec<
     raw_felts.into_iter().map(biguint_to_felt).collect()
 }
 
+pub fn account_data_context_from_python(tx: &PyAny) -> NativeBlockifierResult<AccountDataContext> {
+    Ok(AccountDataContext {
+        transaction_hash: TransactionHash(py_felt_attr(tx, "hash_value")?),
+        max_fee: Fee(tx.getattr("max_fee")?.extract()?),
+        version: TransactionVersion(py_felt_attr(tx, "version")?),
+        signature: TransactionSignature(py_felt_sequence_attr(tx, "signature")?),
+        nonce: Nonce(py_felt_attr(tx, "nonce")?),
+        sender_address: ContractAddress::try_from(py_felt_attr(tx, "sender_address")?)?,
+    })
+}
+
+pub fn declare_from_python(tx: &PyAny) -> NativeBlockifierResult<DeclareTransaction> {
+    let account_data_context = account_data_context_from_python(tx)?;
+
+    Ok(DeclareTransaction {
+        transaction_hash: account_data_context.transaction_hash,
+        max_fee: account_data_context.max_fee,
+        version: account_data_context.version,
+        signature: account_data_context.signature,
+        nonce: account_data_context.nonce,
+        class_hash: ClassHash(py_felt_attr(tx, "class_hash")?),
+        sender_address: account_data_context.sender_address,
+    })
+}
+
+pub fn deploy_account_from_python(tx: &PyAny) -> NativeBlockifierResult<DeployAccountTransaction> {
+    let account_data_context = account_data_context_from_python(tx)?;
+
+    Ok(DeployAccountTransaction {
+        transaction_hash: account_data_context.transaction_hash,
+        max_fee: account_data_context.max_fee,
+        version: account_data_context.version,
+        signature: account_data_context.signature,
+        nonce: account_data_context.nonce,
+        class_hash: ClassHash(py_felt_attr(tx, "class_hash")?),
+        contract_address: account_data_context.sender_address,
+        contract_address_salt: ContractAddressSalt(py_felt_attr(tx, "contract_address_salt")?),
+        constructor_calldata: Calldata(Arc::from(py_felt_sequence_attr(
+            tx,
+            "constructor_calldata",
+        )?)),
+    })
+}
+
 pub fn invoke_function_from_python(tx: &PyAny) -> NativeBlockifierResult<InvokeTransaction> {
     let entry_point_selector: Option<&PyAny> = tx.getattr("entry_point_selector")?.extract()?;
     let entry_point_selector = if let Some(selector) = entry_point_selector {
@@ -35,26 +92,49 @@ pub fn invoke_function_from_python(tx: &PyAny) -> NativeBlockifierResult<InvokeT
     } else {
         None
     };
+    let account_data_context = account_data_context_from_python(tx)?;
 
     Ok(InvokeTransaction {
-        transaction_hash: TransactionHash(py_felt_attr(tx, "hash_value")?),
-        max_fee: Fee(tx.getattr("max_fee")?.extract()?),
-        version: TransactionVersion(py_felt_attr(tx, "version")?),
-        signature: TransactionSignature(py_felt_sequence_attr(tx, "signature")?),
-        nonce: Nonce(py_felt_attr(tx, "nonce")?),
-        sender_address: ContractAddress(
-            PatriciaKey::try_from(py_felt_attr(tx, "sender_address")?).unwrap(),
-        ),
+        transaction_hash: account_data_context.transaction_hash,
+        max_fee: account_data_context.max_fee,
+        version: account_data_context.version,
+        signature: account_data_context.signature,
+        nonce: account_data_context.nonce,
+        sender_address: account_data_context.sender_address,
         entry_point_selector,
+        calldata: Calldata(Arc::from(py_felt_sequence_attr(tx, "calldata")?)),
+    })
+}
+
+pub fn l1_handler_from_python(tx: &PyAny) -> NativeBlockifierResult<L1HandlerTransaction> {
+    Ok(L1HandlerTransaction {
+        transaction_hash: TransactionHash(py_felt_attr(tx, "hash_value")?),
+        version: TransactionVersion(py_felt_attr(tx, "version")?),
+        nonce: Nonce(py_felt_attr(tx, "nonce")?),
+        contract_address: ContractAddress::try_from(py_felt_attr(tx, "contract_address")?)?,
+        entry_point_selector: EntryPointSelector(py_felt_attr(tx, "entry_point_selector")?),
         calldata: Calldata(Arc::from(py_felt_sequence_attr(tx, "calldata")?)),
     })
 }
 
 pub fn tx_from_python(tx: &PyAny, tx_type: &str) -> NativeBlockifierResult<Transaction> {
     match tx_type {
+        "DECLARE" => {
+            let declare_tx = AccountTransaction::Declare(declare_from_python(tx)?);
+            Ok(Transaction::AccountTransaction(declare_tx))
+        }
+        "DEPLOY_ACCOUNT" => {
+            let deploy_account_tx =
+                AccountTransaction::DeployAccount(deploy_account_from_python(tx)?);
+            Ok(Transaction::AccountTransaction(deploy_account_tx))
+        }
         "INVOKE_FUNCTION" => {
             let invoke_tx = AccountTransaction::Invoke(invoke_function_from_python(tx)?);
             Ok(Transaction::AccountTransaction(invoke_tx))
+        }
+        "L1_HANDLER" => {
+            let l1_handler_tx = l1_handler_from_python(tx)?;
+            Ok(Transaction::L1HandlerTransaction(l1_handler_tx))
         }
         _ => unimplemented!(),
     }
