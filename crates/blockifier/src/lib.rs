f34a1d9f06b8d06b51c2487cb950732d761158da
Alon Haramati <alonh@starkware.co>
2023-02-08 14:02:35 +0200

    Create testing feature.

--- crates/blockifier/src/lib.rs
+++ crates/blockifier/src/lib.rs
@@ -2,7 +2,7 @@ pub mod abi;
 pub mod block_context;
 pub mod execution;
 pub mod state;
-#[cfg(test)]
+#[cfg(any(feature = "testing", test))]
 pub mod test_utils;
 pub mod transaction;
 pub mod utils;
