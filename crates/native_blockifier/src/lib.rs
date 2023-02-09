7a4cc06d0728c24bc23b0f3e7555edfde93e2534
Alon Haramati <91828241+alonh5@users.noreply.github.com>
2023-02-09 12:00:26 +0200

    Get invoke tx in from python. (#261)

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -1,4 +1,18 @@
+mod py_transaction;
+
+use py_transaction::tx_from_python;
+use pyo3::exceptions::PyOSError;
 use pyo3::prelude::*;
+use starknet_api::StarknetApiError;
+
+pub type NativeBlockifierResult<T> = Result<T, NativeBlockifierError>;
+
+#[pyfunction]
+fn execute_tx(tx: &PyAny) -> PyResult<()> {
+    let tx_type: &str = tx.getattr("tx_type")?.getattr("name")?.extract()?;
+    let _tx = tx_from_python(tx, tx_type)?;
+    Ok(())
+}
 
 #[pyfunction]
 fn hello_world() {
@@ -14,6 +28,24 @@ fn test_ret_value(x: i32, y: i32) -> i32 {
 fn native_blockifier(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_world, m)?)?;
     m.add_function(wrap_pyfunction!(test_ret_value, m)?)?;
+    m.add_function(wrap_pyfunction!(execute_tx, m)?)?;
 
     Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum NativeBlockifierError {
+    #[error(transparent)]
+    Pyo3Error(#[from] PyErr),
+    #[error(transparent)]
+    StarknetApiError(#[from] StarknetApiError),
+}
+
+impl From<NativeBlockifierError> for PyErr {
+    fn from(error: NativeBlockifierError) -> PyErr {
+        match error {
+            NativeBlockifierError::Pyo3Error(py_error) => py_error,
+            _ => PyOSError::new_err(error.to_string()),
+        }
+    }
 }
