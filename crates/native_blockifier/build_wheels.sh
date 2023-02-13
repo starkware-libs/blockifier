7934230fbfd66bdd4ba5c3b525e7222b52e61893
giladchase <100755702+giladchase@users.noreply.github.com>
2023-02-13 10:53:02 +0200

    Initial storage implementation for native blockifier (#262)

--- crates/native_blockifier/build_wheels.sh
+++ crates/native_blockifier/build_wheels.sh
@@ -33,5 +33,5 @@ done
 for py_bin in ${cpython_bins} ${pypy_bins}; do
     "${py_bin}/pip" install native_blockifier -f /io/dist/
     echo "Testing with $("${py_bin}/python" --version) ..."
-    "${py_bin}/python" -c "import native_blockifier; native_blockifier.hello_world()"
+    "${py_bin}/python" -c "import native_blockifier; print('native_blockifier import success')"
 done
