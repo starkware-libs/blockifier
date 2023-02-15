d77cc391de84493a8607e506c26883eb4711d01c
Gilad Chase <gilad@starkware.co>
2023-02-15 09:39:34 +0200

    Allow returning a PyStateDiff to python

--- crates/native_blockifier/src/py_utils.rs
+++ crates/native_blockifier/src/py_utils.rs
@@ -3,12 +3,25 @@ use std::convert::TryFrom;
 use num_bigint::BigUint;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use starknet_api::core::ContractAddress;
 use starknet_api::hash::StarkFelt;
 
 use crate::NativeBlockifierResult;
 
-#[derive(Eq, FromPyObject, Hash, PartialEq)]
+#[derive(Eq, FromPyObject, Hash, PartialEq, Clone, Copy)]
 pub struct PyFelt(#[pyo3(from_py_with = "pyint_to_stark_felt")] pub StarkFelt);
+
+impl IntoPy<PyObject> for PyFelt {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        BigUint::from_bytes_be(self.0.bytes()).into_py(py)
+    }
+}
+
+impl From<ContractAddress> for PyFelt {
+    fn from(address: ContractAddress) -> Self {
+        Self(*address.0.key())
+    }
+}
 
 fn pyint_to_stark_felt(int: &PyAny) -> PyResult<StarkFelt> {
     let biguint: BigUint = int.extract()?;
