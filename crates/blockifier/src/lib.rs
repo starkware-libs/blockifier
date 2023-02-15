4696471d3d77bd31234d8666634a2d18981fd2b2
Gilad Chase <gilad@starkware.co>
2023-02-15 15:00:19 +0200

    Remove testing feature from test_utils
    Temporary change.
    Currently native_extension needs `DictStateReader`, but it should
    require `State`. Until that's fixed, we remove the testing feature
    from test_utils, because we don't want to compile the extension
    in release mode, but with a `Testing` feature.

--- crates/blockifier/src/lib.rs
+++ crates/blockifier/src/lib.rs
@@ -2,7 +2,8 @@ pub mod abi;
 pub mod block_context;
 pub mod execution;
 pub mod state;
-#[cfg(any(feature = "testing", test))]
+// TODO: uncomment once we fix native_extension; making it no longer require DictStateReader.
+// #[cfg(any(feature = "testing", test))]
 pub mod test_utils;
 pub mod transaction;
 pub mod utils;
