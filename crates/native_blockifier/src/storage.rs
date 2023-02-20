ed62af63829b2e7c56da60a7fe19500da7967f35
Gilad Chase <gilad@starkware.co>
2023-02-20 10:31:52 +0200

    Add docstring to `get_state_marker`

--- crates/native_blockifier/src/storage.rs
+++ crates/native_blockifier/src/storage.rs
@@ -33,6 +33,7 @@ impl Storage {
         Ok(Storage { reader, writer })
     }
 
+    /// Returns the the block number _after_ the last created block's number.
     pub fn get_state_marker(&self) -> NativeBlockifierResult<u64> {
         let block_number = self.reader.begin_ro_txn()?.get_state_marker()?;
         Ok(block_number.0)
