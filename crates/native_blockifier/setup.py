618411205ac293e83215ce3db5155a285780de25
Gilad Chase <gilad@starkware.co>
2023-02-20 15:24:26 +0200

    Deploy native blockifier on tag
    Upload native_blockifier with the version being the git tag.
    
    TODO: add s3pypi support in the CI

--- crates/native_blockifier/setup.py
+++ crates/native_blockifier/setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 from setuptools_rust import Binding, RustExtension
 
+import os
+
 setup(
     name="native_blockifier",
-    version="1.0",
+    version=os.environ.get("GIT_REF", "0.1.0"),
     rust_extensions=[RustExtension("native_blockifier.native_blockifier", binding=Binding.PyO3)],
     author="Starkware",
     author_email="info@starkware.co",
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -ex
 
+function deploy_to_pypi_proxy() {
+    for whl in crates/native_blockifier/dist/*many*.whl
+        do s3pypi -b pypi.starkex.co --unsafe-s3-website --put-root-index $whl
+    done
+}
+
 root_dir="$(cd `dirname $0` && pwd -P)/../"
 
-docker run --rm -v $root_dir:/io quay.io/pypa/manylinux2014_x86_64 bash /io/scripts/build_wheels.sh
+docker run -e GIT_REF=$2 --rm -v $root_dir:/io quay.io/pypa/manylinux2014_x86_64 bash /io/scripts/build_wheels.sh
+
+deploy_to_pypi_proxy()
