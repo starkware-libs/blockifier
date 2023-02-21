e688264b4bcc2a09a1b2de510bbaefcef170ed86
Noa Oved <noa@starkware.co>
2023-02-21 13:42:06 +0200

    Add fields to  so it will be identical to the pythonic class.

--- crates/native_blockifier/src/py_transaction_execution_info.rs
+++ crates/native_blockifier/src/py_transaction_execution_info.rs
@@ -1,8 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use blockifier::execution::entry_point::CallInfo;
 use blockifier::transaction::objects::TransactionExecutionInfo;
 use pyo3::prelude::*;
+use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{EventContent, MessageToL1};
 
 use crate::py_utils::{starkfelt_to_pyfelt_vec, PyFelt};
@@ -37,14 +38,22 @@ impl From<TransactionExecutionInfo> for PyTransactionExecutionInfo {
 #[derive(Clone)]
 pub struct PyCallInfo {
     pub caller_address: PyFelt,
+    pub call_type: usize,
     pub contract_address: PyFelt,
     pub class_hash: Option<PyFelt>,
     pub entry_point_selector: PyFelt,
     pub entry_point_type: usize,
     pub calldata: Vec<PyFelt>,
+    pub gas_consumed: PyFelt,
+    pub failure_flag: PyFelt,
     pub retdata: Vec<PyFelt>,
+    pub execution_resources: PyExecutionResources,
     pub events: Vec<PyEvent>,
     pub l2_to_l1_messages: Vec<PyL2ToL1Message>,
+    pub storage_read_values: Vec<PyFelt>,
+    pub accessed_storage_keys: HashSet<PyFelt>,
+    pub internal_calls: Vec<PyCallInfo>,
+    pub code_address: Option<PyFelt>,
 }
 
 impl From<CallInfo> for PyCallInfo {
@@ -54,18 +63,27 @@ impl From<CallInfo> for PyCallInfo {
 
         Self {
             caller_address: PyFelt::from(call.caller_address),
+            call_type: 0, // CALL
             contract_address: PyFelt::from(call.storage_address),
             class_hash: call.class_hash.map(|class_hash| PyFelt(class_hash.0)),
             entry_point_selector: PyFelt(call.entry_point_selector.0),
             entry_point_type: call.entry_point_type as usize,
             calldata: starkfelt_to_pyfelt_vec(call.calldata.0.to_vec()),
+            gas_consumed: PyFelt(StarkFelt::default()),
+            failure_flag: PyFelt(StarkFelt::default()),
             retdata: starkfelt_to_pyfelt_vec(execution.retdata.0),
+            // TODO(Elin, 01/03/2023): Initialize correctly.
+            execution_resources: PyExecutionResources::default(),
             events: execution.events.into_iter().map(PyEvent::from).collect(),
             l2_to_l1_messages: execution
                 .l2_to_l1_messages
                 .into_iter()
                 .map(PyL2ToL1Message::from)
                 .collect(),
+            storage_read_values: vec![],
+            accessed_storage_keys: HashSet::new(),
+            internal_calls: call_info.inner_calls.into_iter().map(PyCallInfo::from).collect(),
+            code_address: None,
         }
     }
 }
@@ -97,4 +115,12 @@ impl From<MessageToL1> for PyL2ToL1Message {
         let payload = starkfelt_to_pyfelt_vec(message.payload.0);
         Self { to_address: PyFelt::from(message.to_address), payload }
     }
+}
+
+#[pyclass]
+#[derive(Clone, Default)]
+pub struct PyExecutionResources {
+    pub n_steps: usize,
+    pub builtin_instance_counter: HashMap<String, usize>,
+    pub n_memory_holes: usize,
 }
