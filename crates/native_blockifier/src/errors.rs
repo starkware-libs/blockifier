4bff8b7b4be83b9a978e0f52488c27ebb4e42850
Dori Medini <dori@starkware.co>
2023-03-01 17:22:33 +0200

    Implement and expose python exception types
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/native_blockifier/src/errors.rs
+++ crates/native_blockifier/src/errors.rs
@@ -1,29 +1,63 @@
+use std::backtrace::{Backtrace, BacktraceStatus};
+
 use blockifier::transaction::errors::TransactionExecutionError;
-use pyo3::exceptions::PyOSError;
+use pyo3::create_exception;
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use starknet_api::StarknetApiError;
 
 pub type NativeBlockifierResult<T> = Result<T, NativeBlockifierError>;
 
-#[derive(thiserror::Error, Debug)]
-pub enum NativeBlockifierError {
-    #[error(transparent)]
-    Pyo3Error(#[from] PyErr),
-    #[error(transparent)]
-    SerdeError(#[from] serde_json::Error),
-    #[error(transparent)]
-    StarknetApiError(#[from] StarknetApiError),
-    #[error(transparent)]
-    TransactionExecutionError(#[from] TransactionExecutionError),
-    #[error(transparent)]
-    StorageError(#[from] papyrus_storage::StorageError),
+/// Macro to create NativeBlockifierError variants, their respective Python types, and implements
+/// `From<NativeBlockifierError> for PyErr`.
+macro_rules! native_blockifier_errors {
+    ($(($name:ident, $from_type:ty, $py_error_name:ident)),*) => {
+        #[derive(thiserror::Error, Debug)]
+        pub enum NativeBlockifierError {
+            $(
+                #[error("Source error: {source:?}\nBacktrace: {backtrace:?}.")]
+                $name {
+                    #[from]
+                    source: $from_type,
+                    backtrace: Backtrace,
+                }
+            ),*
+        }
+
+        // Creates new types that implement `Into<PyException>`.
+        $(create_exception!(native_blockifier, $py_error_name, PyException);)*
+
+        // Call to register all python exceptions in the native_blockifier module.
+        pub fn add_py_exceptions(py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
+            $(py_module.add(stringify!($py_error_name), py.get_type::<$py_error_name>())?;)*
+            Ok(())
+        }
+
+        impl From<NativeBlockifierError> for PyErr {
+            fn from(error: NativeBlockifierError) -> PyErr {
+                match error {
+                    $(NativeBlockifierError::$name { source, backtrace } => $py_error_name::new_err(
+                        // Constructs with the tuple `(error_code, error_message, backtrace)`, where
+                        // `backtrace` is `Option<String>`.
+                        (
+                            String::from("native_blockifier.") + stringify!($py_error_name),
+                            format!("{:?}", source),
+                            match backtrace.status() {
+                                BacktraceStatus::Captured => Some(format!("{:?}", backtrace)),
+                                _ => None,
+                            }
+                        )
+                    )),*
+                }
+            }
+        }
+    };
 }
 
-impl From<NativeBlockifierError> for PyErr {
-    fn from(error: NativeBlockifierError) -> PyErr {
-        match error {
-            NativeBlockifierError::Pyo3Error(py_error) => py_error,
-            _ => PyOSError::new_err(error.to_string()),
-        }
-    }
-}
+native_blockifier_errors!(
+    (Pyo3Error, PyErr, PyPyo3Error),
+    (SerdeError, serde_json::Error, PySerdeError),
+    (StarknetApiError, StarknetApiError, PyStarknetApiError),
+    (TransactionExecutionError, TransactionExecutionError, PyTransactionExecutionError),
+    (StorageError, papyrus_storage::StorageError, PyStorageError)
+);
--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -1,3 +1,10 @@
+// The `error_generic_member_access` feature is required to solve this issue:
+// https://github.com/rust-lang/rust/issues/99301.
+#![feature(error_generic_member_access)]
+// The `provide_any` feature is required to solve this issue:
+// https://github.com/rust-lang/rust/issues/96024.
+#![feature(provide_any)]
+
 pub mod errors;
 pub mod py_state_diff;
 pub mod py_test_utils;
@@ -6,6 +13,7 @@ pub mod py_transaction_execution_info;
 pub mod py_utils;
 pub mod storage;
 
+use errors::add_py_exceptions;
 use py_transaction::PyTransactionExecutor;
 use py_transaction_execution_info::{
     PyCallInfo, PyExecutionResources, PyOrderedEvent, PyOrderedL2ToL1Message,
@@ -26,6 +34,7 @@ fn native_blockifier(_py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
     py_module.add_class::<PyTransactionExecutionInfo>()?;
     py_module.add_class::<PyTransactionExecutor>()?;
     py_module.add_class::<Storage>()?;
+    add_py_exceptions(_py, py_module)?;
 
     Ok(())
 }
