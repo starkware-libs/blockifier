074255a30d7369cfabf79336a0dd2c6ad616b9ef
dorimedini-starkware <76901494+dorimedini-starkware@users.noreply.github.com>
2023-03-05 20:05:21 +0200

    Dori/py errors (#322)
    * Define Python error types
    
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    * Test
    
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    * Update version
    
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -6,6 +6,7 @@ pub mod py_transaction_execution_info;
 pub mod py_utils;
 pub mod storage;
 
+use errors::add_py_exceptions;
 use py_transaction::PyTransactionExecutor;
 use py_transaction_execution_info::{
     PyCallInfo, PyExecutionResources, PyOrderedEvent, PyOrderedL2ToL1Message,
@@ -15,9 +16,10 @@ use pyo3::prelude::*;
 use storage::Storage;
 
 use crate::py_state_diff::PyStateDiff;
+use crate::py_utils::raise_error_for_testing;
 
 #[pymodule]
-fn native_blockifier(_py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
+fn native_blockifier(py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
     py_module.add_class::<PyCallInfo>()?;
     py_module.add_class::<PyExecutionResources>()?;
     py_module.add_class::<PyOrderedEvent>()?;
@@ -26,6 +28,11 @@ fn native_blockifier(_py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
     py_module.add_class::<PyTransactionExecutionInfo>()?;
     py_module.add_class::<PyTransactionExecutor>()?;
     py_module.add_class::<Storage>()?;
+    add_py_exceptions(py, py_module)?;
+
+    // TODO(Dori, 1/4/2023): If and when supported in the Python build environment, gate this code
+    //   with #[cfg(test)].
+    py_module.add_function(wrap_pyfunction!(raise_error_for_testing, py)?)?;
 
     Ok(())
 }
