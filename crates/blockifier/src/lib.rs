9adfde11f4c07b2f5d1e8fed8a35ead835bc1594
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-03-15 16:10:27 +0100

    build: put test_utils.rs behind the 'testing' feature flag

--- crates/blockifier/src/lib.rs
+++ crates/blockifier/src/lib.rs
@@ -2,8 +2,7 @@ pub mod abi;
 pub mod block_context;
 pub mod execution;
 pub mod state;
-// TODO: uncomment once we fix native_extension; making it no longer require DictStateReader.
-// #[cfg(any(feature = "testing", test))]
+#[cfg(any(feature = "testing", test))]
 pub mod test_utils;
 pub mod transaction;
 pub mod utils;
