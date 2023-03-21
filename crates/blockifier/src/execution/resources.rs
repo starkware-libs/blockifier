2bbbabed6ad70a0f30717b908dd7e4f2589ba0ac
Dori Medini <dori@starkware.co>
2023-03-21 19:52:06 +0200

    Add OsResources
    Signed-off-by: Dori Medini <dori@starkware.co>

---
+++ crates/blockifier/src/execution/resources.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
+use lazy_static::lazy_static;
+
+#[derive(Eq, Hash, PartialEq)]
+pub enum TransactionType {
+    Declare,
+    Deploy,
+    DeployAccount,
+    InitializeBlockInfo,
+    InvokeFunction,
+    L1Handler,
+}
+
+pub struct OsResources {
+    // Mapping from every syscall to its execution resources in the OS (e.g., amount of Cairo
+    // steps).
+    execute_syscalls: HashMap<&'static str, ExecutionResources>,
+    // Mapping from every transaction to its extra execution resources in the OS,
+    // i.e., resources that don't count during the execution itself.
+    execute_txs_inner: HashMap<TransactionType, ExecutionResources>,
+}
+
+/// Shorthand for creating ExecutionResources instances.
+fn execution_resources(n_steps: usize, n_rc: usize, n_pedersen: usize) -> ExecutionResources {
+    ExecutionResources {
+        n_steps,
+        n_memory_holes: 0,
+        builtin_instance_counter: HashMap::from([
+            ("range_check_builtin".to_string(), n_rc),
+            ("pedersen_builtin".to_string(), n_pedersen),
+        ]),
+    }
+    .filter_unused_builtins()
+}
+
+lazy_static! {
+    static ref OS_RESOURCES: OsResources = OsResources {
+        execute_syscalls: HashMap::from([
+            ("call_contract", execution_resources(630, 18, 0)),
+            ("delegate_call", execution_resources(652, 18, 0)),
+            ("delegate_l1_handler", execution_resources(631, 14, 0)),
+            ("deploy", execution_resources(878, 17, 7)),
+            ("emit_event", execution_resources(19, 0, 0)),
+            ("get_block_number", execution_resources(40, 0, 0)),
+            ("get_block_timestamp", execution_resources(38, 0, 0)),
+            ("get_caller_address", execution_resources(32, 0, 0)),
+            ("get_contract_address", execution_resources(36, 0, 0)),
+            ("get_sequencer_address", execution_resources(34, 0, 0)),
+            ("get_tx_info", execution_resources(29, 0, 0)),
+            ("get_tx_signature", execution_resources(44, 0, 0)),
+            ("library_call", execution_resources(619, 18, 0)),
+            ("library_call_l1_handler", execution_resources(598, 14, 0)),
+            ("replace_class", execution_resources(73, 0, 0)),
+            ("send_message_to_l1", execution_resources(84, 0, 0)),
+            ("storage_read", execution_resources(44, 0, 0)),
+            ("storage_write", execution_resources(46, 0, 0)),
+        ]),
+        execute_txs_inner: HashMap::from([
+            (TransactionType::Declare, execution_resources(2581, 61, 15)),
+            (TransactionType::Deploy, execution_resources(0, 0, 0)),
+            (TransactionType::DeployAccount, execution_resources(3434, 80, 23)),
+            (TransactionType::InvokeFunction, execution_resources(3181, 77, 16)),
+            (TransactionType::L1Handler, execution_resources(1006, 16, 11)),
+        ])
+    };
+}
+
+pub fn get_additional_os_resources(
+    syscall_counter: HashMap<String, usize>,
+    tx_type: TransactionType,
+) -> ExecutionResources {
+    // Calculate the additional resources needed for the OS to run the given syscalls;
+    // i.e., the resources of the function execute_syscalls().
+    let mut os_additional_resources = execution_resources(0, 0, 0);
+    for (syscall_name, count) in syscall_counter {
+        // TODO(Dori, 1/5/2023): Use `+=` when available.
+        os_additional_resources = os_additional_resources
+            + (OS_RESOURCES.execute_syscalls.get(syscall_name.as_str()).unwrap().clone() * count);
+    }
+
+    // Calculate the additional resources needed for the OS to run the given transaction;
+    // i.e., the resources of the StarkNet OS function execute_transactions_inner().
+    os_additional_resources + OS_RESOURCES.execute_txs_inner.get(&tx_type).unwrap().clone()
+}
