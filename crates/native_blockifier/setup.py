88d5d9671ee27ac2fade58091e64da0d55c4d344
Dori Medini <dori@starkware.co>
2023-03-24 00:44:25 +0200

    Papyrus storage bugfix + version 0.16.5
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/native_blockifier/setup.py
+++ crates/native_blockifier/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="native_blockifier",
-    version="0.15.10",
+    version="0.16.5",
     rust_extensions=[RustExtension("native_blockifier.native_blockifier", binding=Binding.PyO3)],
     author="Starkware",
     author_email="info@starkware.co",
