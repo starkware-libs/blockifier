8a0140b388cf4e05511198bb5b58103fffce6126
Dori Medini <dori@starkware.co>
2023-03-26 20:18:50 +0300

    PR comments
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/fee/os_usage_test.rs
+++ crates/blockifier/src/fee/os_usage_test.rs
@@ -1,11 +1,15 @@
 use strum::IntoEnumIterator;
 
+use crate::execution::syscalls::SyscallSelector;
 use crate::fee::os_usage::OS_RESOURCES;
-use crate::transaction::objects::TransactionType;
+use crate::transaction::transaction_types::TransactionType;
 
 #[test]
-fn test_tx_types_have_resources_entry() {
+fn test_resources_entries() {
     for tx_type in TransactionType::iter() {
         assert!(OS_RESOURCES.execute_txs_inner.get(&tx_type).is_some());
+    }
+    for syscall_selector in SyscallSelector::iter() {
+        assert!(OS_RESOURCES.execute_syscalls.get(&syscall_selector).is_some());
     }
 }
