f72635fc4c34059b6bddd449fea4f5041fb83c92
Elin Tulchinsky <elin@starkware.co>
2023-03-27 11:48:54 +0300

    Bump `native_blockifier` version.

--- crates/native_blockifier/setup.py
+++ crates/native_blockifier/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="native_blockifier",
-    version="0.16.2",
+    version="0.16.6",
     rust_extensions=[RustExtension("native_blockifier.native_blockifier", binding=Binding.PyO3)],
     author="Starkware",
     author_email="info@starkware.co",
