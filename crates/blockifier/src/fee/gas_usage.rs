299d2f7fb532352c091dd01c1f3974100e7ad91c
Dori Medini <dori@starkware.co>
2023-03-28 17:10:45 +0300

    get_message_segment_length
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/fee/gas_usage.rs
+++ crates/blockifier/src/fee/gas_usage.rs
@@ -1,4 +1,5 @@
 use crate::abi::constants;
+use crate::execution::entry_point::MessageToL1;
 
 /// Returns the number of felts added to the output data availability segment as a result of adding
 /// a transaction to a batch. Note that constant cells - such as the one that holds the number of
@@ -18,4 +19,22 @@ pub fn get_onchain_data_segment_length(
     onchain_data_segment_length += n_storage_changes * 2;
 
     onchain_data_segment_length
+}
+
+/// Returns the number of felts added to the output messages segment as a result of adding a
+/// transaction with the given parameters to a batch. Note that constant cells - such as the one
+/// that holds the segment size - are not counted.
+pub fn get_message_segment_length(
+    l2_to_l1_messages: Vec<MessageToL1>,
+    l1_handler_payload_size: Option<u64>,
+) -> u64 {
+    let message_segment_length: u64 = l2_to_l1_messages
+        .iter()
+        .map(|message| (message.payload.0.len() as u64) + constants::L2_TO_L1_MSG_HEADER_SIZE)
+        .sum();
+
+    match l1_handler_payload_size {
+        None => message_segment_length,
+        Some(size) => message_segment_length + constants::L1_TO_L2_MSG_HEADER_SIZE + size,
+    }
 }
