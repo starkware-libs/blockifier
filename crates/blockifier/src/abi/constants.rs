ba5e2fde85252e8cf5b26c275acf2deba179cf8d
amosStarkware <88497213+amosStarkware@users.noreply.github.com>
2023-03-28 16:53:14 +0300

    Add get_onchain_data_segment_length. (#367)

--- crates/blockifier/src/abi/constants.rs
+++ crates/blockifier/src/abi/constants.rs
@@ -4,3 +4,4 @@ pub const DEFAULT_ENTRY_POINT_SELECTOR: u64 = 0;
 pub const DEFAULT_L1_ENTRY_POINT_NAME: &str = "__l1_default__";
 // The version is considered 0 for L1-Handler transaction hash calculation purposes.
 pub const L1_HANDLER_VERSION: u64 = 0;
+pub const CLASS_UPDATE_SIZE: u64 = 1;
--- crates/blockifier/src/fee/gas_usage.rs
+++ crates/blockifier/src/fee/gas_usage.rs
@@ -1,1 +1,21 @@
+use crate::abi::constants;
 
+/// Returns the number of felts added to the output data availability segment as a result of adding
+/// a transaction to a batch. Note that constant cells - such as the one that holds the number of
+/// modified contracts - are not counted.
+/// This segment consists of deployment info (of contracts deployed by the transaction) and
+/// storage updates.
+pub fn get_onchain_data_segment_length(
+    n_modified_contracts: u64,
+    n_storage_changes: u64,
+    n_class_updates: u64,
+) -> u64 {
+    // For each newly modified contract: contract address, number of modified storage cells.
+    let mut onchain_data_segment_length = n_modified_contracts * 2;
+    // For each class updated (through a deploy or a class replacement).
+    onchain_data_segment_length += n_class_updates * constants::CLASS_UPDATE_SIZE;
+    // For each modified storage cell: key, new value.
+    onchain_data_segment_length += n_storage_changes * 2;
+
+    onchain_data_segment_length
+}
