0a2abbc6f8991742e3f70755d3cdb50f62ccf395
amos rothberg <amos@starkware.co>
2023-03-28 10:59:23 +0300

    Added get_message_segment_length.

--- crates/blockifier/src/abi/constants.rs
+++ crates/blockifier/src/abi/constants.rs
@@ -5,3 +5,5 @@ pub const DEFAULT_L1_ENTRY_POINT_NAME: &str = "__l1_default__";
 // The version is considered 0 for L1-Handler transaction hash calculation purposes.
 pub const L1_HANDLER_VERSION: u64 = 0;
 pub const CLASS_UPDATE_SIZE: u64 = 1;
+pub const L2_TO_L1_MSG_HEADER_SIZE: u64 = 3;
+pub const L1_TO_L2_MSG_HEADER_SIZE: u64 = 5;
