c81572f6860d8650c2277fe7eec4756517cde786
Alon Haramati <alonh@starkware.co>
2023-03-28 10:58:39 +0300

    Make previous_block_id optional.

--- crates/native_blockifier/src/storage.rs
+++ crates/native_blockifier/src/storage.rs
@@ -18,6 +18,8 @@ use crate::py_state_diff::PyBlockInfo;
 use crate::py_utils::PyFelt;
 use crate::PyStateDiff;
 
+const GENESIS_BLOCK_ID: u64 = u64::MAX;
+
 #[pyclass]
 pub struct Storage {
     pub reader: papyrus_storage::StorageReader,
@@ -68,7 +70,7 @@ impl Storage {
     pub fn append_state_diff(
         &mut self,
         block_id: u64,
-        previous_block_id: u64,
+        previous_block_id: Option<u64>,
         py_block_info: PyBlockInfo,
         py_state_diff: PyStateDiff,
         declared_class_hash_to_class: HashMap<PyFelt, String>,
@@ -98,7 +100,7 @@ impl Storage {
 
         let block_header = BlockHeader {
             block_hash: BlockHash(StarkHash::from(block_id)),
-            parent_hash: BlockHash(StarkHash::from(previous_block_id)),
+            parent_hash: BlockHash(StarkHash::from(previous_block_id.unwrap_or(GENESIS_BLOCK_ID))),
             block_number,
             gas_price: GasPrice(py_block_info.gas_price),
             state_root: GlobalRoot::default(),
