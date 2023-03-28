c8e8d1a00a770aca7f19887443a218aa355da86e
Dori Medini <dori@starkware.co>
2023-03-28 18:13:58 +0300

    get_log_message_to_l1_emissions_cost
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/abi/constants.rs
+++ crates/blockifier/src/abi/constants.rs
@@ -2,8 +2,22 @@ pub const CONSTRUCTOR_ENTRY_POINT_NAME: &str = "constructor";
 pub const DEFAULT_ENTRY_POINT_NAME: &str = "__default__";
 pub const DEFAULT_ENTRY_POINT_SELECTOR: u64 = 0;
 pub const DEFAULT_L1_ENTRY_POINT_NAME: &str = "__l1_default__";
+
 // The version is considered 0 for L1-Handler transaction hash calculation purposes.
 pub const L1_HANDLER_VERSION: u64 = 0;
+
+// OS-related constants.
+pub const L2_TO_L1_MSG_HEADER_SIZE: u64 = 3;
 pub const CLASS_UPDATE_SIZE: u64 = 1;
+
 // StarkNet solidity contract-related constants.
 pub const N_DEFAULT_TOPICS: u64 = 1; // Events have one default topic.
+
+// Excluding the default topic.
+pub const LOG_MSG_TO_L1_N_TOPICS: u64 = 2;
+pub const CONSUMED_MSG_TO_L2_N_TOPICS: u64 = 3;
+
+// The headers include the payload size, so we need to add +1 since arrays are encoded with two
+// additional parameters (offset and length) in solidity.
+pub const LOG_MSG_TO_L1_ENCODED_DATA_SIZE: u64 =
+    (L2_TO_L1_MSG_HEADER_SIZE + 1) - LOG_MSG_TO_L1_N_TOPICS;
