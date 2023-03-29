2dfc766859263147b90eef25571f9ed5d0cb78f3
amosStarkware <88497213+amosStarkware@users.noreply.github.com>
2023-03-29 14:53:34 +0300

    Added get_message_segment_length. (#366)

--- crates/blockifier/src/abi/constants.rs
+++ crates/blockifier/src/abi/constants.rs
@@ -4,4 +4,7 @@ pub const DEFAULT_ENTRY_POINT_SELECTOR: u64 = 0;
 pub const DEFAULT_L1_ENTRY_POINT_NAME: &str = "__l1_default__";
 // The version is considered 0 for L1-Handler transaction hash calculation purposes.
 pub const L1_HANDLER_VERSION: u64 = 0;
+// OS-related constants.
 pub const CLASS_UPDATE_SIZE: u64 = 1;
+pub const L2_TO_L1_MSG_HEADER_SIZE: usize = 3;
+pub const L1_TO_L2_MSG_HEADER_SIZE: usize = 5;
