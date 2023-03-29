5fa76c105e1b2d370007d3cd75d7db015d77eb1a
amos rothberg <amos@starkware.co>
2023-03-29 15:37:25 +0300

    Updated according to review.

--- crates/blockifier/src/fee/fee_utils.rs
+++ crates/blockifier/src/fee/fee_utils.rs
@@ -1,12 +1,14 @@
-use crate::transaction::objects::ResourcesMapping;
 use std::collections::HashMap;
 
-pub fn extract_l1_gas_and_cairo_usage(resources: &ResourcesMapping) -> (usize, HashMap<std::string::String, usize>){
+use crate::transaction::objects::ResourcesMapping;
+
+pub fn extract_l1_gas_and_cairo_usage(
+    resources: &ResourcesMapping,
+) -> (usize, HashMap<String, usize>) {
     let mut cairo_resource_usage = resources.0.clone();
-    let l1_gas_usage = match cairo_resource_usage.get("l1_gas_usage") {
-        Some(l1_gas_usage) => *l1_gas_usage,
-        _ => panic!("ResourcesMapping does not have the key l1_gas_usage.")
+    let l1_gas_usage = match cairo_resource_usage.remove("l1_gas_usage") {
+        Some(l1_gas_usage) => l1_gas_usage,
+        None => panic!("ResourcesMapping does not have the key l1_gas_usage."),
     };
-    cairo_resource_usage.remove("l1_gas_usage");
     (l1_gas_usage, cairo_resource_usage)
 }
