0fa7145a79ac75509f62b7b1546150dbab1adc77
Noa Oved <noa@starkware.co>
2023-03-30 15:23:19 +0300

    Add calculate_tx_gas_usage.

--- crates/blockifier/src/fee/gas_usage.rs
+++ crates/blockifier/src/fee/gas_usage.rs
@@ -1,6 +1,50 @@
 use crate::abi::constants;
 use crate::fee::eth_gas_constants;
 
+/// Returns an estimation of the L1 gas amount that will be used (by StarkNet's update state and
+/// the verifier) following the addition of a transaction with the given parameters to a batch;
+/// e.g., a message from L2 to L1 is followed by a storage write operation in StarkNet L1 contract
+/// which requires gas.
+pub fn calculate_tx_gas_usage(
+    l2_to_l1_payloads_length: &[usize],
+    n_modified_contracts: usize,
+    n_storage_changes: usize,
+    l1_handler_payload_size: Option<usize>,
+    n_class_updates: usize,
+) -> usize {
+    // Calculate the addition of the transaction to the output messages segment.
+    let residual_message_segment_length =
+        get_message_segment_length(l2_to_l1_payloads_length, l1_handler_payload_size);
+
+    // Calculate the effect of the transaction on the output data availability segment.
+    let residual_onchain_data_segment_length =
+        get_onchain_data_segment_length(n_modified_contracts, n_storage_changes, n_class_updates);
+
+    let n_l2_to_l1_messages = l2_to_l1_payloads_length.len();
+    let n_l1_to_l2_messages = match l1_handler_payload_size {
+        Some(_) => 1,
+        None => 0,
+    };
+
+    let starknet_gas_usage =
+    // StarkNet's updateState gets the message segment as an argument.
+    residual_message_segment_length * eth_gas_constants::GAS_PER_MEMORY_WORD
+    // StarkNet's updateState increases a (storage) counter for each L2-to-L1 message.
+    + n_l2_to_l1_messages * eth_gas_constants::GAS_PER_ZERO_TO_NONZERO_STORAGE_SET
+    // StarkNet's updateState decreases a (storage) counter for each L1-to-L2 consumed message.
+    // (Note that we will probably get a refund of 15,000 gas for each consumed message but we 
+    // ignore it since refunded gas cannot be used for the current transaction execution).
+    + n_l1_to_l2_messages * eth_gas_constants::GAS_PER_COUNTER_DECREASE
+    + get_consumed_message_to_l2_emissions_cost(l1_handler_payload_size)
+    + get_log_message_to_l1_emissions_cost(l2_to_l1_payloads_length);
+
+    let sharp_gas_usage = residual_message_segment_length
+        * eth_gas_constants::SHARP_GAS_PER_MEMORY_WORD
+        + residual_onchain_data_segment_length * eth_gas_constants::SHARP_GAS_PER_MEMORY_WORD;
+
+    starknet_gas_usage + sharp_gas_usage
+}
+
 /// Returns the number of felts added to the output data availability segment as a result of adding
 /// a transaction to a batch. Note that constant cells - such as the one that holds the number of
 /// modified contracts - are not counted.
