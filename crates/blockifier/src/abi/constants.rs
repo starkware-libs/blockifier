d5131a0365475ae54e6182bd93856a7c592d7d6c
Noa Oved <noa@starkware.co>
2023-03-30 13:17:21 +0300

    Add .

--- crates/blockifier/src/abi/constants.rs
+++ crates/blockifier/src/abi/constants.rs
@@ -22,3 +22,5 @@ pub const CONSUMED_MSG_TO_L2_N_TOPICS: usize = 3;
 // additional parameters (offset and length) in solidity.
 pub const LOG_MSG_TO_L1_ENCODED_DATA_SIZE: usize =
     (L2_TO_L1_MSG_HEADER_SIZE + 1) - LOG_MSG_TO_L1_N_TOPICS;
+pub const CONSUMED_MSG_TO_L2_ENCODED_DATA_SIZE: usize =
+    (L1_TO_L2_MSG_HEADER_SIZE + 1) - CONSUMED_MSG_TO_L2_N_TOPICS;
