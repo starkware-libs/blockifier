841153b78f87b231cbc2f7e5ff66b70be7adbafa
amos rothberg <amos@starkware.co>
2023-04-01 11:36:04 -0400

    Use u128.

--- crates/blockifier/src/block_context.rs
+++ crates/blockifier/src/block_context.rs
@@ -13,7 +13,7 @@ pub struct BlockContext {
     pub sequencer_address: ContractAddress,
     pub fee_token_address: ContractAddress,
     pub cairo_resource_fee_weights: HashMap<String, u32>,
-    pub gas_price: u64, // In wei.
+    pub gas_price: u128, // In wei.
 
     // Limits.
     pub invoke_tx_max_n_steps: u32,
