634f910f1832f6f91ca23a04ee63fb78d59eb732
amos rothberg <amos@starkware.co>
2023-04-02 12:39:08 -0400

    WIP

--- crates/blockifier/src/fee/fee_test.rs
+++ crates/blockifier/src/fee/fee_test.rs
@@ -1,21 +1,27 @@
+use std::collections::HashMap;
+
 use crate::block_context::BlockContext;
 use crate::transaction::objects::ResourcesMapping;
 use crate::abi::constants::N_STEPS_RESOURCE;
 use crate::fee::fee_utils::calculate_l1_gas_by_cairo_usage;
+use ::rstest::rstest;
 
-@pytest.fixture(scope="module")
-def general_config() -> StarknetGeneralConfig:
-    return StarknetGeneralConfig(
-        sequencer_address=1991,
-        starknet_os_config=StarknetOsConfig(fee_token_address=2022),
-        cairo_resource_fee_weights={
-            N_STEPS_RESOURCE: 1.0,
-            **{builtin: 1.0 for builtin in ALL_BUILTINS.except_for(KECCAK_BUILTIN).with_suffix()},
-        },
-    )
+// #[fixture]
+// def general_config() -> StarknetGeneralConfig:
+//     return StarknetGeneralConfig(
+//         sequencer_address=1991,
+//         starknet_os_config=StarknetOsConfig(fee_token_address=2022),
+//         cairo_resource_fee_weights={
+//             N_STEPS_RESOURCE: 1.0,
+//             **{builtin: 1.0 for builtin in ALL_BUILTINS.except_for(KECCAK_BUILTIN).with_suffix()},
+//         },
+//     )
 
-@pytest.fixture(scope="module")
-def cairo_resource_usage() -> Dict[str, int]:
+#[fixture]
+fn cairo_resource_usage() -> &ResourcesMapping{
+    let cairo_resource_usage = ResourcesMapping(HashMap::<String, usize>::new());
+    cairo_resource_usage.0.insert(k, v)
+}
     return {
         N_STEPS_RESOURCE: 1800,
         with_suffix(PEDERSEN_BUILTIN): 10,
@@ -31,22 +37,22 @@ fn test_calculate_l1_gas_by_cairo_usage(
     
     // Positive flow.
     // Verify calculation - in our case, n_steps is the heaviest resource.
-    l1_gas_by_cairo_usage = cairo_resource_usage.get(N_STEPS_RESOURCE);
+    let l1_gas_by_cairo_usage = cairo_resource_usage.0.get(N_STEPS_RESOURCE);
     assert_eq!(l1_gas_by_cairo_usage, calculate_l1_gas_by_cairo_usage(
-            block_context, cairo_resource_usage).ceil()
+            block_context, cairo_resource_usage)
     );
 
     // Negative flow.
     // Pass dict with extra key.
-    let invalid_cairo_resource_usage = cairo_resource_usage.clone();
-    invalid_cairo_resource_usage.insert("bad_resource_name"=17);
-    let result = std::panic::catch_unwind(|| 
-        calculate_l1_gas_by_cairo_usage(block_context, invalid_cairo_resource_usage));
-    assert!(result.is_err());  //probe further for specific error type here, if desired
-
-    with pytest.raises(
-        AssertionError,
-        match="Cairo resource names must be contained in fee weights dict.",
-    ):
-        
+    let invalid_cairo_resource_usage = ResourcesMapping(cairo_resource_usage.clone());
+    invalid_cairo_resource_usage.0.insert(String::from("bad_resource_name"), 17);
+    // The assertion below is similar to pytest.raises(..) in python.
+    assert_eq!(
+        std::panic::catch_unwind(||
+            calculate_l1_gas_by_cairo_usage(block_context, &invalid_cairo_resource_usage);
+        ).err().and_then(|a| a.downcast_ref::<String>().map(|s| {
+            s == "Cairo resource names must be contained in fee weights dict."
+        })),
+        Some(true)
+    );    
 }
