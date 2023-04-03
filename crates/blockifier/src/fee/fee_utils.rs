d290a9a932358ffb977e7fa89485b882f1bf24e6
amos rothberg <amos@starkware.co>
2023-04-03 11:30:38 -0400

    Used u128 and misc

--- crates/blockifier/src/fee/fee_utils.rs
+++ crates/blockifier/src/fee/fee_utils.rs
@@ -22,7 +22,7 @@ pub fn extract_l1_gas_and_cairo_usage(
 pub fn calculate_l1_gas_by_cairo_usage(
     block_context: &BlockContext,
     cairo_resource_usage: &ResourcesMapping,
-) -> usize {
+) -> u128 {
     let cairo_resource_fee_weights = &block_context.cairo_resource_fee_weights;
     let cairo_resource_names = HashSet::<&String>::from_iter(cairo_resource_usage.0.keys());
     assert!(
@@ -31,15 +31,9 @@ pub fn calculate_l1_gas_by_cairo_usage(
     );
 
     // Convert Cairo usage to L1 gas usage.
-    let cairo_l1_gas_usage =
-        max(cairo_resource_fee_weights.iter().map(
-            |(key, resource_val)| match cairo_resource_usage.0.get(key) {
-                Some(usage_val) => (*resource_val as usize) * (*usage_val),
-                None => 0_usize,
-            },
-        ));
-    match cairo_l1_gas_usage {
-        None => panic!("block_context.cairo_resource_fee_weights can not be empty."),
-        Some(gas_usage) => gas_usage,
-    }
+    let cairo_l1_gas_usage = max(cairo_resource_fee_weights.iter().map(|(key, resource_val)| {
+        u128::from(*resource_val)
+            * cairo_resource_usage.0.get(key).cloned().unwrap_or_default() as u128
+    }));
+    cairo_l1_gas_usage.expect("block_context.cairo_resource_fee_weights can not be empty.")
 }
