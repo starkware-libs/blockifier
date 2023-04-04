6f072ff4db498b12ee73cb9943f6f618590c9f90
Noa Oved <noa@starkware.co>
2023-04-04 17:10:42 +0300

    Add CairoResourcesNotContainedInFeeWeights error.

--- crates/blockifier/src/fee/fee_utils.rs
+++ crates/blockifier/src/fee/fee_utils.rs
@@ -3,7 +3,8 @@ use std::collections::{HashMap, HashSet};
 use itertools::max;
 
 use crate::block_context::BlockContext;
-use crate::transaction::objects::ResourcesMapping;
+use crate::transaction::errors::TransactionExecutionError;
+use crate::transaction::objects::{ResourcesMapping, TransactionExecutionResult};
 
 pub fn extract_l1_gas_and_cairo_usage(
     resources: &ResourcesMapping,
@@ -22,18 +23,17 @@ pub fn extract_l1_gas_and_cairo_usage(
 pub fn calculate_l1_gas_by_cairo_usage(
     block_context: &BlockContext,
     cairo_resource_usage: &ResourcesMapping,
-) -> u128 {
+) -> TransactionExecutionResult<u128> {
     let cairo_resource_fee_weights = &block_context.cairo_resource_fee_weights;
     let cairo_resource_names = HashSet::<&String>::from_iter(cairo_resource_usage.0.keys());
-    assert!(
-        cairo_resource_names.is_subset(&HashSet::from_iter(cairo_resource_fee_weights.keys())),
-        "Cairo resource names must be contained in fee weights dict."
-    );
+    if !cairo_resource_names.is_subset(&HashSet::from_iter(cairo_resource_fee_weights.keys())) {
+        return Err(TransactionExecutionError::CairoResourcesNotContainedInFeeWeights);
+    };
 
     // Convert Cairo usage to L1 gas usage.
     let cairo_l1_gas_usage = max(cairo_resource_fee_weights.iter().map(|(key, resource_val)| {
         u128::from(*resource_val)
             * cairo_resource_usage.0.get(key).cloned().unwrap_or_default() as u128
     }));
-    cairo_l1_gas_usage.expect("block_context.cairo_resource_fee_weights can not be empty.")
+    Ok(cairo_l1_gas_usage.expect("`cairo_resource_fee_weights` is empty."))
 }
