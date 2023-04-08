35d6c62af8848356b398870f1154b0c96b78984b
amos rothberg <amos@starkware.co>
2023-04-07 21:35:30 -0400

    Update test for new return values.

--- crates/blockifier/src/fee/fee_test.rs
+++ crates/blockifier/src/fee/fee_test.rs
@@ -1,27 +1,10 @@
 use std::collections::HashMap;
-use std::panic;
-
-use rstest::{fixture, rstest};
 
 use crate::block_context::BlockContext;
 use crate::fee::fee_utils::calculate_l1_gas_by_cairo_usage;
 use crate::transaction::objects::ResourcesMapping;
 
-/// Use this function instead of panic::catch_unwind to achieve silence in output for
-/// expected exceptions. For more information see here:
-/// https://stackoverflow.com/questions/26469715
-pub fn catch_unwind_silent<F: FnOnce() -> R + panic::UnwindSafe, R>(
-    f: F,
-) -> std::thread::Result<R> {
-    let prev_hook = panic::take_hook();
-    panic::set_hook(Box::new(|_| {}));
-    let result = panic::catch_unwind(f);
-    panic::set_hook(prev_hook);
-    result
-}
-
-#[fixture]
-fn block_context() -> BlockContext {
+fn get_block_context() -> BlockContext {
     let mut block_context = BlockContext::create_for_testing();
     let mut cairo_resource_fee_weights = HashMap::<String, u32>::new();
     cairo_resource_fee_weights.insert(String::from("n_steps"), 1);
@@ -36,8 +19,7 @@ fn block_context() -> BlockContext {
     block_context
 }
 
-#[fixture]
-fn cairo_resource_usage() -> ResourcesMapping {
+fn get_cairo_resource_usage() -> ResourcesMapping {
     let mut cairo_resource_usg = ResourcesMapping(HashMap::<String, usize>::new());
     cairo_resource_usg.0.insert(String::from("n_steps"), 1800);
     cairo_resource_usg.0.insert(String::from("pedersen_builtin"), 10);
@@ -48,11 +30,11 @@ fn cairo_resource_usage() -> ResourcesMapping {
     cairo_resource_usg
 }
 
-#[rstest]
-fn test_calculate_l1_gas_by_cairo_usage(
-    block_context: BlockContext,
-    cairo_resource_usage: ResourcesMapping,
-) {
+#[test]
+fn test_calculate_l1_gas_by_cairo_usage() {
+    let block_context = get_block_context();
+    let cairo_resource_usage = get_cairo_resource_usage();
+
     // Positive flow.
     // Verify calculation - in our case, n_steps is the heaviest resource.
     let l1_gas_by_cairo_usage = cairo_resource_usage
@@ -60,24 +42,23 @@ fn test_calculate_l1_gas_by_cairo_usage(
         .get("n_steps")
         .expect("cairo_resource_usage should have the key n_steps");
     assert_eq!(
-        *l1_gas_by_cairo_usage,
+        *l1_gas_by_cairo_usage as u128,
         calculate_l1_gas_by_cairo_usage(&block_context, &cairo_resource_usage)
+            .expect("calculate_l1_gas_by_cairo_usage returned error on valid input.")
     );
 
     // Negative flow.
     // Pass dict with extra key.
     let mut invalid_cairo_resource_usage = ResourcesMapping(cairo_resource_usage.0.clone());
     invalid_cairo_resource_usage.0.insert(String::from("bad_resource_name"), 17);
-    let expected_panic_msg = &"Cairo resource names must be contained in fee weights dict.";
-    // Assert a panic with the message expected_panic_msg was raised.
-    assert_eq!(
-        catch_unwind_silent(|| {
-            calculate_l1_gas_by_cairo_usage(&block_context, &invalid_cairo_resource_usage);
-        })
-        .err()
-        .and_then(|a| a.downcast_ref::<&str>().map(|s| { s == expected_panic_msg })),
-        Some(true),
-        "Panic with expected message '{}' was not raised.",
-        expected_panic_msg
-    );
+    let expected_err_msg = "Cairo resource names must be contained in fee weights dict.";
+    let error = calculate_l1_gas_by_cairo_usage(&block_context, &invalid_cairo_resource_usage)
+        .expect_err(
+            "Expected TransactionExecutionError::CairoResourcesNotContainedInFeeWeights, 
+    got Ok instead.",
+        )
+        .to_string();
+    if !(error == expected_err_msg.to_string()) {
+        panic!("Expected error: {expected_err_msg}.\nGot: {error}")
+    }
 }
