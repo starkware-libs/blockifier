cfaa1d3cb4ede64c2922468d9e97f470af820bcf
Elin Tulchinsky <elin@starkware.co>
2023-04-09 18:06:04 +0300

    Change `ContractClass`, add `DeprecatedContractClassWithoutAbi`.

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -1,55 +1,111 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io;
-use std::io::BufReader;
-use std::path::PathBuf;
+use std::io::Read;
 
+use cairo_vm::types::errors::program_errors::ProgramError;
+use cairo_vm::types::program::Program;
 use serde::{Deserialize, Serialize};
 use starknet_api::deprecated_contract_class::{
-    ContractClass as DeprecatedContractClass, EntryPoint, EntryPointType, Program,
+    ContractClass as DeprecatedContractClass, EntryPoint, EntryPointType,
+    Program as DeprecatedProgram,
 };
 
-/// Represents a StarkNet contract class.
+use crate::execution::execution_utils::sn_api_to_cairo_vm_program;
+
+/// Represents a runnable StarkNet contract class (meaning, the program is runnable by the VM).
 #[derive(Debug, Clone, Default, Eq, PartialEq, Deserialize, Serialize)]
 pub struct ContractClass {
     pub program: Program,
-    /// The selector of each entry point is a unique identifier in the program.
     pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
-    // Not required for execution, thus can be omitted from the raw contract file.
-    pub abi: Option<serde_json::Value>,
 }
 
-impl From<ContractClass> for DeprecatedContractClass {
-    fn from(contract_class: ContractClass) -> Self {
+impl TryFrom<DeprecatedContractClass> for ContractClass {
+    type Error = ProgramError;
+
+    fn try_from(class: DeprecatedContractClass) -> Result<Self, Self::Error> {
+        let class = DeprecatedContractClassWithoutAbi {
+            program: class.program,
+            entry_points_by_type: class.entry_points_by_type,
+        };
+        ContractClass::try_from(class)
+    }
+}
+
+impl TryFrom<DeprecatedContractClassWithoutAbi> for ContractClass {
+    type Error = ProgramError;
+
+    fn try_from(class: DeprecatedContractClassWithoutAbi) -> Result<Self, Self::Error> {
+        Ok(Self {
+            program: sn_api_to_cairo_vm_program(class.program)?,
+            entry_points_by_type: class.entry_points_by_type,
+        })
+    }
+}
+
+impl TryFrom<&str> for ContractClass {
+    type Error = ProgramError;
+
+    fn try_from(raw_contract_class: &str) -> Result<Self, Self::Error> {
+        let deprecated_contract_class =
+            DeprecatedContractClassWithoutAbi::try_from(raw_contract_class)?;
+        ContractClass::try_from(deprecated_contract_class)
+    }
+}
+
+#[cfg(any(feature = "testing", test))]
+impl TryFrom<std::path::PathBuf> for ContractClass {
+    type Error = ProgramError;
+
+    fn try_from(path: std::path::PathBuf) -> Result<Self, Self::Error> {
+        let deprecated_contract_class = DeprecatedContractClassWithoutAbi::from(path);
+        ContractClass::try_from(deprecated_contract_class)
+    }
+}
+
+/// Represents a raw StarkNet contract class (non-runnable;
+/// the equivalent of SN API's struct, without ABI which is not required for execution).
+#[derive(Debug, Clone, Default, Eq, PartialEq, Deserialize, Serialize)]
+pub struct DeprecatedContractClassWithoutAbi {
+    pub program: DeprecatedProgram,
+    pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
+}
+
+impl From<DeprecatedContractClassWithoutAbi> for DeprecatedContractClass {
+    fn from(contract_class: DeprecatedContractClassWithoutAbi) -> Self {
         Self {
             program: contract_class.program,
             entry_points_by_type: contract_class.entry_points_by_type,
-            // ABI is not used for execution.
             abi: None,
         }
     }
 }
 
-impl From<DeprecatedContractClass> for ContractClass {
-    fn from(contract_class: DeprecatedContractClass) -> Self {
-        Self {
-            program: contract_class.program,
-            entry_points_by_type: contract_class.entry_points_by_type,
-            // ABI is not used for execution.
-            abi: None,
-        }
+impl TryFrom<&str> for DeprecatedContractClassWithoutAbi {
+    type Error = serde_json::Error;
+
+    fn try_from(raw_contract_class: &str) -> Result<Self, Self::Error> {
+        let mut raw_contract_class: serde_json::Value = serde_json::from_str(raw_contract_class)?;
+
+        // ABI is not required for execution.
+        raw_contract_class
+            .as_object_mut()
+            .expect("A compiled contract must be a JSON object.")
+            .insert("abi".to_string(), serde_json::Value::Null);
+
+        let deprecated_contract_class: DeprecatedContractClassWithoutAbi =
+            serde_json::from_value(raw_contract_class)?;
+        Ok(deprecated_contract_class)
     }
 }
 
-/// Instantiates a contract class object given a compiled contract file path.
-impl TryFrom<PathBuf> for ContractClass {
-    type Error = io::Error;
+#[cfg(any(feature = "testing", test))]
+impl From<std::path::PathBuf> for DeprecatedContractClassWithoutAbi {
+    fn from(path: std::path::PathBuf) -> Self {
+        let mut file =
+            std::fs::File::open(&path).unwrap_or_else(|_| panic!("File {path:?} does not exist."));
+        let mut raw_contract_class = String::new();
+        file.read_to_string(&mut raw_contract_class).expect("Malformed class JSON.");
 
-    fn try_from(path: PathBuf) -> io::Result<Self> {
-        let file = File::open(path)?;
-        let reader = BufReader::new(file);
-
-        let raw_contract_class = serde_json::from_reader(reader)?;
-        Ok(raw_contract_class)
+        DeprecatedContractClassWithoutAbi::try_from(raw_contract_class.as_str())
+            .expect("Malformed class JSON.")
     }
 }
--- crates/blockifier/src/state/papyrus_state_test.rs
+++ crates/blockifier/src/state/papyrus_state_test.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use indexmap::IndexMap;
 use papyrus_storage::state::{StateStorageReader, StateStorageWriter};
 use starknet_api::block::BlockNumber;
@@ -9,17 +11,18 @@ use starknet_api::transaction::Calldata;
 use starknet_api::{calldata, patricia_key, stark_felt};
 
 use crate::abi::abi_utils::selector_from_name;
+use crate::execution::contract_class::DeprecatedContractClassWithoutAbi;
 use crate::execution::entry_point::{CallEntryPoint, CallExecution, Retdata};
 use crate::retdata;
 use crate::state::cached_state::CachedState;
 use crate::state::papyrus_state::PapyrusStateReader;
 use crate::state::state_api::StateReader;
 use crate::test_utils::{
-    get_test_contract_class, trivial_external_entry_point, TEST_CLASS_HASH, TEST_CONTRACT_ADDRESS,
+    trivial_external_entry_point, TEST_CLASS_HASH, TEST_CONTRACT_ADDRESS, TEST_CONTRACT_PATH,
 };
 
 #[test]
-fn test_entry_point_with_papyrus_state() -> papyrus_storage::StorageResult<()> {
+fn test_entry_point_with_papyrus_state() {
     let (storage_reader, mut storage_writer) = papyrus_storage::test_utils::get_test_storage();
 
     // Initialize Storage: add test contract and class.
@@ -28,17 +31,23 @@ fn test_entry_point_with_papyrus_state() -> papyrus_storage::StorageResult<()> {
         ClassHash(stark_felt!(TEST_CLASS_HASH)),
     )]);
     let state_diff = StateDiff { deployed_contracts, ..Default::default() };
-    let deprecated_declared_classes = IndexMap::from([(
-        ClassHash(stark_felt!(TEST_CLASS_HASH)),
-        DeprecatedContractClass::from(get_test_contract_class()),
-    )]);
-    storage_writer
-        .begin_rw_txn()?
-        .append_state_diff(BlockNumber::default(), state_diff, deprecated_declared_classes)?
-        .commit()?;
 
-    let storage_tx = storage_reader.begin_ro_txn()?;
-    let state_reader = storage_tx.get_state_reader()?;
+    let test_contract_path = PathBuf::from(TEST_CONTRACT_PATH);
+    let test_contract = DeprecatedContractClass::from(
+        DeprecatedContractClassWithoutAbi::try_from(test_contract_path).unwrap(),
+    );
+    let deprecated_declared_classes =
+        IndexMap::from([(ClassHash(stark_felt!(TEST_CLASS_HASH)), test_contract)]);
+    storage_writer
+        .begin_rw_txn()
+        .unwrap()
+        .append_state_diff(BlockNumber::default(), state_diff, deprecated_declared_classes)
+        .unwrap()
+        .commit()
+        .unwrap();
+
+    let storage_tx = storage_reader.begin_ro_txn().unwrap();
+    let state_reader = storage_tx.get_state_reader().unwrap();
 
     // BlockNumber is 1 due to the initialization step above.
     let block_number = BlockNumber(1);
@@ -64,5 +73,4 @@ fn test_entry_point_with_papyrus_state() -> papyrus_storage::StorageResult<()> {
     let storage_key = StorageKey::try_from(key).unwrap();
     let value_from_state = state.get_storage_at(storage_address, storage_key).unwrap();
     assert_eq!(value_from_state, value);
-    Ok(())
 }
