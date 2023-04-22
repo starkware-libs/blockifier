8659735b5b28d5477b1692c48705d2b7dabef17c
Tarrence van As <tarrence@vanas.family>
2023-04-22 13:58:41 -0400

    Implement TryFrom<CasmContractClass> for ContractClass

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -235,6 +235,72 @@ impl TryFrom<CasmContractClass> for ContractClassV1 {
 
 // V0 utilities.
 
+pub fn casm_contract_into_contract_class(
+    casm: CasmContractClass,
+) -> Result<ContractClass, ProgramError> {
+    let mut entry_points_by_type = HashMap::new();
+    let mut external_entry_points = Vec::new();
+    let mut l1_handler_entry_points = Vec::new();
+    let mut constructor_entry_points = Vec::new();
+
+    for entry_point in casm.entry_points_by_type.external {
+        external_entry_points.push(entry_point.try_into().unwrap());
+    }
+
+    for entry_point in casm.entry_points_by_type.l1_handler {
+        l1_handler_entry_points.push(entry_point.try_into().unwrap());
+    }
+
+    for entry_point in casm.entry_points_by_type.constructor {
+        constructor_entry_points.push(entry_point.try_into().unwrap());
+    }
+
+    entry_points_by_type.insert(EntryPointType::External, external_entry_points);
+    entry_points_by_type.insert(EntryPointType::L1Handler, l1_handler_entry_points);
+    entry_points_by_type.insert(EntryPointType::Constructor, constructor_entry_points);
+
+    let hints = casm
+        .pythonic_hints
+        .unwrap()
+        .iter()
+        .map(|(hint_id, hint_codes)| {
+            (
+                *hint_id,
+                hint_codes
+                    .iter()
+                    .map(|code| HintParams {
+                        code: code.clone(),
+                        accessible_scopes: vec![],
+                        flow_tracking_data: FlowTrackingData {
+                            ap_tracking: ApTracking { group: 0, offset: 0 },
+                            reference_ids: HashMap::new(),
+                        },
+                    })
+                    .collect(),
+            )
+        })
+        .collect();
+
+    let program = Program::new(
+        Vec::new(),
+        casm.bytecode
+            .iter()
+            .map(|big_uint_as_hex| {
+                MaybeRelocatable::Int(Felt252::new(big_uint_as_hex.value.clone()))
+            })
+            .collect(),
+        None,
+        hints,
+        // Fill missing fields with empty values.
+        ReferenceManager { references: vec![] },
+        HashMap::new(),
+        Vec::new(),
+        None,
+    )?;
+
+    Ok(ContractClass(Arc::new(ContractClassInner { program, entry_points_by_type })))
+}
+
 /// Converts the program type from SN API into a Cairo VM-compatible type.
 pub fn deserialize_program<'de, D: Deserializer<'de>>(
     deserializer: D,
