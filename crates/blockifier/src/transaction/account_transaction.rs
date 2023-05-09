4e6f5d1ee90ed65e304996c33800d5117e26b056
Kari <evergreenkary@gmail.com>
2023-05-09 15:36:58 +0700

    appect declare transaction v2

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -118,13 +118,17 @@ impl AccountTransaction {
     fn verify_tx_version(&self, version: TransactionVersion) -> TransactionExecutionResult<()> {
         let allowed_versions: Vec<TransactionVersion> = match self {
             // Support `Declare` of version 0 in order to allow bootstrapping of a new system.
-            Self::Declare(_) | Self::Invoke(_) => {
+            Self::Invoke(_) => {
+                vec![TransactionVersion(StarkFelt::from(0)), TransactionVersion(StarkFelt::from(1))]
+            }
+            Self::Declare(_) => {
                 vec![
-                    TransactionVersion(StarkFelt::from(0_u8)),
-                    TransactionVersion(StarkFelt::from(1_u8)),
+                    TransactionVersion(StarkFelt::from(0)),
+                    TransactionVersion(StarkFelt::from(1)),
+                    TransactionVersion(StarkFelt::from(2)),
                 ]
             }
-            _ => vec![TransactionVersion(StarkFelt::from(1_u8))],
+            _ => vec![TransactionVersion(StarkFelt::from(1))],
         };
         if allowed_versions.contains(&version) {
             Ok(())
