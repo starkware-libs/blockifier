f8d7d63270d55e49847c7c561d06e62a6020841f
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2023-05-17 16:10:43 +0300

    Syscall tests: Libraray Call (#470)

--- crates/blockifier/src/execution/syscalls/syscalls_test.rs
+++ crates/blockifier/src/execution/syscalls/syscalls_test.rs
@@ -1,4 +1,5 @@
 use pretty_assertions::assert_eq;
+use starknet_api::core::ClassHash;
 use starknet_api::hash::StarkFelt;
 use starknet_api::state::StorageKey;
 use starknet_api::transaction::Calldata;
@@ -8,7 +9,7 @@ use crate::abi::abi_utils::selector_from_name;
 use crate::execution::entry_point::{CallEntryPoint, CallExecution, Retdata};
 use crate::retdata;
 use crate::state::state_api::StateReader;
-use crate::test_utils::{create_test_cairo1_state, trivial_external_entry_point};
+use crate::test_utils::{create_test_cairo1_state, trivial_external_entry_point, TEST_CLASS_HASH};
 
 #[test]
 fn test_storage_read_write() {
@@ -30,4 +31,27 @@ fn test_storage_read_write() {
     let value_from_state =
         state.get_storage_at(storage_address, StorageKey::try_from(key).unwrap()).unwrap();
     assert_eq!(value_from_state, value);
+}
+
+#[test]
+fn test_library_call() {
+    let mut state = create_test_cairo1_state();
+    let inner_entry_point_selector = selector_from_name("test_storage_read_write");
+    let calldata = calldata![
+        stark_felt!(TEST_CLASS_HASH), // Class hash.
+        inner_entry_point_selector.0, // Function selector.
+        stark_felt!(2),               // Calldata length.
+        stark_felt!(1234),            // Calldata: address.
+        stark_felt!(91)               // Calldata: value.
+    ];
+    let entry_point_call = CallEntryPoint {
+        entry_point_selector: selector_from_name("test_library_call"),
+        calldata,
+        class_hash: Some(ClassHash(stark_felt!(TEST_CLASS_HASH))),
+        ..trivial_external_entry_point()
+    };
+    assert_eq!(
+        entry_point_call.execute_directly(&mut state).unwrap().execution,
+        CallExecution::from_retdata(retdata![stark_felt!(1), stark_felt!(91)])
+    );
 }
