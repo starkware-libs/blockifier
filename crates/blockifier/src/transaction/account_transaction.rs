023a7e17c1ad65c646456ce0e9d981590869066d
Dori Medini <dori@starkware.co>
2023-05-21 15:39:20 +0300

    Separate validate-execute state changes
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -179,6 +179,23 @@ impl AccountTransaction {
         Ok(Some(validate_call_info))
     }
 
+    /// Handle nonce, check balance covers max fee, and (when applicable) run the `validate` phase.
+    /// Returns the `CallInfo` of the `validate` phase, if applicable.
+    fn process_validation_state<S: StateReader>(
+        &self,
+        state: &mut TransactionalState<'_, S>,
+        context: &mut ExecutionContext,
+    ) -> TransactionExecutionResult<Option<CallInfo>> {
+        Self::handle_nonce(&context.account_tx_context, state)?;
+
+        // TODO(Dori, 22/5/2023): Validate fee balance.
+
+        match &self {
+            Self::Declare(_) | Self::Invoke(_) => Ok(self.validate_tx(state, context)?),
+            Self::DeployAccount(_) => Ok(None),
+        }
+    }
+
     fn charge_fee(
         state: &mut dyn State,
         context: &mut ExecutionContext,
@@ -229,6 +246,18 @@ impl AccountTransaction {
 
         Ok(fee_transfer_call.execute(state, context)?)
     }
+
+    fn run_execute<S: State>(
+        &self,
+        state: &mut S,
+        context: &mut ExecutionContext,
+    ) -> TransactionExecutionResult<Option<CallInfo>> {
+        match &self {
+            Self::Declare(tx) => tx.run_execute(state, context),
+            Self::DeployAccount(tx) => tx.run_execute(state, context),
+            Self::Invoke(tx) => tx.run_execute(state, context),
+        }
+    }
 }
 
 impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
@@ -239,35 +268,18 @@ impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
     ) -> TransactionExecutionResult<TransactionExecutionInfo> {
         let account_tx_context = self.get_account_transaction_context();
         self.verify_tx_version(account_tx_context.version)?;
-        Self::handle_nonce(&account_tx_context, state)?;
+        let mut context = ExecutionContext::new(block_context.clone(), account_tx_context);
+
+        // Pre-process the nonce / fee check / `validate` state changes.
+        let early_validate_call_info = self.process_validation_state(state, &mut context)?;
 
         // Handle transaction-type specific execution.
-        let validate_call_info: Option<CallInfo>;
-        let execute_call_info: Option<CallInfo>;
-        let tx_type = self.tx_type();
-        let mut context = ExecutionContext::new(block_context.clone(), account_tx_context);
-        match &self {
-            Self::Declare(tx) => {
-                // Validate.
-                validate_call_info = self.validate_tx(state, &mut context)?;
-
-                // Execute.
-                execute_call_info = tx.run_execute(state, &mut context)?;
-            }
-            Self::DeployAccount(tx) => {
-                // Execute the constructor of the deployed class.
-                execute_call_info = tx.run_execute(state, &mut context)?;
-
-                // Validate.
-                validate_call_info = self.validate_tx(state, &mut context)?;
-            }
-            Self::Invoke(tx) => {
-                // Validate.
-                validate_call_info = self.validate_tx(state, &mut context)?;
-
-                // Execute.
-                execute_call_info = tx.run_execute(state, &mut context)?;
-            }
+        let execute_call_info = self.run_execute(state, &mut context)?;
+        let validate_call_info = if let Self::DeployAccount(_) = self {
+            // The `validate` phase in `DeployAccount` txs happens after execution.
+            self.validate_tx(state, &mut context)?
+        } else {
+            early_validate_call_info
         };
 
         // Handle fee.
@@ -278,7 +290,7 @@ impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
         let actual_resources = calculate_tx_resources(
             context.resources,
             &non_optional_call_infos,
-            tx_type,
+            self.tx_type(),
             state,
             None,
         )?;
