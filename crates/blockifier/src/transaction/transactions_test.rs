e19c613ae331239da40e4d0496f6285644f38b77
Dori Medini <dori@starkware.co>
2023-05-21 14:51:10 +0300

    Separate tests
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/transaction/transactions_test.rs
+++ crates/blockifier/src/transaction/transactions_test.rs
@@ -34,9 +34,9 @@ use crate::test_utils::{
     test_erc20_sequencer_balance_key, validate_tx_execution_info, DictStateReader,
     ACCOUNT_CONTRACT_PATH, BALANCE, ERC20_CONTRACT_PATH, MAX_FEE, TEST_ACCOUNT_CONTRACT_ADDRESS,
     TEST_ACCOUNT_CONTRACT_CLASS_HASH, TEST_CLASS_HASH, TEST_CONTRACT_ADDRESS, TEST_CONTRACT_PATH,
-    TEST_EMPTY_CONTRACT_CLASS_HASH, TEST_EMPTY_CONTRACT_PATH, TEST_ERC20_CONTRACT_ADDRESS,
-    TEST_ERC20_CONTRACT_CLASS_HASH, TEST_FAULTY_ACCOUNT_CONTRACT_ADDRESS,
-    TEST_FAULTY_ACCOUNT_CONTRACT_CLASS_HASH, TEST_FAULTY_ACCOUNT_CONTRACT_PATH,
+    TEST_EMPTY_CONTRACT_CLASS_HASH, TEST_EMPTY_CONTRACT_PATH, TEST_ERC20_CONTRACT_CLASS_HASH,
+    TEST_FAULTY_ACCOUNT_CONTRACT_ADDRESS, TEST_FAULTY_ACCOUNT_CONTRACT_CLASS_HASH,
+    TEST_FAULTY_ACCOUNT_CONTRACT_PATH,
 };
 use crate::transaction::account_transaction::AccountTransaction;
 use crate::transaction::constants;
@@ -351,40 +351,51 @@ fn test_invoke_tx() {
     );
 }
 
+fn assert_max_fee_exceeds_balance(
+    state: &mut CachedState<DictStateReader>,
+    block_context: &BlockContext,
+    invalid_tx: AccountTransaction,
+) {
+    let sent_max_fee = invalid_tx.max_fee();
+    let execution_error = invalid_tx.execute(state, block_context).unwrap_err();
+
+    // Test error.
+    assert_matches!(
+        execution_error,
+        TransactionExecutionError::MaxFeeExceedsBalance{ max_fee, .. }
+        if max_fee == sent_max_fee
+    );
+}
+
 #[test]
-fn test_state_get_fee_token_balance() {
+fn test_max_fee_exceeds_balance() {
     let state = &mut create_state_with_trivial_validation_account();
     let block_context = &BlockContext::create_for_account_testing();
-    let (mint_high, mint_low) = (stark_felt!(54_u8), stark_felt!(39_u8));
-    let recipient = stark_felt!(10_u8);
+    let invalid_max_fee = Fee(BALANCE + 1);
 
-    // Mint some tokens.
-    let entry_point_selector = selector_from_name("permissionedMint");
-    let execute_calldata = calldata![
-        stark_felt!(TEST_ERC20_CONTRACT_ADDRESS), // Contract address.
-        entry_point_selector.0,                   // EP selector.
-        stark_felt!(3_u8),                        // Calldata length.
-        recipient,
-        mint_low,
-        mint_high
-    ];
-    let mint_tx = crate::test_utils::invoke_tx(
-        execute_calldata,
-        ContractAddress(patricia_key!(TEST_ACCOUNT_CONTRACT_ADDRESS)),
-        Fee(MAX_FEE),
-        None,
-    );
-    AccountTransaction::Invoke(InvokeTransaction::V1(mint_tx))
-        .execute(state, block_context)
-        .unwrap();
+    // Invoke.
+    let invalid_tx = AccountTransaction::Invoke(InvokeTransaction::V1(InvokeTransactionV1 {
+        max_fee: invalid_max_fee,
+        ..invoke_tx()
+    }));
+    assert_max_fee_exceeds_balance(state, block_context, invalid_tx);
 
-    // Get balance from state, and validate.
-    let (low, high) = state
-        .get_fee_token_balance(block_context, &ContractAddress(patricia_key!(recipient)))
-        .unwrap();
+    // Deploy.
+    let invalid_tx = AccountTransaction::DeployAccount(DeployAccountTransaction {
+        max_fee: invalid_max_fee,
+        ..deploy_account_tx(TEST_ACCOUNT_CONTRACT_CLASS_HASH, None, None)
+    });
+    assert_max_fee_exceeds_balance(state, block_context, invalid_tx);
 
-    assert_eq!(low, mint_low);
-    assert_eq!(high, mint_high);
+    // Declare.
+    let invalid_tx = AccountTransaction::Declare(DeclareTransaction {
+        tx: starknet_api::transaction::DeclareTransaction::V1(DeclareTransactionV0V1 {
+            max_fee: invalid_max_fee,
+            ..declare_tx(TEST_EMPTY_CONTRACT_CLASS_HASH, TEST_ACCOUNT_CONTRACT_ADDRESS, None)
+        }),
+        contract_class: ContractClass::V0(get_contract_class_v0(TEST_EMPTY_CONTRACT_PATH)),
+    });
+    assert_max_fee_exceeds_balance(state, block_context, invalid_tx);
 }
 
 #[test]
@@ -405,21 +416,6 @@ fn test_negative_invoke_tx_flows() {
     assert_matches!(
         execution_error,
         TransactionExecutionError::FeeTransferError{ max_fee, .. }
-        if max_fee == invalid_max_fee
-    );
-
-    // Max fee greater than balance on account.
-    let invalid_max_fee = Fee(BALANCE as u128 + 1);
-    let invalid_tx = AccountTransaction::Invoke(InvokeTransaction::V1(InvokeTransactionV1 {
-        max_fee: invalid_max_fee,
-        ..valid_invoke_tx.clone()
-    }));
-    let execution_error = invalid_tx.execute(state, block_context).unwrap_err();
-
-    // Test error.
-    assert_matches!(
-        execution_error,
-        TransactionExecutionError::MaxFeeExceedsBalance{ max_fee, .. }
         if max_fee == invalid_max_fee
     );
 
