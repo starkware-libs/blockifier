fc0167b1e4aaeff96817ea5b7707f16d2f481e86
Dori Medini <dori@starkware.co>
2023-06-05 18:39:02 +0300

    Apply validate+nonce changes on execution failures
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -11,7 +11,7 @@ use crate::abi::abi_utils::selector_from_name;
 use crate::block_context::BlockContext;
 use crate::execution::entry_point::{CallEntryPoint, CallInfo, CallType, ExecutionContext};
 use crate::fee::fee_utils::calculate_tx_fee;
-use crate::state::cached_state::TransactionalState;
+use crate::state::cached_state::{CachedState, MutRefState, TransactionalState};
 use crate::state::state_api::{State, StateReader};
 use crate::transaction::constants;
 use crate::transaction::errors::TransactionExecutionError;
@@ -198,14 +198,12 @@ impl AccountTransaction {
         self.max_fee() != Fee(0)
     }
 
-    /// Handles nonce, checks balance covers max fee, and (when applicable) runs the validation
-    /// phase.
-    /// Returns the `CallInfo` of the validation phase, if applicable.
-    fn process_validation_state<S: StateReader>(
+    /// Handles nonce and checks that the account's balance covers max fee.
+    fn handle_nonce_and_check_fee_balance<S: StateReader>(
         &self,
         state: &mut TransactionalState<'_, S>,
         context: &mut ExecutionContext,
-    ) -> TransactionExecutionResult<Option<CallInfo>> {
+    ) -> TransactionExecutionResult<()> {
         // Handle nonce.
         Self::handle_nonce(&context.account_tx_context, state)?;
 
@@ -228,11 +226,7 @@ impl AccountTransaction {
             }
         }
 
-        // Validate transaction (if applicable).
-        match &self {
-            Self::Declare(_) | Self::Invoke(_) => self.validate_tx(state, context),
-            Self::DeployAccount(_) => Ok(None),
-        }
+        Ok(())
     }
 
     fn charge_fee(
@@ -297,6 +291,43 @@ impl AccountTransaction {
             Self::Invoke(tx) => tx.run_execute(state, context),
         }
     }
+
+    /// Runs validation and execution.
+    /// If an Ok() value is returned, either the transaction was successfully validated and
+    /// executed, or the transaction was reverted (not a `DeployAccount` transaction, was
+    /// successfully validated, and failed execution).
+    /// In both cases, if an Ok() value is returned, the `state` object contains the diff that
+    /// should be applied (does not contain execution state diff on revert).
+    /// Returns validate call info, execute call info, and optional revert error.
+    // TODO(Dori, 15/6/2023): Construct an execute call info object for reverted transactions.
+    fn run_or_revert<S: StateReader>(
+        &self,
+        state: &mut TransactionalState<'_, S>,
+        context: &mut ExecutionContext,
+    ) -> TransactionExecutionResult<(Option<CallInfo>, Option<CallInfo>, Option<String>)> {
+        // Nonce and fee check should be done before running user code.
+        self.handle_nonce_and_check_fee_balance(state, context)?;
+
+        // Handle `DeployAccount` transactions separately.
+        if matches!(self, Self::DeployAccount(_)) {
+            let execution_call_info = self.run_execute(state, context)?;
+            return Ok((self.validate_tx(state, context)?, execution_call_info, None));
+        }
+
+        // Run the validation, and if execution later fails, only keep the validation diff.
+        let validation_call_info = self.validate_tx(state, context)?;
+        let mut execution_state = CachedState::new(MutRefState::new(state));
+        match self.run_execute(&mut execution_state, context) {
+            Err(_) => {
+                execution_state.abort();
+                Ok((validation_call_info, None, Some(context.error_trace())))
+            }
+            Ok(execute_call_info) => {
+                execution_state.commit();
+                Ok((validation_call_info, execute_call_info, None))
+            }
+        }
+    }
 }
 
 impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
@@ -309,16 +340,8 @@ impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
         self.verify_tx_version(account_tx_context.version)?;
         let mut context = ExecutionContext::new(block_context.clone(), account_tx_context);
 
-        // Pre-process the nonce / fee check / validation state changes.
-        let early_validate_call_info = self.process_validation_state(state, &mut context)?;
-
-        // Handle transaction-type specific execution.
-        // The validation phase in a `DeployAccount` transaction happens after execution.
-        let execute_call_info = self.run_execute(state, &mut context)?;
-        let validate_call_info = match &self {
-            Self::DeployAccount(_) => self.validate_tx(state, &mut context)?,
-            Self::Declare(_) | Self::Invoke(_) => early_validate_call_info,
-        };
+        let (validate_call_info, execute_call_info, revert_error) =
+            self.run_or_revert(state, &mut context)?;
 
         // Handle fee.
         let non_optional_call_infos = vec![validate_call_info.as_ref(), execute_call_info.as_ref()]
@@ -345,7 +368,7 @@ impl<S: StateReader> ExecutableTransaction<S> for AccountTransaction {
             fee_transfer_call_info,
             actual_fee,
             actual_resources,
-            revert_error: None,
+            revert_error,
         };
         Ok(tx_execution_info)
     }
