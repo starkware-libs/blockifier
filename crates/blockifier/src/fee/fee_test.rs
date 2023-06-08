f883656acfd57969452f01b8f41c379cd3e6bef0
Alon Haramati <alonh@starkware.co>
2023-06-08 15:55:27 +0300

    Update cairo vm version.

--- crates/blockifier/src/fee/fee_test.rs
+++ crates/blockifier/src/fee/fee_test.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
 
 use assert_matches::assert_matches;
+use cairo_vm::vm::runners::builtin_runner::{
+    BITWISE_BUILTIN_NAME, HASH_BUILTIN_NAME, POSEIDON_BUILTIN_NAME, RANGE_CHECK_BUILTIN_NAME,
+    SIGNATURE_BUILTIN_NAME,
+};
 
+use crate::abi::constants;
 use crate::block_context::BlockContext;
 use crate::fee::fee_utils::calculate_l1_gas_by_vm_usage;
 use crate::transaction::errors::TransactionExecutionError;
@@ -9,12 +14,12 @@ use crate::transaction::objects::ResourcesMapping;
 
 fn get_vm_resource_usage() -> ResourcesMapping {
     ResourcesMapping(HashMap::from([
-        (String::from("n_steps"), 1800),
-        (String::from("pedersen"), 10),
-        (String::from("range_check"), 24),
-        (String::from("ecdsa"), 1),
-        (String::from("bitwise"), 1),
-        (String::from("poseidon"), 1),
+        (constants::N_STEPS_RESOURCE.to_string(), 1800),
+        (HASH_BUILTIN_NAME.to_string(), 10),
+        (RANGE_CHECK_BUILTIN_NAME.to_string(), 24),
+        (SIGNATURE_BUILTIN_NAME.to_string(), 1),
+        (BITWISE_BUILTIN_NAME.to_string(), 1),
+        (POSEIDON_BUILTIN_NAME.to_string(), 1),
     ]))
 }
 
@@ -25,7 +30,7 @@ fn test_calculate_l1_gas_by_vm_usage() {
 
     // Positive flow.
     // Verify calculation - in our case, n_steps is the heaviest resource.
-    let l1_gas_by_vm_usage = vm_resource_usage.0.get("n_steps").unwrap();
+    let l1_gas_by_vm_usage = vm_resource_usage.0.get(constants::N_STEPS_RESOURCE).unwrap();
     assert_eq!(
         *l1_gas_by_vm_usage as f64,
         calculate_l1_gas_by_vm_usage(&block_context, &vm_resource_usage).unwrap()
