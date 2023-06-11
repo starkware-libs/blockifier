ff70cd414159dbd6ac700e725286905f8fc01753
Dori Medini <dori@starkware.co>
2023-06-11 13:39:28 +0300

    Builtin name consistency test
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/fee/os_usage_test.rs
+++ crates/blockifier/src/fee/os_usage_test.rs
@@ -1,3 +1,10 @@
+use std::collections::HashSet;
+
+use cairo_vm::vm::runners::builtin_runner::{
+    BITWISE_BUILTIN_NAME, EC_OP_BUILTIN_NAME, HASH_BUILTIN_NAME, KECCAK_BUILTIN_NAME,
+    OUTPUT_BUILTIN_NAME, POSEIDON_BUILTIN_NAME, RANGE_CHECK_BUILTIN_NAME,
+    SEGMENT_ARENA_BUILTIN_NAME, SIGNATURE_BUILTIN_NAME,
+};
 use strum::IntoEnumIterator;
 
 use crate::execution::deprecated_syscalls::DeprecatedSyscallSelector;
@@ -11,5 +18,25 @@ fn test_resources_entries() {
     }
     for syscall_selector in DeprecatedSyscallSelector::iter() {
         assert!(OS_RESOURCES.execute_syscalls.get(&syscall_selector).is_some());
+    }
+}
+
+#[test]
+fn test_resource_name_consistency() {
+    let known_builtin_names: HashSet<&str> = HashSet::from([
+        OUTPUT_BUILTIN_NAME,
+        HASH_BUILTIN_NAME,
+        RANGE_CHECK_BUILTIN_NAME,
+        SIGNATURE_BUILTIN_NAME,
+        BITWISE_BUILTIN_NAME,
+        EC_OP_BUILTIN_NAME,
+        KECCAK_BUILTIN_NAME,
+        POSEIDON_BUILTIN_NAME,
+        SEGMENT_ARENA_BUILTIN_NAME,
+    ]);
+    for resources in OS_RESOURCES.execute_syscalls.values() {
+        for builtin_name in resources.builtin_instance_counter.keys() {
+            assert!(known_builtin_names.contains(builtin_name.as_str()));
+        }
     }
 }
