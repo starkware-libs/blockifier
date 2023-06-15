8a8a7a68c488a38bc937e9f9415133025329be9a
Elin Tulchinsky <elin@starkware.co>
2023-06-15 14:27:32 +0300

    Add a TODO to handle `n_steps` run limitation.

--- crates/blockifier/src/execution/cairo1_execution.rs
+++ crates/blockifier/src/execution/cairo1_execution.rs
@@ -220,7 +220,8 @@ pub fn run_entry_point(
     args: Args,
     program_segment_size: usize,
 ) -> Result<(), VirtualMachineExecutionError> {
-    let run_resources = &mut Some(hint_processor.context.vm_run_resources.clone());
+    // TODO(Dori,30/06/2023): propagate properly once VM allows it.
+    let run_resources = &mut None;
     let verify_secure = true;
     let args: Vec<&CairoArg> = args.iter().collect();
     runner.run_from_entrypoint(
@@ -232,8 +233,6 @@ pub fn run_entry_point(
         vm,
         hint_processor,
     )?;
-    hint_processor.context.vm_run_resources =
-        run_resources.clone().expect("Run resources should not be None");
 
     Ok(())
 }
