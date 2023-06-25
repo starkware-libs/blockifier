8faf78b83e338c10819227952630b4fe865aa8d7
Ilya Lesokhin <ilya@starkware.co>
2023-06-25 16:00:37 +0300

    Add secp256k1_new.

--- crates/blockifier/src/execution/syscalls/secp.rs
+++ crates/blockifier/src/execution/syscalls/secp.rs
@@ -1,13 +1,17 @@
-// Secp256k1 new syscall.
-
+use ark_secp256k1 as secp256k1;
 use cairo_vm::types::relocatable::Relocatable;
 use cairo_vm::vm::vm_core::VirtualMachine;
 use num_bigint::BigUint;
+use num_traits::Zero;
+use starknet_api::hash::StarkFelt;
 
 use crate::execution::execution_utils::{u256_from_ptr, write_maybe_relocatable};
+use crate::execution::syscalls::hint_processor::{SyscallHintProcessor, INVALID_ARGUMENT};
 use crate::execution::syscalls::{
-    SyscallRequest, SyscallResponse, SyscallResult, WriteResponseResult,
+    SyscallExecutionError, SyscallRequest, SyscallResponse, SyscallResult, WriteResponseResult,
 };
+
+// Secp256k1 new syscall.
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Secp256k1NewRequest {
@@ -25,13 +29,15 @@ impl SyscallRequest for Secp256k1NewRequest {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Secp256k1NewResponse {
-    // Some(id) if the point is on the curve, None otherwise.
+    // The syscall returns `Option<Secp256k1Point>` which is represented as two felts in Cairo0.
+    // The first felt is a indicates if it is `Some` (0) or `None` (1).
+    // The second felt is only valid if the first felt is `Some` and contains the id of the point.
+    // The id of the point is the index of the point in the `secp256k1_points` vector.
     pub optional_ec_point_id: Option<usize>,
 }
 
 impl SyscallResponse for Secp256k1NewResponse {
     fn write(self, vm: &mut VirtualMachine, ptr: &mut Relocatable) -> WriteResponseResult {
-        // The syscall returns `Option<Secp256k1Point>` which is represented as two felts in Cairo0.
         match self.optional_ec_point_id {
             Some(id) => {
                 write_maybe_relocatable(vm, ptr, 0)?;
@@ -44,4 +50,35 @@ impl SyscallResponse for Secp256k1NewResponse {
         };
         Ok(())
     }
+}
+
+pub fn secp256k1_new(
+    request: Secp256k1NewRequest,
+    _vm: &mut VirtualMachine,
+    syscall_handler: &mut SyscallHintProcessor<'_>,
+    _remaining_gas: &mut u64,
+) -> SyscallResult<Secp256k1NewResponse> {
+    let modulos = <secp256k1::Fq as ark_ff::PrimeField>::MODULUS.into();
+    let (x, y) = (request.x, request.y);
+    if x >= modulos || y >= modulos {
+        return Err(SyscallExecutionError::SyscallError {
+            error_data: vec![
+                StarkFelt::try_from(INVALID_ARGUMENT).map_err(SyscallExecutionError::from)?,
+            ],
+        });
+    }
+    let p = if x.is_zero() && y.is_zero() {
+        secp256k1::Affine::identity()
+    } else {
+        secp256k1::Affine::new_unchecked(x.into(), y.into())
+    };
+    let optional_ec_point_id = if p.is_on_curve() && p.is_in_correct_subgroup_assuming_on_curve() {
+        let points = &mut syscall_handler.secp256k1_points;
+        let id = points.len();
+        points.push(p);
+        Some(id)
+    } else {
+        None
+    };
+    Ok(Secp256k1NewResponse { optional_ec_point_id })
 }
