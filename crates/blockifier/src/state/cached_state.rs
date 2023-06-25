d1c300151ad4a2def56d5332c2a087bce2538ac8
giladchase <gilad@starkware.co>
2023-06-25 14:21:59 +0300

    Refactor: Add `TransactionalState` constructor (#653)
    * Constructor will be easier to work with in the upcoming global
      contract cache feature.
    * Reorder: CommitmentStateDiff moved down (importance).
    * Moved `TransactionalState` closer to its `impl` (it is a typedef but
      we treat it like a struct).

--- crates/blockifier/src/state/cached_state.rs
+++ crates/blockifier/src/state/cached_state.rs
@@ -16,19 +16,6 @@ use crate::utils::subtract_mappings;
 mod test;
 
 pub type ContractClassMapping = HashMap<ClassHash, ContractClass>;
-pub type TransactionalState<'a, S> = CachedState<MutRefState<'a, CachedState<S>>>;
-
-/// Holds uncommitted changes induced on StarkNet contracts.
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct CommitmentStateDiff {
-    // Contract instance attributes (per address).
-    pub address_to_class_hash: IndexMap<ContractAddress, ClassHash>,
-    pub address_to_nonce: IndexMap<ContractAddress, Nonce>,
-    pub storage_updates: IndexMap<ContractAddress, IndexMap<StorageKey, StarkFelt>>,
-
-    // Global attributes.
-    pub class_hash_to_compiled_class_hash: IndexMap<ClassHash, CompiledClassHash>,
-}
 
 /// Caches read and write requests.
 ///
@@ -45,6 +32,13 @@ pub struct CachedState<S: StateReader> {
 impl<S: StateReader> CachedState<S> {
     pub fn new(state: S) -> Self {
         Self { state, cache: StateCache::default(), class_hash_to_class: HashMap::default() }
+    }
+
+    /// Creates a transactional instance from the given cached state.
+    /// It allows performing buffered modifying actions on the given state, which
+    /// will either all happen (will be committed) or none of them (will be discarded).
+    pub fn create_transactional(state: &mut CachedState<S>) -> TransactionalState<'_, S> {
+        CachedState::new(MutRefState::new(state))
     }
 
     /// Returns the number of storage changes done through this state.
@@ -426,6 +420,8 @@ impl<'a, S: State + ?Sized> State for MutRefState<'a, S> {
     }
 }
 
+pub type TransactionalState<'a, S> = CachedState<MutRefState<'a, CachedState<S>>>;
+
 /// Adds the ability to perform a transactional execution.
 impl<'a, S: StateReader> TransactionalState<'a, S> {
     /// Commits changes in the child (wrapping) state to its parent.
@@ -442,4 +438,16 @@ impl<'a, S: StateReader> TransactionalState<'a, S> {
 
     /// Drops `self`.
     pub fn abort(self) {}
+}
+
+/// Holds uncommitted changes induced on StarkNet contracts.
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CommitmentStateDiff {
+    // Contract instance attributes (per address).
+    pub address_to_class_hash: IndexMap<ContractAddress, ClassHash>,
+    pub address_to_nonce: IndexMap<ContractAddress, Nonce>,
+    pub storage_updates: IndexMap<ContractAddress, IndexMap<StorageKey, StarkFelt>>,
+
+    // Global attributes.
+    pub class_hash_to_compiled_class_hash: IndexMap<ClassHash, CompiledClassHash>,
 }
