b34cd116e792baeff2f32c3be31302a4c8d52a92
Gilad Chase <gilad@starkware.co>
2023-06-29 13:42:08 +0300

    Add transaction::test_utils to `testing`

--- crates/blockifier/src/transaction.rs
+++ crates/blockifier/src/transaction.rs
@@ -2,6 +2,7 @@ pub mod account_transaction;
 pub mod constants;
 pub mod errors;
 pub mod objects;
+#[cfg(any(feature = "testing", test))]
 pub mod test_utils;
 pub mod transaction_execution;
 pub mod transaction_types;
