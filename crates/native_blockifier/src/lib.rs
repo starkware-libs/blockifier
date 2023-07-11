1750ea289c023ad8a7cea5999fdfb99284d5be69
Gilad Chase <gilad@starkware.co>
2023-07-11 16:24:28 +0300

    Refactor `PyBlockExecutor` to Manage Block-Level Information and Access to storage and execution
    This commit refactors the `PyBlockExecutor` to manage all block-level information and control access and initialization/deallocation of both Storage and `TransactionExecutor`. These components are now encapsulated within `PyBlockExecutor` and hidden from Python.
    
    Key changes in py_block_executor.rs include:
    
    - `PyBlockExecutor` now handles the storage and tx_executor APIs, delegating to each respective module. These modules are planned to be submodules of block_executor in future iterations.
    - `Storage` is instantiated when the block executor is created and deallocated when the block executor is deallocated.
    - `TransactionExecutor` is initialized via `start_block`, which should be invoked when a new block is ready for execution. Conversely, `end_block` is called to deallocate the transaction executor once execution is finalized.
    - The `PyBlockInfo`, which is returned from `finalize` back to Python, is the same one that was passed through in `start_block`. This change is due to the Python blockifier instance now being a singleton, rather than being created for each block.
    - Moved `PyGeneralConfig` and `PyOsConfig` from the transaction executor
      into the block executor, since it is now in charge of them.
    
    Change in storage.rs:
    - The storage is now a pure Rust struct instead of a `pyclass`, as it is encapsulated within `BlockExecutor`.
    - The `validate_aligned` function was imported into Python, as Python no longer has direct access to papyrus storage.
    
    Changes in `py_transaction_executor.rs`-> `transaction_executor.rs`:
    - The transaction executor is no longer a pyclass and needs to return `PyBlockInfo` from `finalize`.
    - `Py{Os,General}Config` have been moved into the block executor.
    - `PyBlockInfo` is now used instead of `PyAny`.

--- crates/native_blockifier/src/lib.rs
+++ crates/native_blockifier/src/lib.rs
@@ -1,22 +1,24 @@
 pub mod errors;
 pub mod papyrus_state;
+pub mod py_block_executor;
 pub mod py_state_diff;
 #[cfg(any(feature = "testing", test))]
 pub mod py_test_utils;
 pub mod py_transaction;
 pub mod py_transaction_execution_info;
-pub mod py_transaction_executor;
 pub mod py_utils;
 pub mod storage;
+pub mod transaction_executor;
 
 use errors::add_py_exceptions;
+use py_block_executor::PyBlockExecutor;
+use py_state_diff::PyBlockInfo;
 use py_transaction_execution_info::{
     PyCallInfo, PyOrderedEvent, PyOrderedL2ToL1Message, PyTransactionExecutionInfo,
     PyVmExecutionResources,
 };
-use py_transaction_executor::PyTransactionExecutor;
 use pyo3::prelude::*;
-use storage::{Storage, StorageConfig};
+use storage::StorageConfig;
 
 use crate::py_state_diff::PyStateDiff;
 use crate::py_utils::raise_error_for_testing;
@@ -27,14 +29,14 @@ fn native_blockifier(py: Python<'_>, py_module: &PyModule) -> PyResult<()> {
     // Usage: just create a Python logger as usual, and it'll capture Rust prints.
     pyo3_log::init();
 
+    py_module.add_class::<PyBlockExecutor>()?;
+    py_module.add_class::<PyBlockInfo>()?;
     py_module.add_class::<PyCallInfo>()?;
     py_module.add_class::<PyOrderedEvent>()?;
     py_module.add_class::<PyOrderedL2ToL1Message>()?;
     py_module.add_class::<PyStateDiff>()?;
     py_module.add_class::<PyTransactionExecutionInfo>()?;
-    py_module.add_class::<PyTransactionExecutor>()?;
     py_module.add_class::<PyVmExecutionResources>()?;
-    py_module.add_class::<Storage>()?;
     py_module.add_class::<StorageConfig>()?;
     add_py_exceptions(py, py_module)?;
 
