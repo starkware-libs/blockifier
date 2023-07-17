f234d2c60911f425809abe40e4a56245a7afc915
Alon Haramati <91828241+alonh5@users.noreply.github.com>
2023-07-17 12:01:14 +0300

    Revert "Update SN API version. (#732)" (#748)
    This reverts commit afb6adac498df6ea99d3724384775ac6c885c977.
    
    Besides unrelated `args` fix in `PyTransactionExecutor`.

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -2,7 +2,7 @@ use std::cmp::min;
 
 use itertools::concat;
 use starknet_api::calldata;
-use starknet_api::core::{ContractAddress, EntryPointSelector, Nonce};
+use starknet_api::core::{ContractAddress, EntryPointSelector};
 use starknet_api::deprecated_contract_class::EntryPointType;
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{
@@ -156,14 +156,8 @@ impl AccountTransaction {
                     InvokeTransaction::V1(_) => TransactionVersion(StarkFelt::from(1_u8)),
                 },
                 signature: tx.signature(),
-                nonce: match tx {
-                    InvokeTransaction::V0(_) => Nonce::default(),
-                    InvokeTransaction::V1(tx_v1) => tx_v1.nonce,
-                },
-                sender_address: match tx {
-                    InvokeTransaction::V0(tx_v0) => tx_v0.contract_address,
-                    InvokeTransaction::V1(tx_v1) => tx_v1.sender_address,
-                },
+                nonce: tx.nonce(),
+                sender_address: tx.sender_address(),
             },
         }
     }
