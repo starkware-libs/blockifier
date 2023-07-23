f4db548e991fa59240e5d25805e7f8304009a19d
Gilad Chase <gilad@starkware.co>
2023-07-23 12:43:04 +0200

    Revert "Refactor `PapyrusReader` and `py_transaction_executor.py` to remove unnecessary RO tx storage (#758)"
    This reverts commit 34536f183f5b066cb929ef18eec33c9b07a4e4b6.

--- crates/native_blockifier/src/papyrus_state_test.rs
+++ crates/native_blockifier/src/papyrus_state_test.rs
@@ -8,7 +8,7 @@ use blockifier::test_utils::{
     TEST_CONTRACT_ADDRESS, TEST_CONTRACT_CAIRO0_PATH,
 };
 use indexmap::IndexMap;
-use papyrus_storage::state::StateStorageWriter;
+use papyrus_storage::state::{StateStorageReader, StateStorageWriter};
 use starknet_api::block::BlockNumber;
 use starknet_api::core::{ClassHash, ContractAddress, PatriciaKey};
 use starknet_api::hash::{StarkFelt, StarkHash};
@@ -16,7 +16,7 @@ use starknet_api::state::{StateDiff, StorageKey};
 use starknet_api::transaction::Calldata;
 use starknet_api::{calldata, patricia_key, stark_felt};
 
-use crate::papyrus_state::PapyrusReader;
+use crate::papyrus_state::{PapyrusReader, PapyrusStateReader};
 
 #[test]
 fn test_entry_point_with_papyrus_state() -> papyrus_storage::StorageResult<()> {
@@ -37,9 +37,13 @@ fn test_entry_point_with_papyrus_state() -> papyrus_storage::StorageResult<()> {
         .append_state_diff(BlockNumber::default(), state_diff, deprecated_declared_classes)?
         .commit()?;
 
+    let storage_tx = storage_reader.begin_ro_txn()?;
+    let state_reader = storage_tx.get_state_reader()?;
+
     // BlockNumber is 1 due to the initialization step above.
     let block_number = BlockNumber(1);
-    let papyrus_reader = PapyrusReader::new(storage_reader, block_number);
+    let state_reader = PapyrusStateReader::new(state_reader, block_number);
+    let papyrus_reader = PapyrusReader::new(&storage_tx, state_reader);
     let mut state = CachedState::new(papyrus_reader);
 
     // Call entrypoint that want to write to storage, which updates the cached state's write cache.
