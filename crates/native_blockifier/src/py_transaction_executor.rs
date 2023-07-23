18f399c77f1858cc8663233c58d1db7c472ceb14
giladchase <gilad@starkware.co>
2023-07-23 20:21:05 +0200

    Refactor `PapyrusReader` and `py_transaction_executor.py` to remove unnecessary RO tx storage (#773)
    This commit simplifies the handling of read-only transactions (RO tx) in the `PapyrusReader`
    and `py_transaction_executor.py` modules.
    The previous design saved an open RO tx in the instance, which led to unnecessary
    complexity, including the need for a lifetime/ouroboros pattern.
    This commit modifies the system to open a new RO tx on demand, which is a more efficient and simpler approach.
    
    Key changes:
    
    - The `PapyrusReader` has been refactored to generate the required transactions on demand.
    This removes the need to store an open RO tx.
    
    - The `py_transaction_executor.py` module has been significantly simplified.
    The changes primarily involve the removal of the ouroboros pattern, which required
    certain fields and complex constructor methods.
    With the removal of stored RO tx, we can replace the previous elaborate
    constructor with a regular one and substitute the 'with_X' accessors with standard accessors.
    
    Please note: The significant changes seen in the 'execute' diff mainly
    pertain to the removal of the 'with_mut' closure and other 'with_X' accessors.
    No other modifications have been made to this section.

--- crates/native_blockifier/src/py_transaction_executor.rs
+++ crates/native_blockifier/src/py_transaction_executor.rs
@@ -8,15 +8,12 @@ use blockifier::state::state_api::{State, StateReader};
 use blockifier::transaction::transaction_execution::Transaction;
 use blockifier::transaction::transactions::ExecutableTransaction;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources as VmExecutionResources;
-use ouroboros;
-use papyrus_storage::db::RO;
-use papyrus_storage::state::StateStorageReader;
 use pyo3::prelude::*;
 use starknet_api::block::{BlockHash, BlockNumber, BlockTimestamp};
 use starknet_api::core::{ChainId, ClassHash, ContractAddress};
 
 use crate::errors::{NativeBlockifierError, NativeBlockifierResult};
-use crate::papyrus_state::{PapyrusReader, PapyrusStateReader};
+use crate::papyrus_state::PapyrusReader;
 use crate::py_state_diff::PyStateDiff;
 use crate::py_transaction::py_tx;
 use crate::py_transaction_execution_info::{PyTransactionExecutionInfo, PyVmExecutionResources};
@@ -41,7 +38,7 @@ impl PyTransactionExecutor {
         max_recursion_depth: usize,
     ) -> NativeBlockifierResult<Self> {
         log::debug!("Initializing Transaction Executor...");
-        let executor = TransactionExecutor::create(
+        let executor = TransactionExecutor::new(
             papyrus_storage,
             general_config,
             block_info,
@@ -90,9 +87,6 @@ impl PyTransactionExecutor {
     }
 }
 
-// To access a field you must use `self.borrow_{field_name}()`.
-// Alternately, you can borrow the whole object using `self.with[_mut]()`.
-#[ouroboros::self_referencing]
 pub struct TransactionExecutor {
     pub block_context: BlockContext,
 
@@ -100,18 +94,11 @@ pub struct TransactionExecutor {
     pub executed_class_hashes: HashSet<ClassHash>,
 
     // State-related fields.
-    // Storage reader and transaction are kept merely for lifetime parameter referencing.
-    pub storage_reader: papyrus_storage::StorageReader,
-    #[borrows(storage_reader)]
-    #[covariant]
-    pub storage_tx: papyrus_storage::StorageTxn<'this, RO>,
-    #[borrows(storage_tx)]
-    #[covariant]
-    pub state: CachedState<PapyrusReader<'this>>,
+    pub state: CachedState<PapyrusReader>,
 }
 
 impl TransactionExecutor {
-    pub fn create(
+    pub fn new(
         papyrus_storage: &Storage,
         general_config: PyGeneralConfig,
         block_info: &PyAny,
@@ -121,7 +108,9 @@ impl TransactionExecutor {
         let reader = papyrus_storage.reader().clone();
 
         let block_context = py_block_context(general_config, block_info, max_recursion_depth)?;
-        build_tx_executor(block_context, reader)
+        let state = CachedState::new(PapyrusReader::new(reader, block_context.block_number));
+        let executed_class_hashes = HashSet::<ClassHash>::new();
+        Ok(Self { block_context, executed_class_hashes, state })
     }
 
     /// Executes the given transaction on the state maintained by the executor.
@@ -138,72 +127,68 @@ impl TransactionExecutor {
         let tx: Transaction = py_tx(&tx_type, tx, raw_contract_class)?;
 
         let mut tx_executed_class_hashes = HashSet::<ClassHash>::new();
-        self.with_mut(|executor| {
-            let mut transactional_state = CachedState::create_transactional(executor.state);
-            let tx_execution_result = tx
-                .execute_raw(&mut transactional_state, executor.block_context, true)
-                .map_err(NativeBlockifierError::from);
-            let (py_tx_execution_info, py_casm_hash_calculation_resources) =
-                match tx_execution_result {
-                    Ok(tx_execution_info) => {
-                        tx_executed_class_hashes = tx_execution_info.get_executed_class_hashes();
+        let mut transactional_state = CachedState::create_transactional(&mut self.state);
+        let tx_execution_result = tx
+            .execute_raw(&mut transactional_state, &self.block_context, true)
+            .map_err(NativeBlockifierError::from);
+        let (py_tx_execution_info, py_casm_hash_calculation_resources) = match tx_execution_result {
+            Ok(tx_execution_info) => {
+                tx_executed_class_hashes.extend(tx_execution_info.get_executed_class_hashes());
 
-                        let py_tx_execution_info = Python::with_gil(|py| {
-                            // Allocate this instance on the Python heap.
-                            // This is necessary in order to pass a reference to it to the callback
-                            // (otherwise, if it were allocated on Rust's heap/stack, giving Python
-                            // a reference to the objects will not
-                            // work).
-                            Py::new(py, PyTransactionExecutionInfo::from(tx_execution_info))
-                                .expect("Should be able to allocate on Python heap")
-                        });
+                let py_tx_execution_info = Python::with_gil(|py| {
+                    // Allocate this instance on the Python heap.
+                    // This is necessary in order to pass a reference to it to the callback
+                    // (otherwise, if it were allocated on Rust's heap/stack, giving Python
+                    // a reference to the objects will not
+                    // work).
+                    Py::new(py, PyTransactionExecutionInfo::from(tx_execution_info))
+                        .expect("Should be able to allocate on Python heap")
+                });
 
-                        let py_casm_hash_calculation_resources =
-                            get_casm_hash_calculation_resources(
-                                &mut transactional_state,
-                                executor.executed_class_hashes,
-                                &tx_executed_class_hashes,
-                            )?;
+                let py_casm_hash_calculation_resources = get_casm_hash_calculation_resources(
+                    &mut transactional_state,
+                    &self.executed_class_hashes,
+                    &tx_executed_class_hashes,
+                )?;
 
-                        (py_tx_execution_info, py_casm_hash_calculation_resources)
-                    }
-                    Err(error) => {
-                        transactional_state.abort();
-                        return Err(error);
-                    }
-                };
-
-            let has_enough_room_for_tx = Python::with_gil(|py| {
-                // Can be done because `py_tx_execution_info` is a `Py<PyTransactionExecutionInfo>`,
-                // hence is allocated on the Python heap.
-                let args =
-                    (py_tx_execution_info.borrow(py), py_casm_hash_calculation_resources.clone());
-                enough_room_for_tx.call1(args) // Callback to Python code.
-            });
-
-            match has_enough_room_for_tx {
-                Ok(_) => {
-                    transactional_state.commit();
-                    executor.executed_class_hashes.extend(&tx_executed_class_hashes);
-                    Ok((py_tx_execution_info, py_casm_hash_calculation_resources))
-                }
-                // Unexpected error, abort and let caller know.
-                Err(error) if unexpected_callback_error(&error) => {
-                    transactional_state.abort();
-                    Err(error.into())
-                }
-                // Not enough room in batch, abort and let caller verify on its own.
-                Err(_not_enough_weight_error) => {
-                    transactional_state.abort();
-                    Ok((py_tx_execution_info, py_casm_hash_calculation_resources))
-                }
+                (py_tx_execution_info, py_casm_hash_calculation_resources)
             }
-        })
+            Err(error) => {
+                transactional_state.abort();
+                return Err(error);
+            }
+        };
+
+        let has_enough_room_for_tx = Python::with_gil(|py| {
+            // Can be done because `py_tx_execution_info` is a `Py<PyTransactionExecutionInfo>`,
+            // hence is allocated on the Python heap.
+            let args =
+                (py_tx_execution_info.borrow(py), py_casm_hash_calculation_resources.clone());
+            enough_room_for_tx.call1(args) // Callback to Python code.
+        });
+
+        match has_enough_room_for_tx {
+            Ok(_) => {
+                transactional_state.commit();
+                self.executed_class_hashes.extend(&tx_executed_class_hashes);
+                Ok((py_tx_execution_info, py_casm_hash_calculation_resources))
+            }
+            // Unexpected error, abort and let caller know.
+            Err(error) if unexpected_callback_error(&error) => {
+                transactional_state.abort();
+                Err(error.into())
+            }
+            // Not enough room in batch, abort and let caller verify on its own.
+            Err(_not_enough_weight_error) => {
+                transactional_state.abort();
+                Ok((py_tx_execution_info, py_casm_hash_calculation_resources))
+            }
+        }
     }
 
     /// Returns the state diff resulting in executing transactions.
     pub fn finalize(&mut self) -> PyStateDiff {
-        PyStateDiff::from(self.borrow_state().to_state_diff())
+        PyStateDiff::from(self.state.to_state_diff())
     }
 
     // Block pre-processing; see `block_execution::pre_process_block` documentation.
@@ -214,9 +199,7 @@ impl TransactionExecutor {
         let old_block_number_and_hash = old_block_number_and_hash
             .map(|(block_number, block_hash)| (BlockNumber(block_number), BlockHash(block_hash.0)));
 
-        self.with_mut(|executor| {
-            pre_process_block(executor.state, old_block_number_and_hash);
-        });
+        pre_process_block(&mut self.state, old_block_number_and_hash);
 
         Ok(())
     }
@@ -281,40 +264,6 @@ pub fn py_block_context(
     Ok(block_context)
 }
 
-pub fn build_tx_executor(
-    block_context: BlockContext,
-    storage_reader: papyrus_storage::StorageReader,
-) -> NativeBlockifierResult<TransactionExecutor> {
-    // The following callbacks are required to capture the local lifetime parameter.
-    fn storage_tx_builder(
-        storage_reader: &papyrus_storage::StorageReader,
-    ) -> NativeBlockifierResult<papyrus_storage::StorageTxn<'_, RO>> {
-        Ok(storage_reader.begin_ro_txn()?)
-    }
-
-    fn state_builder<'a>(
-        storage_tx: &'a papyrus_storage::StorageTxn<'a, RO>,
-        block_number: BlockNumber,
-    ) -> NativeBlockifierResult<CachedState<PapyrusReader<'a>>> {
-        let state_reader = storage_tx.get_state_reader()?;
-        let state_reader = PapyrusStateReader::new(state_reader, block_number);
-        let papyrus_reader = PapyrusReader::new(storage_tx, state_reader);
-        Ok(CachedState::new(papyrus_reader))
-    }
-
-    let executed_class_hashes = HashSet::<ClassHash>::new();
-    let block_number = block_context.block_number;
-    // The builder struct below is implicitly created by `ouroboros`.
-    let py_tx_executor_builder = TransactionExecutorTryBuilder {
-        block_context,
-        executed_class_hashes,
-        storage_reader,
-        storage_tx_builder,
-        state_builder: |storage_tx| state_builder(storage_tx, block_number),
-    };
-    py_tx_executor_builder.try_build()
-}
-
 fn unexpected_callback_error(error: &PyErr) -> bool {
     let error_string = error.to_string();
     !(error_string.contains("BatchFull") || error_string.contains("TransactionBiggerThanBatch"))
@@ -323,7 +272,7 @@ fn unexpected_callback_error(error: &PyErr) -> bool {
 /// Returns the estimated VM resources for Casm hash calculation (done by the OS), of the newly
 /// executed classes by the current transaction.
 pub fn get_casm_hash_calculation_resources(
-    state: &mut TransactionalState<'_, PapyrusReader<'_>>,
+    state: &mut TransactionalState<'_, PapyrusReader>,
     executed_class_hashes: &HashSet<ClassHash>,
     tx_executed_class_hashes: &HashSet<ClassHash>,
 ) -> NativeBlockifierResult<PyVmExecutionResources> {
