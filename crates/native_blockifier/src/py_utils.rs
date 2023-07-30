d8083bdd8a8cc2937760438b18bda4f916a30db9
Gilad Chase <gilad@starkware.co>
2023-07-30 18:54:27 +0300

    Make pyo3 testable, add tests and Defaults to `native_blockifier`
    - Make pyo3's' `extension-module` feature optional. This is recommended
      in the PYO3 docs (added a reference to it in `native_blockifier`'s
      Cargo.toml)
    - Add `py_block_executor_test.rs`, and add some `Default` to help
      testing.

--- crates/native_blockifier/src/py_utils.rs
+++ crates/native_blockifier/src/py_utils.rs
@@ -10,7 +10,7 @@ use starknet_api::transaction::EthAddress;
 
 use crate::errors::NativeBlockifierResult;
 
-#[derive(Eq, FromPyObject, Hash, PartialEq, Clone, Copy)]
+#[derive(Clone, Copy, Default, Eq, FromPyObject, Hash, PartialEq)]
 pub struct PyFelt(#[pyo3(from_py_with = "int_to_stark_felt")] pub StarkFelt);
 
 impl IntoPy<PyObject> for PyFelt {
