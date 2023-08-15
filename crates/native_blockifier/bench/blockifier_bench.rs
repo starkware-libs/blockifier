04f7427ca64b16d75a0d7535a91f5bc40a24a22d
Gilad Chase <gilad@starkware.co>
2023-08-15 11:11:34 +0300

    Fix Conflicts

--- crates/native_blockifier/bench/blockifier_bench.rs
+++ crates/native_blockifier/bench/blockifier_bench.rs
@@ -1,4 +1,11 @@
-//! Benchmark for the blockifier crate. Run using `cargo bench --bench blockifier_bench`.
+//! Benchmark module for the blockifier crate. It provides functionalities to benchmark
+//! various aspects related to transferring between accounts, including preparation
+//! and execution of transfers.
+//!
+//! The main benchmark function is `transfers_benchmark`, which measures the performance
+//! of transfers between randomly created accounts, which are iterated over round-robin.
+//!
+//! Run the benchmarks using `cargo bench --bench blockifier_bench`.
 
 use std::collections::HashMap;
 
@@ -13,12 +20,13 @@ use blockifier::test_utils::{
     TEST_ACCOUNT_CONTRACT_CLASS_HASH, TEST_ERC20_CONTRACT_CLASS_HASH,
 };
 use blockifier::transaction::account_transaction::AccountTransaction;
-use blockifier::transaction::transactions::ExecutableTransaction;
+use blockifier::transaction::transactions::{ExecutableTransaction, InvokeTransaction};
 use criterion::{criterion_group, criterion_main, Criterion};
 use starknet_api::core::{ClassHash, ContractAddress, Nonce};
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{
-    Calldata, ContractAddressSalt, Fee, InvokeTransaction, InvokeTransactionV1,
+    Calldata, ContractAddressSalt, Fee, InvokeTransaction as StarknetInvokeTransaction,
+    InvokeTransactionV1, TransactionHash,
 };
 use starknet_api::{calldata, stark_felt};
 
@@ -34,11 +42,11 @@ fn create_state() -> CachedState<DictStateReader> {
         (test_account_class_hash, ContractClassV0::from_file(ACCOUNT_CONTRACT_CAIRO0_PATH).into()),
         (test_erc20_class_hash, ContractClassV0::from_file(ERC20_CONTRACT_PATH).into()),
     ]);
-    // Deploy the erc20 contract.
-    let test_erc20_address = block_context.fee_token_address;
+    // Deploy the ERC20 contract.
+    let test_erc20_address = block_context.deprecated_fee_token_address;
     let address_to_class_hash = HashMap::from([(test_erc20_address, test_erc20_class_hash)]);
 
-    CachedState::new(DictStateReader {
+    CachedState::from(DictStateReader {
         address_to_class_hash,
         class_hash_to_class,
         ..Default::default()
@@ -51,8 +59,8 @@ pub fn transfers_benchmark(c: &mut Criterion) {
     let (accounts, mut nonces) = prepare_accounts(&mut state, block_context);
 
     let mut sender_account = 0;
-    c.bench_function("transfers", |b| {
-        b.iter(|| {
+    c.bench_function("transfers", |benchmark| {
+        benchmark.iter(|| {
             do_transfer(sender_account, &accounts, &mut nonces, block_context, &mut state);
             sender_account = (sender_account + 1) % accounts.len();
         })
@@ -76,20 +84,26 @@ fn do_transfer(
     let entry_point_selector =
         selector_from_name(blockifier::transaction::constants::TRANSFER_ENTRY_POINT_NAME);
     let execute_calldata = calldata![
-        *block_context.fee_token_address.0.key(), // Contract address.
-        entry_point_selector.0,                   // EP selector.
-        stark_felt!(3_u8),                        // Calldata length.
-        *recipient_account_address.0.key(),       // Calldata: recipient.
-        stark_felt!(1_u8),                        // Calldata: lsb amount.
-        stark_felt!(0_u8)                         // Calldata: msb amount.
+        *block_context.deprecated_fee_token_address.0.key(), // Contract address.
+        entry_point_selector.0,                              // EP selector.
+        stark_felt!(3_u8),                                   // Calldata length.
+        *recipient_account_address.0.key(),                  // Calldata: recipient.
+        stark_felt!(1_u8),                                   // Calldata: lsb amount.
+        stark_felt!(0_u8)                                    // Calldata: msb amount.
     ];
 
     let tx = invoke_tx(execute_calldata, sender_account_address, Fee(MAX_FEE), None);
-    let account_tx = AccountTransaction::Invoke(InvokeTransaction::V1(InvokeTransactionV1 {
+    let sn_api_tx = StarknetInvokeTransaction::V1(InvokeTransactionV1 {
         nonce: Nonce(stark_felt!(nonce)),
         ..tx
-    }));
-    account_tx.execute(state, block_context, false).unwrap();
+    });
+    let account_tx = AccountTransaction::Invoke(InvokeTransaction {
+        tx: sn_api_tx,
+        tx_hash: TransactionHash::default(),
+    });
+    let charge_fee = false;
+    let validate = false;
+    account_tx.execute(state, block_context, charge_fee, validate).unwrap();
 }
 
 fn prepare_accounts(
@@ -99,15 +113,21 @@ fn prepare_accounts(
     // Prepare accounts.
     let mut addresses = vec![];
     let mut nonces = vec![];
-    for i in 0..N_ACCOUNTS {
+    for account_salt in 0..N_ACCOUNTS {
         // Deploy an account contract.
+        let class_hash = TEST_ACCOUNT_CONTRACT_CLASS_HASH;
+        let max_fee = Fee(MAX_FEE);
+        let constructor_calldata = None;
+        let constructor_address_salt = ContractAddressSalt(stark_felt!(account_salt as u64));
+        let signature = None;
+        let nonce_manager = &mut NonceManager::default();
         let deploy_account_tx = deploy_account_tx_with_salt(
-            TEST_ACCOUNT_CONTRACT_CLASS_HASH,
-            Fee(MAX_FEE),
-            None,
-            ContractAddressSalt(stark_felt!(i as u64)),
-            None,
-            &mut NonceManager::default(),
+            class_hash,
+            max_fee,
+            constructor_calldata,
+            constructor_address_salt,
+            signature,
+            nonce_manager,
         );
 
         // Update the balance of the account.
@@ -118,14 +138,17 @@ fn prepare_accounts(
             get_storage_var_address("ERC20_balances", &[*deployed_account_address.0.key()])
                 .unwrap();
         state.set_storage_at(
-            block_context.fee_token_address,
+            block_context.deprecated_fee_token_address,
             deployed_account_balance_key,
             stark_felt!(BALANCE * 1000),
         );
 
         let account_tx = AccountTransaction::DeployAccount(deploy_account_tx);
-        account_tx.execute(state, block_context, false).unwrap();
+        let charge_fee = false;
+        let validate = false;
+        account_tx.execute(state, block_context, charge_fee, validate).unwrap();
     }
+
     (addresses, nonces)
 }
 
