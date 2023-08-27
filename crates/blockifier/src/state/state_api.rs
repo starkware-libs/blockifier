25bcb106aaac67330d290563ea7fc5088d770291
Barak <barakb@starkware.co>
2023-08-27 19:24:51 +0300

    Remove get_uint256_storage_var_addresses and get_erc20_balance_var_addresses from abi_utils and use SierraU256 instead.

--- crates/blockifier/src/state/state_api.rs
+++ crates/blockifier/src/state/state_api.rs
@@ -2,7 +2,8 @@ use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
 use starknet_api::hash::StarkFelt;
 use starknet_api::state::StorageKey;
 
-use crate::abi::abi_utils::get_erc20_balance_var_addresses;
+use crate::abi::abi_utils::get_erc20_balance_var_address;
+use crate::abi::sierra_types::next_storage_key;
 use crate::block_context::BlockContext;
 use crate::execution::contract_class::ContractClass;
 use crate::state::cached_state::CommitmentStateDiff;
@@ -55,7 +56,8 @@ pub trait StateReader {
         block_context: &BlockContext,
         contract_address: &ContractAddress,
     ) -> Result<(StarkFelt, StarkFelt), StateError> {
-        let (low_key, high_key) = get_erc20_balance_var_addresses(contract_address)?;
+        let low_key = get_erc20_balance_var_address(contract_address)?;
+        let high_key = next_storage_key(&low_key)?;
         let low = self.get_storage_at(block_context.deprecated_fee_token_address, low_key)?;
         let high = self.get_storage_at(block_context.deprecated_fee_token_address, high_key)?;
 
