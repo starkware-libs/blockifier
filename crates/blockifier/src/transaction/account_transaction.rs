c00b2edf64ef75f66f0d336c3d250144b41277a6
amos rothberg <amos@starkware.co>
2023-09-03 12:22:25 +0300

    Updated according to review.

--- crates/blockifier/src/transaction/account_transaction.rs
+++ crates/blockifier/src/transaction/account_transaction.rs
@@ -181,12 +181,12 @@ impl AccountTransaction {
     }
 
     fn fee_token_address(&self, block_context: &BlockContext) -> ContractAddress {
-        let version_felt = self.get_account_transaction_context().version.0;
-        if version_felt < StarkFelt::from(3_u8) {
-            block_context.deprecated_fee_token_address
-        } else {
-            assert_eq!(version_felt, StarkFelt::from(3_u8));
+        let pays_with_stark = self.get_account_transaction_context().pays_with_strk();
+
+        if pays_with_stark {
             block_context.fee_token_address
+        } else {
+            block_context.deprecated_fee_token_address
         }
     }
 
