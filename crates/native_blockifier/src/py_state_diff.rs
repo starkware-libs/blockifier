ea27f457469ff7f6b3f32c89f8de066322310bd9
amosStarkware <88497213+amosStarkware@users.noreply.github.com>
2023-09-04 11:10:16 +0300

    Added strk_l1_gas_price to PyBlockInfo & BlockContext. (#868)

--- crates/native_blockifier/src/py_state_diff.rs
+++ crates/native_blockifier/src/py_state_diff.rs
@@ -119,5 +119,6 @@ pub struct PyBlockInfo {
     pub block_number: u64,
     pub block_timestamp: u64,
     pub gas_price: u128,
+    pub strk_l1_gas_price: u128,
     pub sequencer_address: PyFelt,
 }
