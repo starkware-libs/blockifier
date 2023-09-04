f206ccac6aea57b05ef1bd4894cbdb11c42b2987
dorimedini-starkware <dori@starkware.co>
2023-09-04 13:46:39 +0300

    Rename gas price (#843)
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/native_blockifier/src/py_state_diff.rs
+++ crates/native_blockifier/src/py_state_diff.rs
@@ -118,7 +118,7 @@ impl From<CommitmentStateDiff> for PyStateDiff {
 pub struct PyBlockInfo {
     pub block_number: u64,
     pub block_timestamp: u64,
-    pub gas_price: u128,
+    pub eth_l1_gas_price: u128,
     pub strk_l1_gas_price: u128,
     pub sequencer_address: PyFelt,
 }
