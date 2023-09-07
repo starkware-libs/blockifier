4dc7e6404bf1be0d54bc9e52a57417347387768a
Barak <barakb@starkware.co>
2023-09-07 14:39:43 +0300

    Adjust Invoke transaction to V3 changes in starknet-api.

--- crates/native_blockifier/src/py_transaction.rs
+++ crates/native_blockifier/src/py_transaction.rs
@@ -20,8 +20,9 @@ use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{
     AccountDeploymentData, Calldata, ContractAddressSalt, DeclareTransactionV0V1,
     DeclareTransactionV2, DeclareTransactionV3, DeployAccountTransactionV1,
-    DeployAccountTransactionV3, Fee, InvokeTransactionV0, InvokeTransactionV1, PaymasterAddress,
-    Resource, ResourceBounds, Tip, TransactionHash, TransactionSignature, TransactionVersion,
+    DeployAccountTransactionV3, Fee, InvokeTransactionV0, InvokeTransactionV1, InvokeTransactionV3,
+    PaymasterAddress, Resource, ResourceBounds, Tip, TransactionHash, TransactionSignature,
+    TransactionVersion,
 };
 
 use crate::errors::{NativeBlockifierInputError, NativeBlockifierResult};
@@ -285,6 +286,26 @@ pub fn py_invoke_function(tx: &PyAny) -> NativeBlockifierResult<InvokeTransactio
             sender_address: account_data_context.sender_address,
             calldata: py_calldata(tx, "calldata")?,
         })),
+        3 => {
+            let v3_new_parameter = get_v3_new_parameters(tx)?;
+            let py_account_init_code: Vec<PyFelt> = py_attr(tx, "account_init_code")?;
+            let invoke_tx = InvokeTransactionV3 {
+                resource: v3_new_parameter.resource,
+                resource_bounds: v3_new_parameter.resource_bounds,
+                tip: v3_new_parameter.tip,
+                signature: account_data_context.signature,
+                nonce: account_data_context.nonce,
+                sender_address: account_data_context.sender_address,
+                calldata: py_calldata(tx, "calldata")?,
+                nonce_data_availability_mode: v3_new_parameter.nonce_data_availability_mode,
+                fee_data_availability_mode: v3_new_parameter.fee_data_availability_mode,
+                paymaster_address: v3_new_parameter.paymaster_address,
+                account_init_code: AccountDeploymentData(
+                    py_account_init_code.into_iter().map(|felt| felt.0).collect(),
+                ),
+            };
+            Ok(starknet_api::transaction::InvokeTransaction::V3(invoke_tx))
+        }
         _ => Err(NativeBlockifierInputError::UnsupportedTransactionVersion {
             tx_type: TransactionType::InvokeFunction,
             version,
