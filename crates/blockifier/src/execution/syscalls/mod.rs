b5bba2529172092c5457e9126fb60279e9170ee9
Arni Hod <arnon@starkware.co>
2023-09-19 10:48:07 +0300

    Restrict the use of get_block_hash syscall in during validate.

--- crates/blockifier/src/execution/syscalls/mod.rs
+++ crates/blockifier/src/execution/syscalls/mod.rs
@@ -16,9 +16,10 @@ use starknet_api::transaction::{
 use self::hint_processor::{
     create_retdata_segment, execute_inner_call, execute_library_call, felt_to_bool,
     read_call_params, read_calldata, read_felt_array, write_segment, SyscallExecutionError,
-    SyscallHintProcessor, BLOCK_NUMBER_OUT_OF_RANGE_ERROR,
+    SyscallHintProcessor, BLOCK_NUMBER_OUT_OF_RANGE_ERROR, INVALID_IN_EXECUTION_MODE_ERROR,
 };
 use crate::abi::constants;
+use crate::execution::common_hints::ExecutionMode;
 use crate::execution::contract_class::ContractClass;
 use crate::execution::deprecated_syscalls::DeprecatedSyscallSelector;
 use crate::execution::entry_point::{
@@ -339,6 +340,14 @@ pub fn get_block_hash(
     syscall_handler: &mut SyscallHintProcessor<'_>,
     _remaining_gas: &mut u64,
 ) -> SyscallResult<GetBlockHashResponse> {
+    if syscall_handler.execution_mode == ExecutionMode::Validate {
+        let invalid_in_execution_mode_error = StarkFelt::try_from(INVALID_IN_EXECUTION_MODE_ERROR)
+            .map_err(SyscallExecutionError::from)?;
+        return Err(SyscallExecutionError::SyscallError {
+            error_data: vec![invalid_in_execution_mode_error],
+        });
+    }
+
     let requested_block_number = request.block_number.0;
     let current_block_number = syscall_handler.context.block_context.block_number.0;
 
