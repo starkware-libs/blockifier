6326b5456b57037bd4859e564db37ae6759fd07b
amos rothberg <amos@starkware.co>
2023-09-19 18:24:16 +0300

    WIP

--- crates/blockifier/src/fee/gas_usage_test.rs
+++ crates/blockifier/src/fee/gas_usage_test.rs
@@ -1,8 +1,11 @@
+use num_bigint::BigUint;
+
+use crate::fee::errors::GasPriceQueryError;
 use crate::fee::eth_gas_constants;
 use crate::fee::gas_usage::{
     calculate_tx_gas_usage, get_consumed_message_to_l2_emissions_cost,
     get_log_message_to_l1_emissions_cost, get_message_segment_length,
-    get_onchain_data_segment_length,
+    get_onchain_data_segment_length, PoolState, PoolStateAggregator,
 };
 use crate::state::cached_state::StateChangesCount;
 
@@ -113,4 +116,55 @@ fn test_calculate_tx_gas_usage_basic() {
         l1_handler_gas_usage + l2_to_l1_messages_gas_usage + storage_writings_gas_usage;
 
     assert_eq!(gas_usage, expected_gas_usage);
+}
+
+/// Sanity tests for STRK<->ETH price computation.
+#[test]
+fn test_get_estimated_strk_price() {
+    let (wei_1, wei_2, wei_3) =
+        (BigUint::from(10_u32), BigUint::from(14_u32), BigUint::from(12_u32));
+    let (strk_1, strk_2, strk_3) =
+        (BigUint::from(50_u32), BigUint::from(42_u32), BigUint::from(24_u32));
+    let state_1 = PoolState { total_wei: wei_1.clone(), total_strk: strk_1.clone() };
+    let state_2 = PoolState { total_wei: wei_2.clone(), total_strk: strk_2.clone() };
+    let state_3 = PoolState { total_wei: wei_3.clone(), total_strk: strk_3.clone() };
+    let state_4 = PoolState { total_wei: wei_1.clone(), total_strk: BigUint::from(150_u32) };
+    let wei_amount = BigUint::from(10_000_000_000_u64);
+
+    // Bad flow: ratio computation on empty array.
+    assert!(matches!(
+        PoolStateAggregator::new(&vec![]),
+        Err(GasPriceQueryError::NoPoolStatesError)
+    ));
+
+    // convert Wei -> STRK with a single pool state.
+    assert_eq!(
+        PoolStateAggregator::new(&vec![state_1.clone()])
+            .unwrap()
+            .convert_wei_to_strk(wei_amount.clone()),
+        (state_1.total_strk.clone() / state_1.total_wei.clone()) * wei_amount.clone()
+    );
+
+    // convert Wei -> STRK with multiple pool states, no equal weight partition.
+    assert_eq!(
+        PoolStateAggregator::new(&vec![state_3.clone(), state_1.clone()])
+            .unwrap()
+            .convert_wei_to_strk(wei_amount.clone()),
+        (state_3.total_strk.clone() / state_3.total_wei.clone()) * wei_amount.clone()
+    );
+    assert_eq!(
+        PoolStateAggregator::new(&vec![state_3, state_1.clone(), state_2.clone()])
+            .unwrap()
+            .convert_wei_to_strk(wei_amount.clone()),
+        (state_2.total_strk.clone() / state_2.total_wei.clone()) * wei_amount.clone()
+    );
+
+    // convert Wei -> STRK with multiple pool states with equal weight partition.
+    assert_eq!(
+        PoolStateAggregator::new(&vec![state_1.clone(), state_4.clone()])
+            .unwrap()
+            .convert_wei_to_strk(wei_amount.clone()),
+        ((state_1.total_strk + state_4.total_strk) / (state_1.total_wei + state_4.total_wei))
+            * wei_amount
+    );
 }
