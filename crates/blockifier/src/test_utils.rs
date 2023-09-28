78fa7c3922ec2fedb5423cfd902fb8348cc98e62
Dori Medini <dori@starkware.co>
2023-09-28 21:25:22 +0300

    Remove nonce_manager arg, pass nonce directly
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/test_utils.rs
+++ crates/blockifier/src/test_utils.rs
@@ -397,7 +397,7 @@ pub struct InvokeTxArgs {
     pub fee_data_availability_mode: DataAvailabilityMode,
     pub paymaster_data: PaymasterData,
     pub account_deployment_data: AccountDeploymentData,
-    pub nonce: Option<Nonce>,
+    pub nonce: Nonce,
 }
 
 impl Default for InvokeTxArgs {
@@ -420,7 +420,7 @@ impl Default for InvokeTxArgs {
             fee_data_availability_mode: DataAvailabilityMode::L1,
             paymaster_data: PaymasterData::default(),
             account_deployment_data: AccountDeploymentData::default(),
-            nonce: None,
+            nonce: Nonce::default(),
         }
     }
 }
@@ -473,10 +473,8 @@ pub fn deploy_account_tx_with_salt(
     DeployAccountTransaction { tx, tx_hash: TransactionHash::default(), contract_address }
 }
 
-pub fn invoke_tx(nonce_manager: &mut NonceManager, invoke_args: InvokeTxArgs) -> InvokeTransaction {
+pub fn invoke_tx(invoke_args: InvokeTxArgs) -> InvokeTransaction {
     let signature = invoke_args.signature.unwrap_or_default();
-    let nonce =
-        invoke_args.nonce.unwrap_or_else(|| nonce_manager.next(invoke_args.account_address));
     match invoke_args.tx_version {
         TransactionVersion::ZERO => InvokeTransactionV0 {
             max_fee: invoke_args.max_fee,
@@ -491,7 +489,7 @@ pub fn invoke_tx(nonce_manager: &mut NonceManager, invoke_args: InvokeTxArgs) ->
         TransactionVersion::ONE => InvokeTransactionV1 {
             max_fee: invoke_args.max_fee,
             sender_address: invoke_args.account_address,
-            nonce,
+            nonce: invoke_args.nonce,
             calldata: invoke_args.calldata,
             signature,
         }
@@ -500,7 +498,7 @@ pub fn invoke_tx(nonce_manager: &mut NonceManager, invoke_args: InvokeTxArgs) ->
             resource_bounds: invoke_args.resource_bounds,
             calldata: invoke_args.calldata,
             sender_address: invoke_args.account_address,
-            nonce,
+            nonce: invoke_args.nonce,
             signature,
             tip: invoke_args.tip,
             nonce_data_availability_mode: invoke_args.nonce_data_availability_mode,
