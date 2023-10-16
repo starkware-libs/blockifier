d96d782df40df3c02522e93d6b16dc455636f85b
Elin <elin@starkware.co>
2023-10-16 19:24:31 +0300

    Rename some files to align deprecated and non-deprecated modules. (#985)

--- crates/blockifier/src/execution.rs
+++ crates/blockifier/src/execution.rs
@@ -1,11 +1,11 @@
-pub mod cairo1_execution;
 pub mod call_info;
 pub mod common_hints;
 pub mod contract_address;
 pub mod contract_class;
-pub mod deprecated_execution;
+pub mod deprecated_entry_point_execution;
 pub mod deprecated_syscalls;
 pub mod entry_point;
+pub mod entry_point_execution;
 pub mod errors;
 pub mod execution_utils;
 pub mod hint_code;
