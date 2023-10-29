830a2364552d7c8055ac1bad6484db18b30c6370
Noa Oved <104720318+noaov1@users.noreply.github.com>
2023-10-29 12:31:04 +0200

    Allow using Cairo1 test contract in transaction test. (#1051)

--- crates/blockifier/src/transaction/test_utils.rs
+++ crates/blockifier/src/transaction/test_utils.rs
@@ -37,6 +37,7 @@ pub fn create_account_tx_test_state(
     account_address: &str,
     erc20_account_balance_key: StorageKey,
     initial_account_balance: u128,
+    test_contract_class: ContractClass,
 ) -> CachedState<DictStateReader> {
     let block_context = BlockContext::create_for_testing();
 
@@ -45,9 +46,7 @@ pub fn create_account_tx_test_state(
     let test_erc20_class_hash = ClassHash(stark_felt!(TEST_ERC20_CONTRACT_CLASS_HASH));
     let class_hash_to_class = HashMap::from([
         (test_account_class_hash, account_class),
-        // TODO(Mohammad,01/08/2023): Use Cairo 1 test contract when running Cairo 1 account
-        // contract.
-        (test_contract_class_hash, ContractClassV0::from_file(TEST_CONTRACT_CAIRO0_PATH).into()),
+        (test_contract_class_hash, test_contract_class),
         (test_erc20_class_hash, ContractClassV0::from_file(ERC20_CONTRACT_PATH).into()),
     ]);
     let test_contract_address = ContractAddress(patricia_key!(TEST_CONTRACT_ADDRESS));
@@ -83,6 +82,8 @@ pub fn create_state_with_trivial_validation_account() -> CachedState<DictStateRe
         TEST_ACCOUNT_CONTRACT_ADDRESS,
         test_erc20_account_balance_key(),
         account_balance,
+        // TODO(Noa,01/12/2023): Use `once_cell::sync::Lazy` to create the contract class.
+        ContractClassV0::from_file(TEST_CONTRACT_CAIRO0_PATH).into(),
     )
 }
 
@@ -94,6 +95,9 @@ pub fn create_state_with_cairo1_account() -> CachedState<DictStateReader> {
         TEST_ACCOUNT_CONTRACT_ADDRESS,
         test_erc20_account_balance_key(),
         account_balance,
+        // TODO(Mohammad,01/08/2023): Use Cairo 1 test contract.
+        // TODO(Noa,01/12/2023): Use `once_cell::sync::Lazy` to create the contract class.
+        ContractClassV0::from_file(TEST_CONTRACT_CAIRO0_PATH).into(),
     )
 }
 
@@ -105,6 +109,8 @@ pub fn create_state_with_falliable_validation_account() -> CachedState<DictState
         TEST_FAULTY_ACCOUNT_CONTRACT_ADDRESS,
         test_erc20_faulty_account_balance_key(),
         account_balance * 2,
+        // TODO(Noa,01/12/2023): Use `once_cell::sync::Lazy` to create the contract class.
+        ContractClassV0::from_file(TEST_CONTRACT_CAIRO0_PATH).into(),
     )
 }
 
