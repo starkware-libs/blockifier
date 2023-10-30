fa6cd1472c37c691054f675d971b5e370791f066
amos rothberg <amos@starkware.co>
2023-10-30 14:24:40 +0200

    Perform pre-validation checks in PyValidator.

--- crates/native_blockifier/src/py_utils.rs
+++ crates/native_blockifier/src/py_utils.rs
@@ -1,6 +1,6 @@
 use std::convert::TryFrom;
 
-use blockifier::transaction::errors::TransactionExecutionError;
+use blockifier::transaction::errors::{TransactionExecutionError, TransactionFeeError};
 use num_bigint::BigUint;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
@@ -95,7 +95,10 @@ pub fn int_to_chain_id(int: &PyAny) -> PyResult<ChainId> {
 // TODO(Dori, 1/4/2023): If and when supported in the Python build environment, use #[cfg(test)].
 #[pyfunction]
 pub fn raise_error_for_testing() -> NativeBlockifierResult<()> {
-    Err(TransactionExecutionError::CairoResourcesNotContainedInFeeCosts.into())
+    Err(TransactionExecutionError::TransactionFeeError(
+        TransactionFeeError::CairoResourcesNotContainedInFeeCosts,
+    )
+    .into())
 }
 
 pub fn py_attr<T>(obj: &PyAny, attr: &str) -> NativeBlockifierResult<T>
