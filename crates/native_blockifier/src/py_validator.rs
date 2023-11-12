a049d1677d17eab683d6fdae5005af2adb507023
amos rothberg <amos@starkware.co>
2023-11-12 12:32:57 +0200

    Perform Pre-validation stage in PyValidator.

--- crates/native_blockifier/src/py_validator.rs
+++ crates/native_blockifier/src/py_validator.rs
@@ -4,10 +4,11 @@ use pyo3::prelude::*;
 use crate::errors::NativeBlockifierResult;
 use crate::py_block_executor::PyGeneralConfig;
 use crate::py_state_diff::PyBlockInfo;
-use crate::py_transaction::PyActualCost;
+use crate::py_transaction::{py_account_tx, PyActualCost};
 use crate::py_transaction_execution_info::{
     PyCallInfo, PyTransactionExecutionInfo, PyVmExecutionResources,
 };
+use crate::py_utils::py_enum_name;
 use crate::state_readers::py_state_reader::PyStateReader;
 use crate::transaction_executor::TransactionExecutor;
 
@@ -107,8 +108,19 @@ impl PyValidator {
         remaining_gas: u64,
         raw_contract_class: Option<&str>,
     ) -> NativeBlockifierResult<Option<PyCallInfo>> {
-        // Pre validations.
-        // TODO(Amos, 09/11/2023): Add pre-validation checks.
+        let tx_type: String = py_enum_name(tx, "tx_type")?;
+        let account_tx = py_account_tx(&tx_type, tx, raw_contract_class)?;
+        let tx_executor = self.tx_executor();
+
+        let strict_nonce_check = false;
+        let charge_fee = true;
+        account_tx.perform_pre_validation_stage(
+            &mut tx_executor.state,
+            &account_tx.get_account_tx_context(),
+            &tx_executor.block_context,
+            charge_fee,
+            strict_nonce_check,
+        )?;
 
         // `__validate__` call.
         let (py_optional_call_info, _actual_cost) =
