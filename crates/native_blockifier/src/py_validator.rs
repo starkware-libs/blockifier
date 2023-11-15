291353f93e90580d0bb342d12882f79bb7aa2bdb
Ayelet Zilber <ayelet@starkware.co>
2023-11-15 11:08:31 +0200

    Checks call succeeded in perform validations

--- crates/native_blockifier/src/py_validator.rs
+++ crates/native_blockifier/src/py_validator.rs
@@ -1,11 +1,20 @@
+use std::collections::HashMap;
+
+use blockifier::abi::abi_utils::selector_from_name;
+use blockifier::abi::constants::CONSTRUCTOR_ENTRY_POINT_NAME;
 use blockifier::fee::actual_cost::{ActualCost, PostExecutionReport};
 use blockifier::state::cached_state::GlobalContractCache;
 use blockifier::transaction::account_transaction::AccountTransaction;
+use blockifier::transaction::constants::{
+    EXECUTE_ENTRY_POINT_NAME, TRANSFER_ENTRY_POINT_NAME, VALIDATE_DECLARE_ENTRY_POINT_NAME,
+    VALIDATE_DEPLOY_ENTRY_POINT_NAME, VALIDATE_ENTRY_POINT_NAME,
+};
 use blockifier::transaction::objects::{AccountTransactionContext, TransactionExecutionResult};
 use blockifier::transaction::transaction_execution::Transaction;
 use pyo3::prelude::*;
+use starknet_api::hash::StarkFelt;
 
-use crate::errors::NativeBlockifierResult;
+use crate::errors::{self, NativeBlockifierResult, NativeBlockifierValidationsError};
 use crate::py_block_executor::PyGeneralConfig;
 use crate::py_state_diff::PyBlockInfo;
 use crate::py_transaction::{py_account_tx, PyActualCost};
@@ -121,14 +130,14 @@ impl PyValidator {
         if let AccountTransaction::DeployAccount(_deploy_account_tx) = account_tx {
             let (_py_tx_execution_info, _py_casm_hash_calculation_resources) =
                 self.execute(tx, raw_contract_class)?;
-            // TODO(Ayelet, 09/11/2023): Check call succeeded.
+            self.check_call_succeeded(_py_tx_execution_info.execute_call_info)?;
+            self.check_call_succeeded(_py_tx_execution_info.validate_call_info)?;
             return Ok(());
         }
         let account_tx_context = account_tx.get_account_tx_context();
 
         // Pre validations.
         // TODO(Amos, 09/11/2023): Add pre-validation checks.
-
         // `__validate__` call.
         let (_py_optional_call_info, py_actual_cost) =
             self.validate(tx, Transaction::initial_gas(), raw_contract_class)?;
@@ -177,6 +186,45 @@ impl PyValidator {
             // Note: the balance cannot be changed in `__validate__` (which cannot call other
             // contracts), so there is no need to recheck that balance >= actual_cost.
             None => Ok(()),
+        }
+    }
+
+    pub fn check_call_succeeded(
+        &mut self,
+        py_optional_call_info: Option<PyCallInfo>,
+    ) -> NativeBlockifierResult<()> {
+        let py_call_info = py_optional_call_info.expect("Call info must be not None.");
+        if py_call_info.failure_flag.0 != StarkFelt::ZERO {
+            return Err(errors::NativeBlockifierError::NativeBlockifierValidationsError(
+                NativeBlockifierValidationsError::ValidationError {
+                    entry_point_name: self.selector_to_name(py_call_info.entry_point_selector.0),
+                    error_data: py_call_info.retdata.iter().map(|v| v.0).collect(),
+                },
+            ));
+        }
+        Ok(())
+    }
+
+    fn selector_to_name(&mut self, entry_point_selector: StarkFelt) -> String {
+        let selector_to_name_map = HashMap::from([
+            (selector_from_name(CONSTRUCTOR_ENTRY_POINT_NAME).0, CONSTRUCTOR_ENTRY_POINT_NAME),
+            (selector_from_name(EXECUTE_ENTRY_POINT_NAME).0, EXECUTE_ENTRY_POINT_NAME),
+            (selector_from_name(TRANSFER_ENTRY_POINT_NAME).0, TRANSFER_ENTRY_POINT_NAME),
+            (selector_from_name(VALIDATE_ENTRY_POINT_NAME).0, VALIDATE_ENTRY_POINT_NAME),
+            (
+                selector_from_name(VALIDATE_DECLARE_ENTRY_POINT_NAME).0,
+                VALIDATE_DECLARE_ENTRY_POINT_NAME,
+            ),
+            (
+                selector_from_name(VALIDATE_DEPLOY_ENTRY_POINT_NAME).0,
+                VALIDATE_DEPLOY_ENTRY_POINT_NAME,
+            ),
+        ]);
+
+        if let Some(name) = selector_to_name_map.get(&entry_point_selector) {
+            name.to_string()
+        } else {
+            "".to_owned()
         }
     }
 }
