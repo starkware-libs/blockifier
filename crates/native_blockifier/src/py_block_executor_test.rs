0b78384b8faf0b98bf93df85480ee4352eeefb2f
Gilad Chase <gilad@starkware.com>
2023-11-20 10:28:17 +0200

    Add Storage mock and test for get_block_id
    Specifically, when retrieving block_id from a Papyrus snapshot
    it can be a large hash, which we had a bug on back when we assumed it
    was a u64.
    
    commit-id:979a9b19

--- crates/native_blockifier/src/py_block_executor_test.rs
+++ crates/native_blockifier/src/py_block_executor_test.rs
@@ -1,12 +1,18 @@
+use std::collections::HashMap;
+
 use blockifier::state::state_api::State;
 use blockifier::test_utils::{get_test_contract_class, TEST_CLASS_HASH};
 use cached::Cached;
+use pretty_assertions::assert_eq;
 use starknet_api::class_hash;
 use starknet_api::core::ClassHash;
-use starknet_api::hash::StarkHash;
+use starknet_api::hash::{StarkFelt, StarkHash};
 
 use crate::py_block_executor::{PyBlockExecutor, PyGeneralConfig};
 use crate::py_state_diff::PyBlockInfo;
+use crate::py_utils::PyFelt;
+use crate::test_utils::FakeStorage;
+
 #[test]
 fn global_contract_cache_update() {
     // Initialize executor and set a contract class on the state.
@@ -36,4 +42,23 @@ fn global_contract_cache_update() {
     block_executor.finalize(is_pending_block);
     assert_eq!(block_executor.global_contract_cache.lock().unwrap().cache_size(), 1);
     block_executor.teardown_block_execution();
+}
+
+#[test]
+fn get_block_id() {
+    let mut max_class_hash = [
+        0x9, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf,
+        0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf,
+    ];
+    let max_class_hash_vec = Vec::from(max_class_hash);
+    let expected_max_class_hash_as_py_felt = PyFelt(StarkFelt::new(max_class_hash).unwrap());
+
+    let storage =
+        FakeStorage { block_number_to_class_hash: HashMap::from([(1138, max_class_hash)]) };
+    let block_executor = PyBlockExecutor::create_for_testing_with_storage(storage);
+
+    assert_eq!(
+        block_executor.get_block_id_at_target(1138).unwrap().unwrap(),
+        expected_max_class_hash_as_py_felt
+    );
 }
