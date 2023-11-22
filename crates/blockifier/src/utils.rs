95353c12ca51ee23dbaa181bf9edbfc217627b74
Gilad Chase <gilad@starkware.com>
2023-11-22 15:31:49 +0200

    Don't iterate over `HashMap/HashSet`
    - Iterating over `Hash{Map,Set}` results in undeterministic behavior,
    as the order of iteration is not deterministic. Using `Index{Map,Set}`
    ensures deterministic iteration order.
    
    - Add a new rust 1.75 clippy lint that catches this behavior.
    This won't work in the CI just yet, which uses rust stable (1.74 ATM),
    but for local devs running nightly clippy this will already be in use.

--- crates/blockifier/src/utils.rs
+++ crates/blockifier/src/utils.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use indexmap::IndexMap;
+
 #[cfg(test)]
 #[path = "utils_test.rs"]
 pub mod test;
@@ -7,7 +9,7 @@ pub mod test;
 /// Returns a `HashMap` containing key-value pairs from `a` that are not included in `b` (if
 /// a key appears in `b` with a different value, it will be part of the output).
 /// Usage: Get updated items from a mapping.
-pub fn subtract_mappings<K, V>(lhs: &HashMap<K, V>, rhs: &HashMap<K, V>) -> HashMap<K, V>
+pub fn subtract_mappings<K, V>(lhs: &IndexMap<K, V>, rhs: &HashMap<K, V>) -> IndexMap<K, V>
 where
     K: Clone + Eq + std::hash::Hash,
     V: Clone + PartialEq,
