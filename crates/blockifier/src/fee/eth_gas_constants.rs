eb227d766e50cb5e5a664b30063cbcb7bc6260f9
Yonatan Iluz <yonatan@starkware.co>
2023-12-11 12:04:53 +0200

    Reduce 10% of DA cost.

--- crates/blockifier/src/fee/eth_gas_constants.rs
+++ crates/blockifier/src/fee/eth_gas_constants.rs
@@ -1,19 +1,29 @@
+// Calldata.
 pub const GAS_PER_MEMORY_ZERO_BYTE: usize = 4;
 pub const GAS_PER_MEMORY_BYTE: usize = 16;
 pub const WORD_WIDTH: usize = 32;
 pub const GAS_PER_MEMORY_WORD: usize = GAS_PER_MEMORY_BYTE * WORD_WIDTH;
-pub const SHARP_ADDITIONAL_GAS_PER_MEMORY_WORD: usize = 100; // This value is not accurate.
-pub const SHARP_GAS_PER_MEMORY_WORD: usize =
-    GAS_PER_MEMORY_WORD + SHARP_ADDITIONAL_GAS_PER_MEMORY_WORD;
+
+// Storage.
 pub const GAS_PER_ZERO_TO_NONZERO_STORAGE_SET: usize = 20000;
 pub const GAS_PER_COLD_STORAGE_ACCESS: usize = 2100;
 pub const GAS_PER_NONZERO_TO_INT_STORAGE_SET: usize = 2900;
 pub const GAS_PER_COUNTER_DECREASE: usize =
     GAS_PER_COLD_STORAGE_ACCESS + GAS_PER_NONZERO_TO_INT_STORAGE_SET;
+
+// Events.
 pub const GAS_PER_LOG: usize = 375;
 pub const GAS_PER_LOG_TOPIC: usize = 375;
 pub const GAS_PER_LOG_DATA_BYTE: usize = 8;
 pub const GAS_PER_LOG_DATA_WORD: usize = GAS_PER_LOG_DATA_BYTE * WORD_WIDTH;
+
+// SHARP empirical costs.
+pub const SHARP_ADDITIONAL_GAS_PER_MEMORY_WORD: usize = 100; // This value is not accurate.
+pub const SHARP_GAS_PER_MEMORY_WORD: usize =
+    GAS_PER_MEMORY_WORD + SHARP_ADDITIONAL_GAS_PER_MEMORY_WORD;
+// 10% discount for data availability.
+pub const DISCOUNT_PER_DA_WORD: usize = (SHARP_GAS_PER_MEMORY_WORD * 10) / 100;
+pub const SHARP_GAS_PER_DA_WORD: usize = SHARP_GAS_PER_MEMORY_WORD - DISCOUNT_PER_DA_WORD;
 
 // TODO(Yoni, 1/1/2025): rename this file to `_utils`.
 pub fn get_calldata_word_cost(n_nonzero_bytes: usize) -> usize {
