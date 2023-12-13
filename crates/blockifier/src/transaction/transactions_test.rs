6d266b15307f4a5224ec5d525126b5049d16baf9
Arni Hod <arnon@starkware.co>
2023-12-13 12:53:56 +0200

    Add invalid scenario to constructor test.

--- crates/blockifier/src/transaction/transactions_test.rs
+++ crates/blockifier/src/transaction/transactions_test.rs
@@ -1372,7 +1372,11 @@ fn test_validate_accounts_tx(
         },
     );
     let error = account_tx.execute(state, block_context, true, true).unwrap_err();
-    check_transaction_execution_error_for_invalid_scenario!(cairo_version, error);
+    check_transaction_execution_error_for_invalid_scenario!(
+        cairo_version,
+        error,
+        ValidateTransactionError,
+    );
 
     // Trying to call another contract (forbidden).
     let account_tx = create_account_tx_for_validate_test(
@@ -1455,6 +1459,22 @@ fn test_constructor_on_deploy_account_acts_as_validate(
         validate_constructor: true,
         ..Default::default()
     };
+
+    // Logic failure.
+    let account_tx = create_account_tx_for_validate_test(
+        &mut NonceManager::default(),
+        FaultyAccountTxCreatorArgs {
+            scenario: INVALID,
+            contract_address_salt: salt_manager.next_salt(),
+            ..default_args
+        },
+    );
+    let error = account_tx.execute(state, block_context, true, true).unwrap_err();
+    check_transaction_execution_error_for_invalid_scenario!(
+        cairo_version,
+        error,
+        ContractConstructorExecutionFailed,
+    );
 
     // Verify that the contract does not call another contract in the constructor of deploy account.
     // Deploy another instance of 'faulty_account' and try to call other contract in the
