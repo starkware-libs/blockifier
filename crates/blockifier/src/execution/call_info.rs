e6881d1037b528d59a9f74cb7349eac71603e6e1
Ayelet Zilber <ayelet@starkware.co>
2023-12-18 16:59:42 +0200

    Checks if call succeeded in perform validations

--- crates/blockifier/src/execution/call_info.rs
+++ crates/blockifier/src/execution/call_info.rs
@@ -6,6 +6,7 @@ use starknet_api::hash::StarkFelt;
 use starknet_api::state::StorageKey;
 use starknet_api::transaction::{EventContent, L2ToL1Payload};
 
+use crate::abi::abi_utils::selector_to_name;
 use crate::execution::entry_point::CallEntryPoint;
 use crate::transaction::errors::TransactionExecutionError;
 use crate::transaction::objects::TransactionExecutionResult;
@@ -113,6 +114,16 @@ impl CallInfo {
                 }),
             },
         )
+    }
+
+    pub fn check_call_succeeded(&self) -> TransactionExecutionResult<()> {
+        if self.execution.failed {
+            return Err(TransactionExecutionError::CallFailedError {
+                entry_point_name: selector_to_name(self.call.entry_point_selector.0),
+                error_data: self.execution.retdata.0.clone(),
+            });
+        }
+        Ok(())
     }
 }
 
