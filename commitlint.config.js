1a758e1201b84e10fdc8fa08b18df0e3c6dc78a1
Dori Medini <dori@starkware.co>
2024-01-04 16:31:58 +0200

    ci: add commitlint in CI
    Signed-off-by: Dori Medini <dori@starkware.co>

---
+++ commitlint.config.js
@@ -0,0 +1,58 @@
+const Configuration = {
+    /*
+     * Resolve and load @commitlint/config-conventional from node_modules.
+     * Referenced packages must be installed.
+     * See [the README](https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/README.md)
+     * for applied rules.
+     */
+    extends: ['@commitlint/config-conventional'],
+    /*
+     * Resolve and load conventional-changelog-atom from node_modules.
+     * Referenced packages must be installed
+     */
+    // parserPreset: 'conventional-changelog-atom',
+    /*
+     * Resolve and load @commitlint/format from node_modules.
+     * Referenced package must be installed
+     */
+    formatter: '@commitlint/format',
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+        'scope-enum': [2, 'always', [
+            'execution',
+            'fee',
+            'native_blockifier',
+            'resources',
+            'state',
+            'transaction',
+        ]],
+    },
+    /*
+     * Functions that return true if commitlint should ignore the given message.
+     */
+    ignores: [(commit) => commit === ''],
+    /*
+     * Whether commitlint uses the default ignore rules.
+     */
+    defaultIgnores: true,
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl:
+        'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+    /*
+     * Custom prompt configs, not used currently.
+     */
+    prompt: {
+        messages: {},
+        questions: {
+            type: {
+                description: 'please input type:',
+            },
+        },
+    },
+};
+
+module.exports = Configuration;
