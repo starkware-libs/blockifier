37b31c08d2681c6f3f68b8735f6fd15a7c915e7b
Yael Doweck <yael@starkware.co>
2024-01-28 09:42:08 +0200

    refactor: restructure old_block_number_and_hash as a struct

--- crates/blockifier/src/block_execution.rs
+++ crates/blockifier/src/block_execution.rs
@@ -10,14 +10,27 @@ use crate::state::state_api::{State, StateResult};
 #[path = "block_execution_test.rs"]
 pub mod test;
 
+pub struct BlockNumberHashPair {
+    pub number: BlockNumber,
+    pub hash: BlockHash,
+}
+
+impl BlockNumberHashPair {
+    pub fn new(block_number: u64, block_hash: StarkFelt) -> BlockNumberHashPair {
+        BlockNumberHashPair { number: BlockNumber(block_number), hash: BlockHash(block_hash) }
+    }
+}
+
 // Block pre-processing.
 // Writes the hash of the (current_block_number - N) block under its block number in the dedicated
 // contract state, where N=STORED_BLOCK_HASH_BUFFER.
 pub fn pre_process_block(
     state: &mut dyn State,
-    old_block_number_and_hash: Option<(BlockNumber, BlockHash)>,
+    old_block_number_and_hash: Option<BlockNumberHashPair>,
 ) -> StateResult<()> {
-    if let Some((block_number, block_hash)) = old_block_number_and_hash {
+    if let Some(BlockNumberHashPair { number: block_number, hash: block_hash }) =
+        old_block_number_and_hash
+    {
         state.set_storage_at(
             ContractAddress::try_from(StarkFelt::from(constants::BLOCK_HASH_CONTRACT_ADDRESS))
                 .expect("Failed to convert `BLOCK_HASH_CONTRACT_ADDRESS` to ContractAddress."),
