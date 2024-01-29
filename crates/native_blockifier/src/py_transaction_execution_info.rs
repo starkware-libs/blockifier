1c60ce92f9c101547f54485bab6123537ffbe579
Ayelet Zilber <ayelet@starkware.co>
2024-01-29 14:12:27 +0200

    refactor(native_blockifier): flat fields in bouncerinfo

--- crates/native_blockifier/src/py_transaction_execution_info.rs
+++ crates/native_blockifier/src/py_transaction_execution_info.rs
@@ -1,8 +1,13 @@
 use std::collections::{HashMap, HashSet};
+use std::ops::Add;
 
+use blockifier::abi::constants;
 use blockifier::execution::call_info::{CallInfo, OrderedEvent, OrderedL2ToL1Message};
 use blockifier::execution::entry_point::CallType;
-use blockifier::transaction::objects::TransactionExecutionInfo;
+use blockifier::transaction::objects::{
+    ResourcesMapping, TransactionExecutionInfo, TransactionExecutionResult,
+};
+use cairo_vm::serde::deserialize_program::BuiltinName;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources as VmExecutionResources;
 use pyo3::prelude::*;
 use starknet_api::deprecated_contract_class::EntryPointType;
@@ -217,14 +222,62 @@ impl From<VmExecutionResources> for PyVmExecutionResources {
 
 #[pyclass]
 #[derive(Clone, Default)]
-// TODO(Ayelet, 24/01/2024): Consider remove message_segment_length, state_diff_size.
 pub struct PyBouncerInfo {
     #[pyo3(get)]
-    // The number of felts needed to store L1<>L2 messages.
-    pub message_segment_length: usize,
+    pub state_diff_size: usize, // The number of felts needed to store the state diff.
     #[pyo3(get)]
-    // The number of felts needed to store the state diff.
-    pub state_diff_size: usize,
+    pub l1_gas_amount: usize,
     #[pyo3(get)]
-    pub tx_weights: HashMap<String, usize>,
+    pub message_segment_length: usize, // The number of felts needed to store L1<>L2 messages.
+    #[pyo3(get)]
+    pub execution_resources: PyVmExecutionResources,
+}
+
+impl PyBouncerInfo {
+    pub fn calculate(
+        tx_actual_resources: &ResourcesMapping,
+        tx_additional_os_resources: VmExecutionResources, // os
+        message_segment_length: usize,
+        state_diff_size: usize,
+    ) -> TransactionExecutionResult<Self> {
+        // TODO(Ayelet, 04/02/2024): Consider defining a constant list.
+        let builtin_ordered_list = vec![
+            BuiltinName::output,
+            BuiltinName::pedersen,
+            BuiltinName::range_check,
+            BuiltinName::ecdsa,
+            BuiltinName::bitwise,
+            BuiltinName::ec_op,
+            BuiltinName::keccak,
+            BuiltinName::poseidon,
+        ];
+        let builtin_instance_counter: HashMap<String, usize> = builtin_ordered_list
+            .iter()
+            .map(|name| {
+                (name.name().to_string(), *tx_actual_resources.0.get(name.name()).unwrap_or(&0))
+            })
+            .collect();
+
+        let actual_resources_execution_resources = VmExecutionResources {
+            n_steps: *tx_actual_resources.0.get(constants::N_STEPS_RESOURCE).unwrap_or(&0),
+            n_memory_holes: *tx_actual_resources.0.get("n_memory_holes").unwrap_or(&0),
+            builtin_instance_counter,
+        };
+
+        let mut merged_resources =
+            tx_additional_os_resources.add(&actual_resources_execution_resources);
+
+        // Memory holes are counted as steps.
+        merged_resources.n_steps += merged_resources.n_memory_holes;
+        merged_resources.n_memory_holes = 0;
+
+        let execution_resources = PyVmExecutionResources::from(merged_resources);
+
+        let l1_gas_amount = *tx_actual_resources
+            .0
+            .get("l1_gas_usage")
+            .expect("Invalid Transaction Execution Info. Field l1_gas_usage was not found.");
+
+        Ok(Self { state_diff_size, l1_gas_amount, message_segment_length, execution_resources })
+    }
 }
