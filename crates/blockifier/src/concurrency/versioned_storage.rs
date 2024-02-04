9b15488342c27fd0f7e698a339964922defaeb78
Mohammad Nassar <mohammad@starkware.co>
2024-02-04 09:58:12 +0200

    feat(execution): implement a versioned storage data structure for implementing concurrency

---
+++ crates/blockifier/src/concurrency/versioned_storage.rs
@@ -0,0 +1,58 @@
+use std::collections::{BTreeMap, HashMap};
+use std::fmt::Debug;
+use std::hash::Hash;
+
+use crate::concurrency::Version;
+
+#[cfg(test)]
+#[path = "versioned_storage_test.rs"]
+pub mod test;
+
+/// A storage unit.
+/// It is versioned in the sense that it holds a state of write operations done on it by
+/// different versions of executions.
+/// This allows maintaining the cells with the correct values in the context of each execution.
+pub struct VersionedStorage<K, V>
+where
+    K: Clone + Copy + Eq + Hash + Debug,
+    V: Clone + Debug,
+{
+    cached_initial_values: HashMap<K, V>,
+    writes: HashMap<K, BTreeMap<Version, V>>,
+}
+
+impl<K, V> Default for VersionedStorage<K, V>
+where
+    K: Clone + Copy + Eq + Hash + Debug,
+    V: Clone + Debug,
+{
+    fn default() -> Self {
+        // We cannot derive `Default` since the derive requires that both `K` and `V` impl
+        // `Default`.
+        VersionedStorage { cached_initial_values: Default::default(), writes: Default::default() }
+    }
+}
+
+impl<K, V> VersionedStorage<K, V>
+where
+    K: Clone + Copy + Eq + Hash + Debug,
+    V: Clone + Debug,
+{
+    pub fn read(&self, version: Version, key: K) -> Option<V> {
+        let value = self.writes.get(&key).and_then(|cell| cell.range(..=version).next_back());
+        value.map(|(_, value)| value).or_else(|| self.cached_initial_values.get(&key)).cloned()
+    }
+
+    pub fn write(&mut self, version: Version, key: K, value: V) {
+        let cell = self.writes.entry(key).or_default();
+        cell.insert(version, value);
+    }
+
+    /// This method inserts the provided key-value pair into the cached initial values map.
+    /// It is typically used when reading a value that is not found in the versioned storage. In
+    /// such a scenario, the value is retrieved from the initial storage and written to the
+    /// cached initial values for future references.
+    pub fn set_initial_value(&mut self, key: K, value: V) {
+        self.cached_initial_values.insert(key, value);
+    }
+}
---
+++ crates/blockifier/src/concurrency/versioned_storage_test.rs
@@ -0,0 +1,38 @@
+use pretty_assertions::assert_eq;
+
+use crate::concurrency::versioned_storage::VersionedStorage;
+
+#[test]
+fn test_versioned_storage() {
+    let mut storage = VersionedStorage::default();
+
+    // Read an uninitialized cell.
+    let value = storage.read(1, 1);
+    assert!(value.is_none());
+
+    // Set initial values.
+    storage.set_initial_value(1, 31);
+    storage.set_initial_value(5, 31);
+    storage.set_initial_value(10, 31);
+
+    // Write.
+    storage.write(1, 1, 42);
+    assert_eq!(storage.read(123, 1).unwrap(), 42);
+
+    // Read initial value.
+    assert_eq!(storage.read(1, 5).unwrap(), 31);
+
+    // Read from the past.
+    storage.write(2, 10, 78);
+    assert_eq!(storage.read(1, 10).unwrap(), 31);
+    assert_eq!(storage.read(2, 10).unwrap(), 78);
+
+    // Read uninitialized cell.
+    assert!(storage.read(1, 100).is_none());
+
+    // Write to uninitialized cell.
+    storage.write(20, 100, 194);
+
+    // Test the write.
+    assert_eq!(storage.read(50, 100).unwrap(), 194);
+}
