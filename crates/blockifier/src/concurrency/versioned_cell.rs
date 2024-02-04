33dd0ff19b9921cf3d3cf268787234d09d368645
Mohammad Nassar <mohammad@starkware.co>
2024-02-04 09:53:17 +0200

    feat(execution): implement a versioned cell data structure for implementing concurrency

---
+++ crates/blockifier/src/concurrency/versioned_cell.rs
@@ -0,0 +1,33 @@
+use std::collections::BTreeMap;
+
+#[cfg(test)]
+#[path = "versioned_cell_test.rs"]
+pub mod test;
+
+pub(crate) type Version = u64;
+
+/// A storage cell.
+/// It is versioned in the sense that it holds a state of read and writes operation done on it by
+/// different versions of executions.
+/// This allows maintaining the cell with the correct value in the context of each execution.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct VersionedCell<T> {
+    pub initial_value: T,
+    pub writes: BTreeMap<Version, T>,
+}
+
+impl<T> VersionedCell<T> {
+    pub fn new(initial_value: T) -> Self {
+        VersionedCell { initial_value, writes: BTreeMap::new() }
+    }
+
+    // Retrieves the most recent value written by a version less than or equal to the provided
+    // version.
+    pub fn read(&self, version: Version) -> &T {
+        self.writes.range(..=version).next_back().map_or(&self.initial_value, |(_, value)| value)
+    }
+
+    pub fn write(&mut self, version: Version, value: T) {
+        self.writes.insert(version, value);
+    }
+}
