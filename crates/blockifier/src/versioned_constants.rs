e8b8e87585b791334c8fb73a851a80a753ad868f
Avi Cohen <avi.cohen@starkware.co>
2024-02-06 18:38:32 +0200

    feat(fee): charge for signatures per byte

--- crates/blockifier/src/versioned_constants.rs
+++ crates/blockifier/src/versioned_constants.rs
@@ -46,12 +46,13 @@ pub struct VersionedConstants {
     // TODO: Consider making this a struct, this will require change the way we access these
     // values.
     vm_resource_fee_cost: Arc<HashMap<String, f64>>,
-    pub milli_gas_per_calldata_byte: usize,
 
     // Cairo OS constants.
     // Note: if loaded from a json file, there are some assumptions made on its structure.
     // See the struct's docstring for more details.
     os_constants: Arc<OSConstants>,
+
+    l2_resource_gas_costs: Arc<L2ResourceGasCosts>,
 }
 
 impl VersionedConstants {
@@ -114,6 +115,10 @@ impl VersionedConstants {
         self.os_resources.get_additional_os_syscall_resources(syscall_counter)
     }
 
+    pub fn get_l2_resource_gas_costs(&self) -> &L2ResourceGasCosts {
+        &self.l2_resource_gas_costs
+    }
+
     #[cfg(any(feature = "testing", test))]
     pub fn create_for_account_testing() -> Self {
         let vm_resource_fee_cost = Arc::new(HashMap::from([
@@ -137,6 +142,15 @@ impl TryFrom<&Path> for VersionedConstants {
     fn try_from(path: &Path) -> Result<Self, Self::Error> {
         Ok(serde_json::from_reader(std::fs::File::open(path)?)?)
     }
+}
+
+#[derive(Clone, Debug, Default, Deserialize)]
+pub struct L2ResourceGasCosts {
+    // TODO(barak, 18/03/2024): Once we start charging per byte change to milligas_per_data_byte,
+    // divide the value by 32 in the JSON file.
+    pub milligas_per_data_word: usize,
+    pub event_key_factor: usize,
+    pub milligas_per_code_word: usize,
 }
 
 #[derive(Clone, Debug, Default, Deserialize)]
