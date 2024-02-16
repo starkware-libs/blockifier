e6384ef18db814fc47e5e715442b58bf3a0f0cd5
Dori Medini <dori@starkware.co>
2024-02-16 12:53:06 +0200

    chore!: limit pointer width in both crates
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/lib.rs
+++ crates/blockifier/src/lib.rs
@@ -1,3 +1,14 @@
+// This crate assumes conversion of usize to u128 never fails. Assuming index type is equal in bit
+// length to pointer type ([not necessarily true](https://github.com/rust-lang/rust/issues/65473),
+// but it is a reasonable assumption for now), this attribute protects against potential overflow
+// when converting usize to u128.
+#![cfg(any(
+    target_pointer_width = "16",
+    target_pointer_width = "32",
+    target_pointer_width = "64",
+    target_pointer_width = "128"
+))]
+
 pub mod abi;
 pub mod blockifier;
 pub mod context;
