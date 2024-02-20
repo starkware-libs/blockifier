d603b9cae79bb6d338f50d8be471793d34308abf
Yael Doweck <yael@starkware.co>
2024-02-20 13:57:03 +0200

    refactor: moving bouncer to blockifier - adding the bouncer object and the transactional part

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -1,14 +1,10 @@
 use std::ops::Sub;
 
-use crate::bouncer::{BouncerWeights, BuiltinCount};
+use crate::bouncer::{Bouncer, BouncerWeights, BuiltinCount};
 
 #[test]
 fn test_block_weights_sub_checked() {
     let max_bouncer_weights = BouncerWeights {
-        gas: 10,
-        n_steps: 10,
-        message_segment_length: 10,
-        state_diff_size: 10,
         builtin_count: BuiltinCount {
             bitwise: 10,
             ecdsa: 10,
@@ -19,13 +15,14 @@ fn test_block_weights_sub_checked() {
             poseidon: 10,
             range_check: 10,
         },
+        gas: 10,
+        message_segment_length: 10,
+        n_events: 10,
+        n_steps: 10,
+        state_diff_size: 10,
     };
 
     let bouncer_weights = BouncerWeights {
-        gas: 7,
-        n_steps: 0,
-        message_segment_length: 10,
-        state_diff_size: 7,
         builtin_count: BuiltinCount {
             bitwise: 6,
             ecdsa: 7,
@@ -36,6 +33,11 @@ fn test_block_weights_sub_checked() {
             poseidon: 9,
             range_check: 10,
         },
+        gas: 7,
+        message_segment_length: 10,
+        n_steps: 0,
+        n_events: 2,
+        state_diff_size: 7,
     };
 
     let result = max_bouncer_weights.checked_sub(bouncer_weights).unwrap();
@@ -43,10 +45,6 @@ fn test_block_weights_sub_checked() {
     assert_eq!(result, difference_bouncer_weights);
 
     let bouncer_weights_exceeds_max = BouncerWeights {
-        gas: 5,
-        n_steps: 5,
-        message_segment_length: 5,
-        state_diff_size: 5,
         builtin_count: BuiltinCount {
             bitwise: 11,
             ecdsa: 5,
@@ -57,8 +55,66 @@ fn test_block_weights_sub_checked() {
             poseidon: 5,
             range_check: 5,
         },
+        gas: 5,
+        message_segment_length: 5,
+        n_steps: 5,
+        n_events: 5,
+        state_diff_size: 5,
     };
 
     let result = max_bouncer_weights.checked_sub(bouncer_weights_exceeds_max);
     assert!(result.is_none());
+}
+
+#[test]
+fn test_transactional_bouncer() {
+    let max_bouncer_weights = BouncerWeights {
+        builtin_count: BuiltinCount {
+            bitwise: 10,
+            ecdsa: 10,
+            ec_op: 10,
+            keccak: 10,
+            output: 10,
+            pedersen: 10,
+            poseidon: 10,
+            range_check: 10,
+        },
+        gas: 10,
+        message_segment_length: 10,
+        n_steps: 10,
+        n_events: 10,
+        state_diff_size: 10,
+    };
+
+    let tx_weights = BouncerWeights {
+        builtin_count: BuiltinCount {
+            bitwise: 6,
+            ecdsa: 7,
+            ec_op: 7,
+            keccak: 8,
+            output: 7,
+            pedersen: 7,
+            poseidon: 9,
+            range_check: 10,
+        },
+        gas: 7,
+        message_segment_length: 10,
+        n_steps: 0,
+        n_events: 2,
+        state_diff_size: 7,
+    };
+
+    let bouncer = Bouncer::new(max_bouncer_weights);
+    let mut transactional_bouncer = bouncer.create_transactional();
+    transactional_bouncer.transaction.capacity =
+        transactional_bouncer.transaction.capacity.checked_sub(tx_weights).unwrap();
+
+    // Test transactional bouncer abort
+    let parent = transactional_bouncer.clone().abort();
+    assert!(parent.capacity == max_bouncer_weights);
+
+    // Test transactional bouncer commit
+    let parent = transactional_bouncer.commit();
+    let expected_capacity = max_bouncer_weights.sub(tx_weights);
+    assert!(parent.capacity == expected_capacity);
 }
