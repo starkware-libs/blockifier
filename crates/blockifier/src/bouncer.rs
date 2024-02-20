38e17545876948a1aff7c8561dc052a587f88e11
Yael Doweck <yael@starkware.co>
2024-02-20 13:57:03 +0200

    refactor: moving bouncer to blockifier - adding the bouncer object and the transactional part

--- crates/blockifier/src/bouncer.rs
+++ crates/blockifier/src/bouncer.rs
@@ -1,4 +1,9 @@
+use std::collections::HashSet;
+
 use serde::Deserialize;
+use starknet_api::core::ClassHash;
+
+use crate::state::cached_state::{StateChangesKeys, StorageEntry};
 
 #[cfg(test)]
 #[path = "bouncer_test.rs"]
@@ -21,29 +26,93 @@ macro_rules! impl_checked_sub {
 #[derive(Clone, Copy, Debug, Default, derive_more::Sub, Deserialize, PartialEq)]
 /// Represents the execution resources counted throughout block creation.
 pub struct BouncerWeights {
-    gas: u64,
-    n_steps: u64,
-    message_segment_length: u64,
-    state_diff_size: u64,
     builtin_count: BuiltinCount,
+    gas: usize,
+    message_segment_length: usize,
+    n_events: usize,
+    n_steps: usize,
+    state_diff_size: usize,
 }
 
 impl BouncerWeights {
-    impl_checked_sub!(gas, n_steps, message_segment_length, state_diff_size, builtin_count);
+    impl_checked_sub!(
+        builtin_count,
+        gas,
+        message_segment_length,
+        n_events,
+        n_steps,
+        state_diff_size
+    );
 }
 
 #[derive(Clone, Copy, Debug, Default, derive_more::Sub, Deserialize, PartialEq)]
 pub struct BuiltinCount {
-    bitwise: u64,
-    ecdsa: u64,
-    ec_op: u64,
-    keccak: u64,
-    output: u64,
-    pedersen: u64,
-    poseidon: u64,
-    range_check: u64,
+    bitwise: usize,
+    ecdsa: usize,
+    ec_op: usize,
+    keccak: usize,
+    output: usize,
+    pedersen: usize,
+    poseidon: usize,
+    range_check: usize,
 }
 
 impl BuiltinCount {
     impl_checked_sub!(bitwise, ecdsa, ec_op, keccak, output, pedersen, poseidon, range_check);
+}
+
+#[derive(Clone)]
+pub struct Bouncer {
+    pub executed_class_hashes: HashSet<ClassHash>,
+    pub visited_storage_entries: HashSet<StorageEntry>,
+    pub state_changes_keys: StateChangesKeys,
+    pub capacity: BouncerWeights,
+}
+
+impl Bouncer {
+    pub fn new(capacity: BouncerWeights) -> Self {
+        Bouncer {
+            executed_class_hashes: HashSet::new(),
+            state_changes_keys: StateChangesKeys::default(),
+            visited_storage_entries: HashSet::new(),
+            capacity,
+        }
+    }
+
+    pub fn create_transactional(self) -> TransactionBouncer {
+        TransactionBouncer::new(self)
+    }
+
+    pub fn merge(&mut self, other: Bouncer) {
+        self.executed_class_hashes.extend(other.executed_class_hashes);
+        self.state_changes_keys.extend(&other.state_changes_keys);
+        self.visited_storage_entries.extend(other.visited_storage_entries);
+        self.capacity = other.capacity;
+    }
+}
+
+#[derive(Clone)]
+pub struct TransactionBouncer {
+    // The parent bouncer can be modified only through the merge method.
+    parent: Bouncer,
+    // The transactional bouncer can be modified only through the update method.
+    transactional: Bouncer,
+}
+
+impl TransactionBouncer {
+    pub fn new(parent: Bouncer) -> TransactionBouncer {
+        let capacity = parent.capacity;
+        TransactionBouncer { parent, transactional: Bouncer::new(capacity) }
+    }
+
+    // TODO update function (in the next PR)
+
+    pub fn commit(mut self) -> Bouncer {
+        self.parent.merge(self.transactional);
+        self.parent
+    }
+
+    pub fn abort(self) -> Bouncer {
+        self.parent
+    }
 }
