15df5cdb06019158793e5c7fb4818d818d82c80e
Yael Doweck <yael@starkware.co>
2024-02-25 12:13:01 +0200

    refactor: bouncer add calculation for builtins and n_steps counting

--- crates/blockifier/src/bouncer.rs
+++ crates/blockifier/src/bouncer.rs
@@ -1,9 +1,17 @@
 use std::collections::HashSet;
 
+use cairo_vm::serde::deserialize_program::BuiltinName;
+use derive_more::Add;
 use serde::Deserialize;
 use starknet_api::core::ClassHash;
 
-use crate::state::cached_state::{StateChangesKeys, StorageEntry};
+use crate::abi::constants;
+use crate::blockifier::transaction_executor::{
+    get_casm_hash_calculation_resources, get_particia_update_resources, TransactionExecutorResult,
+};
+use crate::state::cached_state::{StateChangesKeys, StorageEntry, TransactionalState};
+use crate::state::state_api::StateReader;
+use crate::transaction::objects::ResourcesMapping;
 
 #[cfg(test)]
 #[path = "bouncer_test.rs"]
@@ -45,7 +53,7 @@ impl BouncerWeights {
     );
 }
 
-#[derive(Clone, Copy, Debug, Default, derive_more::Sub, Deserialize, PartialEq)]
+#[derive(Add, Clone, Copy, Debug, Default, derive_more::Sub, Deserialize, PartialEq)]
 pub struct BuiltinCount {
     bitwise: usize,
     ecdsa: usize,
@@ -59,6 +67,21 @@ pub struct BuiltinCount {
 
 impl BuiltinCount {
     impl_checked_sub!(bitwise, ecdsa, ec_op, keccak, output, pedersen, poseidon, range_check);
+}
+
+impl From<&ResourcesMapping> for BuiltinCount {
+    fn from(resources: &ResourcesMapping) -> Self {
+        Self {
+            bitwise: resources.get_or_default(BuiltinName::bitwise.name()),
+            ecdsa: resources.get_or_default(BuiltinName::ecdsa.name()),
+            ec_op: resources.get_or_default(BuiltinName::ec_op.name()),
+            keccak: resources.get_or_default(BuiltinName::keccak.name()),
+            output: resources.get_or_default(BuiltinName::output.name()),
+            pedersen: resources.get_or_default(BuiltinName::pedersen.name()),
+            poseidon: resources.get_or_default(BuiltinName::poseidon.name()),
+            range_check: resources.get_or_default(BuiltinName::range_check.name()),
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -106,7 +129,38 @@ impl TransactionalBouncer {
         TransactionalBouncer { parent, child: Bouncer::new(capacity) }
     }
 
-    // TODO update function (in the next PR)
+    // TODO update function (in the following PRs)
+
+    pub fn calc_n_steps_and_builtin_count<S: StateReader>(
+        &self,
+        execution_info_resources: &ResourcesMapping,
+        state: &mut TransactionalState<'_, S>,
+    ) -> TransactionExecutorResult<(usize, BuiltinCount)> {
+        // Count the additional OS resources that are not present in the transaction exection info.
+        let mut additional_os_resources = get_casm_hash_calculation_resources(
+            state,
+            &self.parent.executed_class_hashes,
+            &self.child.executed_class_hashes,
+        )?;
+        additional_os_resources += &get_particia_update_resources(
+            &self.parent.visited_storage_entries,
+            &self.child.visited_storage_entries,
+        )?;
+        let additional_builtin_count =
+            BuiltinCount::from(&ResourcesMapping(additional_os_resources.builtin_instance_counter));
+
+        // Sum all the builtin resources.
+        let execution_info_builtin_count = BuiltinCount::from(execution_info_resources);
+        let builtin_count = execution_info_builtin_count + additional_builtin_count;
+
+        // The n_steps counter also includes the count of memory holes.
+        let n_steps = additional_os_resources.n_steps
+            + execution_info_resources.get_or_default(constants::N_STEPS_RESOURCE)
+            + additional_os_resources.n_memory_holes
+            + execution_info_resources.get_or_default(constants::N_MEMORY_HOLES);
+
+        Ok((n_steps, builtin_count))
+    }
 
     pub fn commit(mut self) -> Bouncer {
         self.parent.merge(self.child);
