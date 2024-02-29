eefbd0e9fd80e9c55f47d9da3f7c3ee3675ffc7a
Arni Hod <arnon@starkware.co>
2024-02-29 16:21:39 +0200

    test: remove the test util deprecated_create_deploy_test_state

--- crates/blockifier/src/test_utils/cached_state.rs
+++ crates/blockifier/src/test_utils/cached_state.rs
@@ -5,20 +5,14 @@ use starknet_api::hash::{StarkFelt, StarkHash};
 use starknet_api::transaction::{Calldata, ContractAddressSalt};
 use starknet_api::{calldata, class_hash, contract_address, patricia_key, stark_felt};
 
-use crate::execution::contract_class::{ContractClassV0, ContractClassV1};
+use crate::execution::contract_class::ContractClassV1;
 use crate::state::cached_state::{CachedState, ContractClassMapping};
 use crate::test_utils::dict_state_reader::DictStateReader;
 use crate::test_utils::{
-    LEGACY_TEST_CLASS_HASH, LEGACY_TEST_CONTRACT_CAIRO1_PATH, SECURITY_TEST_CLASS_HASH,
-    SECURITY_TEST_CONTRACT_CAIRO0_PATH, TEST_CLASS_HASH, TEST_CONTRACT_ADDRESS,
-    TEST_CONTRACT_CAIRO0_PATH, TEST_CONTRACT_CAIRO1_PATH, TEST_EMPTY_CONTRACT_CAIRO0_PATH,
-    TEST_EMPTY_CONTRACT_CAIRO1_PATH, TEST_EMPTY_CONTRACT_CLASS_HASH,
+    LEGACY_TEST_CLASS_HASH, LEGACY_TEST_CONTRACT_CAIRO1_PATH, TEST_CLASS_HASH,
+    TEST_CONTRACT_ADDRESS, TEST_CONTRACT_CAIRO1_PATH, TEST_EMPTY_CONTRACT_CAIRO1_PATH,
+    TEST_EMPTY_CONTRACT_CLASS_HASH,
 };
-
-pub fn deprecated_create_deploy_test_state() -> CachedState<DictStateReader> {
-    let class_hash_to_class = get_class_hash_to_v0_class_mapping();
-    create_deploy_test_state_from_classes(class_hash_to_class)
-}
 
 pub fn create_deploy_test_state() -> CachedState<DictStateReader> {
     let class_hash_to_class = get_class_hash_to_v1_class_mapping();
@@ -48,23 +42,6 @@ fn create_deploy_test_state_from_classes(
         address_to_class_hash,
         ..Default::default()
     })
-}
-
-fn get_class_hash_to_v0_class_mapping() -> ContractClassMapping {
-    HashMap::from([
-        (
-            class_hash!(TEST_CLASS_HASH),
-            ContractClassV0::from_file(TEST_CONTRACT_CAIRO0_PATH).into(),
-        ),
-        (
-            class_hash!(SECURITY_TEST_CLASS_HASH),
-            ContractClassV0::from_file(SECURITY_TEST_CONTRACT_CAIRO0_PATH).into(),
-        ),
-        (
-            class_hash!(TEST_EMPTY_CONTRACT_CLASS_HASH),
-            ContractClassV0::from_file(TEST_EMPTY_CONTRACT_CAIRO0_PATH).into(),
-        ),
-    ])
 }
 
 fn get_class_hash_to_v1_class_mapping() -> ContractClassMapping {
