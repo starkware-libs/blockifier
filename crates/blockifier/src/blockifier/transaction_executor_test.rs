efc792142456b857bdc5802b7bb7e19dd00ebc0f
Arni Hod <arnon@starkware.co>
2024-03-03 16:45:44 +0200

    test: create test for tx_executor

---
+++ crates/blockifier/src/blockifier/transaction_executor_test.rs
@@ -0,0 +1,72 @@
+use pretty_assertions::assert_eq;
+use rstest::rstest;
+use starknet_api::calldata;
+use starknet_api::core::ContractAddress;
+use starknet_api::hash::StarkFelt;
+use starknet_api::transaction::{Calldata, Fee};
+
+use crate::blockifier::bouncer::BouncerInfo;
+use crate::blockifier::transaction_executor::TransactionExecutor;
+use crate::context::BlockContext;
+use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::initial_test_state::test_state;
+use crate::test_utils::{CairoVersion, BALANCE};
+use crate::transaction::test_utils::block_context;
+use crate::transaction::transaction_execution::Transaction;
+use crate::transaction::transaction_types::TransactionType;
+use crate::transaction::transactions::L1HandlerTransaction;
+
+// Utils.
+
+fn l1_handler(contract_address: ContractAddress) -> Transaction {
+    let calldata = calldata![
+        StarkFelt::from_u128(0x123), // from_address.
+        StarkFelt::from_u128(0x876), // key.
+        StarkFelt::from_u128(0x44)   // value.
+    ];
+    Transaction::L1HandlerTransaction(L1HandlerTransaction::create_for_testing(
+        &calldata,
+        Fee(1908000000000000),
+        contract_address,
+    ))
+}
+
+#[rstest]
+#[case::l1_handler(
+    TransactionType::L1Handler,
+    BouncerInfo {
+        state_diff_size: 4,
+        message_segment_length: 7,
+        n_events: 0,
+        ..Default::default()
+    }
+)]
+
+fn test_tx_executor(
+    block_context: BlockContext,
+    #[case] tx_type: TransactionType,
+    #[case] expected_bouncer_info: BouncerInfo,
+    #[values(true, false)] charge_fee: bool,
+) {
+    // Setup context.
+    let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1);
+    let state = test_state(&block_context.chain_info, BALANCE, &[(test_contract, 1)]);
+
+    // Create the tx executor.
+    let mut tx_executor = TransactionExecutor::new(state, block_context);
+
+    // Create the tested tx.
+    let tx = match tx_type {
+        TransactionType::Declare => todo!(),
+        TransactionType::DeployAccount => todo!(),
+        TransactionType::InvokeFunction => todo!(),
+        TransactionType::L1Handler => l1_handler(test_contract.get_instance_address(0)),
+    };
+    // TODO(Arni, 30/03/2024): Consider adding a test for the transaction execution info. If A test
+    // should not be added, rename the test to `test_bouncer_info`.
+    let (_tx_execution_info, bouncer_info) = tx_executor.execute(tx, charge_fee).unwrap();
+
+    assert_eq!(bouncer_info.state_diff_size, expected_bouncer_info.state_diff_size);
+    assert_eq!(bouncer_info.message_segment_length, expected_bouncer_info.message_segment_length);
+    assert_eq!(bouncer_info.n_events, expected_bouncer_info.n_events);
+}
