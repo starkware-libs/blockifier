9af141344d79b6859f2517118f02e429f705d166
Arni Hod <arnon@starkware.co>
2024-03-03 16:45:44 +0200

    test: add deploy account scenario to test_tx_executor

--- crates/blockifier/src/blockifier/transaction_executor_test.rs
+++ crates/blockifier/src/blockifier/transaction_executor_test.rs
@@ -7,17 +7,20 @@ use cairo_vm::vm::runners::builtin_runner::{
 use pretty_assertions::assert_eq;
 use rstest::rstest;
 use starknet_api::calldata;
-use starknet_api::core::ContractAddress;
+use starknet_api::core::{ClassHash, ContractAddress};
 use starknet_api::hash::StarkFelt;
-use starknet_api::transaction::{Calldata, Fee};
+use starknet_api::transaction::{Calldata, Fee, TransactionVersion};
 
 use crate::blockifier::bouncer::BouncerInfo;
 use crate::blockifier::transaction_executor::TransactionExecutor;
 use crate::context::BlockContext;
+use crate::deploy_account_tx_args;
 use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::deploy_account::deploy_account_tx;
 use crate::test_utils::initial_test_state::test_state;
-use crate::test_utils::{CairoVersion, BALANCE};
-use crate::transaction::test_utils::block_context;
+use crate::test_utils::{CairoVersion, NonceManager, BALANCE, DEFAULT_STRK_L1_GAS_PRICE};
+use crate::transaction::account_transaction::AccountTransaction;
+use crate::transaction::test_utils::{block_context, l1_resource_bounds};
 use crate::transaction::transaction_execution::Transaction;
 use crate::transaction::transaction_types::TransactionType;
 use crate::transaction::transactions::L1HandlerTransaction;
@@ -43,6 +46,17 @@ fn build_expected_builtin_instance_counter(
     expected_builtin_instance_counter
 }
 
+fn deploy_account(class_hash: ClassHash, version: TransactionVersion) -> Transaction {
+    Transaction::AccountTransaction(AccountTransaction::DeployAccount(deploy_account_tx(
+        deploy_account_tx_args! {
+            class_hash,
+            resource_bounds: l1_resource_bounds(0, DEFAULT_STRK_L1_GAS_PRICE),
+            version,
+        },
+        &mut NonceManager::default(),
+    )))
+}
+
 fn l1_handler(contract_address: ContractAddress) -> Transaction {
     let calldata = calldata![
         StarkFelt::from_u128(0x123), // from_address.
@@ -57,8 +71,25 @@ fn l1_handler(contract_address: ContractAddress) -> Transaction {
 }
 
 #[rstest]
+#[case::deploy_account_tx(
+    TransactionType::DeployAccount, TransactionVersion::THREE, BouncerInfo {
+        state_diff_size: 3,
+        gas_weight: 0,
+        message_segment_length: 0,
+        execution_resources: cairo_vm::vm::runners::cairo_runner::ExecutionResources {
+            n_steps: 5549,
+            n_memory_holes: 0,
+            builtin_instance_counter: build_expected_builtin_instance_counter(HashMap::from([
+                (HASH_BUILTIN_NAME.to_string(), 241),
+                (RANGE_CHECK_BUILTIN_NAME.to_string(), 83),
+            ])),
+        },
+        n_events: 0,
+    }
+)]
 #[case::l1_handler(
     TransactionType::L1Handler,
+    TransactionVersion::ZERO, // The version of a L1HandlerTransaction is always zero.
     BouncerInfo {
         state_diff_size: 4,
         gas_weight: 11739,
@@ -79,12 +110,14 @@ fn l1_handler(contract_address: ContractAddress) -> Transaction {
 fn test_tx_executor(
     block_context: BlockContext,
     #[case] tx_type: TransactionType,
+    #[case] version: TransactionVersion,
     #[case] expected_bouncer_info: BouncerInfo,
     #[values(true, false)] charge_fee: bool,
 ) {
     // Setup context.
+    let account = FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0);
     let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1);
-    let state = test_state(&block_context.chain_info, BALANCE, &[(test_contract, 1)]);
+    let state = test_state(&block_context.chain_info, BALANCE, &[(account, 1), (test_contract, 1)]);
 
     // Create the tx executor.
     let mut tx_executor = TransactionExecutor::new(state, block_context);
@@ -92,7 +125,7 @@ fn test_tx_executor(
     // Create the tested tx.
     let tx = match tx_type {
         TransactionType::Declare => todo!(),
-        TransactionType::DeployAccount => todo!(),
+        TransactionType::DeployAccount => deploy_account(account.get_class_hash(), version),
         TransactionType::InvokeFunction => todo!(),
         TransactionType::L1Handler => l1_handler(test_contract.get_instance_address(0)),
     };
