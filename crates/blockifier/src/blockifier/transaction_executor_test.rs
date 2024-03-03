241a0f946cca41ba393a493353c073cbc8390e67
Arni Hod <arnon@starkware.co>
2024-03-03 16:45:44 +0200

    test: add deploy account scenario to test_tx_executor

--- crates/blockifier/src/blockifier/transaction_executor_test.rs
+++ crates/blockifier/src/blockifier/transaction_executor_test.rs
@@ -6,7 +6,7 @@ use cairo_vm::vm::runners::builtin_runner::{
 };
 use pretty_assertions::assert_eq;
 use rstest::rstest;
-use starknet_api::core::{ContractAddress, Nonce};
+use starknet_api::core::{ClassHash, ContractAddress, Nonce};
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{Calldata, Fee, TransactionHash, TransactionVersion};
 
@@ -14,10 +14,13 @@ use crate::abi::abi_utils::selector_from_name;
 use crate::blockifier::bouncer::BouncerInfo;
 use crate::blockifier::transaction_executor::TransactionExecutor;
 use crate::context::BlockContext;
+use crate::deploy_account_tx_args;
 use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::deploy_account::deploy_account_tx;
 use crate::test_utils::initial_test_state::test_state;
-use crate::test_utils::{CairoVersion, BALANCE};
-use crate::transaction::test_utils::block_context;
+use crate::test_utils::{CairoVersion, NonceManager, BALANCE, DEFAULT_STRK_L1_GAS_PRICE};
+use crate::transaction::account_transaction::AccountTransaction;
+use crate::transaction::test_utils::{block_context, l1_resource_bounds};
 use crate::transaction::transaction_execution::Transaction;
 use crate::transaction::transaction_types::TransactionType;
 use crate::transaction::transactions::L1HandlerTransaction;
@@ -43,6 +46,19 @@ fn build_expected_builtin_instance_counter(
     expected_builtin_instance_counter
 }
 
+fn deploy_account_tx_for_test(class_hash: ClassHash, version: TransactionVersion) -> Transaction {
+    let zero_bounds = true;
+    Transaction::AccountTransaction(AccountTransaction::DeployAccount(deploy_account_tx(
+        deploy_account_tx_args! {
+            class_hash,
+            max_fee: Fee(u128::from(!zero_bounds)),
+            resource_bounds: l1_resource_bounds(u64::from(!zero_bounds), DEFAULT_STRK_L1_GAS_PRICE),
+            version,
+        },
+        &mut NonceManager::default(),
+    )))
+}
+
 fn l1_handler_for_test(
     contract_address: ContractAddress,
     version: TransactionVersion,
@@ -66,6 +82,20 @@ fn l1_handler_for_test(
 }
 
 #[rstest]
+#[case::deploy_account_tx(TransactionType::DeployAccount, TransactionVersion::THREE, BouncerInfo {
+    state_diff_size: 3,
+    gas_weight: 0,
+    message_segment_length: 0,
+    execution_resources: cairo_vm::vm::runners::cairo_runner::ExecutionResources {
+        n_steps: 5549,
+        n_memory_holes: 0,
+        builtin_instance_counter: build_expected_builtin_instance_counter(HashMap::from([
+            (HASH_BUILTIN_NAME.to_string(), 241),
+            (RANGE_CHECK_BUILTIN_NAME.to_string(), 83),
+        ])),
+    },
+    n_events: 0,
+})]
 #[case::l1_handler(TransactionType::L1Handler, TransactionVersion::ZERO, BouncerInfo {
     state_diff_size: 4,
     gas_weight: 11739,
@@ -93,8 +123,9 @@ fn test_tx_executor(
     // constants for the test.
 
     // Setup context.
+    let account = FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0);
     let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1);
-    let state = test_state(&block_context.chain_info, BALANCE, &[(test_contract, 1)]);
+    let state = test_state(&block_context.chain_info, BALANCE, &[(account, 1), (test_contract, 1)]);
 
     // Create the tx executor.
     let mut tx_executor = TransactionExecutor::new(state, block_context);
@@ -102,7 +133,9 @@ fn test_tx_executor(
     // Create the tested tx.
     let tx = match tx_type {
         TransactionType::Declare => todo!(),
-        TransactionType::DeployAccount => todo!(),
+        TransactionType::DeployAccount => {
+            deploy_account_tx_for_test(account.get_class_hash(), version)
+        }
         TransactionType::InvokeFunction => todo!(),
         TransactionType::L1Handler => {
             l1_handler_for_test(test_contract.get_instance_address(0), version)
