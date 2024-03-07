b768ebba0d7824b36a65546ac9ca57fb41d6ef50
Yael Doweck <yael@starkware.co>
2024-03-07 11:18:50 +0200

    refactor(execution): bouncer update function

--- crates/blockifier/src/bouncer_test.rs
+++ crates/blockifier/src/bouncer_test.rs
@@ -1,6 +1,19 @@
+use std::collections::HashMap;
 use std::ops::Sub;
 
+use assert_matches::assert_matches;
+use cairo_vm::serde::deserialize_program::BuiltinName;
+
+use super::BouncerConfig;
+use crate::abi::constants;
+use crate::blockifier::transaction_executor::TransactionExecutorError;
 use crate::bouncer::{Bouncer, BouncerWeights, BuiltinCount};
+use crate::context::BlockContext;
+use crate::execution::call_info::ExecutionSummary;
+use crate::state::cached_state::CachedState;
+use crate::test_utils::initial_test_state::test_state;
+use crate::transaction::errors::TransactionExecutionError;
+use crate::transaction::objects::ResourcesMapping;
 
 #[test]
 fn test_block_weights_sub_checked() {
@@ -99,15 +112,190 @@ fn test_transactional_bouncer() {
         state_diff_size: 2,
     };
 
-    let bouncer = Bouncer::new(initial_bouncer_weights);
+    let bouncer = Bouncer::new(initial_bouncer_weights, true);
     let mut transactional_bouncer = bouncer.create_transactional();
-    transactional_bouncer.transactional.capacity = weights_to_commit;
+    transactional_bouncer.transactional.available_capacity = weights_to_commit;
 
     // Test transactional bouncer abort.
     let final_weights = transactional_bouncer.clone().abort();
-    assert!(final_weights.capacity == initial_bouncer_weights);
+    assert!(final_weights.available_capacity == initial_bouncer_weights);
 
     // Test transactional bouncer commit.
     let final_weights = transactional_bouncer.commit();
-    assert!(final_weights.capacity == weights_to_commit);
+    assert!(final_weights.available_capacity == weights_to_commit);
+}
+
+#[test]
+fn test_update_capcity() {
+    let state = &mut test_state(&BlockContext::create_for_account_testing().chain_info, 0, &[]);
+    let mut transactional_state = CachedState::create_transactional(state);
+
+    let execution_summary = ExecutionSummary { ..Default::default() };
+    let bouncer = Bouncer {
+        available_capacity: BouncerWeights {
+            builtin_count: BuiltinCount {
+                bitwise: 10,
+                ecdsa: 10,
+                ec_op: 10,
+                keccak: 0,
+                pedersen: 10,
+                poseidon: 10,
+                range_check: 10,
+            },
+            gas: 10,
+            message_segment_length: 10,
+            n_steps: 10,
+            n_events: 10,
+            state_diff_size: 10,
+        },
+        block_contains_keccak: false,
+        ..Default::default()
+    };
+    let mut transactional_bouncer = bouncer.create_transactional();
+
+    let block_max_capacity = BouncerWeights {
+        builtin_count: BuiltinCount {
+            bitwise: 20,
+            ecdsa: 20,
+            ec_op: 20,
+            keccak: 0,
+            pedersen: 20,
+            poseidon: 20,
+            range_check: 20,
+        },
+        gas: 20,
+        message_segment_length: 20,
+        n_steps: 20,
+        n_events: 20,
+        state_diff_size: 20,
+    };
+    let mut block_max_capacity_with_keccak = block_max_capacity;
+    block_max_capacity_with_keccak.builtin_count.keccak = 1;
+
+    let mut bouncer_config = BouncerConfig { block_max_capacity, block_max_capacity_with_keccak };
+
+    let mut bouncer_resources: HashMap<String, usize> = HashMap::new();
+    bouncer_resources.insert(BuiltinName::bitwise.name().to_string(), 1);
+    bouncer_resources.insert(BuiltinName::ecdsa.name().to_string(), 1);
+    bouncer_resources.insert(BuiltinName::ec_op.name().to_string(), 1);
+    // Keccak is 0 since it has special handling which will be tested separately.
+    bouncer_resources.insert(BuiltinName::keccak.name().to_string(), 0);
+    bouncer_resources.insert(BuiltinName::pedersen.name().to_string(), 1);
+    bouncer_resources.insert(BuiltinName::poseidon.name().to_string(), 1);
+    bouncer_resources.insert(BuiltinName::range_check.name().to_string(), 1);
+    bouncer_resources.insert(constants::BLOB_GAS_USAGE.to_string(), 1);
+    bouncer_resources.insert(constants::L1_GAS_USAGE.to_string(), 1);
+    bouncer_resources.insert(constants::N_STEPS_RESOURCE.to_string(), 1);
+    bouncer_resources.insert(constants::N_MEMORY_HOLES.to_string(), 1);
+    let mut bouncer_resources = ResourcesMapping(bouncer_resources);
+
+    // Test for success.
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(result, Ok(()));
+
+    // Test for BlockFull error.
+    bouncer_resources.0.insert(BuiltinName::bitwise.name().to_string(), 10);
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(
+        result,
+        Err(TransactionExecutorError::TransactionExecutionError(
+            TransactionExecutionError::BlockFull
+        ))
+    );
+
+    // Test for TxTooLarge error.
+    bouncer_resources.0.insert(BuiltinName::bitwise.name().to_string(), 21);
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(
+        result,
+        Err(TransactionExecutorError::TransactionExecutionError(
+            TransactionExecutionError::TxTooLarge
+        ))
+    );
+    bouncer_resources.0.insert(BuiltinName::bitwise.name().to_string(), 1);
+
+    // Test a transaction with keccak.
+    bouncer_resources.0.insert(BuiltinName::keccak.name().to_string(), 1);
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(result, Ok(()));
+
+    // Test for BlockFull error with keccak, transaction weights exceeds limit.
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(
+        result,
+        Err(TransactionExecutorError::TransactionExecutionError(
+            TransactionExecutionError::BlockFull
+        ))
+    );
+
+    // Test for BlockFull error with keccak, keccak_max_capacity is too low.
+    let bouncer = Bouncer {
+        available_capacity: BouncerWeights {
+            builtin_count: BuiltinCount {
+                bitwise: 5,
+                ecdsa: 10,
+                ec_op: 10,
+                keccak: 0,
+                pedersen: 10,
+                poseidon: 10,
+                range_check: 10,
+            },
+            gas: 10,
+            message_segment_length: 10,
+            n_steps: 10,
+            n_events: 10,
+            state_diff_size: 10,
+        },
+        block_contains_keccak: true,
+        ..Default::default()
+    };
+    let mut transactional_bouncer = bouncer.create_transactional();
+    // Reduce the max capacity of bitwise for keccak, so the already used bitwise will not fit in
+    // the block once the capacity is updated with keccak.
+    bouncer_config.block_max_capacity_with_keccak.builtin_count.bitwise = 10;
+    // There is one keccak usage so this resource should fit into a keccak block.
+    bouncer_resources.0.insert(BuiltinName::keccak.name().to_string(), 1);
+    let result = transactional_bouncer.update_available_capacity(
+        &bouncer_config,
+        &mut transactional_state,
+        &execution_summary,
+        &bouncer_resources,
+        None,
+    );
+    assert_matches!(
+        result,
+        Err(TransactionExecutorError::TransactionExecutionError(
+            TransactionExecutionError::BlockFull
+        ))
+    );
 }
