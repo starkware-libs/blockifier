61e8c7026de8216c72a7ab05734a75312b204c0d
Arni Hod <arnon@starkware.co>
2024-03-11 11:03:47 +0200

    test: declare on tx_executor

--- crates/blockifier/src/blockifier/transaction_executor_test.rs
+++ crates/blockifier/src/blockifier/transaction_executor_test.rs
@@ -10,16 +10,19 @@ use crate::context::BlockContext;
 use crate::state::cached_state::CachedState;
 use crate::state::state_api::StateReader;
 use crate::test_utils::contracts::FeatureContract;
+use crate::test_utils::declare::declare_tx;
 use crate::test_utils::deploy_account::deploy_account_tx;
 use crate::test_utils::initial_test_state::test_state;
 use crate::test_utils::{
     create_calldata, CairoVersion, NonceManager, BALANCE, DEFAULT_STRK_L1_GAS_PRICE,
 };
 use crate::transaction::account_transaction::AccountTransaction;
-use crate::transaction::test_utils::{account_invoke_tx, block_context, l1_resource_bounds};
+use crate::transaction::test_utils::{
+    account_invoke_tx, block_context, calculate_class_info_for_testing, l1_resource_bounds,
+};
 use crate::transaction::transaction_execution::Transaction;
 use crate::transaction::transactions::L1HandlerTransaction;
-use crate::{deploy_account_tx_args, invoke_tx_args};
+use crate::{declare_tx_args, deploy_account_tx_args, invoke_tx_args};
 
 fn tx_executor_test_body<S: StateReader>(
     state: CachedState<S>,
@@ -40,7 +43,72 @@ fn tx_executor_test_body<S: StateReader>(
 }
 
 #[rstest]
-fn test_tx_executor_on_deploy_account(
+#[case(
+    TransactionVersion::ZERO,
+    CairoVersion::Cairo0,
+    BouncerInfo {
+        state_diff_size: 0,
+        message_segment_length: 0,
+        n_events: 0,
+        ..Default::default()
+    }
+)]
+#[case(
+    TransactionVersion::ONE,
+    CairoVersion::Cairo0,
+    BouncerInfo {
+        state_diff_size: 2,
+        message_segment_length: 0,
+        n_events: 0,
+        ..Default::default()
+    }
+)]
+#[case(
+    TransactionVersion::TWO,
+    CairoVersion::Cairo1,
+    BouncerInfo {
+        state_diff_size: 4,
+        message_segment_length: 0,
+        n_events: 0,
+        ..Default::default()
+    }
+)]
+#[case(
+    TransactionVersion::THREE,
+    CairoVersion::Cairo1,
+    BouncerInfo {
+        state_diff_size: 4,
+        message_segment_length: 0,
+        n_events: 0,
+        ..Default::default()
+    }
+)]
+fn test_declare(
+    block_context: BlockContext,
+    #[values(true, false)] charge_fee: bool,
+    #[values(CairoVersion::Cairo0, CairoVersion::Cairo1)] account_cairo_version: CairoVersion,
+    #[case] transaction_version: TransactionVersion,
+    #[case] cairo_version: CairoVersion,
+    #[case] expected_bouncer_info: BouncerInfo,
+) {
+    let account_contract = FeatureContract::AccountWithoutValidations(account_cairo_version);
+    let declared_contract = FeatureContract::Empty(cairo_version);
+    let state = test_state(&block_context.chain_info, BALANCE, &[(account_contract, 1)]);
+
+    let tx = Transaction::AccountTransaction(declare_tx(
+        declare_tx_args! {
+            sender_address: account_contract.get_instance_address(0),
+            class_hash: declared_contract.get_class_hash(),
+            version: transaction_version,
+            resource_bounds: l1_resource_bounds(0, DEFAULT_STRK_L1_GAS_PRICE),
+        },
+        calculate_class_info_for_testing(declared_contract.get_class()),
+    ));
+    tx_executor_test_body(state, block_context, tx, charge_fee, expected_bouncer_info);
+}
+
+#[rstest]
+fn test_deploy_account(
     block_context: BlockContext,
     #[values(TransactionVersion::ONE, TransactionVersion::THREE)] version: TransactionVersion,
     #[values(CairoVersion::Cairo0, CairoVersion::Cairo1)] cairo_version: CairoVersion,
@@ -104,7 +172,7 @@ fn test_tx_executor_on_deploy_account(
         ..Default::default()
     }
 )]
-fn test_tx_executor_on_invoke(
+fn test_invoke(
     block_context: BlockContext,
     #[values(true, false)] charge_fee: bool,
     #[values(TransactionVersion::ONE, TransactionVersion::THREE)] version: TransactionVersion,
@@ -132,10 +200,7 @@ fn test_tx_executor_on_invoke(
 }
 
 #[rstest]
-fn test_tx_executor_on_l1_handler(
-    block_context: BlockContext,
-    #[values(true, false)] charge_fee: bool,
-) {
+fn test_l1_handler(block_context: BlockContext, #[values(true, false)] charge_fee: bool) {
     let test_contract = FeatureContract::TestContract(CairoVersion::Cairo1);
     let state = test_state(&block_context.chain_info, BALANCE, &[(test_contract, 1)]);
 
