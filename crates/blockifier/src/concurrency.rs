c597025acbd5b1c41ce54c93fc117e21b6a0bcfb
Barak <barakb@starkware.co>
2024-03-17 17:53:56 +0200

    feat: scheduler basics

--- crates/blockifier/src/concurrency.rs
+++ crates/blockifier/src/concurrency.rs
@@ -1,3 +1,4 @@
+pub mod scheduler;
 pub mod versioned_storage;
 
 type Version = u64;
---
+++ crates/blockifier/src/concurrency/scheduler.rs
@@ -0,0 +1,46 @@
+#[cfg(test)]
+#[path = "scheduler_test.rs"]
+pub mod test;
+
+pub struct Scheduler;
+
+impl Scheduler {
+    pub fn done() -> bool {
+        todo!()
+    }
+
+    pub fn next_task() -> Task {
+        todo!()
+    }
+
+    // TODO(barak, 01/04/2024): Ensure documentation matches logic.
+    /// Schedules the required tasks necessitated by the last task of the thread.
+    /// Returns the next VALIDATION task that should be done by the thread if 'validation_idx` >
+    /// 'tx_idx', otherwise, Task.kind is 'None'.
+    pub fn finish_execution() -> Task {
+        todo!()
+    }
+
+    // TODO(barak, 01/04/2024): Ensure documentation matches logic.
+    /// Schedules the required tasks necessitated by the last task of the thread.
+    /// Returns the next EXECUTION task that should be done by the thread if 'execution_idx` >
+    /// 'tx_idx', otherwise, Task.kind is 'None'.
+    pub fn finish_validation() -> Task {
+        todo!()
+    }
+}
+
+pub struct TaskVersion {
+    pub tx_idx: usize,
+    // pub incarnation_number: usize,
+}
+
+pub enum TaskKind {
+    EXECUTION,
+    VALIDATION,
+}
+
+pub struct Task {
+    pub version: TaskVersion,
+    pub kind: Option<TaskKind>,
+}
