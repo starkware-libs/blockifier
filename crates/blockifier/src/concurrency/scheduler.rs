4782a17a79e404fff84be10266cc34ee83e2bc1e
Barak <barakb@starkware.co>
2024-03-31 15:14:16 +0300

    fix(concurrency): change u32 to usize

--- crates/blockifier/src/concurrency/scheduler.rs
+++ crates/blockifier/src/concurrency/scheduler.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use crate::concurrency::Version;
 
@@ -10,20 +10,20 @@ pub mod test;
 #[allow(dead_code)]
 #[derive(Default)]
 pub struct Scheduler {
-    execution_index: AtomicU32,
-    validation_index: AtomicU32,
+    execution_index: AtomicUsize,
+    validation_index: AtomicUsize,
     /// Read twice upon checking the chunk completion. Used to detect if validation or execution
     /// index decreased from their observed values after ensuring that the number of active tasks
     /// is zero.
-    decrease_counter: AtomicU32,
-    n_active_tasks: AtomicU32,
-    chunk_size: u32,
+    decrease_counter: AtomicUsize,
+    n_active_tasks: AtomicUsize,
+    chunk_size: usize,
 }
 
 // TODO(Avi, 01/04/2024): Remove dead_code attribute.
 #[allow(dead_code)]
 impl Scheduler {
-    pub fn new(chunk_size: u32) -> Scheduler {
+    pub fn new(chunk_size: usize) -> Scheduler {
         Scheduler { chunk_size, validation_index: chunk_size.into(), ..Default::default() }
     }
 
@@ -57,27 +57,27 @@ impl Scheduler {
         todo!()
     }
 
-    fn decrease_validation_index(&self, target_idx: u32) {
-        self.validation_index.fetch_min(target_idx, Ordering::SeqCst);
+    fn decrease_validation_index(&self, target_index: usize) {
+        self.validation_index.fetch_min(target_index, Ordering::SeqCst);
         self.decrease_counter.fetch_add(1, Ordering::SeqCst);
     }
 
-    fn decrease_execution_index(&self, target_idx: u32) {
-        self.execution_index.fetch_min(target_idx, Ordering::SeqCst);
+    fn decrease_execution_index(&self, target_index: usize) {
+        self.execution_index.fetch_min(target_index, Ordering::SeqCst);
         self.decrease_counter.fetch_add(1, Ordering::SeqCst);
     }
 
     /// Updates a transaction's status to `Executing` if it is ready to execute.
-    fn try_incarnate(&self, tx_idx: u32) -> Option<u32> {
-        if tx_idx < self.chunk_size {
+    fn try_incarnate(&self, tx_index: usize) -> Option<usize> {
+        if tx_index < self.chunk_size {
             // TODO(barak, 01/04/2024): complete try_incarnate logic.
-            return Some(tx_idx);
+            return Some(tx_index);
         }
         self.n_active_tasks.fetch_sub(1, Ordering::SeqCst);
         None
     }
 
-    fn next_version_to_validate(&self) -> Option<u32> {
+    fn next_version_to_validate(&self) -> Option<usize> {
         let index_to_validate = self.validation_index.load(Ordering::Acquire);
         if index_to_validate >= self.chunk_size {
             self.check_done();
@@ -93,7 +93,7 @@ impl Scheduler {
         None
     }
 
-    fn next_version_to_execute(&self) -> Option<u32> {
+    fn next_version_to_execute(&self) -> Option<usize> {
         let index_to_execute = self.execution_index.load(Ordering::Acquire);
         if index_to_execute >= self.chunk_size {
             self.check_done();
