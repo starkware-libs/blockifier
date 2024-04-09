eafbeabe6eb6e0c6a6a173556334c2907d3607e4
Barak <barakb@starkware.co>
2024-04-09 15:56:49 +0300

    test(concurrency): test decrease_validation_index

--- crates/blockifier/src/test_utils/struct_impls.rs
+++ crates/blockifier/src/test_utils/struct_impls.rs
@@ -181,3 +181,31 @@ impl ContractClassV1 {
         Self::try_from_json_string(&raw_contract_class).unwrap()
     }
 }
+
+// Concurrency.
+
+#[macro_export]
+macro_rules! default_scheduler {
+    ($chunk_size:ident : $chunk:expr , $($field:ident $(: $value:expr)?),+) => {
+        Scheduler {
+            $chunk_size: $chunk,
+            $($field $(: $value.into())?,)*
+            tx_statuses: std::iter::repeat_with(|| std::sync::Mutex::new(TransactionStatus::ReadyToExecute))
+                .take($chunk)
+                .collect(),
+            ..Default::default()
+        }
+    };
+    ($chunk_size:ident $(, $field:ident $(: $value:expr)?),+) => {
+        Scheduler {
+            $chunk_size,
+            $($field $(: $value.into())?,)*
+            tx_statuses: std::iter::repeat_with(|| std::sync::Mutex::new(TransactionStatus::ReadyToExecute))
+                .take($chunk_size)
+                .collect(),
+            ..Default::default()
+        }
+    };
+
+
+}
