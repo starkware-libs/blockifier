d1ba892834357e0feecb946621c112cb43f437c1
Barak <barakb@starkware.co>
2024-04-09 15:56:49 +0300

    test(concurrency): test decrease_validation_index

--- crates/blockifier/src/test_utils/struct_impls.rs
+++ crates/blockifier/src/test_utils/struct_impls.rs
@@ -219,3 +219,34 @@ impl BuiltinCount {
         }
     }
 }
+
+// Concurrency.
+
+#[cfg(any(feature = "concurrency", test))]
+#[macro_export]
+macro_rules! default_scheduler {
+    ($chunk_size:ident : $chunk:expr , $($field:ident $(: $value:expr)?),+ $(,)?) => {
+        Scheduler {
+            $chunk_size: $chunk,
+            $($field $(: $value.into())?,)*
+            tx_statuses: std::iter::repeat_with(|| std::sync::Mutex::new(
+                    $crate::concurrency::scheduler::TransactionStatus::ReadyToExecute
+                ))
+                .take($chunk)
+                .collect(),
+            ..Default::default()
+        }
+    };
+    ($chunk_size:ident $(, $field:ident $(: $value:expr)?),+ $(,)?) => {
+        Scheduler {
+            $chunk_size,
+            $($field $(: $value.into())?,)*
+            tx_statuses: std::iter::repeat_with(|| std::sync::Mutex::new(
+                    $crate::concurrency::scheduler::TransactionStatus::ReadyToExecute
+                ))
+                .take($chunk_size)
+                .collect(),
+            ..Default::default()
+        }
+    };
+}
