99e0a818845f9c6cc0ad9f4fa8c3f2381a0bc32d
Yoni <78365039+Yoni-Starkware@users.noreply.github.com>
2024-04-14 08:07:41 +0300

    refactor: move BlockFull error (#1785)

--- crates/blockifier/src/transaction/errors.rs
+++ crates/blockifier/src/transaction/errors.rs
@@ -51,8 +51,6 @@ pub enum TransactionFeeError {
 
 #[derive(Debug, Error)]
 pub enum TransactionExecutionError {
-    #[error("Transaction cannot be added to the current block, block capacity reached.")]
-    BlockFull,
     #[error(
         "Declare transaction version {declare_version:?} must have a contract class of Cairo \
          version {cairo_version:?}."
