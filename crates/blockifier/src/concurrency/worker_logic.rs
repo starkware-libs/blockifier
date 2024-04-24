6867979841b55a0c5de10c3c6932c9bd254f47db
meship-starkware <meshi@starkware.co>
2024-04-24 12:11:24 +0300

    feat(concurrency): write commit transaction

--- crates/blockifier/src/concurrency/worker_logic.rs
+++ crates/blockifier/src/concurrency/worker_logic.rs
@@ -1,14 +1,15 @@
 use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 use cairo_felt::Felt252;
-use sha3::digest::generic_array::sequence;
 use starknet_api::core::{ClassHash, ContractAddress};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::StorageKey;
 use starknet_api::transaction::Fee;
 
 use super::versioned_state_proxy::VersionedStateProxy;
-use crate::abi::abi_utils::get_fee_token_var_address;
+use crate::concurrency::fee_utils::fill_sequencer_balance_reads;
 use crate::concurrency::scheduler::{Scheduler, Task};
 use crate::concurrency::utils::lock_mutex_in_array;
 use crate::concurrency::versioned_state_proxy::ThreadSafeVersionedState;
@@ -26,7 +27,7 @@ use crate::transaction::transactions::ExecutableTransaction;
 #[path = "worker_logic_test.rs"]
 pub mod test;
 
-fn _add_fee_to_sequencer_balance(
+fn add_fee_to_sequencer_balance(
     fee_token_adress: ContractAddress,
     pinned_versioned_state: &VersionedStateProxy<impl StateReader>,
     actual_fee: &Fee,
@@ -51,15 +52,14 @@ pub struct ExecutionTaskOutput {
     pub visited_pcs: HashMap<ClassHash, HashSet<usize>>,
     pub result: TransactionExecutionResult<TransactionExecutionInfo>,
 }
-
-pub struct WorkerExecutor<S: StateReader> {
+pub struct WorkersExecutor<S: StateReader> {
     pub scheduler: Scheduler,
     pub state: ThreadSafeVersionedState<S>,
     pub chunk: Box<[Transaction]>,
     pub execution_outputs: Box<[Mutex<Option<ExecutionTaskOutput>>]>,
     pub block_context: BlockContext,
 }
-impl<S: StateReader> WorkerExecutor<S> {
+impl<S: StateReader> WorkersExecutor<S> {
     pub fn run(&self) {
         let mut task = Task::NoTask;
         loop {
@@ -119,5 +119,85 @@ impl<S: StateReader> WorkerExecutor<S> {
 
     fn validate(&self, _tx_index: TxIndex) -> Task {
         todo!();
+    }
+
+    pub fn try_commit_transaction(&mut self, tx_index: TxIndex) -> StateResult<bool> {
+        let mut execution_task_outputs = lock_mutex_in_array(&self.execution_outputs, tx_index);
+        let result_tx_info = &mut execution_task_outputs.as_mut().unwrap().result;
+
+        let tx = &self.chunk[tx_index];
+        let versioned_state = &self.state;
+        // TODO(meshi 20/05/2024): remove the redundant cached state.
+        let mut cached_state = CachedState::from(versioned_state.pin_version(tx_index));
+        let transactional_state = CachedState::create_transactional(&mut cached_state);
+        let tx_versioned_state = versioned_state.pin_version(tx_index);
+
+        // First, re-validate the transaction.
+        if !tx_versioned_state.validate_reads(&transactional_state.cache.borrow().initial_reads) {
+            // Revalidate failed: re-execute the transaction, and commit.
+
+            self.block_context =
+                BlockContext { concurrency_mode: true, ..self.block_context.clone() };
+
+            self.execute_tx(tx_index);
+            let result_tx_info = &mut execution_task_outputs.as_mut().unwrap().result;
+            if result_tx_info.is_err() {
+                // TODO(Meshi, 01/06/2024): Rvert the chanches of the execution before the
+                // re-execution.
+                todo!()
+            }
+            // Another validation after the re-execution for sanity check.
+            assert!(
+                tx_versioned_state
+                    .validate_reads(&transactional_state.cache.borrow().initial_reads)
+            );
+            // TODO(Meshi, 01/06/2024): check if this is also needed in the bouncer.
+            return Ok(true);
+        }
+        // Revalidate succeeded.
+        if result_tx_info.is_err() {
+            // Transaction failed and successfully committed.
+            return Ok(true);
+        }
+        let tx_context = Arc::new(self.block_context.to_tx_context(tx));
+        // Fix the sequencer balance.
+        // There is no need to fix the balance when the sequencer transfers fee to itself, since we
+        // use the sequential fee transfer in this case.
+        if tx_context.tx_info.sender_address() != self.block_context.block_info.sequencer_address {
+            let (sequencer_balance_key_low, sequencer_balance_key_high) =
+                get_sequencer_balance_keys(&tx_context.block_context);
+            let sequencer_balance_low = tx_versioned_state
+                .get_storage_at(tx_context.fee_token_address(), sequencer_balance_key_low)?;
+            let sequencer_balance_high = tx_versioned_state
+                .get_storage_at(tx_context.fee_token_address(), sequencer_balance_key_high)?;
+
+            let tx_info =
+                result_tx_info.as_mut().expect("Transaction info should not be an error here.");
+            if let Some(fee_transfer_call_info) = tx_info.fee_transfer_call_info.as_mut() {
+                // Fix the transfer call info.
+                fill_sequencer_balance_reads(
+                    fee_transfer_call_info,
+                    sequencer_balance_low,
+                    sequencer_balance_high,
+                );
+
+                // Update the sequencer balance in the storage.
+                let sequencer_keys = get_sequencer_balance_keys(&tx_context.block_context);
+                add_fee_to_sequencer_balance(
+                    tx_context.fee_token_address(),
+                    &tx_versioned_state,
+                    &tx_info.actual_fee,
+                    sequencer_keys,
+                    (
+                        transactional_state
+                            .get_storage_at(tx_context.fee_token_address(), sequencer_keys.0)?,
+                        transactional_state
+                            .get_storage_at(tx_context.fee_token_address(), sequencer_keys.1)?,
+                    ),
+                );
+            }
+        }
+        // TODO(Meshi, 01/06/2024): check if this is also needed in the bouncer.
+        Ok(true)
     }
 }
