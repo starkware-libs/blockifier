723f4b9e9dba95d3fa31da07e4cc03807a0569d5
Meshi Peled <141231558+meship-starkware@users.noreply.github.com>
2024-04-24 15:00:15 +0300

    perf(execution): benchmark random transfers

--- crates/native_blockifier/bench/blockifier_bench.rs
+++ crates/native_blockifier/bench/blockifier_bench.rs
@@ -19,12 +19,17 @@ use blockifier::test_utils::{CairoVersion, NonceManager, BALANCE, MAX_FEE};
 use blockifier::transaction::account_transaction::AccountTransaction;
 use blockifier::transaction::transactions::ExecutableTransaction;
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{Rng, SeedableRng};
 use starknet_api::core::ContractAddress;
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{Calldata, Fee, TransactionVersion};
 use starknet_api::{calldata, stark_felt};
 
 const N_ACCOUNTS: u16 = 10000;
+const RANDOMIZATION_SEED: u64 = 0;
+const CHARGE_FEE: bool = false;
+const RUN_VALIDATION: bool = false;
+const TRANSACTION_VERSION: TransactionVersion = TransactionVersion(StarkFelt::ONE);
 
 pub fn transfers_benchmark(c: &mut Criterion) {
     let account_contract = FeatureContract::AccountWithLongValidate(CairoVersion::Cairo0);
@@ -37,34 +42,49 @@ pub fn transfers_benchmark(c: &mut Criterion) {
     let nonce_manager = &mut NonceManager::default();
 
     let mut sender_account = 0;
+    let mut random_generator = rand::rngs::StdRng::seed_from_u64(RANDOMIZATION_SEED);
+    let mut recipient_account = random_generator.gen::<usize>() % accounts.len();
     // Create a benchmark group called "transfers", which iterates over the accounts round-robin
     // and performs transfers.
     c.bench_function("transfers", |benchmark| {
         benchmark.iter(|| {
-            do_transfer(sender_account, &accounts, nonce_manager, block_context, &mut state);
+            do_transfer(
+                sender_account,
+                recipient_account,
+                &accounts,
+                nonce_manager,
+                block_context,
+                &mut state,
+            );
             sender_account = (sender_account + 1) % accounts.len();
+            recipient_account = random_generator.gen::<usize>() % accounts.len();
         })
     });
 }
 
 fn do_transfer(
     sender_account: usize,
+    recipient_account: usize,
     accounts: &[ContractAddress],
     nonce_manager: &mut NonceManager,
     block_context: &BlockContext,
     state: &mut CachedState<DictStateReader>,
 ) {
-    let n_accounts = accounts.len();
-    let recipient_account = (sender_account + 1) % n_accounts;
     let sender_address = accounts[sender_account];
     let recipient_account_address = accounts[recipient_account];
     let nonce = nonce_manager.next(sender_address);
 
     let entry_point_selector =
         selector_from_name(blockifier::transaction::constants::TRANSFER_ENTRY_POINT_NAME);
-    // TODO(gilad, 06/09/2023): NEW_TOKEN_SUPPORT this should depend the version of invoke tx.
-    let contract_address =
-        *block_context.chain_info().fee_token_addresses.eth_fee_token_address.0.key();
+    let contract_address = match TRANSACTION_VERSION {
+        TransactionVersion::ONE => {
+            *block_context.chain_info().fee_token_addresses.eth_fee_token_address.0.key()
+        }
+        TransactionVersion::THREE => {
+            *block_context.chain_info().fee_token_addresses.strk_fee_token_address.0.key()
+        }
+        _ => panic!("Unsupported transaction version: {TRANSACTION_VERSION:?}"),
+    };
 
     let execute_calldata = calldata![
         contract_address,                   // Contract address.
@@ -79,12 +99,12 @@ fn do_transfer(
         max_fee: Fee(MAX_FEE),
         sender_address,
         calldata: execute_calldata,
-        version: TransactionVersion::ONE,
+        version: TRANSACTION_VERSION,
         nonce,
     });
     let account_tx = AccountTransaction::Invoke(tx);
-    let charge_fee = false;
-    let validate = false;
+    let charge_fee = CHARGE_FEE;
+    let validate = RUN_VALIDATION;
     account_tx.execute(state, block_context, charge_fee, validate).unwrap();
 }
 
