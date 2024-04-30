cde5d2b2077eed6a32db3687b6efe847bfe3b0c0
meship-starkware <meshi@starkware.co>
2024-04-30 15:59:08 +0300

    feat(concurrency): move versioned_state_for_testing to concurrency test utils

--- crates/blockifier/src/concurrency/test_utils.rs
+++ crates/blockifier/src/concurrency/test_utils.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use starknet_api::core::{ClassHash, ContractAddress};
+
+use crate::concurrency::versioned_state_proxy::VersionedState;
+use crate::state::cached_state::CachedState;
+use crate::test_utils::dict_state_reader::DictStateReader;
+
 #[macro_export]
 macro_rules! default_scheduler {
     ($chunk_size:ident : $chunk:expr , $($field:ident $(: $value:expr)?),+ $(,)?) => {
@@ -24,4 +33,16 @@ macro_rules! default_scheduler {
             ..Default::default()
         }
     };
+}
+
+pub fn versioned_state_for_testing(
+    contract_address: ContractAddress,
+    class_hash: ClassHash,
+) -> Arc<Mutex<VersionedState<CachedState<DictStateReader>>>> {
+    let mut address_to_class_hash = HashMap::new();
+    address_to_class_hash.insert(contract_address, class_hash);
+
+    let cached_state =
+        CachedState::from(DictStateReader { address_to_class_hash, ..Default::default() });
+    Arc::new(Mutex::new(VersionedState::new(cached_state)))
 }
