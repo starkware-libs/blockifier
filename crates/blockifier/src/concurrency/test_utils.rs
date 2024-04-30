1c5b3529bb988888e989bcca993f2c2a84138753
meship-starkware <meshi@starkware.co>
2024-04-30 15:59:08 +0300

    feat(concurrency): move versioned_state_for_testing to concurrency test utils

--- crates/blockifier/src/concurrency/test_utils.rs
+++ crates/blockifier/src/concurrency/test_utils.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use rstest::fixture;
+use starknet_api::core::{ClassHash, ContractAddress, PatriciaKey};
+use starknet_api::hash::{StarkFelt, StarkHash};
+use starknet_api::{contract_address, patricia_key, stark_felt};
+
+use crate::concurrency::versioned_state_proxy::VersionedState;
+use crate::state::cached_state::CachedState;
+use crate::state::state_api::StateReader;
+use crate::test_utils::dict_state_reader::DictStateReader;
+
 #[macro_export]
 macro_rules! default_scheduler {
     ($chunk_size:ident : $chunk:expr , $($field:ident $(: $value:expr)?),+ $(,)?) => {
@@ -24,4 +37,21 @@ macro_rules! default_scheduler {
             ..Default::default()
         }
     };
+}
+
+#[fixture]
+pub fn initial_state_for_testing() -> DictStateReader {
+    DictStateReader {
+        address_to_class_hash: HashMap::from([(
+            contract_address!("0x1"),
+            ClassHash(stark_felt!(27_u8)),
+        )]),
+        ..DictStateReader::default()
+    }
+}
+
+pub fn versioned_state_for_testing(
+    initial_state: impl StateReader,
+) -> Arc<Mutex<VersionedState<CachedState<impl StateReader>>>> {
+    Arc::new(Mutex::new(VersionedState::new(CachedState::new(initial_state))))
 }
