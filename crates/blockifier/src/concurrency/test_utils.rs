9ef4ade11f3fcef9c44f9867158acc6d430f74ad
Noa Oved <noa@starkware.co>
2024-05-02 18:22:42 +0300

    refactor(concurrency): use ` ThreadSafeVersionedState ` in tests and define appropriate methods

--- crates/blockifier/src/concurrency/test_utils.rs
+++ crates/blockifier/src/concurrency/test_utils.rs
@@ -1,6 +1,4 @@
-use std::sync::{Arc, Mutex};
-
-use crate::concurrency::versioned_state_proxy::VersionedState;
+use crate::concurrency::versioned_state_proxy::{ThreadSafeVersionedState, VersionedState};
 use crate::test_utils::dict_state_reader::DictStateReader;
 
 #[macro_export]
@@ -32,10 +30,8 @@ macro_rules! default_scheduler {
 }
 
 // TODO(meshi, 01/06/2024): Consider making this a macro.
-// TODO: Use `ThreadSafeVersionedState` as a return value, need to change the inner field to public,
-// or add new method that gets `StateReader`.
-pub fn versioned_state_for_testing(
+pub fn safe_versioned_state_for_testing(
     block_state: DictStateReader,
-) -> Arc<Mutex<VersionedState<DictStateReader>>> {
-    Arc::new(Mutex::new(VersionedState::new(block_state)))
+) -> ThreadSafeVersionedState<DictStateReader> {
+    ThreadSafeVersionedState::new(VersionedState::new(block_state))
 }
