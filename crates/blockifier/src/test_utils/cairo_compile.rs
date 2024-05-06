9d2d44c5830a40aca1c8eb865790f00335142b55
Dori Medini <dori@starkware.co>
2024-05-06 21:31:19 +0300

    chore: verify Cairo0 compiler dependencies before compiling
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/test_utils/cairo_compile.rs
+++ crates/blockifier/src/test_utils/cairo_compile.rs
@@ -1,7 +1,12 @@
 use std::process::Command;
+use std::{env, fs};
 
 use cached::proc_macro::cached;
 use serde::{Deserialize, Serialize};
+
+use crate::test_utils::CairoVersion;
+
+const CAIRO0_PIP_REQUIREMENTS_FILE: &str = "tests/requirements.txt";
 
 /// Objects for simple deserialization of Cargo.toml to fetch the Cairo1 compiler version.
 /// The compiler itself isn't actually a dependency, so we compile by using the version of the
@@ -51,6 +56,7 @@ pub fn cairo1_compiler_version() -> String {
 
 /// Compiles a Cairo0 program using the deprecated compiler.
 pub fn cairo0_compile(path: String, extra_arg: Option<String>, debug_info: bool) -> Vec<u8> {
+    verify_compiler_deps(CairoVersion::Cairo0);
     let mut command = Command::new("starknet-compile-deprecated");
     if let Some(extra_arg) = extra_arg {
         command.arg(extra_arg);
@@ -66,5 +72,40 @@ pub fn cairo0_compile(path: String, extra_arg: Option<String>, debug_info: bool)
 
 /// Compiles a Cairo1 program using the compiler version set in the Cargo.toml.
 pub fn cairo1_compile(_path: String) -> Vec<u8> {
+    verify_compiler_deps(CairoVersion::Cairo1);
     todo!();
+}
+
+/// Verifies that the required dependencies are available before compiling.
+fn verify_compiler_deps(cairo_version: CairoVersion) {
+    match cairo_version {
+        CairoVersion::Cairo0 => {
+            // Python compiler. Verify correct version.
+            let cairo_lang_version_output = Command::new("sh")
+                .arg("-c")
+                .arg("pip freeze | grep cairo-lang")
+                .output()
+                .unwrap()
+                .stdout;
+            let cairo_lang_version = String::from_utf8(cairo_lang_version_output).unwrap();
+
+            let requirements_contents = fs::read_to_string(CAIRO0_PIP_REQUIREMENTS_FILE).unwrap();
+            let expected_cairo_lang_version = requirements_contents
+                .lines()
+                .nth(1) // Skip docstring.
+                .expect(
+                    "Expecting requirements file to contain a docstring in the first line, and \
+                    then the required cairo-lang version in the second line."
+                );
+
+            assert_eq!(
+                cairo_lang_version.trim(),
+                expected_cairo_lang_version.trim(),
+                "cairo-lang not found. Please run:\npip3.9 install -r {}/{}\nthen rerun the test.",
+                env::var("CARGO_MANIFEST_DIR").unwrap(),
+                CAIRO0_PIP_REQUIREMENTS_FILE
+            );
+        }
+        CairoVersion::Cairo1 => todo!(),
+    }
 }
