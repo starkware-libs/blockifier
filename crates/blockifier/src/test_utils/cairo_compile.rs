aed63a4268aab482e083da330ae745b6d65f649c
Dori Medini <dori@starkware.co>
2024-05-06 17:17:26 +0300

    chore: fetch Cairo1 compiler version from Cargo.toml
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/blockifier/src/test_utils/cairo_compile.rs
+++ crates/blockifier/src/test_utils/cairo_compile.rs
@@ -1,5 +1,54 @@
 use std::process::Command;
 
+use cached::proc_macro::cached;
+use serde::{Deserialize, Serialize};
+
+/// Objects for simple deserialization of Cargo.toml to fetch the Cairo1 compiler version.
+/// The compiler itself isn't actually a dependency, so we compile by using the version of the
+/// cairo-lang-casm crate.
+/// The choice of cairo-lang-casm is arbitrary, as all compiler crate dependencies should have the
+/// same version.
+/// Deserializes:
+/// """
+/// ...
+/// [workspace.dependencies]
+/// ...
+/// cairo-lang-casm = VERSION
+/// ...
+/// """
+/// where `VERSION` can be a simple "x.y.z" version string or an object with a "version" field.
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum DependencyValue {
+    String(String),
+    Object { version: String },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CairoLangCasmDependency {
+    #[serde(rename = "cairo-lang-casm")]
+    cairo_lang_casm: DependencyValue,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct WorkspaceFields {
+    dependencies: CairoLangCasmDependency,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CargoToml {
+    workspace: WorkspaceFields,
+}
+
+#[cached]
+/// Returns the version of the Cairo1 compiler* defined in the root Cargo.toml.
+pub fn cairo1_compiler_version() -> String {
+    let cargo_toml: CargoToml = toml::from_str(include_str!("../../../../Cargo.toml")).unwrap();
+    match cargo_toml.workspace.dependencies.cairo_lang_casm {
+        DependencyValue::String(version) | DependencyValue::Object { version } => version.clone(),
+    }
+}
+
 /// Compiles a Cairo0 program using the deprecated compiler.
 pub fn cairo0_compile(path: String, extra_arg: Option<String>, debug_info: bool) -> Vec<u8> {
     let mut command = Command::new("starknet-compile-deprecated");
