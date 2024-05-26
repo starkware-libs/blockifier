fab9ce4526ad95dff0ebf2a6fb8f33807c00fc43
Avi Cohen <avi.cohen@starkware.co>
2024-05-26 17:21:01 +0300

    refactor: refactor the benchmark logic into a struct to be reusable

--- crates/blockifier/bench/blockifier_bench.rs
+++ crates/blockifier/bench/blockifier_bench.rs
@@ -35,99 +35,104 @@ const CHARGE_FEE: bool = false;
 const TRANSACTION_VERSION: TransactionVersion = TransactionVersion(StarkFelt::ONE);
 
 pub fn transfers_benchmark(c: &mut Criterion) {
-    let account_contract = FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0);
-    let block_context = BlockContext::create_for_account_testing();
-    let chain_info = &block_context.chain_info().clone();
-    let state = test_state(chain_info, BALANCE * 1000, &[(account_contract, N_ACCOUNTS)]);
-    // TODO(Avi, 20/05/2024): Enable concurrency.
-    let executor_config = TransactionExecutorConfig::default();
-    let executor =
-        &mut TransactionExecutor::new(state, block_context, BouncerConfig::max(), executor_config);
-    let accounts = (0..N_ACCOUNTS)
-        .map(|instance_id| account_contract.get_instance_address(instance_id))
-        .collect::<Vec<_>>();
-    let nonce_manager = &mut NonceManager::default();
-
-    let mut first_sender_index = 0;
-    let mut random_generator = rand::rngs::StdRng::seed_from_u64(RANDOMIZATION_SEED);
+    let mut transfers_simulator = TransfersSimulator::new();
     // Create a benchmark group called "transfers", which iterates over the accounts round-robin
     // and performs transfers.
     c.bench_function("transfers", |benchmark| {
         benchmark.iter(|| {
-            execute_chunk_of_transfers(
-                first_sender_index,
-                &mut random_generator,
-                &accounts,
-                nonce_manager,
-                chain_info,
-                executor,
-            );
-            first_sender_index = (first_sender_index + CHUNK_SIZE) % accounts.len();
+            transfers_simulator.execute_chunk_of_transfers();
         })
     });
 }
-
-fn execute_chunk_of_transfers(
-    first_sender_index: usize,
-    random_generator: &mut rand::rngs::StdRng,
-    accounts: &[ContractAddress],
-    nonce_manager: &mut NonceManager,
-    chain_info: &ChainInfo,
-    executor: &mut TransactionExecutor<DictStateReader>,
-) {
-    let mut chunk: Vec<Transaction> = Vec::with_capacity(CHUNK_SIZE);
-    let mut sender_index = first_sender_index;
-    for _ in 0..CHUNK_SIZE {
-        let recipient_index = random_generator.gen::<usize>() % accounts.len();
-        let account_tx =
-            generate_transfer(accounts, sender_index, recipient_index, nonce_manager, chain_info);
-        chunk.push(Transaction::AccountTransaction(account_tx));
-        sender_index = (sender_index + 1) % accounts.len();
-    }
-    let results = executor.execute_txs(&chunk, CHARGE_FEE);
-    assert_eq!(results.len(), CHUNK_SIZE);
-    for result in results {
-        assert!(!result.unwrap().is_reverted());
-    }
-    // TODO(Avi, 01/06/2024): Run the same transactions concurrently on a new state and compare the
-    // state diffs.
+pub struct TransfersSimulator {
+    accounts: Vec<ContractAddress>,
+    chain_info: ChainInfo,
+    executor: TransactionExecutor<DictStateReader>,
+    nonce_manager: NonceManager,
+    recipient_generator: rand::rngs::StdRng,
+    sender_index: usize,
 }
 
-fn generate_transfer(
-    accounts: &[ContractAddress],
-    sender_index: usize,
-    recipient_index: usize,
-    nonce_manager: &mut NonceManager,
-    chain_info: &ChainInfo,
-) -> AccountTransaction {
-    let sender_address = accounts[sender_index];
-    let recipient_account_address = accounts[recipient_index];
-    let nonce = nonce_manager.next(sender_address);
+impl TransfersSimulator {
+    pub fn new() -> Self {
+        let account_contract = FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0);
+        let block_context = BlockContext::create_for_account_testing();
+        let chain_info = block_context.chain_info().clone();
+        let state = test_state(&chain_info, BALANCE * 1000, &[(account_contract, N_ACCOUNTS)]);
+        // TODO(Avi, 20/05/2024): Enable concurrency.
+        let executor_config = TransactionExecutorConfig::default();
+        let executor =
+            TransactionExecutor::new(state, block_context, BouncerConfig::max(), executor_config);
+        let accounts = (0..N_ACCOUNTS)
+            .map(|instance_id| account_contract.get_instance_address(instance_id))
+            .collect::<Vec<_>>();
+        let nonce_manager = NonceManager::default();
+        let random_generator = rand::rngs::StdRng::seed_from_u64(RANDOMIZATION_SEED);
+        Self {
+            accounts,
+            nonce_manager,
+            chain_info,
+            executor,
+            sender_index: 0,
+            recipient_generator: random_generator,
+        }
+    }
 
-    let entry_point_selector = selector_from_name(TRANSFER_ENTRY_POINT_NAME);
-    let contract_address = match TRANSACTION_VERSION {
-        TransactionVersion::ONE => *chain_info.fee_token_addresses.eth_fee_token_address.0.key(),
-        TransactionVersion::THREE => *chain_info.fee_token_addresses.strk_fee_token_address.0.key(),
-        _ => panic!("Unsupported transaction version: {TRANSACTION_VERSION:?}"),
-    };
+    pub fn execute_chunk_of_transfers(&mut self) {
+        let mut chunk: Vec<Transaction> = Vec::with_capacity(CHUNK_SIZE);
+        for _ in 0..CHUNK_SIZE {
+            let account_tx = self.generate_transfer();
+            chunk.push(Transaction::AccountTransaction(account_tx));
+        }
+        let results = self.executor.execute_txs(&chunk, CHARGE_FEE);
+        assert_eq!(results.len(), CHUNK_SIZE);
+        for result in results {
+            assert!(!result.unwrap().is_reverted());
+        }
+    }
 
-    let execute_calldata = calldata![
-        contract_address,                   // Contract address.
-        entry_point_selector.0,             // EP selector.
-        stark_felt!(3_u8),                  // Calldata length.
-        *recipient_account_address.0.key(), // Calldata: recipient.
-        stark_felt!(1_u8),                  // Calldata: lsb amount.
-        stark_felt!(0_u8)                   // Calldata: msb amount.
-    ];
+    pub fn generate_transfer(&mut self) -> AccountTransaction {
+        let sender_address = self.accounts[self.sender_index];
+        self.sender_index = (self.sender_index + 1) % self.accounts.len();
+        let recipient_index = self.recipient_generator.gen::<usize>() % self.accounts.len();
+        let recipient_address = self.accounts[recipient_index];
+        let nonce = self.nonce_manager.next(sender_address);
 
-    let tx = invoke_tx(invoke_tx_args! {
-        max_fee: Fee(MAX_FEE),
-        sender_address,
-        calldata: execute_calldata,
-        version: TRANSACTION_VERSION,
-        nonce,
-    });
-    AccountTransaction::Invoke(tx)
+        let entry_point_selector = selector_from_name(TRANSFER_ENTRY_POINT_NAME);
+        let contract_address = match TRANSACTION_VERSION {
+            TransactionVersion::ONE => {
+                *self.chain_info.fee_token_addresses.eth_fee_token_address.0.key()
+            }
+            TransactionVersion::THREE => {
+                *self.chain_info.fee_token_addresses.strk_fee_token_address.0.key()
+            }
+            _ => panic!("Unsupported transaction version: {TRANSACTION_VERSION:?}"),
+        };
+
+        let execute_calldata = calldata![
+            contract_address,           // Contract address.
+            entry_point_selector.0,     // EP selector.
+            stark_felt!(3_u8),          // Calldata length.
+            *recipient_address.0.key(), // Calldata: recipient.
+            stark_felt!(1_u8),          // Calldata: lsb amount.
+            stark_felt!(0_u8)           // Calldata: msb amount.
+        ];
+
+        let tx = invoke_tx(invoke_tx_args! {
+            max_fee: Fee(MAX_FEE),
+            sender_address,
+            calldata: execute_calldata,
+            version: TRANSACTION_VERSION,
+            nonce,
+        });
+        AccountTransaction::Invoke(tx)
+    }
+}
+
+impl Default for TransfersSimulator {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 criterion_group!(benches, transfers_benchmark);
