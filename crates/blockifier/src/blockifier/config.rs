12572b9e6540594723e0708fdba55727e714bb9f
Barak <barakb@starkware.co>
2024-05-28 18:46:57 +0300

    feat(concurrency): implement Default for ConcurrencyConfig

--- crates/blockifier/src/blockifier/config.rs
+++ crates/blockifier/src/blockifier/config.rs
@@ -3,9 +3,15 @@ pub struct TransactionExecutorConfig {
     pub concurrency_config: ConcurrencyConfig,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct ConcurrencyConfig {
     pub enabled: bool,
     pub n_workers: usize,
     pub chunk_size: usize,
+}
+
+impl Default for ConcurrencyConfig {
+    fn default() -> Self {
+        Self { enabled: false, n_workers: 0, chunk_size: 0 }
+    }
 }
