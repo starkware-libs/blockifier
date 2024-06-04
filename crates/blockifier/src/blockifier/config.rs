f545e3b7f9df9a6670d4e5568327d6fea53383d8
Barak <barakb@starkware.co>
2024-06-04 12:22:24 +0300

    feat(concurrency): implement execute_chunk

--- crates/blockifier/src/blockifier/config.rs
+++ crates/blockifier/src/blockifier/config.rs
@@ -3,9 +3,17 @@ pub struct TransactionExecutorConfig {
     pub concurrency_config: ConcurrencyConfig,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
+#[cfg_attr(not(feature = "concurrency"), derive(Default))]
 pub struct ConcurrencyConfig {
     pub enabled: bool,
     pub n_workers: usize,
     pub chunk_size: usize,
+}
+
+#[cfg(feature = "concurrency")]
+impl Default for ConcurrencyConfig {
+    fn default() -> Self {
+        Self { enabled: true, n_workers: 4, chunk_size: 64 }
+    }
 }
