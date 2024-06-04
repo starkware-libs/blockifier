2b10dbeda4b7e5e0b13c7af704acd5f53164ea48
Barak <barakb@starkware.co>
2024-06-04 11:46:58 +0300

    refactor(concurrency): use apply_writes in the commit of VersionedState

--- crates/blockifier/src/concurrency/worker_logic.rs
+++ crates/blockifier/src/concurrency/worker_logic.rs
@@ -43,17 +43,17 @@ pub struct ExecutionTaskOutput {
     pub result: TransactionExecutionResult<TransactionExecutionInfo>,
 }
 
-pub struct WorkerExecutor<'a, S: StateReader> {
+pub struct WorkerExecutor<'a, U: UpdatableState> {
     pub scheduler: Scheduler,
-    pub state: ThreadSafeVersionedState<S>,
+    pub state: ThreadSafeVersionedState<U>,
     pub chunk: &'a [Transaction],
     pub execution_outputs: Box<[Mutex<Option<ExecutionTaskOutput>>]>,
     pub block_context: BlockContext,
     pub bouncer: Mutex<&'a mut Bouncer>,
 }
-impl<'a, S: StateReader> WorkerExecutor<'a, S> {
+impl<'a, U: UpdatableState> WorkerExecutor<'a, U> {
     pub fn new(
-        state: ThreadSafeVersionedState<S>,
+        state: ThreadSafeVersionedState<U>,
         chunk: &'a [Transaction],
         block_context: BlockContext,
         bouncer: Mutex<&'a mut Bouncer>,
@@ -301,7 +301,7 @@ impl<'a, S: StateReader> WorkerExecutor<'a, S> {
 
 fn add_fee_to_sequencer_balance(
     fee_token_address: ContractAddress,
-    tx_versioned_state: &mut VersionedStateProxy<impl StateReader>,
+    tx_versioned_state: &mut VersionedStateProxy<impl UpdatableState>,
     actual_fee: Fee,
     block_context: &BlockContext,
     sequencer_balance_value_low: StarkFelt,
