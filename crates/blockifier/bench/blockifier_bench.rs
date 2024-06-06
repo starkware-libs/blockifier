57c7b8537a2e0c54f3d06dadb0ad381ac4b023f8
Barak <barakb@starkware.co>
2024-06-06 16:55:40 +0300

    refactor(concurrency, execution): add concurrency_mode parameter to TransfersGenerator::new

--- crates/blockifier/bench/blockifier_bench.rs
+++ crates/blockifier/bench/blockifier_bench.rs
@@ -11,7 +11,8 @@ use blockifier::test_utils::transfers_generator::TransfersGenerator;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn transfers_benchmark(c: &mut Criterion) {
-    let mut transfers_generator = TransfersGenerator::new();
+    let concurrency_mode = false;
+    let mut transfers_generator = TransfersGenerator::new(concurrency_mode);
     // Create a benchmark group called "transfers", which iterates over the accounts round-robin
     // and performs transfers.
     c.bench_function("transfers", |benchmark| {
