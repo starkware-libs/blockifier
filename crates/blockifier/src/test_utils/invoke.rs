5c11aae051ab3345dcaf88e5a0dfecbdd56a95c3
meship-starkware <meshi@starkware.co>
2024-06-06 12:49:41 +0300

    fix(transaction): set default transaction version to three in invoke function macro

--- crates/blockifier/src/test_utils/invoke.rs
+++ crates/blockifier/src/test_utils/invoke.rs
@@ -37,7 +37,7 @@ impl Default for InvokeTxArgs {
             sender_address: ContractAddress::default(),
             calldata: calldata![],
             // TODO(Dori, 10/10/2023): Change to THREE when supported.
-            version: TransactionVersion::ONE,
+            version: TransactionVersion::THREE,
             resource_bounds: default_testing_resource_bounds(),
             tip: Tip::default(),
             nonce_data_availability_mode: DataAvailabilityMode::L1,
