441c27fc9324016076f2ce21f55032df83e4ca47
barak-b-starkware <110763103+barak-b-starkware@users.noreply.github.com>
2024-06-10 08:09:13 +0300

    refactor(concurrency, execution): add concurrency_mode parameter to TransfersGenerator::new (#1966)

--- crates/blockifier/src/blockifier/transfers_flow_test.rs
+++ crates/blockifier/src/blockifier/transfers_flow_test.rs
@@ -2,6 +2,7 @@ use crate::test_utils::transfers_generator::TransfersGenerator;
 
 #[test]
 pub fn transfers_flow_test() {
-    let mut transfers_generator = TransfersGenerator::new();
+    let concurrency_mode = true;
+    let mut transfers_generator = TransfersGenerator::new(concurrency_mode);
     transfers_generator.execute_chunk_of_transfers();
 }
