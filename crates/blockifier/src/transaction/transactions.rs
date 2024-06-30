fb6fcc2ca3b41cdc108ad633623b477700e69d6b
meship-starkware <meshi@starkware.co>
2024-06-30 12:26:17 +0300

    fix(transaction): disable cairo0 redeclaration

--- crates/blockifier/src/transaction/transactions.rs
+++ crates/blockifier/src/transaction/transactions.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use starknet_api::calldata;
-use starknet_api::core::{ClassHash, ContractAddress, Nonce};
+use starknet_api::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
 use starknet_api::deprecated_contract_class::EntryPointType;
 use starknet_api::transaction::{
     AccountDeploymentData, Calldata, ContractAddressSalt, DeclareTransactionV2,
@@ -166,6 +166,29 @@ impl DeclareTransaction {
     pub fn only_query(&self) -> bool {
         self.only_query
     }
+
+    fn try_declare<S: State>(
+        &self,
+        state: &mut S,
+        class_hash: ClassHash,
+        compiled_class_hash: Option<CompiledClassHash>,
+    ) -> TransactionExecutionResult<()> {
+        match state.get_compiled_contract_class(class_hash) {
+            Err(StateError::UndeclaredClassHash(_)) => {
+                // Class is undeclared; declare it.
+                state.set_contract_class(class_hash, self.contract_class())?;
+                if let Some(compiled_class_hash) = compiled_class_hash {
+                    state.set_compiled_class_hash(class_hash, compiled_class_hash)?;
+                }
+                Ok(())
+            }
+            Err(error) => Err(error)?,
+            Ok(_) => {
+                // Class is already declared, cannot redeclare.
+                Err(TransactionExecutionError::DeclareTransactionError { class_hash })
+            }
+        }
+    }
 }
 
 impl<S: State> Executable<S> for DeclareTransaction {
@@ -173,17 +196,22 @@ impl<S: State> Executable<S> for DeclareTransaction {
         &self,
         state: &mut S,
         _resources: &mut ExecutionResources,
-        _context: &mut EntryPointExecutionContext,
+        context: &mut EntryPointExecutionContext,
         _remaining_gas: &mut u64,
     ) -> TransactionExecutionResult<Option<CallInfo>> {
         let class_hash = self.class_hash();
-
         match &self.tx {
-            // No class commitment, so no need to check if the class is already declared.
             starknet_api::transaction::DeclareTransaction::V0(_)
             | starknet_api::transaction::DeclareTransaction::V1(_) => {
-                state.set_contract_class(class_hash, self.contract_class())?;
-                Ok(None)
+                if context.tx_context.block_context.versioned_constants.disable_cairo0_redeclaration
+                {
+                    self.try_declare(state, class_hash, None)?
+                } else {
+                    // We allow redeclaration of the class for backward compatibility.
+                    // In the past, we allowed redeclaration of Cairo 0 contracts since there was
+                    // no class commitment (so no need to check if the class is already declared).
+                    state.set_contract_class(class_hash, self.contract_class())?;
+                }
             }
             starknet_api::transaction::DeclareTransaction::V2(DeclareTransactionV2 {
                 compiled_class_hash,
@@ -192,23 +220,9 @@ impl<S: State> Executable<S> for DeclareTransaction {
             | starknet_api::transaction::DeclareTransaction::V3(DeclareTransactionV3 {
                 compiled_class_hash,
                 ..
-            }) => {
-                match state.get_compiled_contract_class(class_hash) {
-                    Err(StateError::UndeclaredClassHash(_)) => {
-                        // Class is undeclared; declare it.
-                        state.set_contract_class(class_hash, self.contract_class())?;
-                        state.set_compiled_class_hash(class_hash, *compiled_class_hash)?;
-                        Ok(None)
-                    }
-                    Err(error) => Err(error)?,
-                    Ok(_) => {
-                        // Class is already declared, cannot redeclare
-                        // (i.e., make sure the leaf is uninitialized).
-                        Err(TransactionExecutionError::DeclareTransactionError { class_hash })
-                    }
-                }
-            }
+            }) => self.try_declare(state, class_hash, Some(*compiled_class_hash))?,
         }
+        Ok(None)
     }
 }
 
