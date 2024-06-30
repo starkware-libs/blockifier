76a966140f57ec10efcf7aa577b55c0efdce4ad7
Yael Doweck <yael@starkware.co>
2024-06-30 10:58:14 +0300

    refactor: move skip validation check to py_validator

--- crates/blockifier/src/blockifier/stateful_validator_test.rs
+++ crates/blockifier/src/blockifier/stateful_validator_test.rs
@@ -1,15 +1,16 @@
+use assert_matches::assert_matches;
 use rstest::rstest;
 use starknet_api::transaction::{Fee, TransactionVersion};
 
 use crate::blockifier::stateful_validator::StatefulValidator;
 use crate::context::BlockContext;
-use crate::nonce;
 use crate::test_utils::contracts::FeatureContract;
 use crate::test_utils::initial_test_state::{fund_account, test_state};
 use crate::test_utils::{CairoVersion, BALANCE};
 use crate::transaction::account_transaction::AccountTransaction;
 use crate::transaction::test_utils::{
-    block_context, create_account_tx_for_validate_test_nonce_0, FaultyAccountTxCreatorArgs, VALID,
+    block_context, create_account_tx_for_validate_test_nonce_0, FaultyAccountTxCreatorArgs,
+    INVALID, VALID,
 };
 use crate::transaction::transaction_types::TransactionType;
 
@@ -63,8 +64,31 @@ fn test_transaction_validator(
     }
 
     // Test the stateful validator.
-    let mut stateful_validator = StatefulValidator::create(state, block_context, nonce!(0_u32));
+    let mut stateful_validator = StatefulValidator::create(state, block_context);
 
-    let reuslt = stateful_validator.perform_validations(tx, None);
-    assert!(reuslt.is_ok(), "Validation failed: {:?}", reuslt.unwrap_err());
+    let result = stateful_validator.perform_validations(tx, false);
+    assert!(result.is_ok(), "Validation failed: {:?}", result.unwrap_err());
+}
+
+#[test]
+fn test_transaction_validator_skip_validate() {
+    let block_context = BlockContext::create_for_testing();
+    let faulty_account = FeatureContract::FaultyAccount(CairoVersion::Cairo1);
+    let state = test_state(&block_context.chain_info, BALANCE, &[(faulty_account, 1)]);
+
+    // Create a transaction that does not pass validations.
+    let tx = create_account_tx_for_validate_test_nonce_0(FaultyAccountTxCreatorArgs {
+        scenario: INVALID,
+        tx_type: TransactionType::InvokeFunction,
+        tx_version: TransactionVersion::THREE,
+        sender_address: faulty_account.get_instance_address(0),
+        class_hash: faulty_account.get_class_hash(),
+        max_fee: Fee(BALANCE),
+        ..Default::default()
+    });
+
+    let mut stateful_validator = StatefulValidator::create(state, block_context);
+    // The transaction validations should be skipped and the function should return Ok.
+    let result = stateful_validator.perform_validations(tx, true);
+    assert_matches!(result, Ok(()));
 }
--- crates/native_blockifier/src/py_validator.rs
+++ crates/native_blockifier/src/py_validator.rs
@@ -1,11 +1,15 @@
-use blockifier::blockifier::stateful_validator::StatefulValidator;
+use blockifier::blockifier::stateful_validator::{StatefulValidator, StatefulValidatorResult};
 use blockifier::bouncer::BouncerConfig;
 use blockifier::context::BlockContext;
 use blockifier::state::cached_state::CachedState;
+use blockifier::transaction::account_transaction::AccountTransaction;
+use blockifier::transaction::objects::TransactionInfoCreator;
+use blockifier::transaction::transaction_types::TransactionType;
 use blockifier::versioned_constants::VersionedConstants;
 use pyo3::{pyclass, pymethods, PyAny};
 use starknet_api::core::Nonce;
 use starknet_api::transaction::TransactionHash;
+use starknet_types_core::felt::Felt;
 
 use crate::errors::NativeBlockifierResult;
 use crate::py_block_executor::PyGeneralConfig;
@@ -18,6 +22,7 @@ use crate::state_readers::py_state_reader::PyStateReader;
 #[pyclass]
 pub struct PyValidator {
     pub stateful_validator: StatefulValidator<PyStateReader>,
+    pub max_nonce_for_validation_skip: Nonce,
 }
 
 #[pymethods]
@@ -47,10 +52,9 @@ impl PyValidator {
 
         // Create the stateful validator.
         let max_nonce_for_validation_skip = Nonce(max_nonce_for_validation_skip.0);
-        let stateful_validator =
-            StatefulValidator::create(state, block_context, max_nonce_for_validation_skip);
+        let stateful_validator = StatefulValidator::create(state, block_context);
 
-        Ok(Self { stateful_validator })
+        Ok(Self { stateful_validator, max_nonce_for_validation_skip })
     }
 
     // Transaction Execution API.
@@ -64,8 +68,43 @@ impl PyValidator {
     ) -> NativeBlockifierResult<()> {
         let account_tx = py_account_tx(tx, optional_py_class_info).expect(PY_TX_PARSING_ERR);
         let deploy_account_tx_hash = deploy_account_tx_hash.map(|hash| TransactionHash(hash.0));
-        self.stateful_validator.perform_validations(account_tx, deploy_account_tx_hash)?;
+
+        // We check if the transaction should be skipped due to the deploy account not being
+        // processed.
+        let skip_validate = self
+            .skip_validate_due_to_unprocessed_deploy_account(&account_tx, deploy_account_tx_hash)?;
+        self.stateful_validator.perform_validations(account_tx, skip_validate)?;
 
         Ok(())
+    }
+}
+
+impl PyValidator {
+    // Check if deploy account was submitted but not processed yet. If so, then skip
+    // `__validate__` method for subsequent transactions for a better user experience.
+    // (they will otherwise fail solely because the deploy account hasn't been processed yet).
+    pub fn skip_validate_due_to_unprocessed_deploy_account(
+        &mut self,
+        account_tx: &AccountTransaction,
+        deploy_account_tx_hash: Option<TransactionHash>,
+    ) -> StatefulValidatorResult<bool> {
+        if account_tx.tx_type() != TransactionType::InvokeFunction {
+            return Ok(false);
+        }
+        let tx_info = account_tx.create_tx_info();
+        let nonce = self.stateful_validator.get_nonce(tx_info.sender_address())?;
+
+        let deploy_account_not_processed =
+            deploy_account_tx_hash.is_some() && nonce == Nonce(Felt::ZERO);
+        let tx_nonce = tx_info.nonce();
+        let is_post_deploy_nonce = Nonce(Felt::ONE) <= tx_nonce;
+        let nonce_small_enough_to_qualify_for_validation_skip =
+            tx_nonce <= self.max_nonce_for_validation_skip;
+
+        let skip_validate = deploy_account_not_processed
+            && is_post_deploy_nonce
+            && nonce_small_enough_to_qualify_for_validation_skip;
+
+        Ok(skip_validate)
     }
 }
