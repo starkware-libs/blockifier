9f7421ca4209d89b2f5466c575e95ebdd95a8f39
avi-starkware <avi.cohen@starkware.co>
2024-07-08 15:41:14 +0300

    test(execution): support disjoint recipients in transfers flow test (#1921)

--- crates/blockifier/src/blockifier/transfers_flow_test.rs
+++ crates/blockifier/src/blockifier/transfers_flow_test.rs
@@ -1,7 +1,11 @@
-use crate::test_utils::transfers_generator::TransfersGenerator;
+use crate::test_utils::transfers_generator::{RecipientGeneratorType, TransfersGenerator};
 
 #[test]
 pub fn transfers_flow_test() {
-    let mut transfers_generator = TransfersGenerator::new();
-    transfers_generator.execute_chunk_of_transfers();
+    let mut transfers_generator =
+        TransfersGenerator::new(RecipientGeneratorType::DisjointFromSenders);
+    // Note: to make transactions in each chunk fully independent, make sure the number of accounts
+    // is larger than the chunk size. Otherwise, the same account may be used in multiple
+    // transactions in the same chunk.
+    transfers_generator.execute_transfers_stream();
 }
--- crates/blockifier/src/test_utils/transfers_generator.rs
+++ crates/blockifier/src/test_utils/transfers_generator.rs
@@ -1,3 +1,4 @@
+use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use starknet_api::core::ContractAddress;
 use starknet_api::transaction::{Calldata, Fee, TransactionVersion};
@@ -5,7 +6,7 @@ use starknet_api::{calldata, felt};
 use starknet_types_core::felt::Felt;
 
 use crate::abi::abi_utils::selector_from_name;
-use crate::blockifier::config::TransactionExecutorConfig;
+use crate::blockifier::config::{ConcurrencyConfig, TransactionExecutorConfig};
 use crate::blockifier::transaction_executor::TransactionExecutor;
 use crate::context::{BlockContext, ChainInfo};
 use crate::invoke_tx_args;
@@ -19,56 +20,112 @@ use crate::transaction::constants::TRANSFER_ENTRY_POINT_NAME;
 use crate::transaction::transaction_execution::Transaction;
 
 const N_ACCOUNTS: u16 = 10000;
-const CHUNK_SIZE: usize = 10;
+const STREAM_SIZE: usize = 1000;
 const RANDOMIZATION_SEED: u64 = 0;
-const TRANSACTION_VERSION: TransactionVersion = TransactionVersion(Felt::ONE);
+const TRANSACTION_VERSION: TransactionVersion = TransactionVersion(Felt::THREE);
+#[cfg(feature = "concurrency")]
+const CONCURRENCY_MODE: bool = true;
+#[cfg(not(feature = "concurrency"))]
+const CONCURRENCY_MODE: bool = false;
+const N_WORKERS: usize = 4;
+const CHUNK_SIZE: usize = 100;
+
+pub enum RecipientGeneratorType {
+    Random,
+    RoundRobin,
+    DisjointFromSenders,
+}
 
 pub struct TransfersGenerator {
     account_addresses: Vec<ContractAddress>,
     chain_info: ChainInfo,
     executor: TransactionExecutor<DictStateReader>,
     nonce_manager: NonceManager,
-    recipient_generator: rand::rngs::StdRng,
     sender_index: usize,
+    recipient_generator_type: RecipientGeneratorType,
+    random_recipient_generator: Option<StdRng>,
+    recipient_addresses: Option<Vec<ContractAddress>>,
 }
 
 impl TransfersGenerator {
-    pub fn new() -> Self {
+    pub fn new(recipient_generator_type: RecipientGeneratorType) -> Self {
         let account_contract = FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0);
-        let executor_config = TransactionExecutorConfig::create_for_testing();
         let block_context = BlockContext::create_for_account_testing();
         let chain_info = block_context.chain_info().clone();
         let state = test_state(&chain_info, BALANCE * 1000, &[(account_contract, N_ACCOUNTS)]);
+        let concurrency_config = ConcurrencyConfig {
+            enabled: CONCURRENCY_MODE,
+            n_workers: N_WORKERS,
+            chunk_size: CHUNK_SIZE,
+        };
+        let executor_config = TransactionExecutorConfig { concurrency_config };
         let executor = TransactionExecutor::new(state, block_context, executor_config);
         let account_addresses = (0..N_ACCOUNTS)
             .map(|instance_id| account_contract.get_instance_address(instance_id))
             .collect::<Vec<_>>();
         let nonce_manager = NonceManager::default();
-        let random_generator = rand::rngs::StdRng::seed_from_u64(RANDOMIZATION_SEED);
+        let mut recipient_addresses = None;
+        let mut random_recipient_generator = None;
+        match recipient_generator_type {
+            RecipientGeneratorType::Random => {
+                // Use a random generator to get the next recipient.
+                random_recipient_generator = Some(StdRng::seed_from_u64(RANDOMIZATION_SEED));
+            }
+            RecipientGeneratorType::RoundRobin => {
+                // Use the next account after the sender in the list as the recipient.
+            }
+            RecipientGeneratorType::DisjointFromSenders => {
+                // Use a disjoint set of accounts as recipients. The index of the recipient is the
+                // same as the index of the sender.
+                recipient_addresses = Some(
+                    (N_ACCOUNTS..2 * N_ACCOUNTS)
+                        .map(|instance_id| account_contract.get_instance_address(instance_id))
+                        .collect::<Vec<_>>(),
+                );
+            }
+        };
         Self {
             account_addresses,
-            nonce_manager,
             chain_info,
             executor,
+            nonce_manager,
             sender_index: 0,
-            recipient_generator: random_generator,
+            recipient_generator_type,
+            random_recipient_generator,
+            recipient_addresses,
         }
     }
 
-    pub fn execute_chunk_of_transfers(&mut self) {
-        let mut chunk: Vec<Transaction> = Vec::with_capacity(CHUNK_SIZE);
-        for _ in 0..CHUNK_SIZE {
+    pub fn get_next_recipient(&mut self) -> ContractAddress {
+        match self.recipient_generator_type {
+            RecipientGeneratorType::Random => {
+                let random_recipient_generator = self.random_recipient_generator.as_mut().unwrap();
+                let recipient_index =
+                    random_recipient_generator.gen_range(0..self.account_addresses.len());
+                self.account_addresses[recipient_index]
+            }
+            RecipientGeneratorType::RoundRobin => {
+                let recipient_index = (self.sender_index + 1) % self.account_addresses.len();
+                self.account_addresses[recipient_index]
+            }
+            RecipientGeneratorType::DisjointFromSenders => {
+                self.recipient_addresses.as_ref().unwrap()[self.sender_index]
+            }
+        }
+    }
+
+    pub fn execute_transfers_stream(&mut self) {
+        let mut tx_stream: Vec<Transaction> = Vec::with_capacity(STREAM_SIZE);
+        for _ in 0..STREAM_SIZE {
             let sender_address = self.account_addresses[self.sender_index];
+            let recipient_address = self.get_next_recipient();
             self.sender_index = (self.sender_index + 1) % self.account_addresses.len();
-            let recipient_index =
-                self.recipient_generator.gen::<usize>() % self.account_addresses.len();
-            let recipient_address = self.account_addresses[recipient_index];
 
             let account_tx = self.generate_transfer(sender_address, recipient_address);
-            chunk.push(Transaction::AccountTransaction(account_tx));
+            tx_stream.push(Transaction::AccountTransaction(account_tx));
         }
-        let results = self.executor.execute_txs(&chunk);
-        assert_eq!(results.len(), CHUNK_SIZE);
+        let results = self.executor.execute_txs(&tx_stream);
+        assert_eq!(results.len(), STREAM_SIZE);
         for result in results {
             assert!(!result.unwrap().is_reverted());
         }
@@ -114,6 +171,6 @@ impl TransfersGenerator {
 
 impl Default for TransfersGenerator {
     fn default() -> Self {
-        Self::new()
+        Self::new(RecipientGeneratorType::RoundRobin)
     }
 }
