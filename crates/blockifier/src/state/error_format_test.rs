317c007f91918b889b356aaa261ead51daa94d5e
AvivYossef-starkware <aviv.y@starkware.co>
2024-07-08 16:21:21 +0300

    fix: state error unavailable contract address format

--- crates/blockifier/src/state/error_format_test.rs
+++ crates/blockifier/src/state/error_format_test.rs
@@ -1,6 +1,6 @@
 use cairo_vm::Felt252;
 use rstest::rstest;
-use starknet_api::core::ClassHash;
+use starknet_api::core::{ClassHash, ContractAddress};
 
 use crate::state::errors::StateError;
 
@@ -11,5 +11,15 @@ fn test_error_undeclared_class_hash_forrmat() {
         error.to_string(),
         "Class with hash 0x0000000000000000000000000000000000000000000000000000000000000002 is \
          not declared."
+    );
+}
+
+#[rstest]
+fn test_error_unavailable_contract_address_format() {
+    let error = StateError::UnavailableContractAddress(ContractAddress::from(10_u128));
+    assert_eq!(
+        error.to_string(),
+        "Requested 0x000000000000000000000000000000000000000000000000000000000000000a is \
+         unavailable for deployment."
     );
 }
