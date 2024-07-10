0f6df63bfaa982598c06561f9ee1acca485dc147
AvivYossef-starkware <aviv.y@starkware.co>
2024-07-10 14:46:40 +0300

    fix: declare transaction error format
    commit-id:5ae2b059

--- crates/blockifier/src/transaction/error_format_test.rs
+++ crates/blockifier/src/transaction/error_format_test.rs
@@ -1,10 +1,12 @@
 use rstest::rstest;
+use starknet_api::core::ClassHash;
+use starknet_api::hash::StarkHash;
 use starknet_api::transaction::TransactionVersion;
 
 use crate::transaction::errors::TransactionExecutionError;
 
 #[rstest]
-fn test_contract_class_version_mismatch() {
+fn test_contract_class_version_mismatch_format() {
     let error = TransactionExecutionError::ContractClassVersionMismatch {
         declare_version: TransactionVersion::ONE,
         cairo_version: 2,
@@ -12,5 +14,17 @@ fn test_contract_class_version_mismatch() {
     assert_eq!(
         error.to_string(),
         "Declare transaction version 1 must have a contract class of Cairo version 2."
+    );
+}
+
+#[rstest]
+fn test_declare_transaction_error_format() {
+    let error = TransactionExecutionError::DeclareTransactionError {
+        class_hash: ClassHash(StarkHash::THREE),
+    };
+    assert_eq!(
+        error.to_string(),
+        "Class with hash 0x0000000000000000000000000000000000000000000000000000000000000003 is \
+         already declared."
     );
 }
