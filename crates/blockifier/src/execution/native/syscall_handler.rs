01ef5779c2ba9f066982990d82e49edf250006d4
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-08-05 16:11:54 +0300

    feat: update to cairo 2.7.0

--- crates/blockifier/src/execution/native/syscall_handler.rs
+++ crates/blockifier/src/execution/native/syscall_handler.rs
@@ -617,6 +617,15 @@ impl<'state> StarknetSyscallHandler for &mut NativeSyscallHandler<'state> {
     ) -> SyscallResult<(U256, U256)> {
         Ok((p.x, p.y))
     }
+
+    fn sha256_process_block(
+        &mut self,
+        _prev_state: &[u32; 8],
+        _current_block: &[u32; 16],
+        _remaining_gas: &mut u128,
+    ) -> SyscallResult<[u32; 8]> {
+        todo!()
+    }
 }
 
 use ark_ff::PrimeField;
